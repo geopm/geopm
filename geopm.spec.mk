ca426c55a3a7537f6e9dea14735e0d4d04823595
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2015-10-01 12:54:59 -0700

    Added spec file and a make rpm target, now builds RPMs. - Update to documentation.
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ geopm.spec.mk
@@ -0,0 +1,196 @@
+#  Copyright (c) 2015, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# targets for building rpm
+version ?= 0.0.0
+release ?= 1
+arch = $(shell uname -p)
+
+rpm: geopm-$(version).tar.gz
+	rpmbuild $(rpmbuild_flags) $^ -ta
+
+geopm-$(version).spec:
+	@echo "$$geopm_spec" > $@
+	cat ChangeLog >> $@
+
+.PHONY: rpm
+
+define geopm_spec
+Summary: Global Energy Optimization Power Management
+Name: geopm
+Version: $(version)
+Release: $(release)
+License: See COPYING
+Group: System Environment/Libraries
+Vendor: Intel Corporation
+URL: http://geopm.github.io/geopm
+Source0: geopm-%{version}.tar.gz
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root
+BuildRequires: gcc-c++
+%if %{defined suse_version}
+BuildRequires: libjson-c-devel
+%else
+BuildRequires: json-c-devel
+%endif
+BuildRequires: openmpi-devel
+BuildRequires: hwloc-devel
+Prefix: %{_prefix}
+Prefix: %{_initddir}
+
+%if %{defined suse_version}
+%define docdir %{_defaultdocdir}/geopm
+%else
+%define docdir %{_defaultdocdir}/geopm-%{version}
+%endif
+
+%description
+Global Energy Optimization Power Management is a hierarchical control system
+for optimizing power policy in a power-constrained MPI job to increase
+performance.  It is designed to coordinate power policy and performance across
+all of the compute nodes hosting one MPI job on a portion of a distributed
+computing system.  The root of the control hierarchy tree can communicate
+through shared memory with the system resource management daemon to extend the
+hierarchy above the individual MPI job level and enable management of system
+power resources for multiple MPI jobs and multiple users by the system
+resource manager.  The geopm package provides the libgeopm library the
+libgeopmpolicy library and the geopmctl application.  The libgeopm library can
+be called within MPI applications to enable application feedback for informing
+the control decisions.  If modification of the target application is not
+desired then the geopmctl application can be run concurrently with the target
+application.  In this case, target application feedback is inferred by
+querying the hardware through Model Specific Registers (MSRs).  With either
+method (libgeopm or geopmctl), the control hierarchy tree writes processor
+power policy through MSRs to enact policy decisions.  The libgeopmpolicy
+library is used by a resource manager to set energy policy control parameters
+for MPI jobs.
+
+%prep
+
+%setup
+
+%package devel
+Summary: Global Energy Optimization Power Management - development
+Group: Development/Libraries
+
+%description devel
+
+Development package for GEOPM.  Global Energy Optimization Power Management is
+a hierarchical control system for optimizing power policy in a
+power-constrained MPI job to increase performance.  It is designed to
+coordinate power policy and performance across all of the compute nodes
+hosting one MPI job on a portion of a distributed computing system.  The root
+of the control hierarchy tree can communicate through shared memory with the
+system resource management daemon to extend the hierarchy above the individual
+MPI job level and enable management of system power resources for multiple MPI
+jobs and multiple users by the system resource manager.  The geopm package
+provides the libgeopm library the libgeopmpolicy library and the geopmctl
+application.  The libgeopm library can be called within MPI applications to
+enable application feedback for informing the control decisions.  If
+modification of the target application is not desired then the geopmctl
+application can be run concurrently with the target application.  In this
+case, target application feedback is inferred by querying the hardware through
+Model Specific Registers (MSRs).  With either method (libgeopm or geopmctl),
+the control hierarchy tree writes processor power policy through MSRs to enact
+policy decisions.  The libgeopmpolicy library is used by a resource manager to
+set energy policy control parameters for MPI jobs.
+
+%build
+test -f configure || ./autogen.sh
+
+%if %{defined suse_version}
+CC=%{_libdir}/mpi/gcc/openmpi/bin/mpicc \
+CXX=%{_libdir}/mpi/gcc/openmpi/bin/mpicxx \
+./configure LDFLAGS="-L%{_libdir}/mpi/gcc/openmpi/lib -lmpi_cxx" \
+            --prefix=%{_prefix} --libdir=%{_libdir} \
+            --includedir=%{_includedir} --sbindir=%{_sbindir} \
+            --mandir=%{_mandir} --docdir=%{docdir}
+%else
+CC=%{_libdir}/openmpi/bin/mpicc \
+CXX=%{_libdir}/openmpi/bin/mpicxx \
+./configure LDFLAGS="-L%{_libdir}/openmpi/lib -lmpi_cxx" \
+            --prefix=%{_prefix} --libdir=%{_libdir} \
+            --includedir=%{_includedir} --sbindir=%{_sbindir} \
+            --mandir=%{_mandir} --docdir=%{docdir}
+%endif
+%{__make}
+
+%install
+%{__make} DESTDIR=%{buildroot} install
+rm -f %{buildroot}/%{_libdir}/libgeopm.a
+rm -f %{buildroot}/%{_libdir}/libgeopm.la
+rm -f %{buildroot}/%{_libdir}/libgeopmpolicy.a
+rm -f %{buildroot}/%{_libdir}/libgeopmpolicy.la
+$(extra_install)
+
+%clean
+
+%post
+/sbin/ldconfig
+
+%preun
+
+%postun
+/sbin/ldconfig
+
+%files
+%defattr(-,root,root,-)
+%{_libdir}/libgeopm.so.0.0.0
+%{_libdir}/libgeopm.so.0
+%{_libdir}/libgeopm.so
+%{_libdir}/libgeopmpolicy.so.0.0.0
+%{_libdir}/libgeopmpolicy.so.0
+%{_libdir}/libgeopmpolicy.so
+%{_bindir}/geopmctl
+%{_bindir}/geopmpolicy
+%dir %{docdir}
+%doc %{docdir}/README
+%doc %{docdir}/COPYING
+%doc %{docdir}/VERSION
+%doc %{_mandir}/man3/geopm.3.gz
+%doc %{_mandir}/man3/geopmctl.3.gz
+%doc %{_mandir}/man3/geopm_ctl_c.3.gz
+%doc %{_mandir}/man3/geopmkey.3.gz
+%doc %{_mandir}/man3/geopmpolicy.3.gz
+%doc %{_mandir}/man3/geopm_policy_c.3.gz
+%doc %{_mandir}/man3/geopm_prof_c.3.gz
+%doc %{_mandir}/man3/geopm_version.3.gz
+$(extra_files)
+
+%files devel
+%defattr(-,root,root,-)
+%{_includedir}/geopm.h
+%{_includedir}/geopm_policy.h
+%{_includedir}/geopm_policy_message.h
+
+%changelog
+endef
+
+export geopm_spec
