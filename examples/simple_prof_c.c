aa4488724ac3737151a1426bb7fd9233527dbb1e
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2016-04-13 22:11:47 -0400

    Simple examples are now working (fortran and C).
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- examples/simple_prof_c.c
+++ examples/simple_prof_c.c
@@ -5,6 +5,7 @@ int main(int argc, char **argv)
 {
     int ierr = 0;
     int index = 0;
+    int rank = 0;
     double sum = 0.0;
     struct geopm_prof_c *prof = NULL;
     uint64_t region_id = 0;
@@ -20,10 +21,16 @@ int main(int argc, char **argv)
         ierr = geopm_prof_enter(NULL, region_id);
     }
     if (!ierr) {
-        for (index = 0; index < 100000; ++index) {
+        for (index = 0; index < 100000000; ++index) {
             sum += (double)index;
         }
         ierr = geopm_prof_exit(NULL, region_id);
+    }
+    if (!ierr) {
+        ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    }
+    if (!ierr && !rank) {
+        printf("sum = %e\n\n", sum);
     }
     if (!ierr) {
         ierr = geopm_prof_print(prof, "timed_loop", 0);
--- examples/simple_prof_f.f90
+++ examples/simple_prof_f.f90
@@ -12,16 +12,20 @@ integer index
 real(kind=c_double) sum
 type(c_ptr) :: geopm_prof
 integer(8) :: region_id
+integer rank
 
 call MPI_Init(ierr)
-ierr = geopm_prof_create(c_char_'timed_loop'//c_null_char, c_char_'/geopm_shm_key'//c_null_char, MPI_COMM_WORLD, geopm_prof)
+ierr = geopm_prof_create(c_char_'timed_loop'//c_null_char, c_char_''//c_null_char, MPI_COMM_WORLD, geopm_prof)
 ierr = geopm_prof_region(c_null_ptr, c_char_'loop_0'//c_null_char, GEOPM_POLICY_HINT_UNKNOWN, region_id)
-print *,'region_id=', region_id
 ierr = geopm_prof_enter(c_null_ptr, region_id)
 sum = 0
-do index = 1, 100000
+do index = 1, 100000000
     sum = sum + index
 end do
+call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+if (rank == 0) then
+    print *,'sum=', sum
+end if
 ierr = geopm_prof_exit(c_null_ptr, region_id)
 ierr = geopm_prof_print(geopm_prof, c_char_'timed_loop'//c_null_char, 0)
 ierr = geopm_prof_destroy(geopm_prof)
