2a84ce047e04280b6944c483f49ec36c0317be54
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2016-07-25 16:42:21 -0700

    Adding a build script for tutorial using intel compiler.
    Change-Id: I049f0aa62e81c0651aa6b3f44da3651a9a95defd
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ tutorial/tutorial_build_icc.sh
@@ -0,0 +1,40 @@
+#
+#  Copyright (c) 2015, 2016, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+source tutorial_env.sh
+
+# OMP_FLAGS: Flags for enabling OpenMP
+if [ ! "$OMP_FLAGS" ]; then
+    OMP_FLAGS="-qopenmp"
+fi
+
+make CFLAGS="$GEOPM_CFLAGS $OMP_FLAGS -DTUTORIAL_ENABLE_BLAS -D_GNU_SOURCE -std=c99" LDFLAGS="$GEOPM_LDFLAGS $OMP_FLAGS -lm -lrt -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -std=c++11"
