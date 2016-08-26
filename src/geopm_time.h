geopm/geopm

commit e715349ba30405dacbc7308975c94af30bfeda97

Fix geopm_time_add() to avoid overflowing nsec field.
Change-Id: Ia700e304c74c046255a10f1f4503f2969726cb03
Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

src/geopm_time.h
@@ -82,6 +82,10 @@ static inline void geopm_time_add(const struct geopm_time_s *begin, double elaps
     end->t.tv_sec += elapsed;
     elapsed -= floor(elapsed);
     end->t.tv_nsec += 1E9 * elapsed;
+    if (end->t.tv_nsec >= 100000000) {
+        end->t.tv_nsec -= 100000000;
+        ++(end->t.tv_sec);
+    }
 }
 
 #else

commit f40c4ad9411e904251195391e9abae2d41cef726

Fixes error introduced in commit e715349ba30405dacbc7308975c94af30bfeda97.
- Left off a zero in rounding code.

Change-Id: Ibb7768f7b037d3451111e4e0842f9337974412df
Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

src/geopm_time.h
@@ -82,8 +82,8 @@ static inline void geopm_time_add(const struct geopm_time_s *begin, double elaps
     end->t.tv_sec += elapsed;
     elapsed -= floor(elapsed);
     end->t.tv_nsec += 1E9 * elapsed;
-    if (end->t.tv_nsec >= 100000000) {
-        end->t.tv_nsec -= 100000000;
+    if (end->t.tv_nsec >= 1000000000) {
+        end->t.tv_nsec -= 1000000000;
         ++(end->t.tv_sec);
     }
 }