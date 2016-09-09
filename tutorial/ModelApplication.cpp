84baa8710f3c0affe49475a8ffae6da0631163b0
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2016-09-09 10:35:20 -0500

    Creating the ideal "model" application for geopm.
    - Use shown in tutorial_6.
    - tutorial_6 is the first tutorial written in C++.
    - The ModelApplication will enable integration testing.
    - Application region pattern can be controlled by json file.
    
    Change-Id: Ib70b3f3ae9a9334ee89226e2c6b600e65da8dd1f
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ tutorial/ModelApplication.cpp
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2015, 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <string>
+#include <json-c/json.h>
+
+#include "Exception.hpp"
+#include "ModelApplication.hpp"
+
+namespace geopm
+{
+    void model_parse_config(const std::string config_path, uint64_t &loop_count,
+                            std::vector<std::string> &region_name, std::vector<double> &big_o)
+    {
+        std::ifstream config_stream;
+        config_stream.open(config_path, std::ifstream::in);
+        if (!config_stream.is_open()) {
+            throw Exception("model_parse_config(): could not open file: " + config_path,
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        config_stream.seekg(0, std::ios::end);
+        size_t file_size = config_stream.tellg();
+        if (file_size <= 0) {
+            throw Exception("model_parse_config(): file empty or invalid: " + config_path,
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        config_stream.seekg(0, std::ios::beg);
+        std::string config_string;
+        config_string.reserve(file_size);
+        config_string.assign(std::istreambuf_iterator<char>(config_stream),
+                             std::istreambuf_iterator<char>());
+
+        json_object *object = json_tokener_parse(config_string.c_str());
+
+        enum json_type type = json_object_get_type(object);
+
+        if (type != json_type_object) {
+            throw Exception("model_parse_config(): malformed json configuration file",
+                            GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+        }
+
+        std::string key_string;
+        json_object_object_foreach(object, key, val) {
+            key_string = key;
+            if (key_string == "loop-count") {
+                if (json_object_get_type(val) == json_type_int) {
+                    loop_count = json_object_get_int(val);
+                }
+                else {
+                    throw Exception("model_parse_config(): loop-count expected to be an integer type",
+                                    GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+                }
+            }
+            else if (key_string == "region") {
+                if (json_object_get_type(val) == json_type_array) {
+                    json_object *region_obj;
+                    for (int i = 0; i < json_object_array_length(val); ++i) {
+                        region_obj = json_object_array_get_idx(val, i);
+                        if (json_object_get_type(region_obj) == json_type_string) {
+                            region_name.push_back(json_object_get_string(region_obj));
+                        }
+                        else {
+                            throw Exception("model_parse_config(): region array value is not a string type",
+                                            GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+                        }
+                    }
+                }
+                else {
+                    throw Exception("model_parse_config(): region must specify an array",
+                                    GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+                }
+            }
+            else if (key_string == "big-o") {
+                if (json_object_get_type(val) == json_type_array) {
+                    json_object *big_o_obj;
+                    for (int i = 0; i < json_object_array_length(val); ++i) {
+                        big_o_obj = json_object_array_get_idx(val, i);
+                        if (json_object_get_type(big_o_obj) == json_type_double) {
+                            big_o.push_back(json_object_get_double(big_o_obj));
+                        }
+                        else if (json_object_get_type(big_o_obj) == json_type_int) {
+                            big_o.push_back((double)json_object_get_int(big_o_obj));
+                        }
+                        else {
+                           throw Exception("model_parse_config(): big-o expected to be an integer or double type",
+                                   GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+                        }
+                    }
+                }
+                else {
+                    throw Exception("model_parse_config(): region must specify an array",
+                                    GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+                }
+            }
+            else {
+                throw Exception("model_parse_config(): unknown key: " + key_string,
+                                GEOPM_ERROR_FILE_PARSE, __FILE__, __LINE__);
+            }
+        }
+        config_stream.close();
+    }
+
+    ModelApplication::ModelApplication(uint64_t repeat, std::vector<std::string> region_name, std::vector<double> big_o, int verbosity, int rank)
+        : m_repeat(repeat)
+        , m_rank(rank)
+    {
+        if (region_name.size() != big_o.size()) {
+            throw Exception("ModelApplication: Length of region names is different than the length of big_o",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        auto name_it = region_name.begin();
+        for (auto big_o_it = big_o.begin(); big_o_it != big_o.end(); ++big_o_it, ++name_it) {
+            m_region.push_back(model_region_factory(*name_it, *big_o_it, verbosity));
+        }
+    }
+
+    ModelApplication::~ModelApplication()
+    {
+        for (auto it = m_region.rbegin(); it != m_region.rend(); ++it) {
+            delete *it;
+        }
+    }
+
+    void ModelApplication::run(void)
+    {
+        if (!m_rank) {
+            std::cout << "Beginning loop of " << m_repeat << " iterations." << std::endl << std::flush;
+        }
+        for (uint64_t i = 0; i < m_repeat; ++i) {
+            for (auto it = m_region.begin(); it != m_region.end(); ++it) {
+                (*it)->run();
+            }
+            if (!m_rank) {
+                std::cout << "Iteration: " << i << "\r" << std::flush;
+            }
+        }
+        if (!m_rank) {
+            std::cout << "Completed loop.                    " << std::endl << std::flush;
+        }
+    }
+}
---
+++ tutorial/ModelRegion.hpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2015, 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef MODELREGION_HPP_INCLUDE
+#define MODELREGION_HPP_INCLUDE
+
+#include <string>
+#include <stdint.h>
+
+namespace geopm
+{
+    class ModelRegionBase
+    {
+        public:
+            ModelRegionBase(double big_o, int verbosity);
+            virtual ~ModelRegionBase();
+            std::string name(void);
+            double big_o(void);
+            uint64_t num_ops(void);
+            virtual void big_o(double big_o) = 0;
+            virtual void run(void) = 0;
+        protected:
+            std::string m_name;
+            double m_big_o;
+            int m_verbosity;
+            uint64_t m_num_ops;
+            uint64_t m_region_id;
+    };
+
+    class SleepModelRegion : public ModelRegionBase
+    {
+        public:
+            SleepModelRegion(double big_o, int verbosity);
+            virtual ~SleepModelRegion();
+            void big_o(double big_o);
+            void run(void);
+    };
+
+    class DGEMMModelRegion : public ModelRegionBase
+    {
+        public:
+            DGEMMModelRegion(double big_o, int verbosity);
+            virtual ~DGEMMModelRegion();
+            void big_o(double big_o);
+            void run(void);
+        protected:
+            double *m_matrix_a;
+            double *m_matrix_b;
+            double *m_matrix_c;
+            size_t m_matrix_size;
+            const size_t m_pad_size;
+    };
+
+    class StreamModelRegion : public ModelRegionBase
+    {
+        public:
+            StreamModelRegion(double big_o, int verbosity);
+            virtual ~StreamModelRegion();
+            void big_o(double big_o);
+            void run(void);
+        protected:
+            double *m_array_a;
+            double *m_array_b;
+            double *m_array_c;
+            size_t m_array_len;
+            const size_t m_align;
+    };
+
+    class All2allModelRegion : public ModelRegionBase
+    {
+        public:
+            All2allModelRegion(double big_o, int verbosity);
+            virtual ~All2allModelRegion();
+            void big_o(double big_o);
+            void run(void);
+        protected:
+            char *m_send_buffer;
+            char *m_recv_buffer;
+            size_t m_num_send;
+            int m_num_rank;
+            const size_t m_align;
+    };
+
+    ModelRegionBase *model_region_factory(std::string name, uint64_t big_o, int verbosity);
+}
+
+#endif
