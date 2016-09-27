36084d2fa57a956acafae1e1c41507d96989d24d
Steve S. Sylvester <steve.s.sylvester@intel.com>
2016-09-27 18:25:51 +0000

    GoverningDecider should subtract the max of dram power or total budget overage.
    - Keeps governor from exceeding power limit.
    
    Change-Id: I68cd6c7fb183ef63779764c1050f4cf0ab050c94
    Signed-off-by: Steve S. Sylvester <steve.s.sylvester@intel.com>

--- plugin/GoverningDecider.cpp
+++ plugin/GoverningDecider.cpp
@@ -138,7 +138,9 @@ namespace geopm
                 is_greater = total_power > limit[domain_idx] * (1 + m_guard_band);
                 is_less = total_power < limit[domain_idx] * (1 - m_guard_band);
                 if (is_greater || is_less) {
-                    target[domain_idx] = limit[domain_idx] - dram_power;
+                    double overage = total_power - limit[domain_idx];
+                    target[domain_idx] = limit[domain_idx] - (overage > dram_power ?
+                                                              overage : dram_power);
                     is_updated = true;
                 }
             }
