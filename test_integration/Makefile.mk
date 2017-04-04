4f2c55fe8635ae457374e2286d4fcf40dd0602d5
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-04-04 11:04:05 -0500

    Change file name.
    - Move test_integration/geopm_launcher.py to
      test_integration/geopm_test_launcher.py.
    
    Change-Id: Ib90afd742be20a2841089aa403702624221222dc
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_integration/Makefile.mk
+++ test_integration/Makefile.mk
@@ -55,7 +55,7 @@ endif
 
 EXTRA_DIST += test_integration/geopm_test_integration.py \
               test_integration/geopm_io.py \
-              test_integration/geopm_launcher.py \
+              test_integration/geopm_test_launcher.py \
               test_integration/geopm_test_loop.sh \
               test_integration/README.md \
               # end
