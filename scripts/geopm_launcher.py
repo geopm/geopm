44c90d3527bc07ac2a5c0d5cb7efaaddb90069cf
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-04-05 07:48:51 -0500

    Integrate new launcher into integration test.
    - Integration tests now use same code path as a user executing the
      aprun or srun wrapper script.
    - Enable control of RM CLI through constructor arguments to launcher.
    - In order to use the launcher object without knowing the underlying
      job launch application, we cannot provide launch application specific
      command line arguments.
    - This enables the launcher object to be created by the test infrastructure.
    - Deleted code in test launcher that now exists in scripts/geopm_launcher.py.
    
    Change-Id: I581f375367abacbc981320bff092ad5749810656
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- scripts/geopm_launcher.py
+++ scripts/geopm_launcher.py
@@ -94,12 +94,15 @@ def resource_manager():
 
     return result;
 
-def factory(argv):
+def factory(argv, num_rank=None, num_node=None, cpu_per_rank=None, timeout=None,
+            time_limit=None, job_name=None, node_list=None, host_file=None):
     rm = resource_manager()
     if rm == "SLURM":
-        return SrunLauncher(argv[1:])
+        return SrunLauncher(argv[1:], num_rank, num_node, cpu_per_rank, timeout,
+                            time_limit, job_name, node_list, host_file)
     elif rm == "ALPS":
-        return AprunLauncher(argv[1:])
+        return AprunLauncher(argv[1:], num_rank, num_node, cpu_per_rank, timeout,
+                             time_limit, job_name, node_list, host_file)
 
 class PassThroughError(Exception):
     """
@@ -108,7 +111,7 @@ class PassThroughError(Exception):
 
 class SubsetOptionParser(optparse.OptionParser):
     """
-    Parse a subset of comand line arguements and prepend unrecognized
+    Parse a subset of comand line arguments and prepend unrecognized
     arguments to positional arguments.
     """
     def _process_args(self, largs, rargs, values):
@@ -186,32 +189,42 @@ class Config(object):
         return self.args
 
 class Launcher(object):
-    def __init__(self, argv):
+    def __init__(self, argv, num_rank=None, num_node=None, cpu_per_rank=None, timeout=None,
+                 time_limit=None, job_name=None, node_list=None, host_file=None):
         self.default_handler = signal.getsignal(signal.SIGINT)
         try:
             self.config = Config(argv)
             self.argv = self.config.unparsed()
+            self.num_rank = num_rank
+            self.num_node = num_node
+            if self.num_rank and self.num_node:
+                self.rank_per_node = int(math.ceil(float(self.num_rank) / float(self.num_node)))
+            else:
+                self.rank_per_node = None
+            self.cpu_per_rank = cpu_per_rank
+            self.timeout = timeout
+            self.time_limit = time_limit
+            self.job_name = job_name
+            self.node_list = node_list
+            self.host_file = host_file
             self.parse_alloc()
         except PassThroughError:
             self.config = None
             self.argv = argv
 
-    def run(self):
+    def run(self, stdout=sys.stdout, stderr=sys.stderr):
         argv_mod = self.mpiexec()
-        if self.config is not None:
-            argv_mod.extend(self.num_node_option())
-            argv_mod.extend(self.num_rank_option())
-            argv_mod.extend(self.affinity_option())
+        argv_mod.extend(self.mpiexec_argv())
         argv_mod.extend(self.argv)
         echo = []
-        if self.config:
+        if self.config is not None:
             echo.append(self.config.__str__())
         echo.extend(argv_mod)
         echo = '\n' + ' '.join(echo) + '\n\n'
-        sys.stdout.write(echo)
-        sys.stdout.flush()
+        stdout.write(echo)
+        stdout.flush()
         signal.signal(signal.SIGINT, self.int_handler)
-        subprocess.check_call(argv_mod, env=self.environ())
+        subprocess.check_call(argv_mod, env=self.environ(), stdout=stdout, stderr=stderr)
         signal.signal(signal.SIGINT, self.default_handler)
 
     def environ(self):
@@ -240,6 +253,19 @@ class Launcher(object):
     def mpiexec(self):
         raise NotImplementedError('Launcher.mpiexec() undefined in the base class')
 
+    def mpiexec_argv(self):
+        result = []
+        if self.config is not None:
+            result.extend(self.num_node_option())
+            result.extend(self.num_rank_option())
+            result.extend(self.affinity_option())
+            result.extend(self.timeout_option())
+            result.extend(self.time_limit_option())
+            result.extend(self.job_name_option())
+            result.extend(self.node_list_option())
+            result.extend(self.host_file_option())
+        return result
+
     def num_node_option(self):
         raise NotImplementedError('Launcher.num_node_option() undefined in the base class')
 
@@ -247,11 +273,35 @@ class Launcher(object):
         raise NotImplementedError('Launcher.num_rank_option() undefined in the base class')
 
     def affinity_option(self):
-        raise NotImplementedError('Launcher.affinity_option() undefined in the base class')
+        return []
+
+    def timeout_option(self):
+        return []
+
+    def time_limit_option(self):
+        return []
+
+    def job_name_option(self):
+        return []
+
+    def node_list_option(self):
+        return []
+
+    def host_name_option(self):
+        return []
+
+    def get_idle_nodes(self):
+        raise NotImplementedError('Launcher.get_idle_nodes() undefined in the base class')
+
+    def get_alloc_nodes(self):
+        raise NotImplementedError('Launcher.get_alloc_nodes() undefined in the base class')
+
 
 class SrunLauncher(Launcher):
-    def __init__(self, argv):
-        super(SrunLauncher, self).__init__(argv)
+    def __init__(self, argv, num_rank=None, num_node=None, cpu_per_rank=None, timeout=None,
+                 time_limit=None, job_name=None, node_list=None, host_file=None):
+        super(SrunLauncher, self).__init__(argv, num_rank, num_node, cpu_per_rank, timeout,
+                                           time_limit, job_name, node_list, host_file)
 
     def int_handler(self, signum, frame):
         """
@@ -267,20 +317,37 @@ class SrunLauncher(Launcher):
         parser.add_option('-n', '--ntasks', dest='num_rank', nargs=1, type='int')
         parser.add_option('-N', '--nodes', dest='num_node', nargs=1, type='int')
         parser.add_option('-c', '--cpus-per-task', dest='cpu_per_rank', nargs=1, type='int')
+        parser.add_option('-I', '--immediate', dest='timeout', nargs=1, type='int')
+        parser.add_option('-t', '--time', dest='time_limit', nargs=1, type='int')
+        parser.add_option('-J', '--job-name', dest='job_name', nargs=1, type='string')
+        parser.add_option('-w', '--nodelist', dest='node_list', nargs=1, type='string')
         opts, self.argv = parser.parse_args(self.argv)
 
+        if self.num_rank is None:
+            self.num_rank = opts.num_rank
+        if self.num_node is None:
+            self.num_node = opts.num_node
+        if self.rank_per_node is None and self.num_rank is not None and self.num_node is not None:
+            self.rank_per_node = int(math.ceil(float(self.num_rank) / float(self.num_node)))
+        if self.cpu_per_rank is None:
+            self.cpu_per_rank = opts.cpu_per_rank
+        if self.timeout is None:
+            self.timeout = opts.timeout
+        if self.time_limit is None:
+            self.time_limit = opts.time_limit
+        if self.job_name is None:
+            self.job_name = opts.job_name
+        if self.node_list is None:
+            self.node_list = opts.node_list
+
         # Check required arguements
-        if opts.num_rank is None:
+        if self.num_rank is None:
             raise SyntaxError('Number of tasks must be specified with -n.')
-        if opts.num_node is None:
+        if self.num_node is None:
             raise SyntaxError('Number of nodes must be specified with -N.')
         if any(aa.startswith('--cpu_bind') for aa in self.argv):
             raise SyntaxError('The option --cpu_bind must not be specified, this is controlled by geopm_srun.')
 
-        self.num_rank = opts.num_rank
-        self.num_node = opts.num_node
-        self.rank_per_node = int(math.ceil(float(self.num_rank) / float(self.num_node)))
-        self.cpu_per_rank = opts.cpu_per_rank
         self.mod_alloc()
 
     def mpiexec(self):
@@ -311,9 +378,51 @@ class SrunLauncher(Launcher):
         result.append('v,mask_cpu:' + ','.join(mask_list))
         return result
 
+    def timeout_option(self):
+        if self.timeout is None:
+           result = []
+        else:
+           result = ['-I', str(self.timeout)]
+        return result
+
+    def time_limit_option(self):
+        if self.time_limit is None:
+            result = []
+        else:
+            result = ['-t', str(self.time_limit)]
+        return result
+
+    def job_name_option(self):
+        if self.job_name is None:
+            result = []
+        else:
+            result = ['-J', self.job_name]
+        return result
+
+    def node_list_option(self):
+        if (self.node_list is not None and
+            self.host_name is not None and
+            self.node_list != self.host_name):
+            raise SyntaxError('Node list and host name cannot both be specified.')
+
+        if self.node_list is None and self.host_name is None:
+            result = []
+        elif self.node_list is not None:
+            result = ['-w', self.node_list]
+        elif self.host_file is not None:
+            result = ['-w', self.host_file]
+
+    def get_idle_nodes(self):
+        return subprocess.check_output('sinfo -t idle -hNo %N', shell=True).splitlines()
+
+    def get_alloc_nodes(self):
+        return subprocess.check_output('sinfo -t alloc -hNo %N', shell=True).splitlines()
+
 class AprunLauncher(Launcher):
-    def __init__(self, argv):
-        super(AprunLauncher, self).__init__(argv)
+    def __init__(self, argv, num_rank=None, num_node=None, cpu_per_rank=None, timeout=None,
+                 time_limit=None, job_name=None, node_list=None, host_file=None):
+        super(AprunLauncher, self).__init__(argv, num_rank, num_node, cpu_per_rank, timeout,
+                                            time_limit, job_name, node_list, host_file)
 
     def environ(self):
         result = super(AprunLauncher, self).environ()
@@ -326,22 +435,36 @@ class AprunLauncher(Launcher):
         parser.add_option('-n', '--pes', dest='num_rank', nargs=1, type='int')
         parser.add_option('-N', '--pes-per-node', dest='rank_per_node', nargs=1, type='int')
         parser.add_option('-d', '--cpus-per-pe', dest='cpu_per_rank', nargs=1, type='int')
+        parser.add_option('-t', '--cpu-time-limit', dest='time_limit', nargs=1, type='int')
+        parser.add_option('-L', '--node-list', dest='node_list', nargs=1, type='string')
+        parser.add_option('-l', '--node-list-file', dest='host_file', nargs=1, type='string')
         opts, self.argv = parser.parse_args(self.argv)
 
+        if self.num_rank is None:
+            self.num_rank = opts.num_rank
+        if self.rank_per_node is None:
+            self.rank_per_node = opts.rank_per_node
+        if self.num_node is None and self.num_rank is not None and self.rank_per_node is not None:
+            self.num_node = int(math.ceil(float(self.num_rank) /
+                                          float(self.rank_per_node)))
+        if self.cpu_per_rank is None:
+            self.cpu_per_rank = opts.cpu_per_rank
+        if self.time_limit is None:
+            self.time_limit = opts.time_limit
+        if self.node_list is None:
+            self.node_list = opts.node_list
+        if self.host_file is None:
+            self.host_file = opts.host_file
+
         # Check required arguements
-        if opts.num_rank is None:
+        if self.num_rank is None:
             raise SyntaxError('Number of tasks must be specified with -n.')
-        if opts.rank_per_node is None:
+        if self.rank_per_node is None:
             raise SyntaxError('Number of tasks per node must be specified with -N.')
         if any(aa.startswith('--cpu-binding') or
                aa.startswith('-cc') for aa in self.argv):
             raise SyntaxError('The options --cpu-binding or -cc must not be specified, this is controlled by geopm_launcher.')
 
-        self.num_rank = opts.num_rank
-        self.rank_per_node = opts.rank_per_node
-        self.num_node = int(math.ceil(float(self.num_rank) /
-                                      float(self.rank_per_node)))
-        self.cpu_per_rank = opts.cpu_per_rank
         self.mod_alloc()
 
     def mpiexec(self):
@@ -366,6 +489,27 @@ class AprunLauncher(Launcher):
                           for off in range(off_start, self.cpu_per_rank * self.num_app_mask +
                                            off_start, self.cpu_per_rank)])
         result.append(':'.join(mask_list))
+        return result
+
+    def time_limit_option(self):
+        if self.time_limit is None:
+            result = []
+        else:
+            result = ['-t', str(self.time_limit)]
+        return result
+
+    def node_list_option(self):
+        if self.node_list is None:
+            result = []
+        else:
+            result = ['-L', self.node_list]
+        return result
+
+    def host_file_option(self):
+        if self.host_file is None:
+            result = []
+        else:
+            result = ['-l', self.host_file]
         return result
 
 def main():
