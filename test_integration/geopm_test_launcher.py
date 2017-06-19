bda38e5cb05696e23f4fe111824d5e4d07c3a61c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-06-19 15:48:09 -0500

    Fix SubsetOptionParser.
    - Previous implementation would mangle unrecognized options
      if they were not of the form "--option value".
    - Unrecognized short form arguments were not properly handled.
    - Unrecognized long form arguments that used = would be reprinted
      without the equal sign causing apps like a gtest exectable to fail
      when run with the launcher.
    - Add a unit test for the SubsetOptionParser.
    - Use -- to separate application executable and its options since we
      can't tell the difference between a postional arguement following
      an unknown launcher flag that takes no value and an unknown launcher
      option that takes a value.  Launchers typically assume that the first
      positional argument is the compute application executable to be
      launched and stop parsing options after this.  The geopm_launcher.py
      will stop parsing arguments following the -- sequence and pass the
      arguments that follow unaltered.
    
    Change-Id: I35dcf52a0f36bfc4f3f952770f396094e18cc010
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_integration/geopm_test_launcher.py
+++ test_integration/geopm_test_launcher.py
@@ -101,7 +101,7 @@ class TestLauncher(object):
                 argv.extend(['--geopm-trace', self._trace_path])
             if self._region_barrier:
                 argv.append('--geopm-barrier')
-            argv.extend([exec_path, '--verbose', self._app_conf.get_path()])
+            argv.extend(['--', exec_path, '--verbose', self._app_conf.get_path()])
             launcher = geopmpy.launcher.factory(argv, self._num_rank, self._num_node, self._cpu_per_rank, self._timeout,
                                               self._time_limit, test_name, self._node_list, self._host_file)
             launcher.run(stdout=outfile, stderr=outfile)
