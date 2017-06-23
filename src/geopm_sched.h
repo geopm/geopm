faa13efa8a67e26afc522ef402198597db1971af
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-06-23 10:20:59 -0500

    Do not overlap control thread with OpenMP threads.
    - When launching the controller with the pthread option
      mask off all CPUs used by an OpenMP gang.
    - Move geopm_omp.[ch] to geopm_sched.[ch]
    - Update documentation for geopm_sched*() APIs.
    
    Change-Id: I93b7792beca1fb0e28f6a9b36f01161ce8711c5a
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/geopm_sched.h
+++ src/geopm_sched.h
@@ -30,6 +30,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #ifndef __linux__
 /// @brief cpuset definition for non-linux platforms.
 typedef struct cpu_set_t {
@@ -62,4 +67,12 @@ static inline int  CPU_ISSET(int cpu, cpu_set_t *set)
 #define _GNU_SOURCE
 #endif
 #include <sched.h>
+#endif
+
+int geopm_sched_num_cpu(void);
+
+int geopm_sched_woomp(int num_cpu, cpu_set_t *woomp);
+
+#ifdef __cplusplus
+}
 #endif
