02fe78c794d83982e486d1cc9a8b42d7652a023a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-06-26 15:10:47 -0500

    Update the tutorial to use the launch wrapper.
    Change-Id: I28b46d0b7786a420d2b52604eee7d6d410cc6d3a
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- tutorial/tutorial_0.sh
+++ tutorial/tutorial_0.sh
@@ -31,11 +31,55 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+set err=0
 source tutorial_env.sh
 
-LD_PRELOAD=$GEOPM_LIBDIR/libgeopm.so \
-LD_DYNAMIC_WEAK=true \
-GEOPM_PMPI_CTL=process \
-GEOPM_REPORT=tutorial_0_report \
-GEOPM_TRACE=tutorial_0_trace \
-./tutorial_0
+export PATH=$GEOPM_BINDIR:$PATH
+export PYTHONPATH=$GEOPMPY_PKGDIR:$PYTHONPATH
+export LD_LIBRARY_PATH=$GEOPM_LIBDIR:$LD_LIBRARY_PATH
+
+# Run on two nodes
+# with 8 application MPI ranks
+# load geopm runtime with LD_PRELOAD
+# launch geopm controller as an MPI process
+# create a report file
+# create trace files
+if [ "$GEOPM_RM" == "SLURM" ]; then
+    # Use GEOPM launcher wrapper script with SLURM's srun
+    geopmsrun  -N 2 \
+               -n 8 \
+               --geopm-preload \
+               --geopm-ctl=process \
+               --geopm-report=tutorial_0_report \
+               --geopm-trace=tutorial_0_trace \
+               -- ./tutorial_0
+    err=$?
+elif [ "$GEOPM_RM" == "ALPS" ]; then
+    # Use GEOPM launcher wrapper script with ALPS's aprun
+    geopmaprun -N 4 \
+               -n 8 \
+               --geopm-preload \
+               --geopm-ctl=process \
+               --geopm-report=tutorial_0_report \
+               --geopm-trace=tutorial_0_trace \
+               -- ./tutorial_0
+    err=$?
+elif [ $MPIEXEC ]; then
+    # Use MPIEXEC and set GEOPM environment variables to launch the job
+    LD_PRELOAD=$GEOPM_LIBDIR/libgeopm.so \
+    LD_DYNAMIC_WEAK=true \
+    GEOPM_PMPI_CTL=process \
+    GEOPM_REPORT=tutorial_0_report \
+    GEOPM_TRACE=tutorial_0_trace \
+    $MPIEXEC \
+    ./tutorial_0
+    err=$?
+else
+    echo "Error: tutorial_0.sh: set GEOPM_RM to 'SLURM' or 'ALPS'." 2>&1
+    echo "       If SLURM or ALPS are not available, set MPIEXEC to" 2>&1
+    echo "       a command that will launch an MPI job on your system" 2>&1
+    echo "       using 2 nodes and 10 processes." 2>&1
+    err=1
+fi
+
+exit $err
