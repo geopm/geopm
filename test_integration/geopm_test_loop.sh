fda0b26cbb9a839ea3c99e8820a6be29b9028b7b
Brad Geltz <brad.geltz@intel.com>
2017-08-02 16:17:55 -0700

    Fix integration test failures exposed by new nodes.
    - Handle duplicate node names coming from sinfo when nodes are declared
      in multiple queues.
    - Do not run the all_nodes test inside an allocation.  This test
      requires that the initial Python code is called from outside an
      allocation.
    - Remove the test loop log from thr ignore file so it is picked up with
      "git ls-files"
    - Do not use git or mail from within the loop script since they are not
      available on the compute nodes.
    
    Change-Id: Ic51b03a9d66af9e727dfa4312ba9619616976be3
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- test_integration/geopm_test_loop.sh
+++ test_integration/geopm_test_loop.sh
@@ -30,38 +30,21 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-# TODO Trap CTRL-C?
-
 COUNT=0
-TIMESTAMP=$(date +\%F_\%H\%M)
-TEST_DIR=${HOME}/public_html/cron_runs/${TIMESTAMP}
 LOG_FILE=test_loop_output.log
 
 while [ $? -eq 0 -a ${COUNT} -lt 10 ];
 do
-    git clean -fd .
     COUNT=$((COUNT+1))
     echo "Beginning loop ${COUNT}..." > >(tee -a ${LOG_FILE})
     GEOPM_RUN_LONG_TESTS=true ./geopm_test_integration.py -fv > >(tee -a ${LOG_FILE}) 2>&1
 done
 TEST_RETURN_CODE=$?
 
-# Save the output in a timestamped dir
-echo "Moving files to ${TEST_DIR}..."
-mkdir -p ${TEST_DIR}
-for f in $(git ls-files --others --exclude-standard);
-do
-    mv ${f} ${TEST_DIR}
-done
-mv ${LOG_FILE} ${TEST_DIR}
-
 #Do email only if there was a failure
-if [ ${TEST_RETURN_CODE} -eq 0 ]; then
-    exit 0
+if [ ${TEST_RETURN_CODE} -ne 0 ]; then
+    touch .tests_failed
 fi
 
-ERR_MSG="The integration tests have failed at iteration ${COUNT}.  Please see the output for more information:\nhttp://$(hostname -i)/~test/cron_runs/${TIMESTAMP}"
+echo "Test looper done."
 
-echo -e ${ERR_MSG} | mail -r "do-not-reply" -s "Integration test failure : ${TIMESTAMP}" brad.geltz@intel.com,christopher.m.cantalupo@intel.com,steve.s.sylvester@intel.com,brandon.baker@intel.com
-
-echo "Done."
