d2f2e623b08ec288481faeaca585574287198c76
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2017-08-03 11:01:33 -0500

    Add support to rapl_pkg_limit_test for multi-sockets.
    - Use lscpu to get number of cores and sockets.
    - Loop over sockets for test.
    
    Change-Id: I8c29209dcab9b5b197fbbcb955a856642a837309
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_hw/rapl_pkg_limit_test.c
+++ test_hw/rapl_pkg_limit_test.c
@@ -41,6 +41,7 @@
 #include <math.h>
 #include <errno.h>
 #include <omp.h>
+#include <signal.h>
 
 #include "geopm_time.h"
 
@@ -57,6 +58,18 @@
 #define NAME_MAX 512
 #endif
 
+#define MAX_NUM_SOCKET 16
+
+static volatile unsigned g_is_popen_complete = 0;
+static struct sigaction g_popen_complete_signal_action;
+
+static void popen_complete(int signum)
+{
+    if (signum == SIGCHLD) {
+        g_is_popen_complete = 1;
+    }
+}
+
 int dgemm_(char *transa, char *transb, int *m, int *n, int *k,
            double *alpha, double *a, int *lda, double *b, int *ldb,
            double *beta, double *c, int *ldc);
@@ -72,29 +85,82 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
     const off_t pkg_power_limit_off = 0x610;
     const off_t pkg_energy_status_off = 0x611;
     int err = 0;
-    int msr_fd = 0;
+    int num_core_per_socket = 0;
+    int num_socket = 0;
+    int socket = 0;
     double power_units = 0.0;
     double energy_units = 0.0;
-    double power_used = 0.0;
     double total_time = 0.0;
     uint64_t msr_value = 0;
-    uint64_t save_limit = 0;
-    uint64_t begin_energy = 0;
-    uint64_t end_energy = 0;
+    size_t num_read = 0;
     ssize_t num_byte = 0;
     double *aa = NULL;
     double *bb = NULL;
     double *cc = NULL;
     struct geopm_time_s begin_time = {{0,0}};
     struct geopm_time_s end_time = {{0,0}};
+    struct sigaction save_action;
     char hostname[NAME_MAX] = {0};
     char outfile_name[NAME_MAX] = {0};
+    char lscpu_buffer[NAME_MAX] = {0};
+    char msr_path[NAME_MAX] = {0};
+    int msr_fd[MAX_NUM_SOCKET] = {0};
+    double power_used[MAX_NUM_SOCKET] = {0};
+    uint64_t save_limit[MAX_NUM_SOCKET] = {0};
+    uint64_t begin_energy[MAX_NUM_SOCKET] = {0};
+    uint64_t end_energy[MAX_NUM_SOCKET] = {0};
     FILE *outfile = NULL;
+    FILE *lscpu_fid = NULL;
 
     if (!err) {
         /* Get hostname to insert in output file name */
         err = gethostname(hostname, NAME_MAX);
         if (err) {
+            err = errno ? errno : -1;
+        }
+    }
+    if (!err) {
+        /* Figure out how many sockets there are */
+        g_popen_complete_signal_action.sa_handler = popen_complete;
+        sigemptyset(&g_popen_complete_signal_action.sa_mask);
+        g_popen_complete_signal_action.sa_flags = 0;
+        err = sigaction(SIGCHLD, &g_popen_complete_signal_action, &save_action);
+        if (err) {
+            err = errno ? errno : -1;
+        }
+    }
+    if (!err) {
+        lscpu_fid = popen("printf '%d %d'"
+                          "$(lscpu | "
+                          "egrep '^Socket\\(s\\):|^Core\\(s\\) per socket:' | "
+                          "sort | "
+                          "awk -F: '{print $2}')", "r");
+        if (!lscpu_fid) {
+            err = errno ? errno : -1;
+        }
+    }
+    if (!err) {
+        while (!g_is_popen_complete) {
+
+        }
+        g_is_popen_complete = 0;
+        num_read = fread(lscpu_buffer, 1, NAME_MAX - 1, lscpu_fid);
+        err = pclose(lscpu_fid);
+        if (err) {
+            err = errno ? errno : -1;
+        }
+    }
+    if (!err) {
+        err = sigaction(SIGCHLD, &save_action, NULL);
+        if (err) {
+            err = errno ? errno : -1;
+        }
+    }
+    if (!err) {
+        if (num_read) {
+            num_read = sscanf(lscpu_buffer, "%d %d", &num_core_per_socket, &num_socket);
+        }
+        if (num_read != 2 || num_core_per_socket <= 0 || num_socket <= 0 || num_socket > MAX_NUM_SOCKET) {
             err = errno ? errno : -1;
         }
     }
@@ -135,20 +201,24 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
             aa[i] = 1.0;
             bb[i] = 2.0;
         }
+    }
+    for (socket = 0; !err && socket < num_socket; ++socket) {
         /* Open msr device, try to use msr_safe if available */
-        msr_fd = open("/dev/cpu/0/msr_safe", O_RDWR);
-        if (msr_fd == -1) {
+        snprintf(msr_path, NAME_MAX, "/dev/cpu/%d/msr_safe", socket * num_core_per_socket);
+        msr_fd[socket] = open(msr_path, O_RDWR);
+        if (msr_fd[socket] == -1) {
+            snprintf(msr_path, NAME_MAX, "/dev/cpu/%d/msr", socket * num_core_per_socket);
             errno = 0;
-            msr_fd = open("/dev/cpu/0/msr", O_RDWR);
+            msr_fd[socket] = open(msr_path, O_RDWR);
         }
-        if (msr_fd == -1) {
+        if (msr_fd[socket] == -1) {
             err = errno ? errno : -1;
         }
     }
     if (!err) {
         /* Read RAPL_POWER_UNIT */
         errno = 0;
-        num_byte = pread(msr_fd, &msr_value, sizeof(uint64_t), pkg_power_unit_off);
+        num_byte = pread(msr_fd[0], &msr_value, sizeof(uint64_t), pkg_power_unit_off);
         if (num_byte != sizeof(uint64_t)) {
             err = errno ? errno : -1;
         }
@@ -156,22 +226,35 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
     if (!err) {
         power_units = pow(2.0, -1.0 * (msr_value & 0xF));
         energy_units = pow(2.0, -1.0 * ((msr_value >> 8) & 0x1F));
+    }
+    for (socket = 0; !err && socket < num_socket; ++socket) {
         /* Read RAPL_PKG_POWER_LIMIT to use for restore */
         errno = 0;
-        num_byte = pread(msr_fd, &msr_value, sizeof(uint64_t), pkg_power_limit_off);
+        num_byte = pread(msr_fd[socket], &msr_value, sizeof(uint64_t), pkg_power_limit_off);
         if (num_byte != sizeof(uint64_t)) {
             err = errno ? errno : -1;
         }
-    }
-    if (!err) {
-        save_limit = msr_value;
-        msr_value &= 0xFFFFFFFFFFFF0000;
-        msr_value |= 0x000000000000FFFF & (uint64_t)(power_limit / power_units);
-        /* Write RAPL_PKG_POWER_LIMIT given on command line */
-        errno = 0;
-        num_byte = pwrite(msr_fd, &msr_value, sizeof(uint64_t), pkg_power_limit_off);
-        if (num_byte != sizeof(uint64_t)) {
-            err = errno ? errno : -1;
+        if (!err) {
+            save_limit[socket] = msr_value;
+            /* Write RAPL_PKG_POWER_LIMIT given on command line */
+            msr_value &= 0xFFFFFFFFFFFF0000;
+            msr_value |= 0x000000000000FFFF & (uint64_t)(power_limit / power_units);
+            errno = 0;
+            num_byte = pwrite(msr_fd[socket], &msr_value, sizeof(uint64_t), pkg_power_limit_off);
+            if (num_byte != sizeof(uint64_t)) {
+                err = errno ? errno : -1;
+            }
+        }
+        if (!err) {
+            /* Read package energy */
+            errno = 0;
+            num_byte = pread(msr_fd[socket], &msr_value, sizeof(uint64_t), pkg_energy_status_off);
+            if (num_byte != sizeof(uint64_t)) {
+                err = errno ? errno : -1;
+            }
+        }
+        if (!err) {
+            begin_energy[socket] = msr_value & 0x00000000FFFFFFFF;
         }
     }
     if (!err) {
@@ -179,16 +262,6 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
         err = geopm_time(&begin_time);
     }
     if (!err) {
-        /* Read package energy */
-        errno = 0;
-        num_byte = pread(msr_fd, &msr_value, sizeof(uint64_t), pkg_energy_status_off);
-        if (num_byte != sizeof(uint64_t)) {
-            err = errno ? errno : -1;
-        }
-    }
-    if (!err) {
-        begin_energy = msr_value & 0x00000000FFFFFFFF;
-
         /* Run dgemm in a loop */
         int M = matrix_size;
         int N = matrix_size;
@@ -207,44 +280,46 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
         }
     }
     if (!err) {
-        /* Read package energy */
-        errno = 0;
-        num_byte = pread(msr_fd, &msr_value, sizeof(uint64_t), pkg_energy_status_off);
-        if (num_byte != sizeof(uint64_t)) {
-            err = errno ? errno : -1;
-        }
-    }
-    if (!err) {
         /* Get time of day */
         err = geopm_time(&end_time);
     }
     if (!err) {
-        /* Handle overflow */
-        end_energy = msr_value & 0x00000000FFFFFFFF;
-        if (end_energy < begin_energy) {
-            end_energy += 0xFFFFFFFF;
+        /* Calcuate average power */
+        total_time = geopm_time_diff(&begin_time, &end_time);
+    }
+    for (socket = 0; !err && socket < num_socket; ++socket) {
+        /* Read package energy */
+        errno = 0;
+        num_byte = pread(msr_fd[socket], &msr_value, sizeof(uint64_t), pkg_energy_status_off);
+        if (num_byte != sizeof(uint64_t)) {
+            err = errno ? errno : -1;
+        }
+        if (!err) {
+            end_energy[socket] = msr_value & 0x00000000FFFFFFFF;
+            /* Handle overflow */
+            if (end_energy[socket] < begin_energy[socket]) {
+                end_energy[socket] += 0xFFFFFFFF;
+            }
+            power_used[socket] = energy_units * (end_energy[socket] - begin_energy[socket]) / total_time;
         }
     }
 
-    if (!err) {
-        /* Calcuate average power */
-        total_time = geopm_time_diff(&begin_time, &end_time);
-        power_used = energy_units * (end_energy - begin_energy) / total_time;
-    }
-
-    if (save_limit && msr_fd > 0) {
-        /* Restore original settings */
-        errno = 0;
-        pwrite(msr_fd, &save_limit, sizeof(uint64_t), pkg_power_limit_off);
-        err = errno;
-    }
-    if (msr_fd > 0) {
-        close(msr_fd);
+    for (socket = 0; socket < num_socket; ++socket) {
+        if (save_limit && msr_fd[socket] > 0) {
+            /* Restore original settings */
+            pwrite(msr_fd[socket], &save_limit, sizeof(uint64_t), pkg_power_limit_off);
+        }
+        if (msr_fd[socket] > 0) {
+            close(msr_fd[socket]);
+        }
     }
     if (!err) {
         /* Print results. */
         fprintf(outfile, "Total time (seconds): %f\n", total_time);
-        fprintf(outfile, "Average power (Watts): %f\n", power_used);
+        for (socket = 0; socket < num_socket; ++socket) {
+            fprintf(outfile, "Average power socket %d (Watts): %f\n",
+                    socket, power_used[socket]);
+        }
     }
     /* Pipe error messages to standard error if they occured before
        the outfile was opened. */
@@ -252,11 +327,14 @@ int rapl_pkg_limit_test(double power_limit, int num_rep)
         outfile = stderr;
     }
     /* Detect if over limit and print */
-    if (power_used > power_limit) {
-        fprintf(outfile, "Error: exceeded limit by %f Watts\n", power_used - power_limit);
-        err = -2;
+    for (socket = 0; socket < num_socket; ++socket) {
+        if (power_used[socket] > power_limit) {
+            fprintf(outfile, "Error: socket %d exceeded limit by %f Watts\n",
+                    socket, power_used[socket] - power_limit);
+            err = -2;
+        }
     }
-    else if (err) {
+    if (err != -2) {
         fprintf(outfile, "Error: %d %s\n", err, strerror(err));
     }
     if (outfile != stderr) {
