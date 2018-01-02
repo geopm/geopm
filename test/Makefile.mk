3d94d3486e8bd37b3a77dffaf0dbd7433966e9a0
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-01-02 09:14:04 -0800

    Add pmpi_mock.c to the EXTRA_DIST variable.
    - This source file referenced by a #include, and so
      is not explicitly compiled.
    - To include it in the distribution tarball, it must be
      added to EXTRA_DIST.
    
    Change-Id: Ie7def70f13966f7435e8df56def054e022d7cce4
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -214,6 +214,7 @@ EXTRA_DIST += test/geopm_test.sh \
               test/MPITreeCommunicatorTest.cpp \
               test/MPIInterfaceTest.cpp \
               test/no_omp_cpu.c \
+              test/pmpi_mock.c \
               test/default_policy.json \
               test/invalid_policy.json \
               test/legacy_whitelist.out \
