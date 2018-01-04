328612ce3ae16040b9a7555bf3342d48e24ee877
Diana Guttman <diana.r.guttman@intel.com>
2018-01-04 05:12:55 -0500

    Update doxygen comments for Exception.
    Change-Id: I46d44f5b17fea47cae0e390cc0de75c33cb87501
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/Exception.hpp
+++ src/Exception.hpp
@@ -51,9 +51,9 @@ namespace geopm
 {
     /// @brief Handle a thrown exception and return an error value.
     ///
-    /// This exception handler is used by every geopm C interface to
+    /// This exception handler is used by every GEOPM C interface to
     /// handle any exceptions that are thrown during execution of a
-    /// C++ implementation.  If geopm has been configured with
+    /// C++ implementation.  If GEOPM has been configured with
     /// debugging enabled, then this handler will print an explanatory
     /// message to standard error.  In all cases it will convert the
     /// C++ exception into an error number which can be used with
@@ -66,14 +66,14 @@ namespace geopm
     ///        std::current_exception().
     ///
     /// @return Error number, positive numbers are system errors,
-    ///         negative numbers are geopm errors.
+    ///         negative numbers are GEOPM errors.
     int exception_handler(std::exception_ptr eptr);
 
-    /// @brief Class for all geopm specific exceptions.
+    /// @brief Class for all GEOPM-specific exceptions.
     ///
-    /// All exceptions explicitly thrown by the geopm library will be
+    /// All exceptions explicitly thrown by the GEOPM library will be
     /// of this type.  It derives from std::runtime_error and adds one
-    /// method called err_value() which returns the error code
+    /// method called err_value() that returns the error code
     /// associated with the exception.  There are a number of
     /// different constructors.
     class Exception: public std::runtime_error
@@ -93,7 +93,7 @@ namespace geopm
             ///
             /// @param [in] err Error code, positive values are system
             ///        errors (see errno(3)), negative values are
-            ///        geopm errors.  If zero is specified
+            ///        GEOPM errors.  If zero is specified
             ///        GEOPM_ERROR_RUNTIME (-1) is assumed.
             Exception(int err);
             /// @brief Message and error number constructor.
@@ -107,7 +107,7 @@ namespace geopm
             ///
             /// @param [in] err Error code, positive values are system
             ///        errors (see errno(3)), negative values are
-            ///        geopm errors.  If zero is specified
+            ///        GEOPM errors.  If zero is specified
             ///        GEOPM_ERROR_RUNTIME (-1) is assumed.
             Exception(const std::string &what, int err);
             /// @brief Error number and line number constructor.
@@ -118,20 +118,20 @@ namespace geopm
             ///
             /// @param [in] err Error code, positive values are system
             ///        errors (see errno(3)), negative values are
-            ///        geopm errors.  If zero is specified
+            ///        GEOPM errors.  If zero is specified
             ///        GEOPM_ERROR_RUNTIME (-1) is assumed.
             ///
             /// @param [in] file Name of source file where exception
-            ///        was thrown, e.g. preprocessor __FILE__.
+            ///        was thrown, e.g. preprocessor `__FILE__`.
             ///
             /// @param [in] line Line number in source file where
             ///        exception was thrown, e.g. preprocessor
-            ///        __LINE__.
+            ///        `__LINE__`.
             Exception(int err, const char *file, int line);
             /// @brief Message, error number, file and line
             ///        constructor.
             ///
-            /// User provides message, error cede, file name and line
+            /// User provides message, error code, file name and line
             /// number.  The what() method appends the user specified
             /// message, file name and line number to the abbreviated
             /// message.  This is the most verbose messaging available
@@ -142,15 +142,15 @@ namespace geopm
             ///
             /// @param [in] err Error code, positive values are system
             ///        errors (see errno(3)), negative values are
-            ///        geopm errors.  If zero is specified
+            ///        GEOPM errors.  If zero is specified
             ///        GEOPM_ERROR_RUNTIME (-1) is assumed.
             ///
             /// @param [in] file Name of source file where exception
-            ///        was thrown, e.g. preprocessor __FILE__.
+            ///        was thrown, e.g. preprocessor `__FILE__`.
             ///
             /// @param [in] line Line number in source file where
             ///        exception was thrown, e.g. preprocessor
-            ///        __LINE__.
+            ///        `__LINE__`.
             Exception(const std::string &what, int err, const char *file, int line);
             /// @brief Exception destructor, virtual.
             virtual ~Exception();
@@ -158,7 +158,7 @@ namespace geopm
             ///        the exception.
             ///
             /// Returns the non-zero error code associated with the
-            /// exception.  Negative error codes are geopm specific
+            /// exception.  Negative error codes are GEOPM-specific
             /// and documented in the geopm_error(3) man page.
             /// Positive error codes are system errors and are
             /// documented in the system errno(3) man page.  A brief
