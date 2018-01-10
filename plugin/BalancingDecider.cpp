4acf4af8923b48205d0c55973ff8d220e11f5df6
Brandon Baker <brandon.baker@intel.com>
2018-01-10 17:56:11 +0000

    New plugin registration flow implementation.
    - Removed call to dlsym.
    - Plugins now use constructor attribute to specify a callback
      target for when plugin is loaded.  In this callback the plugin should
      register with its respective Factory.
    - Moved geopm_plugin_description_s definition to geopm.h
    - Removed NAME_MAX definition from source and using limits.h
    
    Change-Id: Ib5b31b1399fb9efcaa4ad0fcb95652cf82d38c9d
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- plugin/BalancingDecider.cpp
+++ plugin/BalancingDecider.cpp
@@ -40,23 +40,6 @@
 #include "BalancingDecider.hpp"
 #include "Exception.hpp"
 
-int geopm_plugin_register(int plugin_type, struct geopm_factory_c *factory, void *dl_ptr)
-{
-    int err = 0;
-
-    try {
-        if (plugin_type == GEOPM_PLUGIN_TYPE_DECIDER) {
-            geopm::IDecider *decider = new geopm::BalancingDecider;
-            geopm_factory_register(factory, decider, dl_ptr);
-        }
-    }
-    catch(...) {
-        err = geopm::exception_handler(std::current_exception());
-    }
-
-    return err;
-}
-
 struct {
     bool operator()(std::pair<int,double> a, std::pair<int,double> b)
     {
