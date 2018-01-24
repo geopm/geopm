1dd5a195bb78a1b9387f0ef1818cf14e1034da53
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-01-24 13:14:11 -0800

    Introducing PlatformTopo, replacement for PlatformTopology.
    - PlatformTopo is built on top of lscpu and does not
      require hwloc.
    
    Change-Id: I6066fae5f81db199c7264b264c8f108f889e8b1d
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -195,6 +195,13 @@ GTEST_TESTS = test/gtest_links/PlatformFactoryTest.platform_register \
               test/gtest_links/RuntimeRegulatorTest.all_reenter \
               test/gtest_links/RuntimeRegulatorTest.one_rank_reenter_and_exit \
               test/gtest_links/RuntimeRegulatorTest.config_rank_then_workers \
+              test/gtest_links/PlatformTopoTest.hsw_num_domain \
+              test/gtest_links/PlatformTopoTest.knl_num_domain \
+              test/gtest_links/PlatformTopoTest.bdx_num_domain \
+              test/gtest_links/PlatformTopoTest.construction \
+              test/gtest_links/PlatformTopoTest.singleton_construction \
+              test/gtest_links/PlatformTopoTest.bdx_domain_idx \
+              test/gtest_links/PlatformTopoTest.bdx_domain_cpus \
               # end
 
 if ENABLE_MPI
@@ -262,6 +269,7 @@ test_geopm_test_SOURCES = test/geopm_test.cpp \
                           plugin/AdaptiveFreqRegion.cpp \
                           test/AdaptiveFreqRegionTest.cpp \
                           test/RuntimeRegulatorTest.cpp \
+                          test/PlatformTopoTest.cpp \
                           # end
 
 test_geopm_test_LDADD = libgtest.a \
