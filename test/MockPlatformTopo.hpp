0a1690cb930262e91436260b03c39bdec1bf75f0
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-02-07 11:10:21 -0800

    Update EfficientFreqDecider to use PlatformIO directly.
    - Don't rely on Policy method to adjust frequency.
    
    Change-Id: I284fcb0b5b130a2edb5bab2693150f95d1fac93f
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ test/MockPlatformTopo.hpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "PlatformTopo.hpp"
+
+class MockPlatformTopo : public geopm::IPlatformTopo
+{
+    public:
+        MOCK_CONST_METHOD1(num_domain,
+                           int(int domain_type));
+        MOCK_CONST_METHOD3(domain_cpus,
+                           void(int domain_type, int domain_idx, std::set<int> &cpu_idx));
+        MOCK_CONST_METHOD2(domain_idx,
+                           int(int domain_type, int cpu_idx));
+        MOCK_METHOD1(define_cpu_group,
+                     int(const std::vector<int> &cpu_domain_idx));
+};
+
