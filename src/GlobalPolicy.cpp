f2b56c2d0d7b799f0e5b4783f5714d1fec96328b
Brandon Baker <brandon.baker@intel.com>
2018-02-14 19:12:00 -0800

    Update GlobalPolicy to use PlatformIO interfaces.
    Change-Id: Ibb706a58a77d92d9e2a787416504a5ad306eac72
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/GlobalPolicy.cpp
+++ src/GlobalPolicy.cpp
@@ -48,8 +48,8 @@
 #include "geopm_policy.h"
 #include "Exception.hpp"
 #include "GlobalPolicy.hpp"
-#include "Platform.hpp"
-#include "PlatformFactory.hpp"
+#include "PlatformIO.hpp"
+#include "PlatformTopo.hpp"
 #include "PolicyFlags.hpp"
 #include "geopm_version.h"
 #include "geopm_env.h"
@@ -1007,18 +1007,57 @@ namespace geopm
 
     void GlobalPolicy::enforce_static_mode()
     {
-        PlatformFactory platform_factory;
-        Platform *platform = platform_factory.platform(std::string("rapl"), true);
+        IPlatformIO &pio = platform_io();
+        IPlatformTopo &topo = platform_topo();
+        const std::string power_ctl_name = "POWER"; // TODO
+        const std::string freq_ctl_name = "FREQ";   // TODO
+        const int power_ctl_domain = pio.control_domain_type(power_ctl_name);
+        const int freq_ctl_domain = pio.control_domain_type(freq_ctl_name);
+        const int num_power_domain = topo.num_domain(power_ctl_domain);
+        const int num_freq_domain = topo.num_domain(freq_ctl_domain);
 
         switch (m_mode) {
             case GEOPM_POLICY_MODE_TDP_BALANCE_STATIC:
-                platform->tdp_limit(tdp_percent());
+                for (int domain_idx = 0; domain_idx < num_power_domain; ++domain_idx) {
+                    pio.write_control(power_ctl_name, power_ctl_domain, domain_idx, tdp_percent());
+                }
                 break;
             case GEOPM_POLICY_MODE_FREQ_UNIFORM_STATIC:
-                platform->manual_frequency(frequency_mhz(), 0, GEOPM_POLICY_AFFINITY_SCATTER);
+                for (int domain_idx = 0; domain_idx < num_freq_domain; ++domain_idx) {
+                    pio.write_control(freq_ctl_name, freq_ctl_domain, domain_idx, frequency_mhz());
+                }
                 break;
             case GEOPM_POLICY_MODE_FREQ_HYBRID_STATIC:
-                platform->manual_frequency(frequency_mhz(), num_max_perf(), affinity());
+                //int affinity = affinity();
+                //for (int i = 0; i < num_logical_cpus; i++) {
+                    //int real_cpu = i % num_real_cpus;
+                    //if (affinity == GEOPM_POLICY_AFFINITY_SCATTER && num_cpu_max_perf > 0) {
+                        //if ((real_cpu % num_cpus_per_package) < num_small_cores_per_package) {
+                            //small = true;
+                        //}
+                    //}
+                    //else if (affinity == GEOPM_POLICY_AFFINITY_COMPACT && num_cpu_max_perf > 0) {
+                        //if (real_cpu < (num_real_cpus - num_cpu_max_perf)) {
+                            //small = true;
+                        //}
+                    //}
+                    //else {
+                        //small = true;
+                    //}
+                    //if (small) {
+                        //freq_perc = ((int64_t)(frequency * 0.01) << 8) & 0xffff;
+                        //m_imp->msr_write(GEOPM_DOMAIN_CPU, i, "IA32_PERF_CTL", freq_perc & 0xffff);
+                    //}
+                    //small = false;
+                //}
+                for (int domain_idx = 0; domain_idx < num_freq_domain; ++domain_idx) {
+                    double target_freq = frequency_mhz();
+                    if (domain_idx < num_max_perf()) {
+                        // TODO implement or find API for max_freq()
+                        //target_freq = max_freq();
+                    }
+                    pio.write_control(freq_ctl_name, freq_ctl_domain, domain_idx, target_freq);
+                }
                 break;
             default:
                 throw Exception("GlobalPolicy: invalid mode specified",
