d9046d116c49de5bdc9d5d10ea90262901b65034
Diana Guttman <diana.r.guttman@intel.com>
2018-02-15 20:38:09 -0500

    Add bounds checking for domain index in IOGroups.
    Change-Id: I1ffde8ac6270f13864d455435de72da6a87e8aab
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/MSRIOGroup.cpp
+++ src/MSRIOGroup.cpp
@@ -147,6 +147,12 @@ namespace geopm
             throw Exception(err_str.str(), GEOPM_ERROR_INVALID, __FILE__, __LINE__);
         }
 
+        /// @todo support for non-CPU domains.
+        if (domain_idx < 0 || domain_idx > geopm_sched_num_cpu()) {
+            throw Exception("MSRIOGroup::push_signal(): domain_idx out of bounds.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+
         int result = -1;
         int cpu_idx = domain_idx;
 
@@ -184,6 +190,12 @@ namespace geopm
             throw Exception("MSRIOGroup::push_control(): cannot push a control after read_batch() or adjust() has been called.",
                             GEOPM_ERROR_INVALID, __FILE__, __LINE__);
         }
+        /// @todo support for non-CPU domains.
+        if (domain_idx < 0 || domain_idx > geopm_sched_num_cpu()) {
+            throw Exception("MSRIOGroup::push_control(): domain_idx out of bounds.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+
         auto nccm_it = m_name_cpu_control_map.find(control_name);
         if (nccm_it == m_name_cpu_control_map.end()) {
             std::ostringstream err_str;
@@ -281,6 +293,11 @@ namespace geopm
                     << signal_name << "\" not found";
             throw Exception(err_str.str(), GEOPM_ERROR_INVALID, __FILE__, __LINE__);
         }
+        /// @todo support for non-CPU domains.
+        if (domain_idx < 0 || domain_idx > geopm_sched_num_cpu()) {
+            throw Exception("MSRIOGroup::read_signal(): domain_idx out of bounds.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
 
         int cpu_idx = domain_idx;
         MSRSignal signal = *(ncsm_it->second[cpu_idx]);
@@ -312,6 +329,11 @@ namespace geopm
             err_str << "MSRIOGroup::write_control(): control name \""
                     << control_name << "\" not found";
             throw Exception(err_str.str(), GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        /// @todo support for non-CPU domains.
+        if (domain_idx < 0 || domain_idx > geopm_sched_num_cpu()) {
+            throw Exception("MSRIOGroup::write_control(): domain_idx out of bounds.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
         }
 
         int cpu_idx = domain_idx;
