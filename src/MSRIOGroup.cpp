9779be68d93b86f7de1545d56039a8f1100d3b43
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-04-27 14:24:58 -0700

    Set enable bit if user requests control of frequency or power.
    Change-Id: I50ffd22af08ae3ba99ef31b147a48c41808c5430
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/MSRIOGroup.cpp
+++ src/MSRIOGroup.cpp
@@ -234,6 +234,12 @@ namespace geopm
                                 GEOPM_ERROR_LOGIC, __FILE__, __LINE__);
             }
 #endif
+            if (control_name == "POWER_PACKAGE") {
+                write_control("MSR::PKG_POWER_LIMIT:SOFT_LIMIT_ENABLE", domain_type, domain_idx, 1.0);
+            }
+            if (control_name == "FREQUENCY") {
+                write_control("MSR::PERF_CTL:ENABLE", domain_type, domain_idx, 1.0);
+            }
             // control_name may be alias, so use active control MSR name
             std::string registered_name = nccm_it->second[*(cpu_idx.begin())]->name();
             if (m_active_control[ii]->name() == registered_name &&
