e062fcb5a299135e85c4a5e5f64996d58bedfee0
Diana Guttman <diana.r.guttman@intel.com>
2018-04-29 15:32:57 -0700

    Change Agent report interface.
    - Agent report info is now a vector of pairs of strings.
    
    Change-Id: Idf0b93636a953520b1e3c2eee105c774e4ba99d8
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/MonitorAgent.cpp
+++ src/MonitorAgent.cpp
@@ -151,17 +151,17 @@ namespace geopm
         return {"POWER_PACKAGE", "FREQUENCY"};;
     }
 
-    std::string MonitorAgent::report_header(void)
+    std::vector<std::pair<std::string, std::string> > MonitorAgent::report_header(void)
     {
-        return "";
+        return {};
     }
 
-    std::string MonitorAgent::report_node(void)
+    std::vector<std::pair<std::string, std::string> > MonitorAgent::report_node(void)
     {
-        return "";
+        return {};
     }
 
-    std::map<uint64_t, std::string> MonitorAgent::report_region(void)
+    std::map<uint64_t, std::vector<std::pair<std::string, std::string> > > MonitorAgent::report_region(void)
     {
         return {};
     }
