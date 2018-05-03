029c2c9506e3902a4237fe23d6f37a4822d007ef
Diana Guttman <diana.r.guttman@intel.com>
2018-05-03 09:50:42 -0700

    Add docstrings for EpochRuntimeRegulator.
    Change-Id: I342bc8b2c24419297dad12ec8a9df69767758f21
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/EpochRuntimeRegulator.hpp
+++ src/EpochRuntimeRegulator.hpp
@@ -52,26 +52,74 @@ namespace geopm
         public:
             IEpochRuntimeRegulator() = default;
             virtual ~IEpochRuntimeRegulator() = default;
+            /// @brief Handle the initial entry into the unmarked
+            ///        region when the application starts.
             virtual void init_unmarked_region() = 0;
+            /// @brief Record a transition between epochs with
+            ///        entry/exit into the epoch region.
             virtual void epoch(int rank, struct geopm_time_s epoch_time) = 0;
+            /// @brief Record entry into a region for one rank at the
+            ///        given time.
+            /// @param [in] region_id The ID of the region.
+            /// @param [in] rank Rank tht entered the region.
+            /// @param [in] entry_time Time of entry.
             virtual void record_entry(uint64_t region_id, int rank, struct geopm_time_s entry_time) = 0;
+            /// @brief Record exit from a region for one rank at the
+            ///        given time.  Also aggregates totals for the
+            ///        epoch region.
+            /// @param [in] region_id The ID of the region.
+            /// @param [in] rank Rank that exited the region.
+            /// @param [in] exit_time Time of exit.
             virtual void record_exit(uint64_t region_id, int rank, struct geopm_time_s exit_time) = 0;
+            /// @brief Returns a reference to the RuntimeRegulator for
+            ///        a given region.  This method is intended for
+            ///        internal use by the ApplicationIO.
+            /// @param [in] region_id The ID of the region.
             virtual const IKruntimeRegulator &region_regulator(uint64_t region_id) const = 0;
+            /// @brief Returns whether or not the region is being
+            ///        tracked by the EpochRuntimeRegulator.
+            /// @param [in] region_id The ID of the region.
             virtual bool is_regulated(uint64_t region_id) const = 0;
+            /// @brief Returns the total runtime of the last iteration
+            ///        of the epoch for each rank.
             virtual std::vector<double> last_epoch_time() const = 0;
+            /// @brief Returns the number of epoch calls seen by each
+            ///        rank.
             virtual std::vector<double> epoch_count() const = 0;
+            /// @brief Returns the total runtime recorded for the
+            ///        given region.
+            /// @param [in] region_id The ID of the region.
             virtual double total_region_runtime(uint64_t region_id) const = 0;
+            /// @brief Returns the total time spent in MPI for a given
+            ///        region.
+            /// @param [in] region_id The ID of the region.
             virtual double total_region_mpi_time(uint64_t region_id) const = 0;
+            /// @brief Returns the total runtime since the first epoch
+            ///        call.  This total does not include MPI time or
+            ///        ignore time.
             virtual double total_epoch_runtime(void) const = 0;
             /// @brief Returns the total time spent in MPI after the
             ///        first epoch call.
             virtual double total_epoch_mpi_time(void) const = 0;
+            /// @brief Returns the total time spent in regions marked
+            ///        with GEOPM_REGION_ID_HINT_IGNORE after the
+            ///        first epoch call.
             virtual double total_epoch_ignore_time(void) const = 0;
+            /// @brief Returns the total time spent in MPI calls since
+            ///        the start of the application.
             virtual double total_app_mpi_time(void) const = 0;
+            /// @brief Returns the total number of times a region was
+            ///        entered and exited.
+            /// @param [in] region_id The ID of the region.
             virtual int total_count(uint64_t region_id) const = 0;
-            /// @todo this level of pass through will go away once this class is
-            /// merged with ApplicationIO
+            /// @todo this level of pass through will go away once
+            ///       this class is merged with ApplicationIO
+            /// @brief Returns the list of all regions entered or
+            ///        exited since the last call to
+            ///        clear_region_info().
             virtual std::list<geopm_region_info_s> region_info(void) const = 0;
+            /// @brief Resets the internal list of region entries and
+            ///        exits.
             virtual void clear_region_info(void) = 0;
     };
 
