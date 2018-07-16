597ba583ccc3860c8919a27ab91abe4ac7f66cf9
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-07-16 14:40:48 -0700

    First draft of new power balancing algorithm.
    - Introducing Agent::aggregate_sample() static helper function for Agents.
    - Note: Agent dox strings need to be cleaned up, many @param sections missing.
    - Fix poorly named variables: some samples were being referred to as signals.
    - Use aggregate_sample() in all agents.
    - Fixed typos.
    
    Change-Id: Ice4940d5d23649d4413473751959f0760734e846
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/PowerBalancerAgent.hpp
+++ src/PowerBalancerAgent.hpp
@@ -35,6 +35,7 @@
 
 #include <vector>
 
+#include "geopm_time.h"
 #include "Agent.hpp"
 
 namespace geopm
@@ -43,38 +44,113 @@ namespace geopm
     class IPlatformTopo;
     template <class type>
     class ICircularBuffer;
-    class IPowerGovernor;
+    class IPowerBalancer;
+    class PowerGovernor;
 
     class PowerBalancerAgent : public Agent
     {
         public:
-            /// @todo May be useful for indexing into the vector of policies or samples.
+            enum m_step_e {
+                /// @brief Measure epoch runtime several times and
+                ///        apply median filter.
+                M_STEP_MEASURE_RUNTIME,
+                /// @brief Aggregate epoch runtime up tree by applying
+                ///        maximum filter to measured values.
+                M_STEP_SEND_UP_RUNTIME,
+                /// @brief Propagate down from root the longest
+                ///        recorded runtime from any node.
+                M_STEP_SEND_DOWN_RUNTIME,
+                /// @brief Decrease power limit on all nodes (other
+                ///        than the slowest) until epoch runtime
+                ///        matches the slowest.
+                M_STEP_REDUCE_LIMIT,
+                /// @brief Aggregate amount power limit was reduced in
+                ///        last step up the tree with sum filter.  (Go
+                ///        to M_STEP_SEND_DOWN_LIMIT next).
+                M_STEP_SEND_UP_EXCESS,
+                /// @brief On first iteration send down resource
+                ///        manager average limit requested, otherwise
+                ///        send down average excess power.
+                M_STEP_SEND_DOWN_LIMIT,
+                /// @brief Number of steps in process.
+                M_NUM_STEP,
+            };
+
             enum m_policy_e {
-                M_POLICY_POWER,
+                /// @brief Step that the root is providing a policy
+                ///        for.  The parent has received a sample
+                ///        matching this step in the last walk up the
+                ///        tree, execpt in the case where the endpoint
+                ///        has recently been updated with a new
+                ///        policy, in this case the step will be
+                ///        M_SEND_DOWN_LIMIT and the policy indexed by
+                ///        M_POLICY_POWER_CAP will have a non-zero
+                ///        value.
+                M_POLICY_STEP_COUNT,
+                /// @brief The power cap enforced on average over all
+                ///        nodes running the application.  This has
+                ///        value 0.0 except in two cases.  In the
+                ///        first case this is the M_SEND_DOWN_LIMIT
+                ///        step at the beginning of the application
+                ///        run.  This value will also be non-zero in
+                ///        the case where the resource mananger has
+                ///        requested a new budget for the application,
+                ///        and thus, the algorithm must be restarted
+                ///        at step M_SEND_DOWN_LIMIT.
+                M_POLICY_POWER_CAP,
+                /// @brief The largest runtime reported by any leaf
+                ///        agent since the last redistribution of
+                ///        power.  This will have value 0.0 until all
+                ///        leaf agents have reported a runtime to the
+                ///        root agent.
+                M_POLICY_MAX_EPOCH_RUNTIME,
+                /// @brief This value is updated in step
+                ///        M_STEP_ADJUST_LIMIT to the amount that each
+                ///        leaf agent should increase their power
+                ///        limit by calling:
+                ///            power_cap(current_limit + slack)
+                ///        by before starting the algorithm over again
+                ///        at step M_STEP_MEASURE_RUNTIME.  For all
+                ///        other steps this value is 0.0.
+                M_POLICY_POWER_SLACK,
+                /// @brief Number of steps in each iteration of the
+                ///        balancing algorithm.
                 M_NUM_POLICY,
             };
+
+            enum m_sample_e {
+                /// @brief The the step counter that is currently in
+                ///        execution.  Note that the step is equal to
+                ///        the step counter modulo M_NUM_STEP and is
+                ///        reset each time a new power cap is provided
+                ///        by sending a policy with a non-zero
+                ///        M_POLICY_POWER_CAP field.
+                M_SAMPLE_STEP_COUNT,
+                /// @brief Value 0.0 implies that all children below
+                ///        have completed the step and are waiting for
+                ///        a policy that is marked with the next step.
+                ///        0.0 implies that at least one child below
+                ///        has not yet completed the step.
+                M_SAMPLE_IS_STEP_COMPLETE,
+                /// @brief Maximum expected runtime for any node
+                ///        below.
+                M_SAMPLE_MAX_EPOCH_RUNTIME,
+                /// @brief The sum of all slack power available from
+                ///        children below the agent.
+                M_SAMPLE_SUM_POWER_SLACK,
+                /// @brief Number of elements in a sample vector.
+                M_NUM_SAMPLE,
+            };
+
             enum m_plat_signal_e {
                 M_PLAT_SIGNAL_EPOCH_RUNTIME,
-                //M_PLAT_SIGNAL_EPOCH_ENERGY,
                 M_PLAT_SIGNAL_EPOCH_COUNT,
-                M_PLAT_SIGNAL_PKG_POWER,
-                M_PLAT_SIGNAL_DRAM_POWER,
                 M_PLAT_NUM_SIGNAL,
             };
             enum m_trace_sample_e {
                 M_TRACE_SAMPLE_EPOCH_RUNTIME,
-                M_TRACE_SAMPLE_PKG_POWER,
-                M_TRACE_SAMPLE_DRAM_POWER,
-                M_TRACE_SAMPLE_IS_CONVERGED,
-                M_TRACE_SAMPLE_PWR_BUDGET,
+                M_TRACE_SAMPLE_POWER_LIMIT,
                 M_TRACE_NUM_SAMPLE,
-            };
-            enum m_sample_e { // Tree samples
-                M_SAMPLE_EPOCH_RUNTIME,
-                M_SAMPLE_POWER,
-                M_SAMPLE_IS_CONVERGED,
-                M_SAMPLE_POWER_ENFORCED,
-                M_NUM_SAMPLE,
             };
 
             PowerBalancerAgent();
@@ -98,47 +174,34 @@ namespace geopm
             static std::vector<std::string> sample_names(void);
         private:
             void init_platform_io(void);
-            bool descend_initial_budget(double power_budget_in, std::vector<double> &power_budget_out);
-            bool descend_updated_budget(double power_budget_in, std::vector<double> &power_budget_out);
-            bool descend_updated_runtimes(double power_budget_in, std::vector<double> &power_budget_out);
-            std::vector<double> split_budget(double avg_power_budget);
-            std::vector<double> split_budget_first(double power_budget_in);
-            std::vector<double> split_budget_helper(double avg_power_budget,
-                                                    double min_power_budget,
-                                                    double max_power_budget);
-
+            void update_policy(const std::vector<double> &sample);
+            int step(void);
+            int step(uint64_t step_count);
             IPlatformIO &m_platform_io;
             IPlatformTopo &m_platform_topo;
-            int m_level; // Needed in order to determine convergence
-            bool m_is_converged;
-            bool m_is_sample_stable;
-            int m_updates_per_sample;
+            int m_level;
             double m_min_power_budget;
             double m_max_power_budget;
-            std::unique_ptr<IPowerGovernor> m_power_gov;
+            std::unique_ptr<PowerGovernor> m_power_gov;
+            std::unique_ptr<IPowerBalancer> m_power_balancer;
             std::vector<int> m_pio_idx;
-            std::vector<std::function<double(const std::vector<double>&)> > m_agg_func;
+            const std::vector<std::function<double(const std::vector<double>&)> > m_agg_func;
             int m_num_children;
-            bool m_is_root;
-            double m_last_power_budget_in;
-            double m_last_power_budget_out;
-            std::vector<double> m_last_runtime0;
-            std::vector<double> m_last_runtime1;
-            std::vector<double> m_last_budget0;
-            std::vector<double> m_last_budget1;
-            std::unique_ptr<ICircularBuffer<double> > m_epoch_runtime_buf;
-            std::unique_ptr<ICircularBuffer<double> > m_epoch_power_buf;
-            std::vector<double> m_sample;
-            double m_last_energy_status;
-            int m_ascend_count;
-            const int m_ascend_period;
-            bool m_is_updated;
-            const double m_convergence_target;
-            int m_num_out_of_range;
-            const int m_min_num_converged;
-            int m_num_converged;
+            /// @todo m_is_level_root is never used.  Either delete
+            ///       this member variable or figure out where the
+            ///       missing code is for the root of the level.
+            bool m_is_level_root;
+            bool m_is_tree_root;
             int m_last_epoch_count;
-            double m_adjusted_power;
+            size_t m_step_count;
+            bool m_is_step_complete;
+            double m_runtime;
+            double m_power_slack;
+            struct geopm_time_s m_last_wait;
+            const double M_WAIT_SEC;
+            std::vector<double> m_sample;
+            std::vector<double> m_policy;
+            int m_num_node;
     };
 }
 
