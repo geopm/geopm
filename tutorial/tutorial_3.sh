21f146e570c0b2dab955762cd50cbc3401b64bfd
Brandon Baker <brandon.baker@intel.com>
2018-08-16 11:39:09 -0700

    Update tutorials to use Agent code path.
    Change-Id: I8a9a94567a63e577dff57078d40e3fab61f2a5f9
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- tutorial/tutorial_3.sh
+++ tutorial/tutorial_3.sh
@@ -48,6 +48,7 @@ if [ "$GEOPM_RM" == "SLURM" ]; then
     geopmsrun  -N 2 \
                -n 8 \
                --geopm-ctl=process \
+               --geopm-agent=power_governor \
                --geopm-report=tutorial_3_governed_report \
                --geopm-trace=tutorial_3_governed_trace \
                --geopm-policy=tutorial_governed_policy.json \
@@ -56,6 +57,7 @@ if [ "$GEOPM_RM" == "SLURM" ]; then
     geopmsrun  -N 2 \
                -n 8 \
                --geopm-ctl=process \
+               --geopm-agent=power_balancer \
                --geopm-report=tutorial_3_balanced_report \
                --geopm-trace=tutorial_3_balanced_trace \
                --geopm-policy=tutorial_balanced_policy.json \
@@ -67,6 +69,7 @@ elif [ "$GEOPM_RM" == "ALPS" ]; then
     geopmaprun -N 4 \
                -n 8 \
                --geopm-ctl=process \
+               --geopm-agent=power_governor \
                --geopm-report=tutorial_3_governed_report \
                --geopm-trace=tutorial_3_governed_trace \
                --geopm-policy=tutorial_governed_policy.json \
@@ -75,12 +78,14 @@ elif [ "$GEOPM_RM" == "ALPS" ]; then
     geopmaprun -N 4 \
                -n 8 \
                --geopm-ctl=process \
+               --geopm-agent=power_balancer \
                --geopm-report=tutorial_3_balanced_report \
                --geopm-trace=tutorial_3_balanced_trace \
                --geopm-policy=tutorial_balanced_policy.json \
                -- ./tutorial_3
     err=$?
 elif [ $MPIEXEC ]; then
+    GEOPM_AGENT="power_governor" \
     LD_DYNAMIC_WEAK=true \
     GEOPM_PMPI_CTL=process \
     GEOPM_REPORT=tutorial_3_governed_report \
@@ -88,6 +93,7 @@ elif [ $MPIEXEC ]; then
     GEOPM_POLICY=tutorial_governed_policy.json \
     $MPIEXEC ./tutorial_3 \
     && \
+    GEOPM_AGENT="power_balancer" \
     LD_DYNAMIC_WEAK=true \
     GEOPM_PMPI_CTL=process \
     GEOPM_REPORT=tutorial_3_balanced_report \
