620e45911aaaa86cfa85e69a46e7757cb33bb06a
Diana Guttman <diana.r.guttman@intel.com>
2018-09-28 08:22:30 -0700

    Implement agg_function for IOGroups, including tutorial.
    Change-Id: I649c1cc3523df37312931ccf26da56f59ca34850
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/CpuinfoIOGroup.hpp
+++ src/CpuinfoIOGroup.hpp
@@ -63,6 +63,7 @@ namespace geopm
             void write_control(const std::string &control_name, int domain_type, int domain_idx, double setting) override;
             void save_control(void) override;
             void restore_control(void) override;
+            std::function<double(const std::vector<double> &)> agg_function(const std::string &signal_name) const override;
             static std::string plugin_name(void);
             static std::unique_ptr<IOGroup> make_plugin(void);
         private:
