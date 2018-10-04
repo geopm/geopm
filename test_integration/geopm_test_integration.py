68a2575d0d31df191dc37e2344dd06fcd8fd0287
Diana Guttman <diana.r.guttman@intel.com>
2018-10-04 13:23:49 -0700

    Add SKX to supported platforms for energy efficient tests.
    Change-Id: I1b10ca265c11c45e467dd153189b6616996cbd7a
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test_integration/geopm_test_integration.py
+++ test_integration/geopm_test_integration.py
@@ -97,10 +97,10 @@ def get_platform():
     return fam, mod
 
 
-def skip_unless_platform_bdx():
+def skip_unless_platform_bdx_skx():
     fam, mod = get_platform()
-    if fam != 6 or mod not in (45, 47, 79):
-        return unittest.skip("Performance test is tuned for BDX server, The family {}, model {} is not supported.".format(fam, mod))
+    if fam != 6 or mod not in (45, 47, 79, 85):
+        return unittest.skip("Performance test is tuned for BDX or SKX server, The family {}, model {} is not supported.".format(fam, mod))
     return lambda func: func
 
 
@@ -1062,6 +1062,9 @@ class TestIntegration(unittest.TestCase):
         if hostname.endswith('.alcf.anl.gov'):
             dgemm_bigo = dgemm_bigo_jlse
             stream_bigo = stream_bigo_jlse
+        elif hostname.startswith('mcfly'):
+            dgemm_bigo = 42.0
+            stream_bigo = 1.75
         else:
             dgemm_bigo = dgemm_bigo_quartz
             stream_bigo = stream_bigo_quartz
@@ -1132,6 +1135,9 @@ class TestIntegration(unittest.TestCase):
         if hostname.endswith('.alcf.anl.gov'):
             dgemm_bigo = dgemm_bigo_jlse
             stream_bigo = stream_bigo_jlse
+        elif hostname.startswith('mcfly'):
+            dgemm_bigo = 42.0
+            stream_bigo = 1.75
         else:
             dgemm_bigo = dgemm_bigo_quartz
             stream_bigo = stream_bigo_quartz
