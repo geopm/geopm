758a9e300d5582e15da73dfef0dcaedfdc20c267
Diana Guttman <diana.r.guttman@intel.com>
2018-10-04 13:23:49 -0700

    Add SKX to supported platforms for energy efficient tests.
    Change-Id: I1b10ca265c11c45e467dd153189b6616996cbd7a
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test_integration/geopm_test_integration.py
+++ test_integration/geopm_test_integration.py
@@ -90,10 +90,10 @@ def get_platform():
     return fam, mod
 
 
-def skip_unless_platform_bdx():
+def skip_unless_platform_bdx_skx():
     fam, mod = get_platform()
-    if fam != 6 or mod not in (45, 47, 79):
-        return unittest.skip("Performance test is tuned for BDX server, The family {}, model {} is not supported.".format(fam, mod))
+    if fam != 6 or mod not in (45, 47, 79, 85):
+        return unittest.skip("Performance test is tuned for BDX or SKX server, The family {}, model {} is not supported.".format(fam, mod))
     return lambda func: func
 
 
@@ -1028,7 +1028,7 @@ class TestIntegration(unittest.TestCase):
             self.assertLessEqual(1, len(gemm_region))
 
     @skip_unless_run_long_tests()
-    @skip_unless_platform_bdx()
+    @skip_unless_platform_bdx_skx()
     @skip_unless_cpufreq()
     def test_agent_energy_efficient_offline(self):
         """
@@ -1056,6 +1056,9 @@ class TestIntegration(unittest.TestCase):
         if hostname.endswith('.alcf.anl.gov'):
             dgemm_bigo = dgemm_bigo_jlse
             stream_bigo = stream_bigo_jlse
+        elif hostname.startswith('mcfly'):
+            dgemm_bigo = 42.0
+            stream_bigo = 1.75
         else:
             dgemm_bigo = dgemm_bigo_quartz
             stream_bigo = stream_bigo_quartz
@@ -1101,7 +1104,7 @@ class TestIntegration(unittest.TestCase):
         self.assertLess(-10.0, runtime_savings_epoch)  # want -10% or better
 
     @skip_unless_run_long_tests()
-    @skip_unless_platform_bdx()
+    @skip_unless_platform_bdx_skx()
     @skip_unless_cpufreq()
     def test_agent_energy_efficient_online(self):
         """
@@ -1130,6 +1133,9 @@ class TestIntegration(unittest.TestCase):
         if hostname.endswith('.alcf.anl.gov'):
             dgemm_bigo = dgemm_bigo_jlse
             stream_bigo = stream_bigo_jlse
+        elif hostname.startswith('mcfly'):
+            dgemm_bigo = 42.0
+            stream_bigo = 1.75
         else:
             dgemm_bigo = dgemm_bigo_quartz
             stream_bigo = stream_bigo_quartz
