54d165a50731934625365020d3db93515220a178
Brandon Baker <brandon.baker@intel.com>
2018-11-14 10:40:21 -0800

    Update meaning of EPOCH_RUNTIME signal.
    Change-Id: I64c6a962b424bcba4f09c299558b94b39df9c267
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/ProfileIOGroup.hpp
+++ src/ProfileIOGroup.hpp
@@ -78,6 +78,8 @@ namespace geopm
             enum m_signal_type {
                 M_SIGNAL_REGION_ID,
                 M_SIGNAL_PROGRESS,
+                M_SIGNAL_EPOCH_RUNTIME_MPI,
+                M_SIGNAL_EPOCH_RUNTIME_IGNORE,
                 M_SIGNAL_EPOCH_RUNTIME,
                 M_SIGNAL_EPOCH_COUNT,
                 M_SIGNAL_RUNTIME,
@@ -103,6 +105,8 @@ namespace geopm
             std::vector<double> m_per_cpu_progress;
             std::vector<double> m_per_cpu_runtime;
             std::vector<double> m_thread_progress;
+            std::vector<double> m_epoch_runtime_mpi;
+            std::vector<double> m_epoch_runtime_ignore;
             std::vector<double> m_epoch_runtime;
             std::vector<double> m_epoch_count;
             std::map<int, int> m_rid_idx; // map from runtime signal index to the region id signal it uses
