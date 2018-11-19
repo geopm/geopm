34eee8f5af8d599efec6b2b3a68c7c64f2d98d9b
Brandon Baker <brandon.baker@intel.com>
2018-11-19 11:07:58 -0800

    Update EpochRuntimeRegulator to include pre-epoch times.
    - Updated to report pre-epoch ignore runtime.
    - Fixes 368.
    
    Change-Id: Ie5ccd6f3e28587fe1e4e48b88bf585c4bbebce9e
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/EpochRuntimeRegulator.cpp
+++ src/EpochRuntimeRegulator.cpp
@@ -60,6 +60,8 @@ namespace geopm
         , m_agg_runtime_mpi(m_rank_per_node, 0.0)
         , m_last_epoch_runtime(m_rank_per_node, 0.0)
         , m_agg_epoch_runtime(m_rank_per_node, 0.0)
+        , m_agg_pre_epoch_runtime_mpi(m_rank_per_node, 0.0)
+        , m_agg_pre_epoch_runtime_ignore(m_rank_per_node, 0.0)
         , m_pre_epoch_region(m_rank_per_node)
         , m_epoch_start_energy_pkg(NAN)
         , m_epoch_start_energy_dram(NAN)
@@ -188,9 +190,13 @@ namespace geopm
                 m_agg_epoch_runtime[rank] += m_last_epoch_runtime[rank];
                 m_agg_epoch_runtime_mpi[rank] += m_curr_runtime_mpi[rank];
                 m_agg_epoch_runtime_ignore[rank] += m_curr_runtime_ignore[rank];
-                m_curr_runtime_mpi[rank] = 0.0;
-                m_curr_runtime_ignore[rank] = 0.0;
             }
+            else {
+                m_agg_pre_epoch_runtime_mpi[rank] += m_curr_runtime_mpi[rank];
+                m_agg_pre_epoch_runtime_ignore[rank] += m_curr_runtime_ignore[rank];
+            }
+            m_curr_runtime_mpi[rank] = 0.0;
+            m_curr_runtime_ignore[rank] = 0.0;
         }
         else if (is_mpi) {
             if (pre_epoch_it == m_pre_epoch_region[rank].end()) {
@@ -305,12 +311,12 @@ namespace geopm
 
     double EpochRuntimeRegulator::total_epoch_runtime_mpi(void) const
     {
-        return total_region_runtime_mpi(GEOPM_REGION_ID_EPOCH);
+        return Agg::average(m_agg_pre_epoch_runtime_mpi) + total_region_runtime_mpi(GEOPM_REGION_ID_EPOCH);
     }
 
     double EpochRuntimeRegulator::total_epoch_runtime_ignore(void) const
     {
-        return Agg::average(m_agg_epoch_runtime_ignore);
+        return Agg::average(m_agg_pre_epoch_runtime_ignore) + Agg::average(m_agg_epoch_runtime_ignore);
     }
 
     double EpochRuntimeRegulator::total_epoch_energy_pkg(void) const
