5f8cf212194460f31a1dc64830b34d1dbf276fca
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-01-11 16:56:06 -0800

    Fix geopmhash example command line tool.
    - Remove help message documenting hidden bits in internal region ids.
    - Print region id in hex with 16 digits.
    
    Change-Id: Ia4aa0ca1f6bd0f4ca720d94481e4400cae11ff25
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- examples/geopmhash.c
+++ examples/geopmhash.c
@@ -40,18 +40,14 @@ int main(int argc, char **argv)
     const char *usage = "%s string\n\n"
                         "    Returns the geopm_hash() of the input string.\n"
                         "    this is useful for determining the region name\n"
-                        "    from the region_id printed in the trace.\n"
-                        "    Note the mpi-runtime and epoch regions have\n"
-                        "    special region IDs:\n"
-                        "        mpi-runtime: 4611686018427387904\n"
-                        "        epoch:  9223372036854775808\n\n";
+                        "    from the region_id printed in the trace.\n";
     if (argc == 1 ||
         strncmp("-h", argv[1], strlen("-h")) == 0 ||
         strncmp("--help", argv[1], strlen("--help")) == 0) {
         printf(usage, argv[0]);
     }
     else {
-        printf("%llu\n", (unsigned long long)geopm_crc32_str(0, argv[1]));
+        printf("0x%.16llx\n", (unsigned long long)geopm_crc32_str(0, argv[1]));
     }
     return 0;
 }
