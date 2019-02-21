165922ce2e9aea411b189fd7ca3ffd9de4847317
Brandon Baker <brandon.baker@intel.com>
2019-02-20 16:38:50 -0800

    Fix Energy Efficient Agent.
    - Fixed agent logic errors.
      + Implemented with mode subclass separation.
      + Properly account for tree samples and trace signals.
      + Do not call init_platform prior to knowing coordinates.
      + Only push signals/controls for leaf Agents.
    - Fixed region logic errors.
      + Initialize efficient region bounds on creation.
      ++ Uninitialized members hindered learning algorithm.
      + FreqContext class and map of unique_ptr.
      + Do not use double as key into FreqContext map.
      + Properly gate frequency updates based on median
        filter sampling requirements.
    
    Change-Id: I5f852c9873d211025b068e2fcfb215db2bbf74ec
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/EnergyEfficientRegion.hpp
+++ src/EnergyEfficientRegion.hpp
@@ -36,56 +36,71 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <memory>
 
 #include "CircularBuffer.hpp"
 
 namespace geopm
 {
-
     class IPlatformIO;
+    struct FreqContext;
 
     /// @brief Holds the performance history of a Region.
     class EnergyEfficientRegion
     {
         public:
             EnergyEfficientRegion(IPlatformIO &platform_io,
+                                  double freq_min, double freq_max, double freq_step,
                                   int runtime_idx,
                                   int pkg_energy_idx);
             virtual ~EnergyEfficientRegion() = default;
             double freq(void) const;
-            void update_freq_range(const double freq_min, const double freq_max, const double freq_step);
+            void update_freq_range(double freq_min, double freq_max, double freq_step);
             void update_entry(void);
             void update_exit(void);
+            std::vector<std::string> trace_names(void) const;
+            void trace_values(std::vector<double> &values);
         private:
+            enum trace_col_e {
+                TRACE_COL_M_IS_LEARNING,
+                TRACE_COL_M_LEARN_COUNT,
+                TRACE_COL_M_CURR_STEP,
+                TRACE_COL_M_TARGET,
+                TRACE_COL_NUM_INCREASE,
+                TRACE_COL_PERF_METRIC,
+                TRACE_COL_ENERGY_METRIC,
+                TRACE_COL_MED_FILT_PERF_METRIC,
+                TRACE_COL_MED_FILT_ENERGY_METRIC,
+                TRACE_COL_NUM,
+            };
+
             // Used to determine whether performance degraded or not.
             // Higher is better.
             virtual double perf_metric();
             virtual double energy_metric();
 
+            const int M_PRECISION;
             const double M_PERF_MARGIN;
             const double M_ENERGY_MARGIN;
             const size_t M_MIN_BASE_SAMPLE;
             const size_t M_MAX_INCREASE;
 
             IPlatformIO &m_platform_io;
-            double m_curr_freq;
-            double m_target;
-
             bool m_is_learning;
-            struct m_freq_ctx_s {
-                size_t num_increase;
-                CircularBuffer<double> perf;
-                CircularBuffer<double> energy;
-            };
-
-            std::map<size_t, struct m_freq_ctx_s> m_freq_ctx_map;
-            double m_freq_step;
-            std::set<double> m_allowed_freq;
-            double m_curr_freq_max;
+            int m_curr_step;
+            double m_target;
+            double m_curr_perf;
             double m_start_energy;
+            double m_curr_energy;
+            double m_freq_min;
+            double m_freq_max;
+            double m_freq_step;
+            uint64_t m_max_step;
 
             int m_runtime_idx;
             int m_pkg_energy_idx;
+            std::map<uint64_t, std::unique_ptr<FreqContext> > m_freq_ctx_map;
+            std::set<double> m_allowed_freq;
     };
 
 } // namespace geopm
