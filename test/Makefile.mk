e4c4a221e59f43ac12109a4129b5b76937623551
Brandon Baker <brandon.baker@intel.com>
2019-03-05 11:27:01 -0800

    Introduce FrequencyMapAgent.
    Change-Id: I7a3de7c34e6883906cf6f907d316f1c9a5cd214b
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -100,6 +100,9 @@ GTEST_TESTS = test/gtest_links/AgentFactoryTest.static_info_monitor \
               test/gtest_links/EpochRuntimeRegulatorTest.rank_enter_exit_trace \
               test/gtest_links/EpochRuntimeRegulatorTest.unknown_region \
               test/gtest_links/ExceptionTest.hello \
+              test/gtest_links/FrequencyMapAgentTest.hint \
+              test/gtest_links/FrequencyMapAgentTest.map \
+              test/gtest_links/FrequencyMapAgentTest.name \
               test/gtest_links/HelperTest.string_begins_with \
               test/gtest_links/HelperTest.string_ends_with \
               test/gtest_links/HelperTest.string_split \
@@ -301,6 +304,7 @@ test_geopm_test_SOURCES = src/EnergyEfficientAgent.cpp \
                           test/EnvironmentTest.cpp \
                           test/EpochRuntimeRegulatorTest.cpp \
                           test/ExceptionTest.cpp \
+                          test/FrequencyMapAgentTest.cpp \
                           test/HelperTest.cpp \
                           test/IOGroupTest.cpp \
                           test/MSRIOGroupTest.cpp \
