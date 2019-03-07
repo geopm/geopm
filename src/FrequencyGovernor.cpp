cce7d0b0c9f2c9caaaa767cd575a7f8052c5f370
Brandon Baker <brandon.baker@intel.com>
2019-03-06 23:44:30 -0800

    Introduce FrequencyGovernor.
    Change-Id: I40c626d3d34f41532c1e0d295f745de2bd816f57
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ src/FrequencyGovernor.cpp
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cmath>
+#include <vector>
+#include <unistd.h>
+
+#include "Exception.hpp"
+#include "Helper.hpp"
+#include "PlatformIO.hpp"
+#include "PlatformTopo.hpp"
+#include "FrequencyGovernor.hpp"
+#include "config.h"
+
+namespace geopm
+{
+    FrequencyGovernorImp::FrequencyGovernorImp(PlatformIO &platform_io, PlatformTopo &platform_topo)
+        : m_platform_io(platform_io)
+        , m_platform_topo(platform_topo)
+        , m_freq_min(get_limit("CPUINFO::FREQ_MIN"))
+        , m_freq_max(get_limit("CPUINFO::FREQ_MAX"))
+        , M_FREQ_STEP(get_limit("CPUINFO::FREQ_STEP"))
+        , M_PLAT_FREQ_MIN(get_limit("CPUINFO::FREQ_MIN"))
+        , M_PLAT_FREQ_MAX(get_limit("CPUINFO::FREQ_MAX"))
+        , m_last_freq(NAN)
+    {
+
+    }
+
+    FrequencyGovernorImp::~FrequencyGovernorImp()
+    {
+
+    }
+
+    double FrequencyGovernorImp::get_limit(const std::string &sig_name) const
+    {
+        const int domain_type = m_platform_io.signal_domain_type(sig_name);
+        double result = NAN;
+        const double sticker_freq = m_platform_io.read_signal("CPUINFO::FREQ_STICKER", domain_type, 0);
+        if (sig_name == "CPUINFO::FREQ_MIN") {
+            result = m_platform_io.read_signal(sig_name, domain_type, 0);
+        }
+        else if (sig_name == "CPUINFO::FREQ_MAX") {
+            result = m_platform_io.read_signal(sig_name, domain_type, 0);
+        }
+        else if (sig_name == "CPUINFO::FREQ_STEP") {
+            result = m_platform_io.read_signal(sig_name, domain_type, 0);
+        }
+#ifdef GEOPM_DEBUG
+        else {
+            throw Exception("FrequencyGovernorImp::" + std::string(__func__) + "(): requested invalid signal name.",
+                            GEOPM_ERROR_LOGIC, __FILE__, __LINE__);
+        }
+#endif
+        return result;
+    }
+
+    void FrequencyGovernorImp::init_platform_io(void)
+    {
+        init_platform_io(m_platform_io.control_domain_type("FREQUENCY"));
+    }
+
+    void FrequencyGovernorImp::init_platform_io(int freq_ctl_domain_type)
+    {
+        m_freq_ctl_domain_type = freq_ctl_domain_type;
+        const int num_freq_ctl_domain = m_platform_topo.num_domain(m_freq_ctl_domain_type);
+        for (int ctl_dom_idx = 0; ctl_dom_idx != num_freq_ctl_domain; ++ctl_dom_idx) {
+            m_control_idx.push_back(m_platform_io.push_control("FREQUENCY",
+                                                               m_freq_ctl_domain_type,
+                                                               ctl_dom_idx));
+        }
+    }
+
+    int FrequencyGovernorImp::frequency_domain_type(void) const
+    {
+        return m_freq_ctl_domain_type;
+    }
+
+    bool FrequencyGovernorImp::adjust_platform(const std::vector<double> &frequency_request, std::vector<double> &frequency_actual)
+    {
+        if (frequency_request.size() != m_control_idx.size()) {
+            throw Exception("FrequencyGovernorImp::" + std::string(__func__) +
+                            "(): size of request vector does not match size of control domain.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        if (frequency_actual.size() != m_control_idx.size()) {
+            throw Exception("FrequencyGovernorImp::" + std::string(__func__) +
+                            "(): size of actual vector does not match size of control domain.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        for (int idx = 0; idx < m_control_idx.size(); ++idx) {
+            if (frequency_request[idx] > m_freq_max) {
+                frequency_actual[idx] = m_freq_max;
+            }
+            else if (frequency_request[idx] < m_freq_min) {
+                frequency_actual[idx] = m_freq_min;
+            }
+            else {
+                frequency_actual[idx] = frequency_request[idx];
+            }
+            m_platform_io.adjust(m_control_idx[idx], frequency_actual[idx]);
+        }
+        return true;
+    }
+
+    bool FrequencyGovernorImp::set_frequency_bounds(double freq_min, double freq_max)
+    {
+        if (freq_min < M_PLAT_FREQ_MIN || freq_max > M_PLAT_FREQ_MAX ||
+            freq_min > freq_max) {
+            throw Exception("FrequencyGovernorImp::" + std::string(__func__) + "(): invalid frequency bounds.",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+
+        }
+        bool result = false;
+        if (m_freq_min != freq_min ||
+            m_freq_max != freq_max) {
+            m_freq_min = freq_min;
+            m_freq_max = freq_max;
+            result = true;
+        }
+        return result;
+    }
+
+    void FrequencyGovernorImp::get_frequency_bounds(double &freq_min, double &freq_max) const
+    {
+        freq_min = m_freq_min;
+        freq_max = m_freq_max;
+    }
+
+    void FrequencyGovernorImp::validate_policy(double &freq_min, double &freq_max) const
+    {
+        //double target_freq_min = std::isnan(freq_min) ? get_limit("CPUINFO::FREQ_MIN") : freq_min;
+        //double target_freq_max = std::isnan(freq_max) ? get_limit("CPUINFO::FREQ_MAX") : freq_max;
+        // if (std::isnan(target_freq_min) || std::isnan(target_freq_max) ||
+        //     target_freq_min > target_freq_max) {
+        //     throw Exception("FrequencyGovernorImp::" + std::string(__func__) + "(): invalid frequency bounds.",
+        //                     GEOPM_ERROR_RUNTIME, __FILE__, __LINE__);
+        // }
+    }
+
+    double FrequencyGovernorImp::get_frequency_step() const
+    {
+        return M_FREQ_STEP;
+    }
+}
