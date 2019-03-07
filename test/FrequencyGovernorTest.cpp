59813e0567920fcd352c6a9c39c18073cb9b6521
Brandon Baker <brandon.baker@intel.com>
2019-03-06 23:44:30 -0800

    Introduce FrequencyGovernor.
    Change-Id: I40c626d3d34f41532c1e0d295f745de2bd816f57
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ test/FrequencyGovernorTest.cpp
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <memory>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "FrequencyGovernor.hpp"
+#include "Helper.hpp"
+#include "geopm_test.hpp"
+#include "MockPlatformIO.hpp"
+#include "MockPlatformTopo.hpp"
+
+using geopm::FrequencyGovernor;
+using testing::NiceMock;
+using testing::_;
+using testing::Return;
+using testing::Throw;
+
+class FrequencyGovernorTest : public ::testing::Test
+{
+    protected:
+        void SetUp(void);
+        NiceMock<MockPlatformIO> m_platio;
+        NiceMock<MockPlatformTopo> m_topo;
+
+        std::unique_ptr<FrequencyGovernor> m_gov;
+
+        const int M_CTL_DOMAIN = GEOPM_DOMAIN_CORE;
+        const int M_NUM_CORE = 4;
+        const double M_PLAT_MAX_FREQ = 2.1e9;
+        const double M_PLAT_STICKER_FREQ = 2.0e9;
+        const double M_PLAT_MIN_FREQ = 1.0e9;
+        const double M_PLAT_STEP_FREQ = 1e8;
+        const std::vector<int> M_FREQ_CTL_IDX = {42, 43, 44, 45};
+};
+
+void FrequencyGovernorTest::SetUp(void)
+{
+    ON_CALL(m_platio, control_domain_type("FREQUENCY")).WillByDefault(Return(M_CTL_DOMAIN));
+    ON_CALL(m_topo, num_domain(M_CTL_DOMAIN)).WillByDefault(Return(M_NUM_CORE));
+    ON_CALL(m_topo, num_domain(GEOPM_DOMAIN_CPU)).WillByDefault(Return(2*M_NUM_CORE));
+    ON_CALL(m_platio, read_signal("CPUINFO::FREQ_STEP", _, _)).WillByDefault(Return(M_PLAT_STEP_FREQ));
+    ON_CALL(m_platio, read_signal("CPUINFO::FREQ_MIN", _, _)).WillByDefault(Return(M_PLAT_MIN_FREQ));
+    ON_CALL(m_platio, read_signal("CPUINFO::FREQ_MAX", _, _)).WillByDefault(Return(M_PLAT_MAX_FREQ));
+
+    ASSERT_EQ(M_NUM_CORE, M_FREQ_CTL_IDX.size());
+    for (int idx = 0; idx < M_NUM_CORE; ++idx) {
+        ON_CALL(m_platio, push_control("FREQUENCY", M_CTL_DOMAIN, idx)).
+            WillByDefault(Return(M_FREQ_CTL_IDX[idx]));
+    }
+    ON_CALL(m_platio, push_control("FREQUENCY", GEOPM_DOMAIN_CPU, _))
+        .WillByDefault(Throw(geopm::Exception("invalid domain for frequency control",
+                                              GEOPM_ERROR_INVALID, __FILE__, __LINE__)));
+
+    m_gov = FrequencyGovernor::make_unique(m_platio, m_topo);
+    m_gov->init_platform_io();
+}
+
+TEST_F(FrequencyGovernorTest, frequency_control_domain)
+{
+    auto gov = FrequencyGovernor::make_unique(m_platio, m_topo);
+    gov->init_platform_io(GEOPM_DOMAIN_BOARD);
+    int domain = gov->frequency_domain_type();
+    EXPECT_EQ(GEOPM_DOMAIN_BOARD, domain);
+}
+
+TEST_F(FrequencyGovernorTest, frequency_control_domain_default)
+{
+    auto gov = FrequencyGovernor::make_unique(m_platio, m_topo);
+    gov->init_platform_io();
+    int domain = gov->frequency_domain_type();
+    EXPECT_EQ(M_CTL_DOMAIN, domain);
+}
+
+TEST_F(FrequencyGovernorTest, frequency_control_domain_error)
+{
+    auto gov = FrequencyGovernor::make_unique(m_platio, m_topo);
+    GEOPM_EXPECT_THROW_MESSAGE(gov->init_platform_io(GEOPM_DOMAIN_CPU),
+                               GEOPM_ERROR_INVALID,
+                               "invalid domain");
+}
+
+TEST_F(FrequencyGovernorTest, adjust_platform)
+{
+    std::vector<double> request;
+    std::vector<double> actual;
+    int domain = m_gov->frequency_domain_type();
+    EXPECT_EQ(M_CTL_DOMAIN, domain);
+    // todo: should caller use platform topo, or get this through governor?
+    int num_domain = m_topo.num_domain(domain);
+    EXPECT_EQ(M_NUM_CORE, num_domain);
+    request = {1.1e9, 1.2e9, 1.5e9, 1.7e9};
+    ASSERT_EQ(num_domain, request.size());
+    actual = {NAN, NAN, NAN, NAN};
+    // check that controls are actually applied
+    for (int idx = 0; idx < num_domain; ++idx) {
+        EXPECT_CALL(m_platio, adjust(M_FREQ_CTL_IDX[idx], request[idx]));
+    }
+    bool result = m_gov->adjust_platform(request, actual);
+    EXPECT_TRUE(result);
+    EXPECT_EQ(request, actual);
+}
+
+TEST_F(FrequencyGovernorTest, adjust_platform_clamping)
+{
+    std::vector<double> request;
+    std::vector<double> actual;
+    int domain = m_gov->frequency_domain_type();
+    EXPECT_EQ(M_CTL_DOMAIN, domain);
+    int num_domain = m_topo.num_domain(domain);
+    EXPECT_EQ(M_NUM_CORE, num_domain);
+    request = {3.1e9, 1.2e9, 1.5e9, 0.7e9};
+    actual = {NAN, NAN, NAN, NAN};
+    ASSERT_EQ(num_domain, request.size());
+    ASSERT_EQ(num_domain, actual.size());
+    std::vector<double> expected = {M_PLAT_MAX_FREQ, 1.2e9, 1.5e9, M_PLAT_MIN_FREQ};
+    // check that controls are actually applied
+    for (int idx = 0; idx < num_domain; ++idx) {
+        EXPECT_CALL(m_platio, adjust(M_FREQ_CTL_IDX[idx], expected[idx]));
+    }
+    bool result = m_gov->adjust_platform(request, actual);
+    EXPECT_TRUE(result);
+    EXPECT_EQ(expected, actual);
+    EXPECT_NE(actual, request);
+}
+
+TEST_F(FrequencyGovernorTest, adjust_platform_error)
+{
+    std::vector<double> request;
+    std::vector<double> actual;
+    GEOPM_EXPECT_THROW_MESSAGE(m_gov->adjust_platform(request, actual),
+                               GEOPM_ERROR_INVALID,
+                               "size of request vector");
+    request = {1.1e9, 1.1e9, 1.1e9, 1.1e9};
+    GEOPM_EXPECT_THROW_MESSAGE(m_gov->adjust_platform(request, actual),
+                               GEOPM_ERROR_INVALID,
+                               "size of actual vector");
+}
+
+TEST_F(FrequencyGovernorTest, frequency_bounds_in_range)
+{
+    double min = NAN;
+    double max = NAN;
+    double step = NAN;
+    // default settings
+    m_gov->get_frequency_bounds(min, max, step);
+    EXPECT_DOUBLE_EQ(M_PLAT_MIN_FREQ, min);
+    EXPECT_DOUBLE_EQ(M_PLAT_MAX_FREQ, max);
+    EXPECT_DOUBLE_EQ(M_PLAT_STEP_FREQ, step);
+
+    // change settings
+    double new_min = M_PLAT_MIN_FREQ + M_PLAT_STEP_FREQ;
+    double new_max = M_PLAT_MAX_FREQ - M_PLAT_STEP_FREQ;
+    bool result = m_gov->set_frequency_bounds(new_min, new_max);
+    EXPECT_TRUE(result);
+    m_gov->get_frequency_bounds(min, max, step);
+    EXPECT_DOUBLE_EQ(new_min, min);
+    EXPECT_DOUBLE_EQ(new_max, max);
+
+    // same settings
+    result = m_gov->set_frequency_bounds(new_min, new_max);
+    EXPECT_FALSE(result);
+    m_gov->get_frequency_bounds(min, max, step);
+    EXPECT_DOUBLE_EQ(new_min, min);
+    EXPECT_DOUBLE_EQ(new_max, max);
+}
+
+TEST_F(FrequencyGovernorTest, frequency_bounds_invalid)
+{
+    GEOPM_EXPECT_THROW_MESSAGE(m_gov->set_frequency_bounds(M_PLAT_MIN_FREQ - 1, M_PLAT_MAX_FREQ),
+                               GEOPM_ERROR_INVALID,
+                               "invalid frequency bounds");
+    GEOPM_EXPECT_THROW_MESSAGE(m_gov->set_frequency_bounds(M_PLAT_MIN_FREQ, M_PLAT_MAX_FREQ + 1),
+                               GEOPM_ERROR_INVALID,
+                               "invalid frequency bounds");
+    GEOPM_EXPECT_THROW_MESSAGE(m_gov->set_frequency_bounds(M_PLAT_MAX_FREQ, M_PLAT_MIN_FREQ),
+                               GEOPM_ERROR_INVALID,
+                               "invalid frequency bounds");
+}
+
+TEST_F(FrequencyGovernorTest, validate_policy)
+{
+    double min = NAN;
+    double max = NAN;
+    m_gov->validate_policy(min, max);
+    EXPECT_FALSE(std::isnan(min));
+    EXPECT_FALSE(std::isnan(max));
+
+    min = M_PLAT_MIN_FREQ + 1;
+    max = M_PLAT_MAX_FREQ - 1;
+    m_gov->validate_policy(min, max);
+    EXPECT_DOUBLE_EQ(M_PLAT_MIN_FREQ + 1, min);
+    EXPECT_DOUBLE_EQ(M_PLAT_MAX_FREQ - 1, max);
+
+    min = M_PLAT_MIN_FREQ - 1;
+    max = M_PLAT_MAX_FREQ + 1;
+    m_gov->validate_policy(min, max); // should throw??
+    EXPECT_DOUBLE_EQ(M_PLAT_MIN_FREQ - 1, min);
+    EXPECT_DOUBLE_EQ(M_PLAT_MAX_FREQ + 1, max);
+}
