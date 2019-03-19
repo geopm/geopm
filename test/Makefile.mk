39de3d3bcbb7b1bd8b5b0716c0a35228ede859a5
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-03-18 19:06:47 -0700

    Renames is_domain_within() to is_nested_domain()
    Change-Id: Idb91a3804d00982f2502a1509a11b376a31959e8
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -176,7 +176,7 @@ GTEST_TESTS = test/gtest_links/AgentFactoryTest.static_info_monitor \
               test/gtest_links/PlatformIOTest.write_control_override \
               test/gtest_links/PlatformIOTest.write_control_agg \
               test/gtest_links/PlatformTopoTest.bdx_domain_idx \
-              test/gtest_links/PlatformTopoTest.bdx_is_domain_within \
+              test/gtest_links/PlatformTopoTest.bdx_is_nested_domain \
               test/gtest_links/PlatformTopoTest.bdx_nested_domains \
               test/gtest_links/PlatformTopoTest.bdx_num_domain \
               test/gtest_links/PlatformTopoTest.construction \
