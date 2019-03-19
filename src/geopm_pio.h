ccefdc90cbb0a6b3bcadce91fcc78e797750378b
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-03-18 18:34:26 -0700

    Defines the PlatformIO and PlatformTopo C interfaces
    - Create header files that define the geopm_pio_*()
      and geopm_topo_*() interfaces.
    - Copy PlatformTopo::m_domain_e to a C enum outside of
      class.
    
    Change-Id: Ibeb360c36f2882d2ee56feb24f02fb1396b6ca20
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ src/geopm_pio.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GEOPM_PIO_H_INCLUDE
+#define GEOPM_PIO_H_INCLUDE
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int geopm_pio_num_signal_name(void);
+
+int geopm_pio_signal_name(int name_idx,
+                          size_t result_len,
+                          char *result);
+
+int geopm_pio_num_control_name(void);
+
+int geopm_pio_control_name(int name_index,
+                           size_t result_len,
+                           char *result);
+
+int geopm_pio_signal_domain_type(const char *signal_name);
+
+int geopm_pio_control_domain_type(const char *control_name);
+
+int geopm_pio_read_signal(const char *signal_name,
+                          int domain_type,
+                          int domain_idx);
+
+int geopm_pio_write_control(const char *control_name,
+                            int domain_type,
+                            int domain_idx,
+                            double setting);
+
+int geopm_pio_push_signal(const char *signal_name,
+                          int domain_type,
+                          int domain_idx);
+
+int geopm_pio_push_control(const char *control_name,
+                           int domain_type,
+                           int domain_idx);
+
+int geopm_pio_num_signal(void);
+
+int geopm_pio_num_control(void);
+
+int geopm_pio_sample(int signal_idx,
+                     double *result);
+
+int geopm_pio_adjust(int control_idx,
+                     double setting);
+
+int geopm_pio_read_batch(void);
+
+int geopm_pio_write_batch(void);
+
+int geopm_pio_save_control(void);
+
+int geopm_pio_restore_control(void);
+
+int geopm_pio_signal_description(const char *signal_name,
+                                 size_t description_max,
+                                 char *description);
+
+int geopm_pio_control_description(const char *control_name,
+                                  size_t description_max,
+                                  char *description);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
