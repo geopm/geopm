f02a7ac31d0fa819d5c3be60ca0c128d7995025a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-03-29 16:29:35 -0700

    Fixes problem where MPI calls are profiled even if controller handshake fails
    Change-Id: I2ac3e26d95f92601922163bfe14122a6a749a089
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/Profile.hpp
+++ src/Profile.hpp
@@ -255,6 +255,8 @@ namespace geopm
             void init_cpu_affinity(int shm_num_rank);
             void init_tprof_table(const std::string &tprof_key, PlatformTopo &topo);
             void init_table(const std::string &sample_key);
+        protected:
+            bool m_is_enabled;
         private:
             enum m_profile_const_e {
                 M_PROF_SAMPLE_PERIOD = 1,
@@ -278,7 +280,6 @@ namespace geopm
             ///        to this name if multiple files are created.
             ///
             void print(const std::string file_name);
-            bool m_is_enabled;
             /// @brief holds the string name of the profile.
             std::string m_prof_name;
             /// @brief Holds the 64 bit unique region identifier
