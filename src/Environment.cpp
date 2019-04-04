5c932579c4a9fac41e182806aa6d298412e1d3cf
Brandon Baker <brandon.baker@intel.com>
2019-04-04 13:17:23 -0700

    Update FrequencyMapAgent to use Environment class
    -Fixes #564
    
    Change-Id: Idd9f367691ceaa7e82e8b8fa8b08cb952ac88c2f
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -81,6 +81,7 @@ namespace geopm
         m_trace = "";
         m_plugin_path = "";
         m_profile = "";
+        m_frequency_map = "";
         m_max_fan_out = 16;
         m_pmpi_ctl = GEOPM_CTL_NONE;
         m_do_region_barrier = false;
@@ -120,6 +121,7 @@ namespace geopm
         }
         (void)get_env("GEOPM_DEBUG_ATTACH", m_debug_attach);
         m_do_profile = get_env("GEOPM_PROFILE", m_profile);
+        (void)get_env("GEOPM_FREQUENCY_MAP", m_frequency_map);
         (void)get_env("GEOPM_MAX_FAN_OUT", m_max_fan_out);
         if (m_report.length() ||
             m_do_trace ||
@@ -198,6 +200,11 @@ namespace geopm
     std::string Environment::profile(void) const
     {
         return m_profile;
+    }
+
+    std::string Environment::frequency_map(void) const
+    {
+        return m_frequency_map;
     }
 
     std::string Environment::plugin_path(void) const
