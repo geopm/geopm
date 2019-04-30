d61409cfd2e549c581fbdf98d9b9de32fcc1489d
Diana Guttman <diana.r.guttman@intel.com>
2019-04-30 14:34:06 -0700

    Add python bindings for geopm_agent interface
    - Resolves #593
    
    Change-Id: I93578f2c7c84efde5e3d053f747d67a5a88a326d
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ scripts/geopmpy/agent.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import cffi
+import error
+import json
+
+
+_ffi = cffi.FFI()
+_ffi.cdef("""
+
+int geopm_agent_supported(const char *agent_name);
+
+int geopm_agent_num_policy(const char *agent_name,
+                           int *num_policy);
+
+int geopm_agent_policy_name(const char *agent_name,
+                            int policy_idx,
+                            size_t policy_name_max,
+                            char *policy_name);
+
+int geopm_agent_policy_json(const char *agent_name,
+                            const double *policy_array,
+                            size_t json_string_max,
+                            char *json_string);
+
+int geopm_agent_num_sample(const char *agent_name,
+                           int *num_sample);
+
+int geopm_agent_sample_name(const char *agent_name,
+                            int sample_idx,
+                            size_t sample_name_max,
+                            char *sample_name);
+
+int geopm_agent_num_avail(int *num_agent);
+
+int geopm_agent_name(int agent_idx,
+                     size_t agent_name_max,
+                     char *agent_name);
+""")
+_dl = _ffi.dlopen('libgeopmpolicy.so')
+
+
+def is_supported(agent_name):
+    """Query if system supports an agent type.
+
+    Args:
+        agent_name (str): Name of agent type.
+
+    Returns:
+        True if agent_name matches a known agent type, False
+        otherwise.
+    """
+    agent_name_cstr = _ffi.new("char[]", str(agent_name))
+    result = _dl.geopm_agent_supported(agent_name_cstr)
+    if result < 0:
+        raise RuntimeError("geopm_agent_supported() failed: {}".format(
+            error.message(result)))
+    return result == 0
+
+
+def policy_names(agent_name):
+    """Get the names of the policies for a given agent.
+
+    Args:
+        agent_name (str): Name of agent type.
+
+    Returns:
+        list of str: Policy names required for the agent configuration.
+
+    """
+    agent_name_cstr = _ffi.new("char[]", str(agent_name))
+    num_policy = _ffi.new("int *")
+    err = _dl.geopm_agent_num_policy(agent_name_cstr, num_policy)
+    if err < 0:
+        raise RuntimeError("geopm_agent_num_policy() failed: {}".format(
+            error.message(err)))
+    name_max = 1024
+    result = []
+    for policy_idx in range(num_policy[0]):
+        buff = _ffi.new("char[]", name_max)
+        err = _dl.geopm_agent_policy_name(agent_name_cstr, policy_idx, name_max, buff)
+        if err < 0:
+            raise RuntimeError("geopm_agent_policy_name() failed: {}".format(
+                error.message(err)))
+        result.append(_ffi.string(buff))
+    return result
+
+
+def policy_json(agent_name, policy_values):
+    """Create a JSON policy for the given agent.
+
+    This can be written to a file to control the agent statically.
+
+    Args:
+        agent_name (str): Name of agent type.
+        policy_values (list of float): Values to use for each
+        respective policy field.
+
+    Returns:
+        str: JSON str containing a valid policy using the given values.
+
+    """
+    names = policy_names(agent_name)
+    if len(policy_values) != len(names):
+        raise RuntimeError("policy_values must be same length as policy_names().")
+    result = {}
+    for name, value in zip(names, policy_values):
+        result[name] = value
+    return json.dumps(result)
+
+
+def sample_names(agent_name):
+    """Get all samples produced by the given agent.
+
+    Args:
+        agent_name (str): Name of agent type.
+
+    Returns:
+
+    """
+    agent_name_cstr = _ffi.new("char[]", str(agent_name))
+    num_sample = _ffi.new("int *")
+    err = _dl.geopm_agent_num_sample(agent_name_cstr, num_sample)
+    if err < 0:
+        raise RuntimeError("geopm_agent_num_sample() failed: {}".format(
+            error.message(err)))
+    name_max = 1024
+    result = []
+    for sample_idx in range(num_sample[0]):
+        buff = _ffi.new("char[]", name_max)
+        err = _dl.geopm_agent_sample_name(agent_name_cstr, sample_idx, name_max, buff)
+        if err < 0:
+            raise RuntimeError("geopm_agent_sample_name() failed: {}".format(
+                error.message(err)))
+        result.append(_ffi.string(buff))
+    return result
+
+
+def names():
+    """Get the list of all available agents.
+
+    Returns:
+        list of str: List of all agent names.
+    """
+    name_max = 1024
+    num_agent = _ffi.new("int *")
+    err = _dl.geopm_agent_num_avail(num_agent)
+    if err < 0:
+        raise RuntimeError("geopm_agent_num_avail() failed: {}".format(
+            error.message(err)))
+    buff = _ffi.new("char[]", name_max)
+    result = []
+    for agent_idx in range(num_agent[0]):
+        err = _dl.geopm_agent_name(agent_idx, name_max, buff)
+        if err < 0:
+            raise RuntimeError("geopm_agent_name() failed: {}".format(
+                error.message(err)))
+        result.append(_ffi.string(buff))
+    return result
