e93f17b90aba93c923a3d96c6871e2c60dfe55ad
Diana Guttman <diana.r.guttman@intel.com>
2019-05-01 09:55:24 -0700

    Add DebugIOGroup signals to EnergyEfficientAgent
    Change-Id: I12dc7c014aa8f7de1867703af8866c3ef2171f89
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/EnergyEfficientAgent.hpp
+++ src/EnergyEfficientAgent.hpp
@@ -50,6 +50,7 @@ namespace geopm
     class PlatformTopo;
     class EnergyEfficientRegion;
     class FrequencyGovernor;
+    class DebugIOGroup;
 
     class EnergyEfficientAgent : public Agent
     {
@@ -85,6 +86,7 @@ namespace geopm
         private:
             bool update_policy(const std::vector<double> &in_policy);
             void init_platform_io(void);
+            void update_debug_values(void);
 
             enum m_policy_e {
                 M_POLICY_FREQ_MIN,
@@ -105,8 +107,8 @@ namespace geopm
             PlatformIO &m_platform_io;
             const PlatformTopo &m_platform_topo;
             std::shared_ptr<FrequencyGovernor> m_freq_governor;
-            int m_freq_ctl_domain_type;
-            int m_num_freq_ctl_domain;
+            const int m_freq_ctl_domain_type;
+            const int m_num_freq_ctl_domain;
             std::vector<struct geopm_region_info_s>  m_last_region;
             std::vector<std::map<uint64_t, double> > m_adapt_freq_map;
             std::vector<std::map<uint64_t, std::shared_ptr<EnergyEfficientRegion> > > m_region_map;
@@ -116,6 +118,8 @@ namespace geopm
             int m_num_children;
             bool m_do_send_policy;
             double m_perf_margin;
+            std::shared_ptr<std::vector<double> > m_debug_values;
+            std::shared_ptr<DebugIOGroup> m_debug_iog;
     };
 }
 
