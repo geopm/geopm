1a988f7286795c58b2a44b33214227df0251214d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-05-22 20:52:06 -0700

    Write an integration test with special test binary
    Change-Id: I7027434d88fc4d8053fbb4c6ee3efc88468b16e7
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_integration/__main__.py
+++ test_integration/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 #  Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -29,23 +31,8 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-EXTRA_DIST += test_integration/geopm_context.py \
-              test_integration/geopm_test_integration.py \
-              test_integration/geopm_test_launcher.py \
-              test_integration/geopm_test_loop.sh \
-              test_integration/__init__.py \
-              test_integration/__main__.py \
-              test_integration/README.md \
-              test_integration/test_ee_stream_dgemm_mix.py \
-              # end
+import unittest
+from __init__ import *
 
-noinst_PROGRAMS += test_integration/test_ee_stream_dgemm_mix \
-                   # end
-
-test_integration_test_ee_stream_dgemm_mix_SOURCES = test_integration/test_ee_stream_dgemm_mix.cpp \
-                                                    src/ModelRegion.cpp \
-                                                    src/ModelRegion.hpp \
-                                                    # end
-test_integration_test_ee_stream_dgemm_mix_LDADD = libgeopm.la $(MATH_LIB) $(MPI_CLIBS)
-test_integration_test_ee_stream_dgemm_mix_LDFLAGS = $(AM_LDFLAGS) $(MPI_CLDFLAGS) $(MATH_CLDFLAGS)
-test_integration_test_ee_stream_dgemm_mix_CXXFLAGS = $(AM_CXXFLAGS) $(MPI_CFLAGS) -D_GNU_SOURCE -std=c++11 $(MATH_CFLAGS)
+if __name__ == '__main__':
+    unittest.main()
