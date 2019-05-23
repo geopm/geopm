9842a341a6d6232bf0c7f7f7c08aff537016e412
Stephanie Brink <brink2@llnl.gov>
2019-05-22 20:18:42 -0700

    Check that MPIEXEC is set first before checking value of GEOPM_LAUNCHER.
    - Fixes #613
    
    Change-Id: I6b478827974dc2b0b17f5b965222e2f861dead8e
    Signed-off-by: Stephanie Brink <brink2@llnl.gov>

--- tutorial/tutorial_6.sh
+++ tutorial/tutorial_6.sh
@@ -43,7 +43,16 @@ export LD_LIBRARY_PATH=$GEOPM_LIB:$LD_LIBRARY_PATH
 # launch geopm controller as an MPI process
 # create a report file
 # create trace files
-if [ "$GEOPM_LAUNCHER" = "srun" ]; then
+if [ "$MPIEXEC" ]; then
+    # Use MPIEXEC and set GEOPM environment variables to launch the job
+    LD_DYNAMIC_WEAK=true \
+    GEOPM_CTL=process \
+    GEOPM_REPORT=tutorial_6_report \
+    GEOPM_TRACE=tutorial_6_trace \
+    $MPIEXEC \
+    geopmbench tutorial_6_config.json
+    err=$?
+elif [ "$GEOPM_LAUNCHER" = "srun" ]; then
     # Use GEOPM launcher wrapper script with SLURM's srun
     geopmlaunch srun \
                 -N 2 \
@@ -62,15 +71,6 @@ elif [ "$GEOPM_LAUNCHER" = "aprun" ]; then
                 --geopm-report=tutorial_6_report \
                 --geopm-trace=tutorial_6_trace \
                 -- geopmbench tutorial_6_config.json
-    err=$?
-elif [ "$MPIEXEC" ]; then
-    # Use MPIEXEC and set GEOPM environment variables to launch the job
-    LD_DYNAMIC_WEAK=true \
-    GEOPM_CTL=process \
-    GEOPM_REPORT=tutorial_6_report \
-    GEOPM_TRACE=tutorial_6_trace \
-    $MPIEXEC \
-    geopmbench tutorial_6_config.json
     err=$?
 else
     echo "Error: tutorial_2.sh: set GEOPM_LAUNCHER to 'srun' or 'aprun'." 2>&1
