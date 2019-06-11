c23b14eea3c12a99ec546e34753ecb0e3ea7c7b6
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-06-11 02:31:32 -0700

    Change Environment class variable names for better readability
    Change-Id: I24be1fcb836ccfcffcd59eb72937bc7076c58435
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/Environment.hpp
+++ src/Environment.hpp
@@ -94,11 +94,11 @@ namespace geopm
             void parse_environment();
             void parse_environment_file(const std::string &settings_path);
             bool is_set(const std::string &env_var) const;
-            std::string at_env(const std::string &env_var) const;
-            const std::set<std::string> m_known_vars;
-            const std::set<std::string> m_require_runtime;
-            std::set<std::string> m_user_vars;
-            std::map<std::string, std::string> m_vars;
+            std::string lookup(const std::string &env_var) const;
+            const std::set<std::string> m_all_names;
+            const std::set<std::string> m_runtime_names;
+            std::set<std::string> m_user_defined_names;
+            std::map<std::string, std::string> m_name_value_map;
     };
 
     const Environment &environment(void);
