d004046b7e8eb8d544943df30cfc2b52886185b3
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-07-01 11:31:13 -0700

    Adds unit tests for CSV class
    Change-Id: I4cbfa0ee46c44412719ebd6226c06460a51703c2
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ test/CSVTest.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include <sstream>
+#include "gtest/gtest.h"
+#include "geopm_error.h"
+#include "Helper.hpp"
+#include "CSV.hpp"
+#include "geopm_version.h"
+#include "geopm_hash.h"
+
+
+class CSVTest: public :: testing :: Test
+{
+    protected:
+        void setUp(void);
+        void tearDown(void);
+        std::string m_host_name;
+        std::string m_start_time;
+        size_t m_buffer_size;
+};
+
+void CSVTest::setUp(void)
+{
+    m_host_name = "csv-test-host";
+    m_start_time = "Mon Jul  1 11:10:08 PDT 2019";
+    m_buffer_size = 128;
+}
+
+TEST_F(CSVTest, header)
+{
+    std::string output_path = "CSVTest-header-output.csv";
+    {
+        std::unique_ptr<geopm::CSV> tmp = geopm::make_unique<geopm::CSVImp>(output_path, m_host_name, m_start_time, m_buffer_size);
+    }
+
+    std::string output_string = geopm::read_file(output_path);
+    std::vector<std::string> output_lines = geopm::string_split(output_string, "\n");
+    EXPECT_TRUE(geopm::string_begins_with(output_lines[0], "# geopm_version:"));
+    EXPECT_TRUE(geopm::string_begins_with(output_lines[1], "# start_time:"));
+    EXPECT_TRUE(geopm::string_begins_with(output_lines[2], "# profile_name:"));
+    EXPECT_TRUE(geopm::string_begins_with(output_lines[3], "# node_name:"));
+    EXPECT_TRUE(geopm::string_begins_with(output_lines[4], "# agent:"));
+    EXPECT_EQ(geopm_version(), geopm::string_split(output_lines[0], ": ")[1]);
+    EXPECT_EQ(m_start_time, geopm::string_split(output_lines[1], ": ")[1]);
+    EXPECT_EQ(m_host_name, geopm::string_split(output_lines[3], ": ")[1]);
+    unlink(output_path.c_str());
+}
+
+TEST_F(CSVTest, columns)
+{
+    std::vector<std::string> column_names {"COLUMN_DOUBLE",
+                                           "COLUMN_FLOAT",
+                                           "COLUMN_INTEGER",
+                                           "COLUMN_HEX",
+                                           "COLUMN_RAW64",
+                                           "COLUMN_DEFAULT"};
+    std::ostringstream expect_legend_stream;
+    std::string sep;
+    for (const auto &cn : column_names) {
+        expect_legend_stream << sep << cn;
+        sep = "|";
+    }
+    std::string expect_legend(expect_legend_stream.str());
+    std::string expect_values = "2.4414062500000000e-04|0.500000|1024|0x0020000000000000|0xffffffffffffffff|2.4414062500000000e-04";
+    std::string output_path = "CSVTest-columns-output.csv";
+    {
+        std::unique_ptr<geopm::CSV> csv = geopm::make_unique<geopm::CSVImp>(output_path, m_host_name, m_start_time, m_buffer_size);
+        csv->add_column(column_names[0], "double");
+        csv->add_column(column_names[1], "float");
+        csv->add_column(column_names[2], "integer");
+        csv->add_column(column_names[3], "hex");
+        csv->add_column(column_names[4], "raw64");
+        csv->add_column(column_names[5]);
+        double small = 0.000244140625;
+        double half = 0.5;
+        double big = 1024;
+        double huge = 0x20000000000000ULL;
+        double all_one = geopm_field_to_signal(0xFFFFFFFFFFFFFFFFULL);
+        std::vector<double> sample = {small,
+                                      half,
+                                      big,
+                                      huge,
+                                      all_one,
+                                      small};
+        csv->activate();
+        csv->update(sample);
+    }
+
+    std::string output_string = geopm::read_file(output_path);
+    std::vector<std::string> output_lines = geopm::string_split(output_string, "\n");
+    ASSERT_TRUE(output_lines.size() == 8);
+    for (size_t line_idx = 0; line_idx < 5; ++line_idx) {
+        EXPECT_TRUE(geopm::string_begins_with(output_lines[line_idx], "# "));
+    }
+    EXPECT_EQ(expect_legend, output_lines[5]);
+    EXPECT_EQ(expect_values, output_lines[6]);
+    ASSERT_EQ("", output_lines[7]);
+    unlink(output_path.c_str());
+}
