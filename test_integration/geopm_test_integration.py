5f0b4bb98174763f2cf99ca4054197236176e296
Diana Guttman <diana.r.guttman@intel.com>
2019-07-09 09:43:08 -0700

    Fix test_ignore_runtime
    Change-Id: I97945e19a87ad2a81c06172d9d355a7e448f1891
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test_integration/geopm_test_integration.py
+++ test_integration/geopm_test_integration.py
@@ -1002,7 +1002,10 @@ class TestIntegration(unittest.TestCase):
         for nn in node_names:
             ignore_data = self._output.get_report_data(node_name=nn, region='ignore')
             app_data = self._output.get_app_total_data(node_name=nn)
-            self.assertEqual(ignore_data['runtime'].item(), app_data['ignore-runtime'].item())
+            # ignore runtime includes MPI regions
+            mpi_data = self._output.get_report_data(node_name=nn, region='MPI_Barrier')
+            self.assertNear(ignore_data['runtime'].item() + mpi_data['runtime'].item(),
+                            app_data['ignore-runtime'].item(), 0.00005)
 
     @skip_unless_config_enable('ompt')
     def test_unmarked_ompt(self):
