fc84c2fdc29ad2ff7980a25d7ef944767d2f790b
Diana Guttman <diana.r.guttman@intel.com>
2019-07-16 11:32:20 -0700

    WIP: Add integration test of policy from profile
    - This depends on: changes to Endpoint, Daemon class,
      and EndpointPolicyTracer.
    - Resolves #553
    
    Change-Id: I014ae5e0411548bc8c524d0aa035df36e27c354d
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test_integration/Makefile.mk
+++ test_integration/Makefile.mk
@@ -37,6 +37,7 @@ EXTRA_DIST += test_integration/geopm_context.py \
               test_integration/__main__.py \
               test_integration/README.md \
               test_integration/test_ee_stream_dgemm_mix.py \
+              test_integration/test_profile_policy.py \
               test_integration/util.py \
               # end
 
@@ -52,3 +53,10 @@ test_integration_test_ee_stream_dgemm_mix_LDADD = libgeopm.la $(MATH_LIB) $(MPI_
 test_integration_test_ee_stream_dgemm_mix_LDFLAGS = $(AM_LDFLAGS) $(MPI_CLDFLAGS) $(MATH_CLDFLAGS)
 test_integration_test_ee_stream_dgemm_mix_CXXFLAGS = $(AM_CXXFLAGS) $(MPI_CFLAGS) -D_GNU_SOURCE -std=c++11 $(MATH_CFLAGS)
 endif
+
+noinst_PROGRAMS += test_integration/test_profile_policy
+test_integration_test_profile_policy_SOURCES = test_integration/test_profile_policy.cpp \
+                                               # end
+test_integration_test_profile_policy_LDADD = libgeopmpolicy.la
+test_integration_test_profile_policy_LDFLAGS = $(AM_LDFLAGS)
+test_integration_test_profile_policy_CXXFLAGS = $(AM_CXXFLAGS) -std=c++11
