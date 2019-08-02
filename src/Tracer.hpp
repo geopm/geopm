a431fce9c85985e6781887ba79f6753fb0e1316c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-08-02 10:00:40 -0700

    Update the Tracer to use the CSV class
    Change-Id: I965a53b09c4d5e0521d2a67b0902190815e347ac
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/Tracer.hpp
+++ src/Tracer.hpp
@@ -39,8 +39,10 @@
 #include <sstream>
 #include <set>
 #include <list>
+#include <memory>
 
 #include "PlatformIO.hpp"
+#include "CSV.hpp"
 
 struct geopm_region_info_s;
 
@@ -54,7 +56,8 @@ namespace geopm
             virtual ~Tracer() = default;
             /// @brief Set up default columns and add columns to be
             //         provided by the Agent.
-            virtual void columns(const std::vector<std::string> &agent_cols) = 0;
+            virtual void columns(const std::vector<std::string> &agent_cols,
+                                 const std::vector<std::function<std::string(double)> > &agent_formats) = 0;
             /// @brief Update the trace with telemetry samples and
             ///        region info.  The Tracer samples values for
             ///        default and environment columns and the
@@ -71,19 +74,11 @@ namespace geopm
             /// @brief Write the remaining trace data to the file and
             ///        stop tracing.
             virtual void flush(void) = 0;
-            /// @brief Returns the column header to be displayed in
-            ///        the trace.  The string is based on the signal
-            ///        name.  If the domain type is board, the name
-            ///        only will be used.  Otherwise, the column
-            ///        header includes the name, domain type, and
-            ///        domain index.
-            /// @param [in] col The request structure containing the
-            ///        signal name, domain type, and domain index.
-            static std::string pretty_name(const PlatformIO::m_request_s &col);
     };
 
     class PlatformIO;
     class PlatformTopo;
+    class CSV;
 
     /// @brief Class used to write a trace of the telemetry and policy.
     class TracerImp : public Tracer
@@ -94,42 +89,45 @@ namespace geopm
             TracerImp(const std::string &start_time,
                       const std::string &file_path,
                       const std::string &hostname,
-                      const std::string &agent,
-                      const std::string &profile_name,
                       bool do_trace,
                       PlatformIO &platform_io,
                       const PlatformTopo &platform_topo,
-                      const std::string &env_column,
-                      int precision);
+                      const std::string &env_column);
             /// @brief TracerImp destructor, virtual.
-            virtual ~TracerImp();
-            void columns(const std::vector<std::string> &agent_cols) override;
+            virtual ~TracerImp() = default;
+            void columns(const std::vector<std::string> &agent_cols,
+                         const std::vector<std::function<std::string(double)> > &agent_formats) override;
             void update(const std::vector<double> &agent_signals,
                         std::list<geopm_region_info_s> region_entry_exit) override;
             void flush(void) override;
         private:
-            /// @brief Format and write the values in m_last_telemetry to the trace.
-            void write_line(void);
+            struct m_request_s {
+                std::string name;
+                int domain_type;
+                int domain_idx;
+                std::function<std::string(double)> format;
+            };
+
+            std::vector<std::string> env_signals(void);
+            std::vector<int> env_domains(void);
+            std::vector<std::function<std::string(double)> > env_formats(void);
+
             std::string m_file_path;
             std::string m_header;
             std::string m_hostname;
             bool m_is_trace_enabled;
-            std::ofstream m_stream;
-            std::ostringstream m_buffer;
-            off_t m_buffer_limit;
 
             PlatformIO &m_platform_io;
             const PlatformTopo &m_platform_topo;
             std::string m_env_column; // extra columns from environment
-            int m_precision;
             std::vector<int> m_column_idx; // columns sampled by TracerImp
-            std::set<int> m_hex_column;
             std::vector<double> m_last_telemetry;
-            int m_region_hash_idx = -1;
-            int m_region_hint_idx = -1;
-            int m_region_count_idx = -1;
-            int m_region_progress_idx = -1;
-            int m_region_runtime_idx = -1;
+            const size_t M_BUFFER_SIZE;
+            std::unique_ptr<CSV> m_csv;
+            int m_region_hash_idx;
+            int m_region_hint_idx;
+            int m_region_progress_idx;
+            int m_region_runtime_idx;
     };
 }
 
