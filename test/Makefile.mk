8a93487244f471ee4c599c80694ef6f56eddb792
Diana Guttman <diana.r.guttman@intel.com>
2019-08-02 16:43:48 -0700

    Rename ManagerIO* classes to Endpoint
    - Separation into FileEndpointUser and ShmemEndpointUser will come
      in a later patch.
    
    Change-Id: I561bda2d7c11620f1127e7bdbdb634d1a2a1f622
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -84,6 +84,16 @@ GTEST_TESTS = test/gtest_links/AgentFactoryTest.static_info_monitor \
               test/gtest_links/CSVTest.columns \
               test/gtest_links/CSVTest.header \
               test/gtest_links/CSVTest.negative \
+              test/gtest_links/EndpointTest.negative_write_json_file \
+              test/gtest_links/EndpointTest.write_json_file \
+              test/gtest_links/EndpointTest.write_shm \
+              test/gtest_links/EndpointTestIntegration.write_shm \
+              test/gtest_links/EndpointUserTest.negative_bad_files \
+              test/gtest_links/EndpointUserTest.negative_parse_json_file \
+              test/gtest_links/EndpointUserTest.negative_parse_shm \
+              test/gtest_links/EndpointUserTest.parse_json_file \
+              test/gtest_links/EndpointUserTest.parse_shm \
+              test/gtest_links/EndpointUserTestIntegration.parse_shm \
               test/gtest_links/EnergyEfficientAgentTest.aggregate_sample \
               test/gtest_links/EnergyEfficientAgentTest.do_write_batch \
               test/gtest_links/EnergyEfficientAgentTest.enforce_policy \
@@ -163,16 +173,6 @@ GTEST_TESTS = test/gtest_links/AgentFactoryTest.static_info_monitor \
               test/gtest_links/MSRTest.msr_signal \
               test/gtest_links/MSRTest.string_to_function \
               test/gtest_links/MSRTest.string_to_units \
-              test/gtest_links/ManagerIOSamplerTest.negative_bad_files \
-              test/gtest_links/ManagerIOSamplerTest.negative_parse_json_file \
-              test/gtest_links/ManagerIOSamplerTest.negative_parse_shm \
-              test/gtest_links/ManagerIOSamplerTest.parse_json_file \
-              test/gtest_links/ManagerIOSamplerTest.parse_shm \
-              test/gtest_links/ManagerIOSamplerTestIntegration.parse_shm \
-              test/gtest_links/ManagerIOTest.negative_write_json_file \
-              test/gtest_links/ManagerIOTest.write_json_file \
-              test/gtest_links/ManagerIOTest.write_shm \
-              test/gtest_links/ManagerIOTestIntegration.write_shm \
               test/gtest_links/ModelApplicationTest.parse_config_errors \
               test/gtest_links/MonitorAgentTest.policy_names \
               test/gtest_links/MonitorAgentTest.sample_names \
