92cb3de83ae5d1fa021b4134319523b59222adc5
Diana Guttman <diana.r.guttman@intel.com>
2019-08-05 19:17:38 -0700

    Add pthread mutex to beginning of SharedMemory regions
    - SharedMemory will allocate the requested size plus 40 bytes for the
      mutex.
    - The pointer() methods now return the next address after the mutex.
      The mutex should only be manipulated indirectly through the
      get_scoped_lock() method.
    - Fixes #690
    
    Change-Id: If535ad9e7ff3ea10402402c0981666ecf160d9a5
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- src/SharedMemoryImp.hpp
+++ src/SharedMemoryImp.hpp
@@ -36,6 +36,8 @@
 #include "SharedMemory.hpp"
 #include "SharedMemoryUser.hpp"
 
+#include <pthread.h>
+
 namespace geopm
 {
     class SharedMemoryImp : public SharedMemory
@@ -55,7 +57,10 @@ namespace geopm
             /// @return Key to the shared memory region.
             std::string key(void) const override;
             size_t size(void) const override;
+            std::unique_ptr<SharedMemoryScopedLock> get_scoped_lock(void) override;
         private:
+            static void setup_mutex(pthread_mutex_t *lock);
+
             /// @brief Shared memory key for the region.
             std::string m_shm_key;
             /// @brief Size of the region.
@@ -81,6 +86,7 @@ namespace geopm
             std::string key(void) const override;
             size_t size(void) const override;
             void unlink(void) override;
+            std::unique_ptr<SharedMemoryScopedLock> get_scoped_lock(void) override;
         private:
             /// Shared memory key for the region.
             std::string m_shm_key;
