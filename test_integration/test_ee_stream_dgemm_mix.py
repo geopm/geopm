5834c8fd5b43ca0048eac6bd4dca55a234c5da38
Brandon Baker <brandon.baker@intel.com>
2019-08-19 10:17:00 -0700

    Use test launcher additions in stream/dgemm mix test
    Change-Id: I926020673378a6ac8f6441e2b59809925984b5ac
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- test_integration/test_ee_stream_dgemm_mix.py
+++ test_integration/test_ee_stream_dgemm_mix.py
@@ -43,6 +43,7 @@ import glob
 import geopm_context
 import geopmpy.io
 import geopm_test_launcher
+import util
 
 _g_skip_launch = False
 
@@ -68,6 +69,8 @@ class AppConf(object):
         return []
 
 
+@util.skip_unless_cpufreq()
+@util.skip_unless_optimized()
 class TestIntegrationEEStreamDGEMMMix(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -83,10 +86,12 @@ class TestIntegrationEEStreamDGEMMMix(unittest.TestCase):
         if  not cls._skip_launch:
             num_node = 2
             num_rank = 2
+            min_freq = geopm_test_launcher.geopmread("CPUINFO::FREQ_MIN board 0")
+            sticker_freq = geopm_test_launcher.geopmread("CPUINFO::FREQ_STICKER board 0")
             agent_conf = geopmpy.io.AgentConf(cls._agent_conf_path,
                                               'energy_efficient',
-                                              {'frequency_min':1.0e9,
-                                               'frequency_max':2.1e9})
+                                              {'frequency_min':min_freq,
+                                               'frequency_max':sticker_freq})
             launcher = geopm_test_launcher.TestLauncher(AppConf(),
                                                         agent_conf,
                                                         cls._report_path,
@@ -109,6 +114,7 @@ class TestIntegrationEEStreamDGEMMMix(unittest.TestCase):
             for ff in glob.glob(cls._trace_path + '*'):
                 os.unlink(ff)
 
+    @util.skip_unless_run_long_tests()
     def test_monotone_frequency(self):
         """Test that agent selects lower frequencies for regions with more
         stream.
