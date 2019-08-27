b1a886ff3560c21165da9fda13781c3ec18e6b51
Diana Guttman <diana.r.guttman@intel.com>
2019-08-26 20:10:04 -0700

    Add PolicyTracer to Controller
    - Have Controller check age of policy and whether it changed before
      sending down the tree.
    - Fixes #708.
    
    Change-Id: I63c139cace76223e83ee9878da85d564bcebbbfa
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -372,6 +372,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/MockIOGroup.hpp \
                           test/MockPlatformIO.hpp \
                           test/MockPlatformTopo.hpp \
+                          test/MockPolicyTracer.hpp \
                           test/MockPowerBalancer.hpp \
                           test/MockPowerGovernor.hpp \
                           test/MockProfileIOSample.hpp \
