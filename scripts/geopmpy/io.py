529fcfd3709c600e33953865dfa52ac0e5cb4cdd
Daniel Wilson <daniel1.wilson@intel.com>
2019-08-27 09:10:04 -0700

    Match expected datatypes in popen calls
    - Popen uses byte arrays for pipes by default. Some of our code intermixes
      piped stdout with sys.stderr, which expects strings to be written.
      This change matches expected and actual datatypes for code that
      interacts with popen.
    
    Signed-off-by: Daniel Wilson <daniel1.wilson@intel.com>
    Change-Id: Ieae8f4fb03b04a42af137d54aca30920573e4231

--- scripts/geopmpy/io.py
+++ scripts/geopmpy/io.py
@@ -55,7 +55,7 @@ from geopmpy import __version__
 from geopmpy import agent
 
 try:
-    _, os.environ['COLUMNS'] = subprocess.check_output(['stty', 'size']).split()
+    _, os.environ['COLUMNS'] = subprocess.check_output(['stty', 'size']).decode().split()
 except subprocess.CalledProcessError:
     os.environ['COLUMNS'] = "200"
 
