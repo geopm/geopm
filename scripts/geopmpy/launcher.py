636407c620faf00b175f02544d96457376ef1ae4
Daniel Wilson <daniel1.wilson@intel.com>
2019-08-26 17:25:03 -0700

    Explicitly use float division or floor division
    - Import __future__.division so all '/' operators do float division
    - Review instances of old-style division. Change to '//' where floor
      division is desired.
    
    Signed-off-by: Daniel Wilson <daniel1.wilson@intel.com>
    Change-Id: I87b9cc4c86c36abae301637222bec84368468ab8

--- scripts/geopmpy/launcher.py
+++ scripts/geopmpy/launcher.py
@@ -41,6 +41,7 @@ page for details about the command line interface.
 """
 
 from __future__ import absolute_import
+from __future__ import division
 
 from future import standard_library
 standard_library.install_aliases()
@@ -100,7 +101,7 @@ def int_ceil_div(aa, bb):
     """
     Shortcut for the ceiling of the ratio of two integers.
     """
-    return int(math.ceil(float(aa) / float(bb)))
+    return int(math.ceil(aa / bb))
 
 
 def range_str(values):
