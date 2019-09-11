898172f317316f705a4ab7d7b36e90871f4c743f
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-09-11 15:05:14 -0700

    Fix date generation in geopm_time.h
    - Original implementation assumes CLOCK_MONOTONIC_RAW is
      in reference to 1970 time, but it is not.
    - Add the delta between CLOCK_REALTIME and CLOCK_MONOTONIC_RAW
      when generating a date string.
    - Drop support for MacOS time structure (uses gettimeofday()).
    
    Change-Id: Ifdc98ed48f4ba0524da6501803e5e8b053db8b8b
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/geopm_time.h
+++ src/geopm_time.h
@@ -52,10 +52,10 @@ static inline bool geopm_time_comp(const struct geopm_time_s *aa, const struct g
 static inline void geopm_time_add(const struct geopm_time_s *begin, double elapsed, struct geopm_time_s *end);
 static inline double geopm_time_since(const struct geopm_time_s *begin);
 
-#ifdef __linux__
 #include <time.h>
 
-/// @brief structure to abstract the difference between a timespec on linux or a timeval on OSX.
+/// @brief structure to abstract the timespec on linux from other
+///        representations of time.
 struct geopm_time_s {
     struct timespec t;
 };
@@ -92,49 +92,16 @@ static inline void geopm_time_add(const struct geopm_time_s *begin, double elaps
     }
 }
 
-#else
-#include <sys/time.h>
-
-/// @brief structure to abstract the difference between a timespec on linux or a timeval on OSX.
-struct geopm_time_s {
-    struct timeval t;
-};
-
-static inline int geopm_time(struct geopm_time_s *time)
-{
-    return gettimeofday((struct timeval *)time, NULL);
-}
-
-static inline double geopm_time_diff(const struct geopm_time_s *begin, const struct geopm_time_s *end)
-{
-    return (end->t.tv_sec - begin->t.tv_sec) +
-           (end->t.tv_usec - begin->t.tv_usec) * 1E-6;
-}
-
-static inline bool geopm_time_comp(const struct geopm_time_s *aa, const struct geopm_time_s *bb)
-{
-    bool result = aa->t.tv_sec < bb->t.tv_sec;
-    if (!result && aa->t.tv_sec == bb->t.tv_sec) {
-        result = aa->t.tv_usec < bb->t.tv_usec;
-    }
-    return result;
-}
-
-static inline void geopm_time_add(const struct geopm_time_s *begin, double elapsed, struct geopm_time_s *end)
-{
-    *end = *begin;
-    end->t.tv_sec += elapsed;
-    elapsed -= floor(elapsed);
-    end->t.tv_usec += 1E6 * elapsed;
-}
-
-#endif
-
 static inline int geopm_time_to_string(const struct geopm_time_s *time, int buf_size, char *buf)
 {
     int err = 0;
     struct tm tm;
-    localtime_r(&(time->t.tv_sec), &tm);
+    struct geopm_time_s ref_time_real;
+    struct geopm_time_s ref_time_mono;
+    clock_gettime(CLOCK_REALTIME, &(ref_time_real.t));
+    clock_gettime(CLOCK_MONOTONIC_RAW, &(ref_time_mono.t));
+    time_t sec_since_1970 = geopm_time_diff(&ref_time_mono, &ref_time_real) + time->t.tv_sec;
+    localtime_r(&sec_since_1970, &tm);
     size_t num_byte = strftime(buf, buf_size, "%a %b %d %H:%M:%S %Y", &tm);
     if (!num_byte) {
         err = EINVAL;
