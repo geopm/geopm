96ce30f90e1bd4ad4052cb77adbb5bac18df8acd
Diana Guttman <diana.r.guttman@intel.com>
2019-10-16 17:48:02 -0700

    Add Daemon class and C interface
    - Holds logic that combines calls to the PolicyStore and Endpoint
    
    Change-Id: I4ec3ce3604b2ced39fb8ec5c8fdc25aed20a788b
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ src/Daemon.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef DAEMON_HPP_INCLUDE
+#define DAEMON_HPP_INCLUDE
+
+#include <memory>
+
+namespace geopm
+{
+    /// Methods using the Endpoint interface in combination with other
+    /// utilities to perform some system admin functions.
+    class Daemon
+    {
+        public:
+            virtual ~Daemon() = default;
+
+            virtual void update_endpoint_from_policystore(volatile bool &cancel) = 0;
+
+            static std::unique_ptr<Daemon> make_unique(const std::string &endpoint_name,
+                                                       const std::string &db_path);
+    };
+}
+
+#endif
