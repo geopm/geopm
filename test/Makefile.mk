3e1cec5bb8a80d665a9c90a18377913f4bdf483b
Diana Guttman <diana.r.guttman@intel.com>
2019-10-16 17:48:02 -0700

    Add Daemon class and C interface
    - Holds logic that combines calls to the PolicyStore and Endpoint
    - Resolves #786
    
    Change-Id: I4ec3ce3604b2ced39fb8ec5c8fdc25aed20a788b
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -89,6 +89,8 @@ GTEST_TESTS = test/gtest_links/AgentFactoryTest.static_info_monitor \
               test/gtest_links/CSVTest.columns \
               test/gtest_links/CSVTest.header \
               test/gtest_links/CSVTest.negative \
+              test/gtest_links/DaemonTest.get_default_policy \
+              test/gtest_links/DaemonTest.get_profile_policy \
               test/gtest_links/DebugIOGroupTest.is_valid \
               test/gtest_links/DebugIOGroupTest.push \
               test/gtest_links/DebugIOGroupTest.read_signal \
@@ -365,6 +367,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/ControllerTest.cpp \
                           test/CpuinfoIOGroupTest.cpp \
                           test/CSVTest.cpp \
+                          test/DaemonTest.cpp \
                           test/DebugIOGroupTest.cpp \
                           test/EndpointTest.cpp \
                           test/EndpointPolicyTracerTest.cpp \
@@ -386,6 +389,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/MockApplicationIO.hpp \
                           test/MockComm.hpp \
                           test/MockControlMessage.hpp \
+                          test/MockEndpoint.hpp \
                           test/MockEndpointPolicyTracer.hpp \
                           test/MockEndpointUser.hpp \
                           test/MockEnergyEfficientRegion.hpp \
@@ -394,6 +398,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/MockIOGroup.hpp \
                           test/MockPlatformIO.hpp \
                           test/MockPlatformTopo.hpp \
+                          test/MockPolicyStore.hpp \
                           test/MockPowerBalancer.hpp \
                           test/MockPowerGovernor.hpp \
                           test/MockProfileIOSample.hpp \
