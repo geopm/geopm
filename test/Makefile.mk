72579a4c4228073ae21032d7d1ec0560390c24dc
Diana Guttman <diana.r.guttman@intel.com>
2019-10-16 17:48:02 -0700

    Add Daemon class and C interface
    - Holds logic that combines calls to the PolicyStore and Endpoint
    - Resolves #786
    
    Change-Id: I4ec3ce3604b2ced39fb8ec5c8fdc25aed20a788b
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -363,6 +363,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/ControllerTest.cpp \
                           test/CpuinfoIOGroupTest.cpp \
                           test/CSVTest.cpp \
+                          test/DaemonTest.cpp \
                           test/DebugIOGroupTest.cpp \
                           test/EndpointTest.cpp \
                           test/EndpointUserTest.cpp \
@@ -383,6 +384,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/MockApplicationIO.hpp \
                           test/MockComm.hpp \
                           test/MockControlMessage.hpp \
+                          test/MockEndpoint.hpp \
                           test/MockEndpointUser.hpp \
                           test/MockEnergyEfficientRegion.hpp \
                           test/MockEpochRuntimeRegulator.hpp \
@@ -390,6 +392,7 @@ test_geopm_test_SOURCES = test/AgentFactoryTest.cpp \
                           test/MockIOGroup.hpp \
                           test/MockPlatformIO.hpp \
                           test/MockPlatformTopo.hpp \
+                          test/MockPolicyStore.hpp \
                           test/MockPowerBalancer.hpp \
                           test/MockPowerGovernor.hpp \
                           test/MockProfileIOSample.hpp \
