85dfe9d77011f917710fcc400c4d16719a500c09
Brandon Baker <brandon.baker@intel.com>
2020-01-23 15:03:15 -0800

    Add integration test of static policy plugin
    - Asserts that expected site policy is enforced on non-GEOPM jobs
    - Fixes #874
    
    Change-Id: I32b4197323e2814e20ba122ab4f32b6ca2eef604
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- test_integration/__init__.py
+++ test_integration/__init__.py
@@ -42,4 +42,5 @@ from test_integration.test_omp_outer_loop import *
 from test_integration.test_ee_stream_dgemm_mix import *
 from test_integration.test_profile_policy import *
 from test_integration.test_static_policy import *
+from test_integration.test_plugin_static_policy import *
 from geopmpy.version import __version__
