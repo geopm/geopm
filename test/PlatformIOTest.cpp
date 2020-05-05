594baaa0255e43f5e8fdce87695d9044319f8b6a
Diana Guttman <diana.r.guttman@intel.com>
2020-05-05 11:45:15 -0700

    Use make_topo() in PlatformIOTest to replace ON_CALL expectations
    Change-Id: Ibf9ab4434fe44c4d5bc5e446c6ef5b512e524109
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test/PlatformIOTest.cpp
+++ test/PlatformIOTest.cpp
@@ -126,7 +126,7 @@ class PlatformIOTest : public ::testing::Test
         void SetUp();
         std::list<std::shared_ptr<PlatformIOTestMockIOGroup> > m_iogroup_ptr;
         std::unique_ptr<PlatformIOImp> m_platio;
-        MockPlatformTopo m_topo;
+        std::shared_ptr<MockPlatformTopo> m_topo;
         std::set<int> m_cpu_set_board;
         std::set<int> m_cpu_set0;
         std::set<int> m_cpu_set1;
@@ -157,34 +157,12 @@ void PlatformIOTest::SetUp()
     m_override_iogroup->set_valid_controls({{"MODE", GEOPM_DOMAIN_BOARD}});
 
     // Settings for PlatformTopo: 2 socket, 4 cpus each
+    m_topo = make_topo(2, 4, 8);
     m_cpu_set_board = {0, 1, 2, 3, 4, 5, 6, 7};
     m_cpu_set0 = {0, 1, 4, 5};
     m_cpu_set1 = {2, 3, 6, 7};
-    ON_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_BOARD))
-        .WillByDefault(Return(true));
-    ON_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_BOARD_MEMORY))
-        .WillByDefault(Return(true));
-    ON_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE))
-        .WillByDefault(Return(true));
-    ON_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_BOARD, _))
-        .WillByDefault(Return(m_cpu_set_board));
-    ON_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_BOARD_MEMORY, 0))
-        .WillByDefault(Return(m_cpu_set0));
-    ON_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_BOARD_MEMORY, 1))
-        .WillByDefault(Return(m_cpu_set1));
-    ON_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0))
-        .WillByDefault(Return(m_cpu_set0));
-    ON_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 1))
-        .WillByDefault(Return(m_cpu_set1));
-    ON_CALL(m_topo, num_domain(GEOPM_DOMAIN_BOARD))
-        .WillByDefault(Return(1));
-    ON_CALL(m_topo, num_domain(GEOPM_DOMAIN_CPU))
-        .WillByDefault(Return(m_cpu_set_board.size()));
-    ON_CALL(m_topo, num_domain(GEOPM_DOMAIN_PACKAGE))
-        .WillByDefault(Return(2));
-    ON_CALL(m_topo, num_domain(GEOPM_DOMAIN_BOARD_MEMORY))
-        .WillByDefault(Return(2));
-
+    // suppress warnings about num_domain calls
+    EXPECT_CALL(*m_topo, num_domain(_)).Times(AtLeast(0));
 
     m_iogroup_ptr = {
         m_time_iogroup,
@@ -197,7 +175,7 @@ void PlatformIOTest::SetUp()
         iogroup_list.emplace_back(ptr);
     }
 
-    m_platio.reset(new PlatformIOImp(iogroup_list, m_topo));
+    m_platio.reset(new PlatformIOImp(iogroup_list, *m_topo));
 }
 
 TEST_F(PlatformIOTest, signal_control_names)
@@ -288,9 +266,9 @@ TEST_F(PlatformIOTest, push_signal)
 
 TEST_F(PlatformIOTest, push_signal_agg)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
+    EXPECT_CALL(*m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
                                          GEOPM_DOMAIN_PACKAGE));
-    EXPECT_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
+    EXPECT_CALL(*m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
 
     EXPECT_CALL(*m_control_iogroup, signal_domain_type("FREQ")).Times(AtLeast(1));
     for (auto cpu : m_cpu_set0) {
@@ -321,9 +299,9 @@ TEST_F(PlatformIOTest, push_control)
 
 TEST_F(PlatformIOTest, push_control_agg)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
+    EXPECT_CALL(*m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
                                          GEOPM_DOMAIN_PACKAGE));
-    EXPECT_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
+    EXPECT_CALL(*m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
     EXPECT_EQ(0, m_platio->num_control_pushed());
     EXPECT_CALL(*m_control_iogroup, control_domain_type("FREQ")).Times(AtLeast(1));
     for (auto cpu : m_cpu_set0) {
@@ -365,9 +343,9 @@ TEST_F(PlatformIOTest, sample)
 
 TEST_F(PlatformIOTest, sample_agg)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
+    EXPECT_CALL(*m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
                                          GEOPM_DOMAIN_PACKAGE));
-    EXPECT_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
+    EXPECT_CALL(*m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
     EXPECT_CALL(*m_control_iogroup, signal_domain_type("FREQ")).Times(AtLeast(1));
     EXPECT_CALL(*m_control_iogroup, agg_function("FREQ"))
         .WillOnce(Return(geopm::Agg::average));
@@ -414,9 +392,9 @@ TEST_F(PlatformIOTest, adjust)
 
 TEST_F(PlatformIOTest, adjust_agg)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
+    EXPECT_CALL(*m_topo, is_nested_domain(GEOPM_DOMAIN_CPU,
                                          GEOPM_DOMAIN_PACKAGE));
-    EXPECT_CALL(m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
+    EXPECT_CALL(*m_topo, domain_nested(GEOPM_DOMAIN_CPU, GEOPM_DOMAIN_PACKAGE, 0));
     double value = 1.23e9;
     EXPECT_CALL(*m_control_iogroup, control_domain_type("FREQ")).Times(AtLeast(1));
     for (auto cpu : m_cpu_set0) {
@@ -438,7 +416,7 @@ TEST_F(PlatformIOTest, adjust_agg)
 
 TEST_F(PlatformIOTest, read_signal)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
     EXPECT_CALL(*m_control_iogroup, signal_domain_type("FREQ"));
     EXPECT_CALL(*m_control_iogroup, read_signal("FREQ", GEOPM_DOMAIN_CPU, 0))
         .WillOnce(Return(4e9));
@@ -459,8 +437,8 @@ TEST_F(PlatformIOTest, read_signal)
 
 TEST_F(PlatformIOTest, read_signal_agg)
 {
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
-    EXPECT_CALL(m_topo, domain_nested(_, _, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, domain_nested(_, _, _));
     EXPECT_CALL(*m_control_iogroup, signal_domain_type("FREQ")).Times(AtLeast(1));
     EXPECT_CALL(*m_control_iogroup, agg_function("FREQ")).WillOnce(Return(Agg::average));
     for (auto cpu : m_cpu_set0) {
@@ -480,7 +458,7 @@ TEST_F(PlatformIOTest, write_control)
     EXPECT_CALL(*m_override_iogroup, write_batch()).Times(0);
 
     double value = 3e9;
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
     EXPECT_CALL(*m_override_iogroup, control_domain_type("MODE")).Times(AtLeast(1));
     EXPECT_CALL(*m_override_iogroup, write_control("MODE", GEOPM_DOMAIN_BOARD, 0, value));
     m_platio->write_control("MODE", GEOPM_DOMAIN_BOARD, 0, value);
@@ -496,8 +474,8 @@ TEST_F(PlatformIOTest, write_control_agg)
     EXPECT_CALL(*m_override_iogroup, write_batch()).Times(0);
 
     double value = 3e9;
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
-    EXPECT_CALL(m_topo, domain_nested(_, _, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, domain_nested(_, _, _));
     EXPECT_CALL(*m_control_iogroup, control_domain_type("FREQ")).Times(AtLeast(1));
     for (auto cpu : m_cpu_set0) {
         EXPECT_CALL(*m_control_iogroup, write_control("FREQ", GEOPM_DOMAIN_CPU, cpu, value));
@@ -513,7 +491,7 @@ TEST_F(PlatformIOTest, read_signal_override)
     EXPECT_CALL(*m_control_iogroup, signal_domain_type("MODE")).Times(0);
     EXPECT_CALL(*m_control_iogroup, read_signal(_, _, _)).Times(0);
 
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
     EXPECT_CALL(*m_override_iogroup, signal_domain_type("MODE")).Times(AtLeast(1));
     EXPECT_CALL(*m_override_iogroup, read_signal("MODE", GEOPM_DOMAIN_BOARD, 0))
         .WillOnce(Return(5e9));
@@ -532,7 +510,7 @@ TEST_F(PlatformIOTest, write_control_override)
     EXPECT_CALL(*m_control_iogroup, write_control(_, _, _, _)).Times(0);
 
     double value = 10;
-    EXPECT_CALL(m_topo, is_nested_domain(_, _));
+    EXPECT_CALL(*m_topo, is_nested_domain(_, _));
     EXPECT_CALL(*m_override_iogroup, control_domain_type("MODE")).Times(AtLeast(1));
     EXPECT_CALL(*m_override_iogroup, write_control("MODE", GEOPM_DOMAIN_BOARD, 0, value));
     m_platio->write_control("MODE", GEOPM_DOMAIN_BOARD, 0, value);
