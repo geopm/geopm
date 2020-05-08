eb1a51541a10fcedf1765f1fa4601af4bc772cb1
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2020-05-08 10:56:00 -0700

    Fix remaining old policy strings for frequency map agent
    Change-Id: I6d89b88a78f04ad895837073f39394db06b4a337
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_integration/test_scaling_region.py
+++ test_integration/test_scaling_region.py
@@ -104,8 +104,7 @@ class TestIntegrationScalingRegion(unittest.TestCase):
             freq_sticker = geopm_test_launcher.geopmread("CPUINFO::FREQ_STICKER board 0")
             freq_step = geopm_test_launcher.geopmread("CPUINFO::FREQ_STEP board 0")
             num_step = int((freq_sticker - freq_min) / freq_step + 0.5)
-            agent_conf_dict = {'FREQ_DEFAULT':freq_sticker,
-                               'FREQ_UNCORE':float('nan')}
+            agent_conf_dict = {'FREQ_DEFAULT':freq_sticker}
             cls._region_freq = [freq_min + idx * freq_step
                                 for idx in range(num_step)]
             freq_idx = 0
