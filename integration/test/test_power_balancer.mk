4f3a325d76e57e6746a5f85bde3e99eeaace60f6
Diana Guttman <diana.r.guttman@intel.com>
2020-07-09 15:46:35 -0700

    Rename test_socket_imbalance to test_power_balancer and remove python
    - Will be added as a binary to test_power_balancer
    
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/test/test_power_balancer.mk
+++ integration/test/test_power_balancer.mk
@@ -30,3 +30,14 @@
 #
 
 EXTRA_DIST += integration/test/test_power_balancer.py
+
+if ENABLE_MPI
+noinst_PROGRAMS += integration/test/test_power_balancer
+integration_test_test_power_balancer_SOURCES = integration/test/test_power_balancer.cpp
+integration_test_test_power_balancer_SOURCES += $(model_source_files)
+integration_test_test_power_balancer_LDADD = libgeopm.la $(MATH_LIB) $(MPI_CLIBS)
+integration_test_test_power_balancer_LDFLAGS = $(AM_LDFLAGS) $(MPI_CLDFLAGS) $(MATH_CLDFLAGS)
+integration_test_test_power_balancer_CXXFLAGS = $(AM_CXXFLAGS) $(MPI_CFLAGS) -D_GNU_SOURCE -std=c++11 $(MATH_CFLAGS)
+else
+EXTRA_DIST += integration/test/test_power_balancer.cpp
+endif
