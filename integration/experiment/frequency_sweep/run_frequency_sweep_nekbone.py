3881baca0a4536c6eb510e953e3efbedff55563d
Diana Guttman <diana.r.guttman@intel.com>
2020-08-19 09:14:40 -0700

    Add nekbone frequency sweep
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ integration/experiment/frequency_sweep/run_frequency_sweep_nekbone.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2015, 2016, 2017, 2018, 2019, 2020, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+'''
+Example frequency sweep experiment using Nekbone.
+'''
+
+import argparse
+
+from experiment import common_args
+from experiment.frequency_sweep import frequency_sweep
+from experiment import machine
+from apps.nekbone import nekbone
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    common_args.add_output_dir(parser)
+    common_args.add_nodes(parser)
+    common_args.add_min_frequency(parser)
+    common_args.add_max_frequency(parser)
+
+    args, extra_cli_args = parser.parse_known_args()
+
+    output_dir = args.output_dir
+    num_nodes = args.nodes
+    mach = machine.init_output_dir(output_dir)
+
+    # application parameters
+    app_conf = nekbone.NekboneAppConf()
+
+    # experiment parameters
+    min_freq = args.min_frequency
+    max_freq = args.max_frequency
+    step_freq = None
+    freqs = frequency_sweep.setup_frequency_bounds(mach, min_freq, max_freq, step_freq,
+                                                   add_turbo_step=True)
+    iterations = 2
+    frequency_sweep.launch_frequency_sweep(output_dir=output_dir,
+                                           iterations=iterations,
+                                           freq_range=freqs,
+                                           agent_types=['frequency_map'],
+                                           num_node=num_nodes,
+                                           app_conf=app_conf,
+                                           experiment_cli_args=extra_cli_args)
