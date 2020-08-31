db2cf6c61d74eae189580eee216bb7f64b011897
Fuat Keceli <fuat.keceli@intel.com>
2020-08-31 14:58:28 -0700

    Minor code cleanup.

--- src/EditDistPeriodicityDetector.cpp
+++ src/EditDistPeriodicityDetector.cpp
@@ -106,17 +106,17 @@ namespace geopm
         if (ii <= m_record_count - m_history_buffer_size) {
             // This value is supposed to be INF but not so large that it gets wrapped around when
             // a small value is added to it.
-            return std::numeric_limits<uint32_t>::max()/2;
+            return std::numeric_limits<uint32_t>::max() / 2;
         }
         if (jj >= m_history_buffer_size) {
             // This value is supposed to be INF but not so large that it gets wrapped around when
             // a small value is added to it.
-            return std::numeric_limits<uint32_t>::max()/2;
+            return std::numeric_limits<uint32_t>::max() / 2;
         }
         if (mm <= m_record_count - m_history_buffer_size) {
             // This value is supposed to be INF but not so large that it gets wrapped around when
             // a small value is added to it.
-            return std::numeric_limits<uint32_t>::max()/2;
+            return std::numeric_limits<uint32_t>::max() / 2;
         }
         return m_DP[((ii % m_history_buffer_size)  * m_history_buffer_size
                    + (jj % m_history_buffer_size)) * m_history_buffer_size
@@ -132,14 +132,14 @@ namespace geopm
         int num_recs_in_hist = m_history_buffer.size();
 
         for (int ii = std::max({0, m_record_count - m_history_buffer_size}); ii < m_record_count; ++ii) {
-            Dset(ii, 0, m_record_count-1, 0);
+            Dset(ii, 0, m_record_count - 1, 0);
         }
         for (int mm = std::max({0, m_record_count - m_history_buffer_size}); mm < m_record_count; ++mm) {
             Dset(0, m_record_count - mm, mm, m_record_count - mm);
         }
 
-        for (int mm = std::max({1, m_record_count-m_history_buffer_size}); mm < m_record_count; ++mm) {
-            for (int ii = std::max({1, m_record_count-m_history_buffer_size}); ii < mm+1; ++ii) {
+        for (int mm = std::max({1, m_record_count - m_history_buffer_size}); mm < m_record_count; ++mm) {
+            for (int ii = std::max({1, m_record_count - m_history_buffer_size}); ii < mm + 1; ++ii) {
                 int term;
                 if (m_record_count - (ii - 1) <= num_recs_in_hist) {
                     if (m_squash_records) {
@@ -156,7 +156,8 @@ namespace geopm
                                 m_history_buffer.value(num_recs_in_hist - 1)) ?
                             2 : 0;
                     }
-                } else {
+                }
+                else {
                     term = m_squash_records ? 2 * m_repeat_count.value(num_recs_in_hist - 1) : 2;
                 }
 
