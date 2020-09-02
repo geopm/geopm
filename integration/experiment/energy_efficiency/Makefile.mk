93a13083de5d71c0f644312ea55190fb2a83f13c
Diana Guttman <diana.r.guttman@intel.com>
2020-09-01 17:15:57 -0700

    Add more run scripts for AMG
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/experiment/energy_efficiency/Makefile.mk
+++ integration/experiment/energy_efficiency/Makefile.mk
@@ -34,9 +34,10 @@ EXTRA_DIST += integration/experiment/energy_efficiency/barrier_frequency_sweep.p
               integration/experiment/energy_efficiency/__init__.py \
               integration/experiment/energy_efficiency/power_balancer_energy.py \
               integration/experiment/energy_efficiency/run_barrier_frequency_sweep_nekbone.py \
+              integration/experiment/energy_efficiency/run_power_balancer_energy_amg.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_dgemm.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_hpcg.py \
-              integration/experiment/energy_efficiency/run_power_balancer_energy_nasft.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_minife.py \
+              integration/experiment/energy_efficiency/run_power_balancer_energy_nasft.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_nekbone.py \
               # end
--- integration/experiment/frequency_sweep/run_frequency_sweep_amg.py
+++ integration/experiment/frequency_sweep/run_frequency_sweep_amg.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 #  Copyright (c) 2015, 2016, 2017, 2018, 2019, 2020, Intel Corporation
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -29,18 +31,20 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-EXTRA_DIST += integration/experiment/power_sweep/gen_balancer_comparison.py \
-              integration/experiment/power_sweep/gen_power_sweep_summary.py \
-              integration/experiment/power_sweep/gen_plot_balancer_comparison.py \
-              integration/experiment/power_sweep/gen_plot_balancer_power_limit.py \
-              integration/experiment/power_sweep/gen_plot_node_efficiency.py \
-              integration/experiment/power_sweep/__init__.py \
-              integration/experiment/power_sweep/power_sweep.py \
-              integration/experiment/power_sweep/README.md \
-              integration/experiment/power_sweep/run_power_sweep_amg.py \
-              integration/experiment/power_sweep/run_power_sweep_dgemm.py \
-              integration/experiment/power_sweep/run_power_sweep_dgemm_tiny.py \
-              integration/experiment/power_sweep/run_power_sweep_hpcg.py \
-              integration/experiment/power_sweep/run_power_sweep_minife.py \
-              integration/experiment/power_sweep/run_power_sweep_nekbone.py \
-              # end
+'''
+Example frequency sweep experiment using AMG.
+'''
+
+import argparse
+
+from experiment.frequency_sweep import frequency_sweep
+from apps.amg import amg
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    frequency_sweep.setup_run_args(parser)
+    args, extra_cli_args = parser.parse_known_args()
+    app_conf = amg.AmgAppConf(args.node_count)
+    frequency_sweep.launch(app_conf=app_conf, args=args,
+                           experiment_cli_args=extra_cli_args)
