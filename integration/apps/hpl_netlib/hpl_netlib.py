6fc8da8708c3d6b452bbc93d4ac367ed47cd1535
Fuat Keceli <fuat.keceli@intel.com>
2020-10-22 09:37:01 -0700

    Moved HPL.dat file creation to trial_setup()
    Signed-off-by: Fuat Keceli <fuat.keceli@intel.com>

--- integration/apps/hpl_netlib/hpl_netlib.py
+++ integration/apps/hpl_netlib/hpl_netlib.py
@@ -81,45 +81,44 @@ class HplNetlibAppConf(apps.AppConf):
         sys.stdout.write('Cores for app: {}\n'.format(cores_per_node))
         sys.stdout.write('N={}\n'.format(self.N))
 
-    def get_bash_setup_commands(self):
-        input_file = textwrap.dedent('''
-        HPLinpack benchmark input file
-        Innovative Computing Laboratory, University of Tennessee
-        HPL.out      output file name (if any)
-        6            device out (6=stdout,7=stderr,file)
-        1            # of problems sizes (N)
-        {N}          Ns
-        1            # of NBs
-        {NBs}        NBs
-        0            PMAP process mapping (0=Row-,1=Column-major)
-        1            # of process grids (P x Q)
-        {P}          Ps
-        {Q}          Qs
-        16.0         threshold
-        1            # of panel fact
-        1            PFACTs (0=left, 1=Crout, 2=Right)1
-        1            # of recursive stopping criterium
-        4            NBMINs (>= 1)
-        1            # of panels in recursion
-        2            NDIVs
-        1            # of recursive panel fact.
-        1            RFACTs (0=left, 1=Crout, 2=Right)
-        1            # of broadcast
-        0            BCASTs (0=1rg,1=1rM,2=2rg,3=2rM,4=Lng,5=LnM)
-        1            # of lookahead depth
-        0            DEPTHs (>=0)
-        2            SWAP (0=bin-exch,1=long,2=mix)
-        64           swapping threshold
-        0            L1 in (0=transposed,1=no-transposed) form
-        0            U  in (0=transposed,1=no-transposed) form
-        1            Equilibration (0=no,1=yes)
-        8            memory alignment in double (> 0)
-        EOF
-        '''.format(N=self.N, NBs=self.NBs, P=self.P, Q=self.Q))
-
-        setup_commands = 'cat > ./HPL.dat << EOF {}\n'.format(input_file)
-
-        return setup_commands
+    def trial_setup(self, run_id, output_dir):
+        dat_file_path = os.path.join(output_dir + "/HPL.dat")
+        if not os.path.isfile(dat_file_path):
+            dat_file_text = textwrap.dedent('''\
+            HPLinpack benchmark input file
+            Innovative Computing Laboratory, University of Tennessee
+            HPL.out      output file name (if any)
+            6            device out (6=stdout,7=stderr,file)
+            1            # of problems sizes (N)
+            {N}          Ns
+            1            # of NBs
+            {NBs}        NBs
+            0            PMAP process mapping (0=Row-,1=Column-major)
+            1            # of process grids (P x Q)
+            {P}          Ps
+            {Q}          Qs
+            16.0         threshold
+            1            # of panel fact
+            1            PFACTs (0=left, 1=Crout, 2=Right)1
+            1            # of recursive stopping criterium
+            4            NBMINs (>= 1)
+            1            # of panels in recursion
+            2            NDIVs
+            1            # of recursive panel fact.
+            1            RFACTs (0=left, 1=Crout, 2=Right)
+            1            # of broadcast
+            0            BCASTs (0=1rg,1=1rM,2=2rg,3=2rM,4=Lng,5=LnM)
+            1            # of lookahead depth
+            0            DEPTHs (>=0)
+            2            SWAP (0=bin-exch,1=long,2=mix)
+            64           swapping threshold
+            0            L1 in (0=transposed,1=no-transposed) form
+            0            U  in (0=transposed,1=no-transposed) form
+            1            Equilibration (0=no,1=yes)
+            8            memory alignment in double (> 0)
+            '''.format(N=self.N, NBs=self.NBs, P=self.P, Q=self.Q))
+            with open(dat_file_path, "w") as dat_file:
+                dat_file.write(dat_file_text)
 
     def get_rank_per_node(self):
         return 1
