14d3f5eda92e15717fb6a8bf3ee3d9845d2eebf1
Brad Geltz <brad.geltz@intel.com>
2020-11-05 11:22:53 -0800

    Add SMNG env file
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/config/smng_env.sh
+++ integration/config/smng_env.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 #  Copyright (c) 2015, 2016, 2017, 2018, 2019, 2020, Intel Corporation
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -29,16 +31,34 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+# SMNG BUILD ENVIRONMENT
+#
+# This script is intended to be sourced within an existing script or shell ONLY.
+# It is NOT intended to be ./ executed.
 
-EXTRA_DIST += integration/README.md \
-              integration/config/build_env.sh \
-              integration/config/run_env.sh \
-              integration/config/dudley_env.sh \
-              integration/config/endeavor_env.sh \
-              integration/config/smng_env.sh \
-              # end
+# Default Intel Toolchain compiler overrides
+# The MPI compiler wrappers are supported by Intel, but are not Intel specific.
+module swap intel intel/19.1
+module load python/3.6_intel
+unset OMP_NUM_THREADS # Set to 1 by a default lmod module
 
-include integration/apps/Makefile.mk
-include integration/experiment/Makefile.mk
-include integration/test/Makefile.mk
-include integration/smoke/Makefile.mk
+# One time setup:
+# python -m pip install --user -U pip
+# python -m pip install --user --ignore-installed -r geopm/scripts/requirements.txt
+
+export MPIFORT=mpiifort
+export MPIFC=mpiifort
+export MPIF77=mpiifort
+export MPIF90=mpiifort
+
+export GEOPM_USER_ACCOUNT=h1906
+export GEOPM_SYSTEM_DEFAULT_QUEUE=test
+export GEOPM_SBATCH_EXTRA_LINES="
+#SBATCH --constraint=\"noscratch\"
+#SBATCH --ear off
+${GEOPM_SBATCH_EXTRA_LINES}
+"
+
+# Notes
+##SBATCH --export=NONE # This breaks the env of srun calls within SBATCH scripts
+
