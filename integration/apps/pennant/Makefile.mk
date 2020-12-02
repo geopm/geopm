895d484111031f94ff4acb38550cbf5370025822
Fuat Keceli <fuat.keceli@intel.com>
2020-12-02 10:27:43 -0800

    Fixed the Makefile to include the new pennant patch file.
    Signed-off-by: Fuat Keceli <fuat.keceli@intel.com>

--- integration/apps/pennant/Makefile.mk
+++ integration/apps/pennant/Makefile.mk
@@ -36,4 +36,5 @@ EXTRA_DIST += integration/apps/pennant/pennant.py \
               integration/apps/pennant/README.md \
               integration/apps/pennant/0001-Fixed-Makefile-to-GEOPM-and-Intel-conventions.patch \
               integration/apps/pennant/0002-Added-geopm-epoch-markup.patch \
+              integration/apps/pennant/0003-Added-GEOPM-region-markup.patch \
               # end
