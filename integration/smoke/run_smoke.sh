e4f4181d264b03b7830f11602d0550055cf50743
Ali Mohammad <alawibaba@gmail.com>
2020-12-18 15:29:49 -0800

    Smoke test for gen_policy_recommendation

--- integration/smoke/run_smoke.sh
+++ integration/smoke/run_smoke.sh
@@ -58,8 +58,10 @@ function run_all_power_sweep {
     APPLICATIONS="dgemm dgemm_tiny nekbone minife amg nasft hpcg"
     EXP_SUBDIR=power_sweep
     EXP_TYPE=power_sweep
-    ARGS="--min-power=220 --max-power=230"
+    ARGS="--min-power=190 --max-power=230"
     run_all
+    python3 ${EXP_DIR}/power_sweep/gen_policy_recommendation.py \
+            --path ${SLURM_JOB_ID}_dgemm_power_sweep
 }
 
 function run_all_freq_sweep {
