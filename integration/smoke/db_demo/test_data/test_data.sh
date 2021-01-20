1254931d1c458ea9f800888d35ef92248004e230
Diana Guttman <diana.r.guttman@intel.com>
2021-01-20 15:11:32 -0800

    Add example script for inserting experiments
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ integration/smoke/db_demo/test_data/test_data.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+../main.py save --output-dir=5815581_hpcg_power_balancer_energy --jobid=5815581 --exp-type=power_balancer_energy
+../main.py save --output-dir=5815583_nekbone_power_balancer_energy --jobid=5815583 --exp-type=power_balancer_energy
+../main.py save --output-dir=5815584_nasft_power_balancer_energy --jobid=5815584 --exp-type=power_balancer_energy
+../main.py save --output-dir=5815585_nekbone_barrier_frequency_sweep --jobid=5815585 --exp-type=barrier_frequency_sweep
+../main.py save --output-dir=5815586_hpcg_frequency_sweep --jobid=5815586 --exp-type=frequency_sweep
+../main.py save --output-dir=5815587_nekbone_frequency_sweep --jobid=5815587 --exp-type=frequency_sweep
+../main.py save --output-dir=5815588_nasft_frequency_sweep --jobid=5815588 --exp-type=frequency_sweep
+../main.py save --output-dir=5815589_minife_power_balancer_energy --jobid=5815589 --exp-type=power_balancer_energy
+../main.py save --output-dir=5815590_minife_frequency_sweep --jobid=5815590 --exp-type=frequency_sweep
+../main.py save --output-dir=5816536_nekbone_power_balancer_energy --jobid=5816536 --exp-type=power_balancer_energy
+../main.py save --output-dir=5817058_nasft_power_balancer_energy --jobid=5817058 --exp-type=power_balancer_energy
