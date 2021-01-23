be855134b3e578bc563d34e67004ffd3e7f96ad0
Diana Guttman <diana.r.guttman@intel.com>
2021-01-22 17:09:52 -0800

    Fix test_geopmagent
    - Add context management for Popen in test_geopmagent
    - Update makefiles
    
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/test/Makefile.mk
+++ integration/test/Makefile.mk
@@ -64,3 +64,4 @@ include integration/test/test_ompt.mk
 include integration/test/test_geopmio.mk
 include integration/test/test_launch_application.mk
 include integration/test/test_launch_pthread.mk
+include integration/test/test_geopmagent.mk
