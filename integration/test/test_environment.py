583401a19e9070b94844dc2defafdc08bdfd17d8
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-01-25 13:06:21 -0800

    Change test to use frequency map agent instead of ee agent
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/test/test_environment.py
+++ integration/test/test_environment.py
@@ -34,11 +34,13 @@
 import sys
 import unittest
 import os
+import json
 
-import geopm_context
+from integration.test import geopm_context
 import geopmpy.io
 
 import util
+import geopm_test_launcher
 
 environment_default_path = os.path.join(util.get_config_value('GEOPM_CONFIG_PATH'), 'environment-default.json')
 environment_override_path = os.path.join(util.get_config_value('GEOPM_CONFIG_PATH'), 'environment-override.json')
@@ -57,15 +59,12 @@ class TestIntegrationEnvironment(unittest.TestCase):
         num_node = 1
         num_rank = 1
         app_conf = geopmpy.io.BenchConf(context + '.app.config')
-        self._tmp_files.append(app_conf.get_path())
         app_conf.append_region('sleep', 0.01)
         user_agent_conf = geopmpy.io.AgentConf(context + '.user.agent.config', self._agent, {} if user_policy is None else user_policy)
-        self._tmp_files.append(user_agent_conf.get_path())
         launcher = geopm_test_launcher.TestLauncher(app_conf, user_agent_conf, report_path)
         launcher.set_num_node(num_node)
         launcher.set_num_rank(num_rank)
         launcher.run(context, include_geopm_policy=user_policy is not None)
-        self._tmp_files.append(report_path)
 
         actual_policy = geopmpy.io.RawReport(report_path).meta_data()['Policy']
         for expected_key, expected_value in expected_policy.items():
@@ -90,24 +89,24 @@ class TestIntegrationEnvironment(unittest.TestCase):
                 util.run_script_on_compute_nodes(
                     'mv -f {policy_file} {policy_file}.backup ; '
                     'echo \'{policy}\' > {policy_file} && '
-                    'echo \'{{"GEOPM_POLICY": "{policy_file}", "GEOPM_AGENT": "energy_efficient"}}\' > {etc_file}'.format(
+                    'echo \'{{"GEOPM_POLICY": "{policy_file}", "GEOPM_AGENT": "frequency_map"}}\' > {etc_file}'.format(
                         policy=policy_string, policy_file=policy_file_path, etc_file=etc_config_path),
                     dev_null, dev_null)
 
         test_name = 'test_geopm_environment'
-        self._agent = 'energy_efficient'
-        user_policy = { 'PERF_MARGIN': 0.13 }
+        self._agent = 'frequency_map'
+        user_policy = { 'FREQ_DEFAULT': 1.5e9 }
 
         with util.temporarily_remove_compute_node_file(environment_default_path), \
                 util.temporarily_remove_compute_node_file(environment_override_path):
             # Only the default is set. Can be overridden by the user.
-            default_policy = { 'PERF_MARGIN': 0.11 }
+            default_policy = { 'FREQ_DEFAULT': 1.6e9 }
             create_policy_file_on_compute_node(default_policy, test_name + '.default.agent.config', environment_default_path)
             self.assert_geopm_uses_policy(default_policy, test_name + '_default_no_user')
             self.assert_geopm_uses_policy(user_policy, test_name + '_default_with_user', user_policy=user_policy)
 
             # Both default and override are set. Override is always used.
-            override_policy = { 'PERF_MARGIN': 0.12 }
+            override_policy = { 'FREQ_DEFAULT': 1.7e9 }
             create_policy_file_on_compute_node(override_policy, test_name + '.override.agent.config', environment_override_path)
             self.assert_geopm_uses_policy(override_policy, test_name + '_override_and_default_no_user')
             self.assert_geopm_uses_policy(override_policy, test_name + '_override_and_default_with_user', user_policy=user_policy)
