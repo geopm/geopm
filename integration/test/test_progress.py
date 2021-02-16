3bce8221839ae072fb1fd34a2ec9ae12ca28166a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-02-16 12:09:40 -0800

    Add integration test for progress reporting
    - A couple of clean up issues with test_hint_time
      + Add to __main__ and remove unused member
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ integration/test/test_progress.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2015, 2016, 2017, 2018, 2019, 2020, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import sys
+import unittest
+import os
+import pandas
+import numpy
+import socket
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from integration.test import geopm_context
+from integration.test import util
+import geopmpy.io
+import geopmpy.hash
+from integration.experiment import machine
+import geopm_test_launcher
+
+
+class AppConf(object):
+    """Class that is used by the test launcher in place of a
+    geopmpy.io.BenchConf when running the progress benchmark.
+
+    """
+    def write(self):
+        """Called by the test launcher prior to executing the test application
+        to write any files required by the application.
+
+        """
+        pass
+
+    def get_exec_path(self):
+        """Path to benchmark filled in by template automatically.
+
+        """
+        return util.get_exec_path('test_progress')
+
+    def get_exec_args(self):
+        """Returns a list of strings representing the command line arguments
+        to pass to the test-application for the next run.  This is
+        especially useful for tests that execute the test-application
+        multiple times.
+
+        """
+        return []
+
+
+class TestIntegration_progress(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Create launcher, execute benchmark and set up class variables.
+
+        """
+        sys.stdout.write('(' + os.path.basename(__file__).split('.')[0] +
+                         '.' + cls.__name__ + ') ...')
+        cls._test_name = 'test_progress'
+        cls._report_path = '{}.report'.format(cls._test_name)
+        cls._trace_path = '{}.trace'.format(cls._test_name)
+        cls._agent_conf_path = cls._test_name + '-agent-config.json'
+        # Clear out exception record for python 2 support
+        geopmpy.error.exc_clear()
+
+        # Create machine
+        cls._machine = machine.Machine()
+        try:
+            cls._machine.load()
+            sys.stderr.write('Warning: {}: using existing file "machine.json", delete if invalid\n'.format(cls._test_name))
+        except RuntimeError:
+            cls._machine.save()
+
+        # Set the job size parameters
+        cls._num_node = 1
+        cls._num_rank = 10
+        cls._num_active_cpu = cls._machine.num_core() - 2 * cls._machine.num_package()
+        cls._thread_per_rank = int(cls._num_active_cpu / cls._num_rank)
+
+        app_conf = AppConf()
+
+        trace_signals = 'REGION_PROGRESS@cpu'
+        agent_conf = geopmpy.io.AgentConf(cls._test_name + '_agent.config')
+
+        # Create the test launcher with the above configuration
+        launcher = geopm_test_launcher.TestLauncher(app_conf=app_conf,
+                                                    agent_conf=agent_conf,
+                                                    report_path=cls._report_path,
+                                                    trace_path=cls._trace_path,
+                                                    trace_signals=trace_signals)
+        launcher.set_num_node(cls._num_node)
+        launcher.set_num_rank(cls._num_rank)
+        # Run the test application
+        launcher.run(cls._test_name)
+
+        # Output to be reused by all tests
+        cls._report = geopmpy.io.RawReport(cls._report_path)
+        cls._trace = geopmpy.io.AppOutput(cls._trace_path + '*')
+
+    def get_hash(self, function_name):
+        host = self._report.host_names()[0]
+        region_names = self._report.region_names(host)
+        full_name = [rn for rn in region_names if function_name in rn]
+        if len(full_name) != 1:
+            raise RuntimeError("Single match not found for {}".format(function_name))
+        return hex(self._report.raw_region(host, full_name[0])['hash'])
+
+    def check_progress(self, time, progress, expected_max, msg):
+        poly_out = numpy.polyfit(time,
+                                 progress,
+                                 1, full=True)
+        min_prog = min(progress)
+        max_prog = max(progress)
+        epsilon = 0.05
+        abs_epsilon = epsilon * expected_max
+        self.assertLessEqual(0.0, min_prog)
+        util.assertNear(self, 0.0, min_prog, epsilon=abs_epsilon,
+                        msg='{}: minimum value'.format(msg))
+        util.assertNear(self, expected_max, max_prog, epsilon=epsilon,
+                        msg='{}: maximum value'.format(msg))
+        # Assert that the expected deviation from the fit is is no
+        # larger than 1%.
+        rr = max_prog - min_prog
+        error = (poly_out[1][0] / len(progress)) ** 0.5 / rr
+        self.assertLess(error, 0.01, msg)
+
+    def sum_progress(self, cpu_wp, df):
+        result = numpy.zeros(len(df))
+        for cpu in cpu_wp:
+            name = 'REGION_PROGRESS-cpu-{}'.format(cpu)
+            result += df[name]
+        return result
+
+    def test_num_host(self):
+        host_names = self._report.host_names()
+        self.assertEqual(len(host_names), self._num_node)
+
+    def get_cpu_with_progress(self):
+        df = self._trace.get_trace_df()
+        result = set()
+        for cpu in range(self._machine.num_cpu()):
+            name = 'REGION_PROGRESS-cpu-{}'.format(cpu)
+            if len(df[name].unique()) > 1:
+                result.add(cpu)
+        return result
+
+    def test_num_active_cpu(self):
+        cpu_with_progress = self.get_cpu_with_progress()
+        err_msg ='CPUs with progress:\n    {}\n'.format(cpu_with_progress)
+        self.assertEqual(self._num_active_cpu, len(cpu_with_progress),
+                         msg=err_msg)
+
+    def test_linear_progress_triad(self):
+        df = self._trace.get_trace_df()
+        grouped_df = df.groupby('REGION_HASH')
+        cpu_with_progress = self.get_cpu_with_progress()
+        max_progress = 1.0 / self._thread_per_rank
+
+        triad_post_hash = self.get_hash('triad_with_post')
+        triad_post_df = grouped_df.get_group(triad_post_hash)
+        total_progress = self.sum_progress(cpu_with_progress, triad_post_df)
+        err_msg = 'Bad fit for triad total proggress'
+        self.check_progress(triad_post_df['TIME'],
+                            total_progress,
+                            self._num_rank,
+                            err_msg)
+        for cpu in cpu_with_progress:
+            name = 'REGION_PROGRESS-cpu-{}'.format(cpu)
+            err_msg = 'Bad fit for triad cpu {} proggress'.format(cpu)
+            self.check_progress(triad_post_df['TIME'],
+                                triad_post_df[name],
+                                max_progress,
+                                err_msg)
+
+    def test_linear_progress_dgemm(self):
+        df = self._trace.get_trace_df()
+        grouped_df = df.groupby('REGION_HASH')
+        cpu_with_progress = self.get_cpu_with_progress()
+        max_progress = 1.0 / self._thread_per_rank
+
+        dgemm_post_hash = self.get_hash('dgemm_with_post')
+        dgemm_post_df = grouped_df.get_group(dgemm_post_hash)
+        total_progress = self.sum_progress(cpu_with_progress, dgemm_post_df)
+        err_msg = 'Bad fit for dgemm total proggress'
+        self.check_progress(dgemm_post_df['TIME'],
+                            total_progress,
+                            self._num_rank,
+                            err_msg)
+        for cpu in cpu_with_progress:
+            name = 'REGION_PROGRESS-cpu-{}'.format(cpu)
+            err_msg = 'Bad fit for dgemm cpu {} proggress'.format(cpu)
+            self.check_progress(dgemm_post_df['TIME'],
+                                dgemm_post_df[name],
+                                max_progress,
+                                err_msg)
+
+    def test_zero_progress(self):
+        df = self._trace.get_trace_df()
+        grouped_df = df.groupby('REGION_HASH')
+        cpu_with_progress = self.get_cpu_with_progress()
+
+        triad_post_hash = self.get_hash('triad_with_post')
+        dgemm_post_hash = self.get_hash('dgemm_with_post')
+        unmarked_hash = hex(geopmpy.hash.crc32_str('GEOPM_REGION_HASH_UNMARKED'))
+        other_groups = [gg for gg in list(grouped_df.groups)
+                        if gg not in (triad_post_hash,
+                                      dgemm_post_hash,
+                                      unmarked_hash,
+                                      'NAN')]
+        for gg in other_groups:
+           for cpu in cpu_with_progress:
+               name = 'REGION_PROGRESS-cpu-{}'.format(cpu)
+               uu = grouped_df.get_group(gg)[name].dropna().unique()
+               if len(uu) != 0:
+                   err_msg = 'Expected all zero, group {} had {}'.format(gg, uu)
+                   self.assertEqual(1, len(uu), msg=err_msg)
+                   self.assertEqual(0.0, uu[0], msg=err_msg)
+
+        self.assertLessEqual(0, min(df['REGION_PROGRESS'].dropna()))
+        self.assertGreaterEqual(1.0, max(df['REGION_PROGRESS'].dropna()))
+
+    def check_overhead(self, key, epsilon):
+        for host in self._report.host_names():
+            region_names = self._report.region_names(host)
+            ref_energy = None
+            ref_time = None
+            actual_energy = None
+            actual_time = None
+            for rn in region_names:
+                if '{}_no_post'.format(key) in rn:
+                    raw_region = self._report.raw_region(host, rn)
+                    ref_energy = raw_region['package-energy (J)']
+                    ref_time = raw_region['runtime (s)']
+                elif '{}_with_post'.format(key) in rn:
+                    raw_region = self._report.raw_region(host, rn)
+                    actual_energy = raw_region['package-energy (J)']
+                    actual_time = raw_region['runtime (s)']
+            self.assertTrue(ref_energy is not None,
+                            'Reference energy not found for {}'.format(key))
+            self.assertTrue(ref_time is not None,
+                            'Reference time not found for {}'.format(key))
+            self.assertTrue(actual_energy is not None,
+                            'Actual energy not found for {}'.format(key))
+            self.assertTrue(actual_time is not None,
+                            'Actual time not found for {}'.format(key))
+            util.assertNear(self, ref_energy, actual_energy, epsilon=epsilon,
+                            msg='Energy overhead of post for {} is too high'.format(key))
+            util.assertNear(self, ref_time, actual_time, epsilon=epsilon,
+                            msg='Time overhead of post for {} is too high'.format(key))
+
+    def test_overhead_dgemm(self):
+        self.check_overhead('dgemm', 0.05)
+
+    def test_overhead_triad(self):
+        self.check_overhead('triad', 1.5)
+
+if __name__ == '__main__':
+    unittest.main()
