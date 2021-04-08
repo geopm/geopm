7b08508027345cbf5f57c6dc7eb992700fc75be6
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-04-08 13:22:03 -0700

    Get build working
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/src/MSRIOGroup.cpp
+++ service/src/MSRIOGroup.cpp
@@ -46,8 +46,7 @@
 #include "contrib/json11/json11.hpp"
 
 #include "geopm_sched.h"
-#include "geopm_hash.h"
-#include "Environment.hpp"
+#include "geopm_internal.h"
 #include "Exception.hpp"
 #include "Agg.hpp"
 #include "MSRIOImp.hpp"
@@ -912,12 +911,7 @@ namespace geopm
     {
         std::set<std::string> data_files;
         // search path for additional json files to parse
-        std::string env_plugin_path = environment().plugin_path();
         std::vector<std::string> plugin_paths {GEOPM_DEFAULT_PLUGIN_PATH};
-        if (!env_plugin_path.empty()) {
-            std::vector<std::string> dirs = string_split(env_plugin_path, ":");
-            plugin_paths.insert(plugin_paths.end(), dirs.begin(), dirs.end());
-        }
         std::vector<std::unique_ptr<MSR> > msr_arr_custom;
         for (const auto &dir : plugin_paths) {
             auto files = list_directory_files(dir);
