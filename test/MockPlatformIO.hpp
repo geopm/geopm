2cb86e5f304f91cfb7aea236b88be0975a689d64
Brad Geltz <brad.geltz@intel.com>
2021-05-03 17:47:20 -0700

    Add new API to mocks
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>
    Change-Id: I5d36d0c0edceef2e4b26d382389479806f3c9195

--- test/MockPlatformIO.hpp
+++ test/MockPlatformIO.hpp
@@ -92,6 +92,10 @@ class MockPlatformIO : public geopm::PlatformIO
                            std::string(const std::string &control_name));
         MOCK_CONST_METHOD1(signal_behavior,
                            int(const std::string &signal_name));
+        MOCK_METHOD1(save_control,
+                     void(const std::string &save_dir));
+        MOCK_METHOD1(restore_control,
+                     void(const std::string &save_dir));
 };
 
 #endif
