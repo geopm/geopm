91284d49a46a6b94cad0c448f7392a7d930ec6f0
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-05-06 07:28:42 -0700

    Add cleanup if process ends without closing session
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/setup.py
+++ service/setup.py
@@ -75,6 +75,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
 install_requires = ['cffi>=1.6.0',
                     'setuptools>=39.2.0',
                     'future>=0.17.1',
+                    'psutil',
                     'dasbus']
 
 setup(name='geopmdpy',
