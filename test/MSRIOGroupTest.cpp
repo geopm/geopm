de35c839472bc0542d9e9a386d5bf010d4fa4e06
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-05-12 15:02:22 -0700

    Get rid of checks for MSRIOGroup to throw on unsupported CPUs
    - We now load architectural MSRs on all platforms
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/MSRIOGroupTest.cpp
+++ test/MSRIOGroupTest.cpp
@@ -121,10 +121,6 @@ TEST_F(MSRIOGroupTest, supported_cpuid)
                    << std::hex << id << std::dec << ": " << ex.what();
         }
     }
-
-    // unsupported cpuid
-    GEOPM_EXPECT_THROW_MESSAGE(MSRIOGroup(*m_topo, m_msrio, 0x9999, m_num_cpu),
-                               GEOPM_ERROR_RUNTIME, "Unsupported CPUID");
 }
 
 TEST_F(MSRIOGroupTest, valid_signal_names)
