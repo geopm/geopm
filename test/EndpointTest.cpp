3a0c55544d4b463267d24fcf6cbe75555f35ccfe
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-05-17 15:57:16 -0700

    Change all EndpointImp use to shared pointers
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/EndpointTest.cpp
+++ test/EndpointTest.cpp
@@ -57,6 +57,7 @@
 #include "EndpointUser.hpp"
 #include "SharedMemory.hpp"
 
+using geopm::Endpoint;
 using geopm::EndpointImp;
 using geopm::EndpointUserImp;
 using geopm::SharedMemory;
@@ -142,9 +143,9 @@ TEST_F(EndpointTest, parse_shm_sample)
 TEST_F(EndpointTestIntegration, write_shm)
 {
     std::vector<double> values = {777, 12.3456, 2.1e9};
-    EndpointImp mio(m_shm_path, nullptr, nullptr, values.size(), 0);
-    mio.open();
-    mio.write_policy(values);
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, nullptr, nullptr, values.size(), 0);
+    mio->open();
+    mio->write_policy(values);
 
     auto smp = SharedMemory::make_unique_user(m_shm_path + "-policy", 1);
     struct geopm_endpoint_policy_shmem_s *data = (struct geopm_endpoint_policy_shmem_s *) smp->pointer();
@@ -155,19 +156,19 @@ TEST_F(EndpointTestIntegration, write_shm)
 
     values[0] = 888;
 
-    mio.write_policy(values);
+    mio->write_policy(values);
     ASSERT_LT(0u, data->count);
     std::vector<double> result2(data->values, data->values + data->count);
     EXPECT_EQ(values, result2);
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTestIntegration, write_read_policy)
 {
     std::vector<double> values = {777, 12.3456, 2.1e9};
-    EndpointImp mio(m_shm_path, nullptr, nullptr, values.size(), 0);
-    mio.open();
-    mio.write_policy(values);
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, nullptr, nullptr, values.size(), 0);
+    mio->open();
+    mio->write_policy(values);
     EndpointUserImp mios(m_shm_path, nullptr, nullptr, "myagent", 0, "", "", {});
 
     std::vector<double> result(values.size());
@@ -175,13 +176,13 @@ TEST_F(EndpointTestIntegration, write_read_policy)
     EXPECT_EQ(values, result);
 
     values[0] = 888;
-    mio.write_policy(values);
+    mio->write_policy(values);
     usleep(10);
     double age = mios.read_policy(result);
     EXPECT_EQ(values, result);
     EXPECT_LT(0.0, age);
     EXPECT_LT(age, 0.01);
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTestIntegration, write_read_sample)
@@ -189,53 +190,53 @@ TEST_F(EndpointTestIntegration, write_read_sample)
     std::vector<double> values = {777, 12.3456, 2.1e9, 2.3e9};
     std::set<std::string> hosts = {"node5", "node6", "node8"};
     std::string hostlist_path = "EndpointTestIntegration_hostlist";
-    EndpointImp mio(m_shm_path, nullptr, nullptr, 0, values.size());
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, nullptr, nullptr, 0, values.size());
+    mio->open();
     EndpointUserImp mios(m_shm_path, nullptr, nullptr, "power_balancer",
                            values.size(), "myprofile", hostlist_path, hosts);
-    EXPECT_EQ("power_balancer", mio.get_agent());
-    EXPECT_EQ("myprofile", mio.get_profile_name());
-    EXPECT_EQ(hosts, mio.get_hostnames());
+    EXPECT_EQ("power_balancer", mio->get_agent());
+    EXPECT_EQ("myprofile", mio->get_profile_name());
+    EXPECT_EQ(hosts, mio->get_hostnames());
 
     mios.write_sample(values);
     std::vector<double> result(values.size());
-    mio.read_sample(result);
+    mio->read_sample(result);
     EXPECT_EQ(values, result);
 
     values[0] = 888;
     mios.write_sample(values);
-    mio.read_sample(result);
+    mio->read_sample(result);
     EXPECT_EQ(values, result);
-    mio.close();
+    mio->close();
     unlink(hostlist_path.c_str());
 }
 
 TEST_F(EndpointTest, get_agent)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
-    EXPECT_EQ("monitor", mio.get_agent());
-    mio.close();
+    EXPECT_EQ("monitor", mio->get_agent());
+    mio->close();
 }
 
 TEST_F(EndpointTest, get_profile_name)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     strncpy(data->profile_name, "my_prof", GEOPM_ENDPOINT_PROFILE_NAME_MAX);
-    EXPECT_EQ("my_prof", mio.get_profile_name());
-    mio.close();
+    EXPECT_EQ("my_prof", mio->get_profile_name());
+    mio->close();
 }
 
 TEST_F(EndpointTest, get_hostnames)
 {
     std::set<std::string> hosts = {"node0", "node1", "node2", "node3", "node4"};
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     std::string hostlist_path = "EndpointTest_hostlist";
     std::ofstream hostlist(hostlist_path);
     for (auto host : hosts) {
@@ -244,40 +245,39 @@ TEST_F(EndpointTest, get_hostnames)
     hostlist.close();
     strncpy(data->hostlist_path, hostlist_path.c_str(), GEOPM_ENDPOINT_HOSTLIST_PATH_MAX - 1);
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
-    EXPECT_EQ(hosts, mio.get_hostnames());
-    mio.close();
+    EXPECT_EQ(hosts, mio->get_hostnames());
+    mio->close();
     unlink(hostlist_path.c_str());
 }
 
 TEST_F(EndpointTest, stop_wait_loop)
 {
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
-    mio.reset_wait_loop();
-
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
+    mio->reset_wait_loop();
     auto run_thread = std::async(std::launch::async,
-                                 &EndpointImp::wait_for_agent_attach,
-                                 &mio,
+                                 &Endpoint::wait_for_agent_attach,
+                                 mio,
                                  m_timeout);
     ASSERT_TRUE(run_thread.valid());
-    mio.stop_wait_loop();
+    mio->stop_wait_loop();
     // wait for less than timeout; should exit before time limit without throwing
     auto result = run_thread.wait_for(std::chrono::seconds(m_timeout - 1));
     EXPECT_NE(result, std::future_status::timeout);
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, attach_wait_loop_timeout_throws)
 {
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
 
     geopm_time_s before;
     geopm_time(&before);
 
     auto run_thread = std::async(std::launch::async,
-                                 &EndpointImp::wait_for_agent_attach,
-                                 &mio,
+                                 &Endpoint::wait_for_agent_attach,
+                                 mio,
                                  m_timeout);
     ASSERT_TRUE(run_thread.valid());
     // throw from our timeout should happen before longer async timeout
@@ -289,14 +289,14 @@ TEST_F(EndpointTest, attach_wait_loop_timeout_throws)
     double elapsed = geopm_time_since(&before);
     EXPECT_NEAR(m_timeout, elapsed, 0.100);
 
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, detach_wait_loop_timeout_throws)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     // simulate agent attach
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
 
@@ -304,8 +304,8 @@ TEST_F(EndpointTest, detach_wait_loop_timeout_throws)
     geopm_time(&before);
 
     auto run_thread = std::async(std::launch::async,
-                                 &EndpointImp::wait_for_agent_detach,
-                                 &mio,
+                                 &Endpoint::wait_for_agent_detach,
+                                 mio,
                                  m_timeout);
     ASSERT_TRUE(run_thread.valid());
     // throw from our timeout should happen before longer async timeout
@@ -317,18 +317,18 @@ TEST_F(EndpointTest, detach_wait_loop_timeout_throws)
     double elapsed = geopm_time_since(&before);
     EXPECT_NEAR(m_timeout, elapsed, 0.100);
 
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, wait_stops_when_agent_attaches)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
 
     auto run_thread = std::async(std::launch::async,
-                                 &EndpointImp::wait_for_agent_attach,
-                                 &mio,
+                                 &Endpoint::wait_for_agent_attach,
+                                 mio,
                                  m_timeout);
     ASSERT_TRUE(run_thread.valid());
     // simulate agent attach
@@ -336,41 +336,41 @@ TEST_F(EndpointTest, wait_stops_when_agent_attaches)
     // wait for less than timeout; should exit before time limit without throwing
     auto result = run_thread.wait_for(std::chrono::seconds(m_timeout - 1));
     EXPECT_NE(result, std::future_status::timeout);
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, wait_attach_timeout_0)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
 
     // if agent is not already attached, throw immediately
-    GEOPM_EXPECT_THROW_MESSAGE(mio.wait_for_agent_attach(0), GEOPM_ERROR_RUNTIME, "timed out");
+    GEOPM_EXPECT_THROW_MESSAGE(mio->wait_for_agent_attach(0), GEOPM_ERROR_RUNTIME, "timed out");
 
     // simulate agent attach
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
     // wait for less than timeout; should exit before time limit without throwing
 
     // once agent is attached, timeout of 0 should succeed
-    mio.wait_for_agent_attach(0);
-    EXPECT_EQ("monitor", mio.get_agent());
+    mio->wait_for_agent_attach(0);
+    EXPECT_EQ("monitor", mio->get_agent());
 
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, wait_stops_when_agent_detaches)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     // simulate agent attach
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
-    ASSERT_EQ("monitor", mio.get_agent());
+    ASSERT_EQ("monitor", mio->get_agent());
 
     auto run_thread = std::async(std::launch::async,
-                                 &EndpointImp::wait_for_agent_detach,
-                                 &mio,
+                                 &Endpoint::wait_for_agent_detach,
+                                 mio,
                                  m_timeout);
 
     ASSERT_TRUE(run_thread.valid());
@@ -380,24 +380,24 @@ TEST_F(EndpointTest, wait_stops_when_agent_detaches)
     // wait for less than timeout; should exit before time limit without throwing
     auto result = run_thread.wait_for(std::chrono::seconds(m_timeout - 1));
     EXPECT_NE(result, std::future_status::timeout);
-    mio.close();
+    mio->close();
 }
 
 TEST_F(EndpointTest, wait_detach_timeout_0)
 {
     struct geopm_endpoint_sample_shmem_s *data = (struct geopm_endpoint_sample_shmem_s *) m_sample_shmem->pointer();
-    EndpointImp mio(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
-    mio.open();
+    std::shared_ptr<Endpoint> mio = std::make_shared<EndpointImp>(m_shm_path, m_policy_shmem, m_sample_shmem, 0, 0);
+    mio->open();
     // simulate agent attach
     strncpy(data->agent, "monitor", GEOPM_ENDPOINT_AGENT_NAME_MAX);
 
     // if agent is still attached, throw immediately
-    GEOPM_EXPECT_THROW_MESSAGE(mio.wait_for_agent_detach(0), GEOPM_ERROR_RUNTIME, "timed out");
+    GEOPM_EXPECT_THROW_MESSAGE(mio->wait_for_agent_detach(0), GEOPM_ERROR_RUNTIME, "timed out");
 
     // simulate agent detach
     strncpy(data->agent, "", GEOPM_ENDPOINT_AGENT_NAME_MAX);
     // once agent is detached, timeout of 0 should succeed
-    mio.wait_for_agent_detach(0);
-    EXPECT_EQ("", mio.get_agent());
-    mio.close();
+    mio->wait_for_agent_detach(0);
+    EXPECT_EQ("", mio->get_agent());
+    mio->close();
 }
