707e0e72f6088edb1f0eb87f9f8df8d051a41243
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-07-12 12:30:47 -0700

    Add curly braces to variable expansion in unit test bash scripts
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/geopm_test.sh
+++ test/geopm_test.sh
@@ -37,24 +37,24 @@ unset GEOPM_TRACE
 
 test_name=$(basename $0)
 test_dir=$(dirname $0)
-obj_dir=$(readlink -f $test_dir/../..)
-lib_path=$obj_dir/.libs
-exec_path=$obj_dir/test/.libs
+obj_dir=$(readlink -f ${test_dir}/../..)
+lib_path=${obj_dir}/.libs
+exec_path=${obj_dir}/test/.libs
 real_path=$(readlink -f $0)
-top_dir=$(readlink -f $real_path/..)
-xml_dir=$test_dir
+top_dir=$(readlink -f ${real_path}/..)
+xml_dir=${test_dir}
 run_test=true
 
 
-export LD_LIBRARY_PATH=$lib_path:$obj_dir/openmp/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${lib_path}:${obj_dir}/openmp/lib:${LD_LIBRARY_PATH}
 
-if [[ $GTEST_XML_DIR ]]; then
-    xml_dir=$GTEST_XML_DIR
+if [[ ${GTEST_XML_DIR} ]]; then
+    xml_dir=${GTEST_XML_DIR}
 fi
 err=0
 
 # Check for crc32 intrinsic support before running ProfileTable tests
-if [[ $test_name =~ ^ProfileTable ]]; then
+if [[ ${test_name} =~ ^ProfileTable ]]; then
     if  ! ./examples/geopm_platform_supported crc32; then
         echo "Warning: _mm_crc32_u64 intrisic not supported."
         run_test=false
@@ -62,32 +62,32 @@ if [[ $test_name =~ ^ProfileTable ]]; then
 fi
 
 # Skipped on Mac because the implementation is lax about invalid shmem construction
-if [[ $test_name == SharedMemoryTest.invalid_construction ]]; then
+if [[ ${test_name} == SharedMemoryTest.invalid_construction ]]; then
     if  [[ $(uname) == Darwin ]]; then
         run_test=false
     fi
 fi
 
-if [ "$run_test" == "true" ]; then
+if [ "${run_test}" == "true" ]; then
     exec_name=geopm_test
-    if [[ $test_name =~ ^MPIInterface ]]; then
+    if [[ ${test_name} =~ ^MPIInterface ]]; then
         exec_name=geopm_mpi_test_api
     fi
-    log_file=$test_dir/$test_name.log
-    $exec_path/$exec_name \
-        --gtest_filter=$test_name \
-        --gtest_output=xml:$xml_dir/$test_name.xml >& $log_file
+    log_file=${test_dir}/${test_name}.log
+    ${exec_path}/${exec_name} \
+        --gtest_filter=${test_name} \
+        --gtest_output=xml:${xml_dir}/${test_name}.xml >& ${log_file}
     err=$?
 
     # Parse output log to see if the test actually ran.
-    if (grep -Fq "[==========] Running 0 tests from 0 test cases." $log_file); then
-        echo "ERROR: Test $test_name does not exist!"
+    if (grep -Fq "[==========] Running 0 tests from 0 test cases." ${log_file}); then
+        echo "ERROR: Test ${test_name} does not exist!"
         err=1
     fi
 else
-    echo "SKIP: $test_name"
+    echo "SKIP: ${test_name}"
 fi
 
 
 
-exit $err
+exit ${err}
