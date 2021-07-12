76e5e55fc78076a504ccdecc3c9b480f9ee266bc
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-07-12 12:30:47 -0700

    Add curly braces to variable expansion in unit test bash scripts
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- scripts/test/geopmpy_test.sh
+++ scripts/test/geopmpy_test.sh
@@ -32,17 +32,17 @@
 
 test_name=$(basename $0)
 test_dir=$(dirname $0)
-obj_dir=$(readlink -f $test_dir/../../..)
-lib_path=$obj_dir/.libs
+obj_dir=$(readlink -f ${test_dir}/../../..)
+lib_path=${obj_dir}/.libs
 real_path=$(dirname $(readlink -f $0))
-top_dir=$(readlink -f $real_path/../..)
+top_dir=$(readlink -f ${real_path}/../..)
 
-test_class=$(echo $test_name | awk -F. '{print $1}')
-scripts_dir=$top_dir/scripts
-lib_dir=$obj_dir/.libs
-export PYTHONPATH=$scripts_dir:$PYTHONPATH
-export LD_LIBRARY_PATH=$lib_dir:$LD_LIBRARY_PATH
+test_class=$(echo ${test_name} | awk -F. '{print $1}')
+scripts_dir=${top_dir}/scripts
+lib_dir=${obj_dir}/.libs
+export PYTHONPATH=${scripts_dir}:${PYTHONPATH}
+export LD_LIBRARY_PATH=${lib_dir}:${LD_LIBRARY_PATH}
 
 # PYTHON is expected to be set when running make check. However, the default
 # value may be used when invoking this script directly from the command line.
-"${PYTHON:-python3}" $scripts_dir/test/$test_class.py --verbose $test_name >& $test_dir/$test_name.log
+"${PYTHON:-python3}" ${scripts_dir}/test/${test_class}.py --verbose ${test_name} >& ${test_dir}/${test_name}.log
