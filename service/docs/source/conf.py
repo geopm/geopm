3664de8f020d12e7634bbc64f72a5b9cbd64a6ad
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-07-19 16:37:43 -0700

    Clean up GEOPM icon
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/docs/source/conf.py
+++ service/docs/source/conf.py
@@ -82,3 +82,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = 'images/geopm-logo-clear.png'
+logo_only = True
