a112db8362312cc9a6878ec10071897a5dc528eb
Brad Geltz <brad.geltz@intel.com>
2021-08-06 11:22:50 -0700

    Add unit tests for service.py
    - Resolves #1713.
    - Add TestTopoService.
    - Minor documentation updates in the implementation.
    - Other minor updates to support functionality outlined in
      the docstrings.
    - Exception text enhancements.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- service/geopmdpy_test/Makefile.mk
+++ service/geopmdpy_test/Makefile.mk
@@ -37,9 +37,41 @@ EXTRA_DIST += geopmdpy_test/__main__.py \
               geopmdpy_test/TestTopo.py \
               # end
 
-GEOPMDPY_TESTS = geopmdpy_test/pytest_links/TestPlatformService.test_get_group_access_empty \
+GEOPMDPY_TESTS = geopmdpy_test/pytest_links/TestPlatformService.test__read_allowed_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test__check_client \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_close_session_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_close_session_read \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_close_session_write \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_config_parser \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_control_info \
                  geopmdpy_test/pytest_links/TestPlatformService.test_get_group_access_default \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_group_access_empty \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_group_access_invalid \
                  geopmdpy_test/pytest_links/TestPlatformService.test_get_group_access_named \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_signal_info \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_access_default \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_access_empty \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_access_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_access_root \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_access_valid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_groups_current \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_user_groups_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_lock_control \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_open_session \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_open_session_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_read_signal \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_read_signal_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_set_group_access_empty \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_set_group_access_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_set_group_access_named \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_start_batch \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_start_batch_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_stop_batch \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_stop_batch_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_unlock_control \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_write_control \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_write_control_invalid \
+                 geopmdpy_test/pytest_links/TestPlatformService.test_get_cache \
                  geopmdpy_test/pytest_links/TestDBusXML.test_xml_parse_no_doc \
                  geopmdpy_test/pytest_links/TestDBusXML.test_xml_parse_with_doc \
                  geopmdpy_test/pytest_links/TestError.test_error_message \
