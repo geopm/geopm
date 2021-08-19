a16273a3a66351edb2798343e33cf538e9917ec4
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-08-19 16:01:42 -0700

    Add doc string to ServiceProxy interface.
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/ServiceProxy.hpp
+++ src/ServiceProxy.hpp
@@ -41,45 +41,131 @@ struct sd_bus;
 struct sd_bus_message;
 struct geopm_request_s;
 
+    .
 namespace geopm
 {
+    /// @brief Information pertaining to a particular signal supported
+    /// by an PlatformIO
     struct signal_info_s {
+        /// @brief Name of the signal
         std::string name;
+        /// @brief Description of the signal
         std::string description;
+        /// @brief Topology domain that supports the signal.  One of
+        ///        the geopm_domain_e values defined in geopm_topo.h.
         int domain;
+        /// @brief The signal aggregation type.  One of the the
+        ///        geopm::Agg::m_type_e values defined in Agg.hpp
         int aggregation;
+        /// @brief The format method to convert a signal to a string.
+        ///        One of the geopm::string_format_e values defined in
+        ///        Helper.hpp.
         int string_format;
+        /// @brief The signal behavior type.  One of the
+        ///        geopm::IOGroup::m_signal_behavior_e values defined
+        ///        in IOGroup.hpp.
         int behavior;
     };
 
+    /// @brief Information pertaining to a particular control
+    ///        supported by an PlatformIO
     struct control_info_s {
+        /// @brief Name of the control
         std::string name;
+        /// @brief Description of the control
         std::string description;
+        /// @brief Topology domain that supports the signal.  One of
+        ///        the geopm_domain_e values defined in geopm_topo.h.
         int domain;
     };
 
     /// @brief Proxy object for the io.github.geopm D-Bus interface
-    /// used to implement the ServiceIOGroup
+    ///        used to implement the ServiceIOGroup
     class ServiceProxy
     {
         public:
+            /// @brief ServiceProxy constructor
             ServiceProxy() = default;
+            /// @brief ServiceProxy destructor
             virtual ~ServiceProxy() = default;
+            /// @brief Create a unique pointer to a ServiceProxy object
+            /// @return A unique_ptr to a newly created ServiceProxy object
             static std::unique_ptr<ServiceProxy> make_unique(void);
-            virtual void platform_get_user_access(std::vector<std::string> &signal_names,
-                                                  std::vector<std::string> &control_names) = 0;
-            virtual std::vector<signal_info_s> platform_get_signal_info(const std::vector<std::string> &signal_names) = 0;
-            virtual std::vector<control_info_s> platform_get_control_info(const std::vector<std::string> &control_names) = 0;
+            /// @brief Calls the PlatformGetUserAccess API defined in
+            ///        the io.github.geopm D-Bus namespace.
+            /// @param signal_names [out] Vector of strings containing
+            ///                     all signals that the calling
+            ///                     process has access to.
+            /// @param control_names [out] Vector of strings
+            ///                      containing all controls that the
+            ///                      calling process has access to.
+            virtual void platform_get_user_access(
+                std::vector<std::string> &signal_names,
+                std::vector<std::string> &control_names) = 0;
+            /// @brief Calls the PlatformGetSignalInfo API defined in
+            ///        the io.github.geopm D-Bus namespace.
+            /// @param signal_names [in] Vector of strings containing
+            ///                     all signal names to query.
+            /// @return Vector of structures describing the queried
+            ///         signals
+            virtual std::vector<signal_info_s> platform_get_signal_info(
+                const std::vector<std::string> &signal_names) = 0;
+            /// @brief Calls the PlatformGetControlInfo API defined in
+            ///        the io.github.geopm D-Bus namespace.
+            /// @param control_names [in] Vector of strings containing
+            ///                      all control names to query.
+            /// @return Vector of structures describing the queried
+            ///         controls
+            virtual std::vector<control_info_s> platform_get_control_info(
+                const std::vector<std::string> &control_names) = 0;
+            /// @brief Calls the PlatformOpenSession API defined in
+            ///        the io.github.geopm D-Bus namespace.
             virtual void platform_open_session(void) = 0;
+            /// @brief Calls the PlatformCloseSession API defined in
+            ///        the io.github.geopm D-Bus namespace.
             virtual void platform_close_session(void) = 0;
-            virtual void platform_start_batch(const std::vector<struct geopm_request_s> &signal_config,
-                                              const std::vector<struct geopm_request_s> &control_config,
-                                              int &server_pid,
-                                              std::string &server_key) = 0;
+            /// @brief Calls the PlatformStartBatch API defined in the
+            ///        io.github.geopm D-Bus namespace.
+            /// @param signal_config [in] Vector of signal requests
+            ///                      that will be supported by the
+            ///                      batch server that is created.
+            /// @param signal_config [in] Vector of control requests
+            ///                      that will be supported by the
+            ///                      batch server that is created.
+            /// @param server_pid [out] Linux PID of the server
+            ///                   process created.
+            /// @param server_key [out] Unique key used to connect to
+            ///                   the created server.
+            virtual void platform_start_batch(
+                const std::vector<struct geopm_request_s> &signal_config,
+                const std::vector<struct geopm_request_s> &control_config,
+                int &server_pid,
+                std::string &server_key) = 0;
+            /// @brief Calls the PlatformStopBatch API defined in the
+            ///        io.github.geopm D-Bus namespace.
+            /// @param server_pid [in] The Linux PID of the batch
+            ///                   server to stop.
             virtual void platform_stop_batch(int server_pid) = 0;
+            /// @brief Calls the PlatformReadSignal API defined in the
+            ///        io.github.geopm D-Bus namespace.
+            /// @param signal_name [in] Name of the signal to read
+            /// @param domain [in] Topology domain to read the signal
+            ///               from.  One of the geopm_domain_e values
+            ///               defined in geopm_topo.h.
+            /// @param domain_idx [in] Index of the domain to read the
+            ///                   signal from.
             virtual double platform_read_signal(const std::string &signal_name,
                                                 int domain,
                                                 int domain_idx) = 0;
+            /// @brief Calls the PlatformWriteControl API defined in the
+            ///        io.github.geopm D-Bus namespace.
+            /// @param control_name [in] Name of the control to write
+            /// @param domain [in] Topology domain to write to.  One
+            ///               of the geopm_domain_e values defined in
+            ///               geopm_topo.h.
+            /// @param domain_idx [in] Index of the domain to write
+            ///                   to.
+            /// @param setting [in] Value of the control to write.
             virtual void platform_write_control(const std::string &control_name,
                                                 int domain,
                                                 int domain_idx,
