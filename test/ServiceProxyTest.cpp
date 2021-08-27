8593782f1740626f40cde244f613ddce83f389f5
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-08-27 16:11:22 -0700

    Add test for platform_get_user_access
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test/ServiceProxyTest.cpp
+++ test/ServiceProxyTest.cpp
@@ -30,7 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-include <memory>
+#include <memory>
 
 #include "ServiceProxy.hpp"
 #include "MockSDBus.hpp"
@@ -41,6 +41,8 @@ using geopm::control_info_s;
 using geopm::ServiceProxyImp;
 using geopm::SDBus;
 using geopm::SDBusMessage;
+using testing::Return;
+using testing::_;
 
 class ServiceProxyTest : public ::testing::Test
 {
@@ -62,5 +64,35 @@ void ServiceProxyTest::SetUp()
 
 TEST_F(ServiceProxyTest, platform_get_user_access)
 {
+    int num_container = 3; // One struct and two arrays of strings
+    EXPECT_CALL(*m_bus, call_method("PlatformGetUserAccess"))
+        .WillOnce(Return(m_bus_reply));
+    EXPECT_CALL(*m_bus_reply, enter_container(_,_))
+        .Times(num_container);
+    EXPECT_CALL(*m_bus_reply, exit_container())
+        .Times(num_container);
+    EXPECT_CALL(*m_bus_reply, read_string())
+        .WillOnce(Return("instructions"))
+        .WillOnce(Return("misses"))
+        .WillOnce(Return("ops"))
+        .WillOnce(Return(""))
+        .WillOnce(Return("frequency"))
+        .WillOnce(Return("power"))
+        .WillOnce(Return(""));
+    EXPECT_CALL(*m_bus_reply, was_success())
+        .WillOnce(Return(true))
+        .WillOnce(Return(true))
+        .WillOnce(Return(true))
+        .WillOnce(Return(false))
+        .WillOnce(Return(true))
+        .WillOnce(Return(true))
+        .WillOnce(Return(false));
+    std::vector<std::string> signal_actual;
+    std::vector<std::string> control_actual;
+    m_proxy->platform_get_user_access(signal_actual, control_actual);
+    std::vector<std::string> signal_expect = {"instructions", "misses", "ops"};
+    std::vector<std::string> control_expect = {"frequency", "power"};
+    EXPECT_EQ(signal_expect, signal_actual);
+    EXPECT_EQ(control_expect, control_actual);
 
 }
