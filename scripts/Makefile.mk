2c34d5519a0cd88f7ec6f7d3c868ef08e8e401a4
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-09-13 13:48:15 -0700

    Add a build script that runs both builds
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- scripts/Makefile.mk
+++ scripts/Makefile.mk
@@ -58,7 +58,6 @@ EXTRA_DIST += scripts/MANIFEST.in \
               scripts/test/TestPolicyStore.py \
               scripts/test/TestPolicyStoreIntegration.py \
               scripts/test/__init__.py \
-              scripts/test/geopmpy_test.sh \
               scripts/test/check_python3_compatibility.sh \
               # end
 
