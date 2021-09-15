4b7ece9c39f2945a9ee386832c0b5af77c3d945b
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-09-15 11:47:43 -0700

    Change sphinx html_logo to a URL
    - This enables us to cleanly avoid putting binary files into our source tree.
    - Avoids build requirement to create symbolic link to web page repo.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/docs/source/conf.py
+++ service/docs/source/conf.py
@@ -81,6 +81,5 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_logo = 'images/geopm-logo-clear.png'
+html_logo = 'https://geopm.github.io/images/geopm-logo-clear.png'
 logo_only = True
