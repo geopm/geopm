491d66439ba1294cc5c525565cb225b118d8037e
Brad Geltz <brad.geltz@intel.com>
2021-09-22 15:20:54 -0700

    Add custom plugin handling back
    - Was removed from plugin_load and MSRIOGroup.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- service/src/geopm_plugin.cpp
+++ service/src/geopm_plugin.cpp
@@ -31,10 +31,12 @@
  */
 
 #include <dlfcn.h>
+#include <stdlib.h>
 
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <string>
 
 #include "geopm_error.h"
 #include "geopm_plugin.hpp"
@@ -48,10 +50,17 @@ namespace geopm
 {
     void plugin_load(const std::string &plugin_prefix)
     {
+        std::string env_plugin_path_str(geopm::get_env("GEOPM_PLUGIN_PATH"));
         std::vector<std::string> plugin_paths {GEOPM_DEFAULT_PLUGIN_PATH};
         std::string so_suffix = ".so." GEOPM_ABI_VERSION;
         std::replace(so_suffix.begin(), so_suffix.end(), ':', '.');
 
+        if (!env_plugin_path_str.empty()) {
+            // load paths in reverse order from environment variable list
+            auto user_paths = geopm::string_split(env_plugin_path_str, ":");
+            std::reverse(user_paths.begin(), user_paths.end());
+            plugin_paths.insert(plugin_paths.end(), user_paths.begin(), user_paths.end());
+        }
         std::vector<std::string> plugins;
         for (const auto &path : plugin_paths) {
             std::vector<std::string> files = geopm::list_directory_files(path);
