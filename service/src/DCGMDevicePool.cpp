bf54499fb1b5f392e8631ca1fb58a39abca2010a
lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>
2021-10-01 13:44:20 -0700

    Added DCGMDevicePool
    Signed-off-by: lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>

---
+++ service/src/DCGMDevicePool.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2015 - 2021, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include <cmath>
+
+#include <iostream>
+#include <string>
+
+#include "geopm/Exception.hpp"
+#include "DCGMDevicePoolImp.hpp"
+
+namespace geopm
+{
+
+    DCGMDevicePool &dcgm_device_pool()
+    {
+        static DCGMDevicePoolImp instance;
+        return instance;
+    }
+
+    DCGMDevicePoolImp::DCGMDevicePoolImp()
+        : m_update_freq(1000)    // 1 millisecond
+        , m_max_keep_age(1.0)    // 1 second
+        , m_max_keep_sample(100) // 100 samples
+    {
+        // Guarantee geopm_dcgm_field_ids_e order is used
+        m_dcgm_field_ids[M_SM_ACTIVE] = DCGM_FI_PROF_SM_ACTIVE;
+        m_dcgm_field_ids[M_SM_OCCUPANCY] = DCGM_FI_PROF_SM_OCCUPANCY;
+        m_dcgm_field_ids[M_DRAM_ACTIVE] = DCGM_FI_PROF_DRAM_ACTIVE;
+        m_dcgm_field_ids[M_PCIE_TX_BYTES] = DCGM_FI_PROF_PCIE_TX_BYTES;
+        m_dcgm_field_ids[M_PCIE_RX_BYTES] = DCGM_FI_PROF_PCIE_RX_BYTES;
+
+        dcgmReturn_t result;
+
+        //Initialize DCGM
+        result = dcgmInit();
+        check_dcgm_result(result, "Error Initializing DCGM.");
+
+        // We are assuming a local version of DCGM.  This could transition to a
+        // dcgmStartEmbedded at a later date.
+        char *host_ip = "127.0.0.1";
+        result = dcgmConnect(host_ip, &m_dcgm_handle);
+        check_dcgm_result(result, "Error connecting to standalone DCGM instance");
+
+        //Check all devices are DCGM enabled
+        unsigned int dcgm_dev_id_list[DCGM_MAX_NUM_DEVICES];
+        result = dcgmGetAllSupportedDevices(m_dcgm_handle, dcgm_dev_id_list, &m_dcgm_dev_count);
+        check_dcgm_result(result, "Error fetching DCGM supported devices.");
+
+        for(int i = 0; i < m_dcgm_dev_count; ++i) {
+            std::vector<dcgmFieldValue_v1> field_values(sizeof(m_dcgm_field_ids)/sizeof(m_dcgm_field_ids[0]));
+            m_dcgm_field_values.push_back(field_values);;
+        }
+
+        //Setup Field Group
+        result = dcgmFieldGroupCreate(m_dcgm_handle, sizeof(m_dcgm_field_ids)/sizeof(m_dcgm_field_ids[0]), &m_dcgm_field_ids[0],
+                                      (char *)"geopm_fields", &m_field_group_id);
+        check_dcgm_result(result, "Error creating DCGM geopm_fields group.");
+
+        // Note: Currently we are using dcgmWatchFields, but may transition to using
+        //       dcgmProfWatchFields at a later date
+        result = dcgmWatchFields(m_dcgm_handle, DCGM_GROUP_ALL_GPUS, m_field_group_id, m_update_freq,
+                                 m_max_keep_age, m_max_keep_sample);
+
+        check_dcgm_result(result, "Error setting default watch field configuration.");
+    }
+
+    DCGMDevicePoolImp::~DCGMDevicePoolImp()
+    {
+        //Shutdown DCGM
+        dcgmStatusDestroy(NULL);
+        dcgmGroupDestroy(m_dcgm_handle, DCGM_GROUP_ALL_GPUS);
+    }
+
+    void DCGMDevicePoolImp::check_dcgm_result(const dcgmReturn_t result, const std::string error)
+    {
+        if (result != DCGM_ST_OK) {
+            throw Exception("DCGMDevicePool::" + std::string(__func__) + ": "
+                            + error + ": " + errorString(result),
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+    }
+
+    int DCGMDevicePoolImp::dcgm_device() const {
+        return m_dcgm_dev_count;
+    }
+
+    double DCGMDevicePoolImp::field_value(int accel_idx, int geopm_field_id) const {
+        double result;
+        result = m_dcgm_field_values.at(accel_idx).at(geopm_field_id).value.dbl;
+        return result;
+    }
+
+    void DCGMDevicePoolImp::update_field_value(int accel_idx)
+    {
+        dcgmReturn_t result;
+        result = dcgmGetLatestValuesForFields(m_dcgm_handle, accel_idx,
+                        &m_dcgm_field_ids[0], sizeof(m_dcgm_field_ids)/sizeof(m_dcgm_field_ids[0]),
+                        &(m_dcgm_field_values.at(accel_idx))[0]);
+        check_dcgm_result(result, "Error getting latest values for fields in read_batch");
+    }
+
+    void DCGMDevicePoolImp::field_update_rate(int accel_idx, int field_update_rate)
+    {
+        dcgmReturn_t result;
+        m_update_freq = field_update_rate;
+        result = dcgmWatchFields(m_dcgm_handle, DCGM_GROUP_ALL_GPUS, m_field_group_id, m_update_freq,
+                                 m_max_keep_age, m_max_keep_sample);
+        check_dcgm_result(result, "Error setting Field Update Rate");
+    }
+
+    void DCGMDevicePoolImp::max_storage_time(int accel_idx, int max_storage_time)
+    {
+        dcgmReturn_t result;
+        m_max_keep_age = max_storage_time;
+        result = dcgmWatchFields(m_dcgm_handle, DCGM_GROUP_ALL_GPUS, m_field_group_id, m_update_freq,
+                                 m_max_keep_age, m_max_keep_sample);
+        check_dcgm_result(result, "Error setting Max Storage Time");
+    }
+
+    void DCGMDevicePoolImp::max_samples(int accel_idx, int max_samples)
+    {
+        dcgmReturn_t result;
+        m_max_keep_sample = max_samples;
+        result = dcgmWatchFields(m_dcgm_handle, DCGM_GROUP_ALL_GPUS, m_field_group_id, m_update_freq,
+                                 m_max_keep_age, m_max_keep_sample);
+        check_dcgm_result(result, "Error setting Max Samples");
+    }
+}
