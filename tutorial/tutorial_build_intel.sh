40d24bf16e2ece85c40ecf205deefe942590f19d
Brad Geltz <brad.geltz@intel.com>
2021-10-06 13:37:57 -0700

    Move compiler overrides out of make lines
    - Avoids parameter expansion issues when CC is
      set to 'cc -dynamic'.
    
    Change-Id: I79c190609af5b087dd809ded312a60a8a36c5daa
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- tutorial/tutorial_build_intel.sh
+++ tutorial/tutorial_build_intel.sh
@@ -38,8 +38,14 @@ if [ ! "$OMP_FLAGS" ]; then
     OMP_FLAGS="-qopenmp"
 fi
 
+export CC=${CC:-icc}
+export CXX=${CXX:-icpc}
+export MPICC=${MPICC:-mpiicc}
+export MPICXX=${MPICXX:-mpiicpc}
+export MPIFC=${MPIFC:-mpiifort}
+export MPIF77=${MPIF77:-mpiifort}
+
 make \
-CC=${CC:-icc} CXX=${CXX:-icpc} MPICC=${MPICC:-mpiicc} MPICXX=${MPICXX:-mpiicpc} MPIFC=${MPIFC:-mpiifort} MPIF77=${MPIF77:-mpiifort} \
 CFLAGS="$GEOPM_CFLAGS $OMP_FLAGS -DTUTORIAL_ENABLE_MKL -D_GNU_SOURCE -std=c99 -xAVX $CFLAGS" \
 CXXFLAGS="$GEOPM_CFLAGS $OMP_FLAGS -DTUTORIAL_ENABLE_MKL -D_GNU_SOURCE -std=c++11 -xAVX $CXXFLAGS" \
 LDFLAGS="$GEOPM_LDFLAGS $OMP_FLAGS -lm -lrt -mkl -xAVX $LDFLAGS"
