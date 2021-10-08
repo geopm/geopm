290afa2dadbd1be67ee917ccef9fe94bb7499bcd
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-10-07 17:01:57 -0700

    Add pyunit runner for service bash integrationt tests
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ service/integration/test/TestBashExamples.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+#  Copyright (c) 2015 - 2021, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import os
+import sys
+import unittest
+import subprocess
+
+class TestBashExamples(unittest.TestCase):
+    def setUp(self):
+        script_path = os.path.realpath(__file__)
+        script_dir = os.path.dirname(script_path)
+        all_files = os.listdir(script_dir)
+        self.bash_tests = [os.path.join(script_dir, fn) for fn in all_files
+                           if (fn.startswith('test_') and fn.endswith('.sh'))]
+
+    def test_all(self):
+        for script in self.bash_tests:
+            with self.subTest(bash_example=os.path.basename(script)):
+                pid = subprocess.Popen([script],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+                out, err = pid.communicate()
+                output = f'\n\nSTDOUT:\n{out.decode("utf-8")}\n\nSTDERR:\n{err.decode("utf-8")}'
+                if pid.returncode != 0:
+                    self.assertEqual(0, pid.returncode, output)
+                else:
+                    sys.stdout.write(output)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
