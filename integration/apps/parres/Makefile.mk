a2ff7dda0085e613ab2eb9d969bfb48b49839c52
slcoumer <sari.coumeri@intel.com>
2021-10-19 07:53:22 -0700

    - Updated dgemm and nstream to have a FOM  - Added comment to parres.experiment.setup that commands require root access  - Updated nstream to the cuda version which has better performance than the cublas version
    Signed-off-by: Sari Coumeri <sari.coumeri@intel.com>

--- integration/apps/parres/Makefile.mk
+++ integration/apps/parres/Makefile.mk
@@ -29,7 +29,7 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-EXTRA_DIST += integration/apps/parres/0001-Added-nstream-cublas-mpi-init-finalize-for-GEOPM.patch \
+EXTRA_DIST += integration/apps/parres/0001-Added-nstream-mpi-cuda-init-finalize-for-GEOPM.patch \
               integration/apps/parres/__init__.py \
               integration/apps/parres/build.sh \
               integration/apps/parres/README.md \
