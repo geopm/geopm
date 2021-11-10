66f3ee05a541df616cf3335118509e9bfbc917f2
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-11-10 12:15:38 -0800

    Some clean up of unit tests for BatchServerTest
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/test/BatchServerTest.cpp
+++ service/test/BatchServerTest.cpp
@@ -40,6 +40,8 @@
 #include "MockSharedMemory.hpp"
 #include "geopm/Helper.hpp"
 
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -60,9 +62,6 @@ class BatchServerTest : public ::testing::Test
 {
     protected:
         void SetUp();
-        void SetUp_full_server();
-        void SetUp_only_signals();
-        void SetUp_only_controls();
         void TearDown();
         std::shared_ptr<MockPlatformIO> m_pio_ptr;
         std::shared_ptr<MockBatchStatus> m_batch_status;
@@ -74,6 +73,9 @@ class BatchServerTest : public ::testing::Test
         std::vector<geopm_request_s> m_signal_config;
         std::vector<geopm_request_s> m_control_config;
         std::shared_ptr<BatchServerImp> m_batch_server;
+        std::shared_ptr<BatchServerImp> m_batch_server_signals;
+        std::shared_ptr<BatchServerImp> m_batch_server_controls;
+        std::shared_ptr<BatchServerImp> m_batch_server_pid;
 
 };
 
@@ -81,13 +83,9 @@ void BatchServerTest::SetUp()
 {
     m_pio_ptr = std::make_shared<MockPlatformIO>();
     m_batch_status = std::make_shared<MockBatchStatus>();
-    m_control_shmem = std::make_shared<MockSharedMemory>(sizeof(double));
+    m_posix_signal = std::make_shared<MockPOSIXSignal>();
     m_server_pid = 1234;
     m_client_pid = 4321;
-}
-
-void BatchServerTest::SetUp_full_server()
-{
     m_signal_shmem = std::make_shared<MockSharedMemory>(2 * sizeof(double));
     m_control_shmem = std::make_shared<MockSharedMemory>(sizeof(double));
     m_signal_config = {geopm_request_s {1, 0, "CPU_FREQUENCY"},
@@ -102,40 +100,38 @@ void BatchServerTest::SetUp_full_server()
                                                       m_signal_shmem,
                                                       m_control_shmem,
                                                       m_server_pid);
+
+    m_batch_server_signals = std::make_shared<BatchServerImp>(m_client_pid,
+                                                              m_signal_config,
+                                                              std::vector<geopm_request_s>{},
+                                                              *m_pio_ptr,
+                                                              m_batch_status,
+                                                              m_posix_signal,
+                                                              m_signal_shmem,
+                                                              nullptr,
+                                                              m_server_pid);
+
+    m_batch_server_controls = std::make_shared<BatchServerImp>(m_client_pid,
+                                                               std::vector<geopm_request_s>{},
+                                                               m_control_config,
+                                                               *m_pio_ptr,
+                                                               m_batch_status,
+                                                               m_posix_signal,
+                                                               nullptr,
+                                                               m_control_shmem,
+                                                               m_server_pid);
+
+    m_batch_server_pid = std::make_shared<BatchServerImp>(getpid(),
+                                                          m_signal_config,
+                                                          m_control_config,
+                                                          *m_pio_ptr,
+                                                          m_batch_status,
+                                                          m_posix_signal,
+                                                          m_signal_shmem,
+                                                          m_control_shmem,
+                                                          m_server_pid);
 }
 
-void BatchServerTest::SetUp_only_signals()
-{
-    m_signal_shmem = std::make_shared<MockSharedMemory>(2 * sizeof(double));
-    m_control_shmem = std::make_shared<MockSharedMemory>(0);
-    m_signal_config = {geopm_request_s {1, 0, "CPU_FREQUENCY"},
-                       geopm_request_s {2, 1, "TEMPERATURE"}};
-    m_control_config = {};
-    m_batch_server = std::make_shared<BatchServerImp>(m_client_pid,
-                                                      m_signal_config,
-                                                      m_control_config,
-                                                      *m_pio_ptr,
-                                                      m_batch_status,
-                                                      m_signal_shmem,
-                                                      m_control_shmem,
-                                                      m_server_pid);
-}
-
-void BatchServerTest::SetUp_only_controls()
-{
-    m_signal_shmem = std::make_shared<MockSharedMemory>(0);
-    m_control_shmem = std::make_shared<MockSharedMemory>(sizeof(double));
-    m_signal_config = {};
-    m_control_config = {geopm_request_s {1, 0, "MAX_CPU_FREQUENCY"}};
-    m_batch_server = std::make_shared<BatchServerImp>(m_client_pid,
-                                                      m_signal_config,
-                                                      m_control_config,
-                                                      *m_pio_ptr,
-                                                      m_batch_status,
-                                                      m_signal_shmem,
-                                                      m_control_shmem,
-                                                      m_server_pid);
-}
 
 void BatchServerTest::TearDown()
 {
@@ -146,30 +142,32 @@ void BatchServerTest::TearDown()
                               BatchStatus::M_MESSAGE_TERMINATE));
         m_batch_server.reset();
     }
+    EXPECT_CALL(*m_posix_signal,
+                sig_queue(m_server_pid,
+                          SIGTERM,
+                          BatchStatus::M_MESSAGE_TERMINATE))
+        .Times(3);
+    m_batch_server_signals.reset();
+    m_batch_server_controls.reset();
+    m_batch_server_pid.reset();
 }
 
 TEST_F(BatchServerTest, get_server_pid)
 {
     EXPECT_EQ(m_server_pid, m_batch_server->server_pid());
-    SetUp_full_server();
 
     int server_pid = m_batch_server->server_pid();
-    // In test case, there is no fork
-    EXPECT_EQ(0, server_pid);
+    EXPECT_EQ(m_server_pid, server_pid);
 }
 
 TEST_F(BatchServerTest, get_server_key)
 {
-    SetUp_full_server();
-
     std::string key = m_batch_server->server_key();
     EXPECT_EQ(std::to_string(m_client_pid), key);
 }
 
 TEST_F(BatchServerTest, stop_batch)
 {
-    SetUp_full_server();
-
     EXPECT_TRUE(m_batch_server->is_active());
     EXPECT_CALL(*m_posix_signal,
                 sig_queue(m_server_pid,
@@ -181,23 +179,20 @@ TEST_F(BatchServerTest, stop_batch)
 
 TEST_F(BatchServerTest, run_batch_read)
 {
-    SetUp_full_server();
-
     InSequence sequence;
 
     int idx = 0;
     std::vector<double> result = {240.042, 250.052};
 
-    // { geopm_request_s {1, 0, "CPU_FREQUENCY"}, geopm_request_s {2, 1, "TEMPERATURE"} }
     for (const auto &request : m_signal_config) {
         EXPECT_CALL(*m_pio_ptr, push_signal(request.name, request.domain,
                                             request.domain_idx))
+
             .WillOnce(Return(idx))
             .RetiresOnSaturation();
         ++idx;
     }
 
-    // { geopm_request_s {1, 0, "MAX_CPU_FREQUENCY"} }
     for (const auto &request : m_control_config) {
         EXPECT_CALL(*m_pio_ptr, push_control(request.name, request.domain,
                                              request.domain_idx))
@@ -242,23 +237,11 @@ TEST_F(BatchServerTest, run_batch_read)
 
 TEST_F(BatchServerTest, run_batch_read_empty)
 {
-    SetUp_only_controls();
-
     InSequence sequence;
 
     int idx = 0;
     std::vector<double> result = {};
 
-    // {}
-    for (const auto &request : m_signal_config) {
-        EXPECT_CALL(*m_pio_ptr, push_signal(request.name, request.domain,
-                                            request.domain_idx))
-            .WillOnce(Return(idx))
-            .RetiresOnSaturation();
-        ++idx;
-    }
-
-    // { geopm_request_s {1, 0, "MAX_CPU_FREQUENCY"} }
     for (const auto &request : m_control_config) {
         EXPECT_CALL(*m_pio_ptr, push_control(request.name, request.domain,
                                              request.domain_idx))
@@ -266,10 +249,6 @@ TEST_F(BatchServerTest, run_batch_read_empty)
             .RetiresOnSaturation();
         ++idx;
     }
-
-    EXPECT_CALL(*m_batch_status, send_message(BatchStatus::M_MESSAGE_CONTINUE))
-        .Times(1)
-        .RetiresOnSaturation();
 
     EXPECT_CALL(*m_batch_status, receive_message())
         .WillOnce(Return(BatchStatus::M_MESSAGE_READ))
@@ -285,25 +264,20 @@ TEST_F(BatchServerTest, run_batch_read_empty)
         .RetiresOnSaturation();
 
     EXPECT_CALL(*m_batch_status,
-                send_message(BatchStatus::M_MESSAGE_CONTINUE))
+                send_message(BatchStatus::M_MESSAGE_QUIT))
         .Times(1)
         .RetiresOnSaturation();
 
-    m_batch_server->run_batch();
-
-    EXPECT_EQ(result.size(), m_signal_shmem->size());  // both should be == 0
+    m_batch_server_controls->run_batch();
 }
 
 TEST_F(BatchServerTest, run_batch_write)
 {
-    SetUp_full_server();
-
     InSequence sequence;
 
     int idx = 0;
 
-    // { geopm_request_s {1, 0, "CPU_FREQUENCY"}, geopm_request_s {2, 1, "TEMPERATURE"} }
-    for (const auto &request : m_signal_config) {
+     for (const auto &request : m_signal_config) {
         EXPECT_CALL(*m_pio_ptr, push_signal(request.name, request.domain,
                                             request.domain_idx))
             .WillOnce(Return(idx))
@@ -311,8 +285,7 @@ TEST_F(BatchServerTest, run_batch_write)
         ++idx;
     }
 
-    // { geopm_request_s {1, 0, "MAX_CPU_FREQUENCY"} }
-    for (const auto &request : m_control_config) {
+     for (const auto &request : m_control_config) {
         EXPECT_CALL(*m_pio_ptr, push_control(request.name, request.domain,
                                              request.domain_idx))
             .WillOnce(Return(idx))
@@ -353,13 +326,10 @@ TEST_F(BatchServerTest, run_batch_write)
 
 TEST_F(BatchServerTest, run_batch_write_empty)
 {
-    SetUp_only_signals();
-
     InSequence sequence;
 
     int idx = 0;
 
-    // { geopm_request_s {1, 0, "CPU_FREQUENCY"}, geopm_request_s {2, 1, "TEMPERATURE"} }
     for (const auto &request : m_signal_config) {
         EXPECT_CALL(*m_pio_ptr, push_signal(request.name, request.domain,
                                             request.domain_idx))
@@ -367,19 +337,6 @@ TEST_F(BatchServerTest, run_batch_write_empty)
             .RetiresOnSaturation();
         ++idx;
     }
-
-    // {}
-    for (const auto &request : m_control_config) {
-        EXPECT_CALL(*m_pio_ptr, push_control(request.name, request.domain,
-                                             request.domain_idx))
-            .WillOnce(Return(idx))
-            .RetiresOnSaturation();
-        ++idx;
-    }
-
-    EXPECT_CALL(*m_batch_status, send_message(BatchStatus::M_MESSAGE_CONTINUE))
-        .Times(1)
-        .RetiresOnSaturation();
 
     EXPECT_CALL(*m_batch_status, receive_message())
         .WillOnce(Return(BatchStatus::M_MESSAGE_WRITE))
@@ -395,44 +352,40 @@ TEST_F(BatchServerTest, run_batch_write_empty)
         .RetiresOnSaturation();
 
     EXPECT_CALL(*m_batch_status,
-                send_message(BatchStatus::M_MESSAGE_CONTINUE))
+                send_message(BatchStatus::M_MESSAGE_QUIT))
         .Times(1)
         .RetiresOnSaturation();
 
-    m_batch_server->run_batch();
-
-    EXPECT_EQ(0u, m_control_shmem->size());
+    m_batch_server_signals->run_batch();
 }
 
 TEST_F(BatchServerTest, create_shmem)
 {
-    m_client_pid = getpid();
-    SetUp_full_server();
-
-    uid_t uid = pid_to_uid(m_client_pid);
-    gid_t gid = pid_to_gid(m_client_pid);
+    int this_pid = getpid();
+    uid_t uid = pid_to_uid(this_pid);
+    gid_t gid = pid_to_gid(this_pid);
 
     const size_t signal_shmem_size  = (m_signal_config.size()  * sizeof(double)) + geopm::hardware_destructive_interference_size;
     const size_t control_shmem_size = (m_control_config.size() * sizeof(double)) + geopm::hardware_destructive_interference_size;
 
     const std::string shmem_dir = "/dev/shm/";
-    const std::string shmem_prefix_signal  = shmem_dir + std::string(BatchServer::M_SHMEM_PREFIX) + std::to_string(m_client_pid) + "-signal";
-    const std::string shmem_prefix_control = shmem_dir + std::string(BatchServer::M_SHMEM_PREFIX) + std::to_string(m_client_pid) + "-control";
+    const std::string shmem_prefix_signal  = shmem_dir + std::string(BatchServer::M_SHMEM_PREFIX) + std::to_string(this_pid) + "-signal";
+    const std::string shmem_prefix_control = shmem_dir + std::string(BatchServer::M_SHMEM_PREFIX) + std::to_string(this_pid) + "-control";
 
-    m_batch_server->create_shmem();
+    m_batch_server_pid->create_shmem();
 
     struct stat data;
     int result = stat((shmem_prefix_signal).c_str(), &data);
     ASSERT_EQ(0, result);                                // check that the file exists
-    EXPECT_EQ((size_t)data.st_size, signal_shmem_size);  // check the size of the file
-    EXPECT_EQ(data.st_uid, uid);                         // check the chown succeeded in uid
-    EXPECT_EQ(data.st_gid, gid);                         // check the chown succeeded in gid
+    EXPECT_EQ(signal_shmem_size, (size_t)data.st_size);  // check the size of the file
+    EXPECT_EQ(uid, data.st_uid);                         // check the chown succeeded in uid
+    EXPECT_EQ(gid, data.st_gid);                         // check the chown succeeded in gid
 
     result = stat(shmem_prefix_control.c_str(), &data);
     ASSERT_EQ(0, result);                                 // check that the file exists
-    EXPECT_EQ((size_t)data.st_size, control_shmem_size);  // check the size of the file
-    EXPECT_EQ(data.st_uid, uid);                          // check the chown succeeded in uid
-    EXPECT_EQ(data.st_gid, gid);                          // check the chown succeeded in gid
+    EXPECT_EQ(control_shmem_size, (size_t)data.st_size);  // check the size of the file
+    EXPECT_EQ(uid, data.st_uid);                          // check the chown succeeded in uid
+    EXPECT_EQ(gid, data.st_gid);                          // check the chown succeeded in gid
 
     EXPECT_EQ(0, unlink(shmem_prefix_signal.c_str()));
     EXPECT_EQ(0, unlink(shmem_prefix_control.c_str()));
@@ -440,8 +393,6 @@ TEST_F(BatchServerTest, create_shmem)
 
 TEST_F(BatchServerTest, fork_with_setup)
 {
-    SetUp_full_server();
-
     size_t *counter_mem = (size_t*) mmap(
         NULL,                        // void *addr
         sizeof(size_t),              // size_t length
@@ -455,17 +406,20 @@ TEST_F(BatchServerTest, fork_with_setup)
 
     std::function<void(void)> setup = [&counter](void)
     {
-        ASSERT_EQ(0u, counter);
-        ++counter;
+        if(counter == 0u) {
+            ++counter;
+        }
     };
 
-    std::function<void(void)> run = [&counter](void)
+    std::function<void(void)> run = [&counter, this](void)
     {
-        ASSERT_EQ(1u, counter);
-        ++counter;
+        if (counter == 1u) {
+            ++counter;
+        }
+        this->TearDown();
     };
 
-    int forked_pid = m_batch_server->fork_with_setup(setup, run);
+    int forked_pid = m_batch_server_pid->fork_with_setup(setup, run);
     waitpid(forked_pid, NULL, 0);
 
     ASSERT_EQ(2u, counter);
