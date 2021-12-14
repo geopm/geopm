38c848ba320152e5fea74cb600c99b3739e710cc
lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>
2021-12-13 18:42:10 -0800

    Added gpu_activity to TestAgent list
    Signed-off-by: lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>

--- scripts/test/TestAgent.py
+++ scripts/test/TestAgent.py
@@ -32,7 +32,7 @@ class TestAgent(unittest.TestCase):
     def test_agent_names(self):
         agent_names = set(geopmpy.agent.names())
         expected_agent_names = {'power_balancer', 'power_governor',
-                                'frequency_map', 'monitor'}
+                                'frequency_map', 'monitor', 'gpu_activity'}
         if geopmpy.version.__beta__:
             expected_agent_names.add('cpu_activity')
         self.assertEqual(expected_agent_names, agent_names)
