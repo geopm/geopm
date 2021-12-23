bd7f48725467f0259576cd4d6b6ce82937b8fcd4
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-12-22 17:12:55 -0800

    Introducing the SaveControl class
    - This is a helper class for IOGroups to implement
      the save_control(string) / restore_control(string)
      methods.
    - These methods require that the control settings for
      the system are saved to disk.
    - Implementation will support all IOGroups that provide the
      value of any contol setting by reading a signal with
      the same name as the control.
    - This class can build a structure that records all settings
      and can serialize this structure into a JSON string.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ service/src/SaveControl.cpp
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2015 - 2021, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include "SaveControl.hpp"
+
+#include "geopm/json11.hpp"
+#include "geopm/Helper.hpp"
+#include "geopm/Exception.hpp"
+#include "geopm/IOGroup.hpp"
+#include "geopm/PlatformTopo.hpp"
+
+using json11::Json;
+
+namespace geopm
+{
+    std::unique_ptr<SaveControl>
+    SaveControl::make_unique(const std::vector<m_setting_s> &settings)
+    {
+        return geopm::make_unique<SaveControlImp>(settings);
+    }
+
+    std::unique_ptr<SaveControl>
+    SaveControl::make_unique(const std::string &json_string)
+    {
+        return geopm::make_unique<SaveControlImp>(json_string);
+    }
+
+    std::unique_ptr<SaveControl>
+    SaveControl::make_unique(IOGroup &io_group)
+    {
+        return geopm::make_unique<SaveControlImp>(io_group, platform_topo());
+    }
+
+    SaveControlImp::SaveControlImp(const std::vector<m_setting_s> &settings)
+        : m_settings(settings)
+    {
+
+    }
+
+    SaveControlImp::SaveControlImp(const std::string &json_string)
+        : m_json(json_string)
+    {
+
+    }
+
+    SaveControlImp::SaveControlImp(IOGroup &io_group, const PlatformTopo &topo)
+        : m_settings(settings(io_group, topo))
+    {
+
+    }
+
+    std::string SaveControlImp::json(void) const
+    {
+        std::string result = m_json;
+        if (result.empty()) {
+            result = json(m_settings);
+        }
+        return result;
+    }
+
+    std::vector<SaveControl::m_setting_s> SaveControlImp::settings(void) const
+    {
+        std::vector<m_setting_s> result = m_settings;
+        if (result.empty()) {
+            result = settings(m_json);
+        }
+        return result;
+    }
+
+
+    std::string SaveControlImp::json(const std::vector<m_setting_s> &settings)
+    {
+        std::vector<std::map<std::string, Json> > json_settings;
+        for (const auto &ss : settings) {
+            json_settings.push_back({{"name", ss.name},
+                                     {"domain_type", ss.domain_type},
+                                     {"domain_idx", ss.domain_idx},
+                                     {"setting", ss.setting}});
+        }
+        Json json_obj(json_settings);
+        return json_obj.dump();
+    }
+
+    std::vector<SaveControl::m_setting_s>
+    SaveControlImp::settings(const std::string &json_string)
+    {
+        std::vector<m_setting_s> result;
+        std::string err;
+        Json root = Json::parse(json_string, err);
+        if (!err.empty() || !root.is_array()) {
+            throw Exception("SaveControlImp::settings(): Expected a JSON array, unable to parse: " + err,
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        for (const auto &jss : root.array_items()) {
+            if (!jss.is_object()) {
+                throw Exception("SaveControlImp::settings(): Expected a JSON object, unable to parse",
+                                GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+            }
+            auto jss_map = jss.object_items();
+            std::vector<std::string> required_keys = {"name",
+                                                      "domain_type",
+                                                      "domain_idx",
+                                                      "setting"};
+            if (jss_map.size() != required_keys.size()) {
+                throw Exception("SaveControlImp::Settins(): JSON object representing m_setting_s must have four fields",
+                                GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+            }
+            for (const auto &rk : required_keys) {
+                if (jss_map.count(rk) == 0) {
+                    throw Exception("SaveControlImp::settings(): Invalid settings object JSON, missing a required field: \"" + rk + "\"",
+                                    GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+                }
+            }
+            result.push_back(m_setting_s{jss["name"].string_value(),
+                                         jss["domain_type"].int_value(),
+                                         jss["domain_idx"].int_value(),
+                                         jss["setting"].number_value()});
+        }
+        return result;
+    }
+
+    void SaveControlImp::write_json(const std::string &save_path) const
+    {
+        write_file(save_path, json());
+    }
+
+    void SaveControlImp::restore(IOGroup &io_group) const
+    {
+        for (const auto &ss : settings()) {
+            io_group.write_control(ss.name,
+                                   ss.domain_type,
+                                   ss.domain_idx,
+                                   ss.setting);
+        }
+    }
+
+    std::vector<SaveControl::m_setting_s>
+    SaveControlImp::settings(IOGroup &io_group,
+                             const PlatformTopo &topo)
+    {
+        std::vector<m_setting_s> result;
+        std::string prefix = io_group.name() + "::";
+        for (const auto &name : io_group.control_names()) {
+            if (string_begins_with(name, prefix)) {
+                int dom_type = io_group.control_domain_type(name);
+                int num_dom = topo.num_domain(dom_type);
+                for (int dom_idx = 0; dom_idx != num_dom; ++dom_idx) {
+                    double setting = io_group.read_signal(name,
+                                                          dom_type,
+                                                          dom_idx);
+                    result.push_back({name,
+                                      dom_type,
+                                      dom_idx,
+                                      setting});
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ service/test/SaveControlTest.cpp
@@ -0,0 +1,216 @@
+/*
+ * Copyright (c) 2015 - 2021, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include "SaveControl.hpp"
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "geopm_test.hpp"
+#include "geopm/Helper.hpp"
+#include "MockIOGroup.hpp"
+#include "MockPlatformTopo.hpp"
+
+using geopm::SaveControl;
+using geopm::SaveControlImp;
+
+using testing::Return;
+using testing::_;
+
+class SaveControlTest : public ::testing::Test
+{
+    protected:
+        void SetUp(void);
+        void TearDown(void);
+        void check_settings(const std::vector<SaveControl::m_setting_s> &actual_settings);
+
+        std::vector<SaveControl::m_setting_s> m_settings;
+        std::string m_settings_json;
+        std::string m_tmp_path;
+        std::shared_ptr<MockIOGroup> m_mock_io_group;
+        std::shared_ptr<MockPlatformTopo> m_mock_topo;
+};
+
+void SaveControlTest::SetUp(void)
+{
+    m_settings = {{"TEST::FREQUENCY", 2, 0, 1.0e9},
+                  {"TEST::FREQUENCY", 2, 1, 2.0e9},
+                  {"TEST::POWER", 1, 0, 300},
+                  {"TEST::POWER", 1, 1, 310}};
+    m_settings_json = "[{\"domain_idx\": 0, "
+                        "\"domain_type\": 2, "
+                        "\"name\": \"TEST::FREQUENCY\", "
+                        "\"setting\": 1000000000}, "
+                       "{\"domain_idx\": 1, "
+                        "\"domain_type\": 2, "
+                        "\"name\": \"TEST::FREQUENCY\", "
+                        "\"setting\": 2000000000}, "
+                       "{\"domain_idx\": 0, "
+                        "\"domain_type\": 1, "
+                        "\"name\": \"TEST::POWER\", "
+                        "\"setting\": 300}, "
+                       "{\"domain_idx\": 1, "
+                        "\"domain_type\": 1, "
+                        "\"name\": \"TEST::POWER\", "
+                        "\"setting\": 310}]";
+    m_tmp_path = "test_save_control_settings.json";
+    m_mock_io_group = std::make_shared<MockIOGroup>();
+    m_mock_topo = std::make_shared<MockPlatformTopo>();
+}
+
+void SaveControlTest::TearDown(void)
+{
+    (void)remove(m_tmp_path.c_str());
+}
+
+void SaveControlTest::check_settings(const std::vector<SaveControl::m_setting_s> &actual_settings)
+{
+    ASSERT_EQ(m_settings.size(), actual_settings.size());
+    for (int idx = 0; idx != (int)m_settings.size(); ++idx) {
+        ASSERT_EQ(m_settings[idx].name, actual_settings[idx].name);
+        ASSERT_EQ(m_settings[idx].domain_type, actual_settings[idx].domain_type);
+        ASSERT_EQ(m_settings[idx].domain_idx, actual_settings[idx].domain_idx);
+        ASSERT_EQ(m_settings[idx].setting, actual_settings[idx].setting);
+    }
+}
+
+TEST_F(SaveControlTest, static_json)
+{
+    std::string json_string = SaveControlImp::json(m_settings);
+    ASSERT_EQ(m_settings_json, json_string);
+}
+
+TEST_F(SaveControlTest, static_settings)
+{
+    check_settings(SaveControlImp::settings(m_settings_json));
+}
+
+TEST_F(SaveControlTest, bad_json)
+{
+    std::string no_array_json = "{\"domain_idx\": 0, "
+                                 "\"domain_type\": 2, "
+                                 "\"name\": \"TEST::FREQUENCY\", "
+                                 "\"setting\": 1000000000}";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(no_array_json),
+                               GEOPM_ERROR_INVALID,
+                               "Expected a JSON array");
+    std::string no_object_json = "[[" + no_array_json + "]]";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(no_object_json),
+                               GEOPM_ERROR_INVALID,
+                               "Expected a JSON object");
+    std::string wrong_field_json = "[{\"domain_idx\": 0, "
+                                     "\"domain_kind\": 2, "
+                                     "\"name\": \"TEST::FREQUENCY\", "
+                                     "\"setting\": 1000000000}]";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(wrong_field_json),
+                               GEOPM_ERROR_INVALID,
+                               "Invalid settings object JSON, missing a required field: \"domain_type\"");
+
+    std::string missing_field_json = "[{\"domain_idx\": 0, "
+                                       "\"name\": \"TEST::FREQUENCY\", "
+                                       "\"setting\": 1000000000}]";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(missing_field_json),
+                               GEOPM_ERROR_INVALID,
+                               "JSON object representing m_setting_s must have four fields");
+    std::string extra_field_json = "[{\"domain_idx\": 0, "
+                                     "\"domain_type\": 2, "
+                                     "\"domain_kind\": 2, "
+                                     "\"name\": \"TEST::FREQUENCY\", "
+                                     "\"setting\": 1000000000}]";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(extra_field_json),
+                               GEOPM_ERROR_INVALID,
+                               "JSON object representing m_setting_s must have four fields");
+    std::string invalid_json = "][";
+    GEOPM_EXPECT_THROW_MESSAGE(SaveControlImp::settings(invalid_json),
+                               GEOPM_ERROR_INVALID,
+                               "unable to parse");
+}
+
+TEST_F(SaveControlTest, make_from_struct)
+{
+    auto save_ctl = SaveControl::make_unique(m_settings);
+    check_settings(save_ctl->settings());
+    ASSERT_EQ(m_settings_json, save_ctl->json());
+}
+
+TEST_F(SaveControlTest, make_from_string)
+{
+    auto save_ctl = SaveControl::make_unique(m_settings_json);
+    check_settings(save_ctl->settings());
+    ASSERT_EQ(m_settings_json, save_ctl->json());
+}
+
+TEST_F(SaveControlTest, make_from_io_group)
+{
+    m_settings = {{"TEST::FREQUENCY", 2, 0, 1.0e9},
+                  {"TEST::FREQUENCY", 2, 1, 2.0e9},
+                  {"TEST::POWER", 1, 0, 300},
+                  {"TEST::POWER", 1, 1, 310}};
+    EXPECT_CALL(*m_mock_io_group, name())
+        .WillOnce(Return("TEST"));
+    EXPECT_CALL(*m_mock_io_group, control_domain_type(_))
+        .WillOnce(Return(2))
+        .WillOnce(Return(1));
+    EXPECT_CALL(*m_mock_io_group, control_names())
+        .WillOnce(Return(std::set<std::string> {
+            "FREQUENCY",
+            "POWER",
+            "TEST::FREQUENCY",
+            "TEST::POWER"}));
+    EXPECT_CALL(*m_mock_topo, num_domain(_))
+        .WillOnce(Return(2))
+        .WillOnce(Return(2));
+    EXPECT_CALL(*m_mock_io_group, read_signal(_, _, _))
+        .WillOnce(Return(1.0e9))
+        .WillOnce(Return(2.0e9))
+        .WillOnce(Return(300.0))
+        .WillOnce(Return(310.0));
+    SaveControlImp save_ctl(*m_mock_io_group, *m_mock_topo);
+    check_settings(save_ctl.settings());
+    ASSERT_EQ(m_settings_json, save_ctl.json());
+    {
+        EXPECT_CALL(*m_mock_io_group, write_control(_, _, _, _))
+            .Times(4);
+        save_ctl.restore(*m_mock_io_group);
+    }
+}
+
+TEST_F(SaveControlTest, write_file)
+{
+    auto save_ctl = SaveControl::make_unique(m_settings);
+    save_ctl->write_json(m_tmp_path);
+    std::string actual_json_string = geopm::read_file(m_tmp_path);
+    ASSERT_EQ(m_settings_json, actual_json_string);
+}
