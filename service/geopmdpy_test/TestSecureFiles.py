3ef047c9558a5381dc8eddaa07df96a6bc7afcd5
Konstantin Rebrov <konstantin.rebrov@intel.com>
2022-02-06 23:17:45 -0800

    Added new tests related to directory creation to TestSecureFiles
    - test_default_creation
    - test_creation_link_not_dir
    - test_creation_file_not_dir
    - test_creation_bad_perms
    - test_creation_bad_user_owner
    - test_creation_bad_group_owner
    - test_pre_exists
    
    Signed-off-by: Konstantin Rebrov <konstantin.rebrov@intel.com>

--- service/geopmdpy_test/TestSecureFiles.py
+++ service/geopmdpy_test/TestSecureFiles.py
@@ -36,6 +36,7 @@ from unittest import mock
 import os
 import stat
 import tempfile
+from pathlib import Path
 
 from geopmdpy.varrun import secure_make_dirs
 from geopmdpy.varrun import secure_make_file
@@ -69,6 +70,29 @@ class TestSecureFiles(unittest.TestCase):
         self.assertEqual(os.getuid(), user_owner)
         self.assertEqual(os.getgid(), group_owner)
 
+    def test_pre_exists(self):
+        """Usage of pre existing geopm-service directory
+
+        Test calls secure_make_dirs() when the geopm-service
+        directory has already been created.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service'
+        os.mkdir(sess_path, mode=0o700)
+        secure_make_dirs(sess_path)
+        self.check_dir_perms(sess_path)
+
+    def test_default_creation(self):
+        """Default creation of geopm-service directory
+
+        Test calls secure_make_dirs() when the geopm-service
+        directory is not present.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service'
+        secure_make_dirs(sess_path)
+        self.check_dir_perms(sess_path)
+
     def test_creation_link_not_dir(self):
         """The path specified is a link not a directory.
 
@@ -89,6 +113,119 @@ class TestSecureFiles(unittest.TestCase):
             calls = [
                 mock.call(f'Warning: <geopm-service> {sess_path} is a symbolic link, the link will be renamed to {renamed_path}'),
                 mock.call(f'Warning: <geopm-service> the symbolic link points to /root')
+            ]
+            mock_err.assert_has_calls(calls)
+        self.assertTrue(os.path.exists(renamed_path))
+        self.check_dir_perms(sess_path)
+
+    def test_creation_file_not_dir(self):
+        """The path specified is a file not a directory.
+
+        Test calls secure_make_dirs() when the geopm-service
+        provided path is not a directory, but a file. It asserts that
+        a warning message is printed to standard error and that a
+        new directory is created while the file is renamed.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service.txt'
+        filename = Path(sess_path)
+        filename.touch(exist_ok=True)
+        with mock.patch('os.path.islink', return_value=False), \
+             mock.patch('os.path.isdir', return_value=False), \
+             mock.patch('uuid.uuid4', return_value='uuid4'), \
+             mock.patch('sys.stderr.write', return_value=None) as mock_err:
+            secure_make_dirs(sess_path)
+            renamed_path = f'{sess_path}-uuid4-INVALID'
+            msg = f'Warning: <geopm-service> {sess_path} is not a directory, it will be renamed to {renamed_path}'
+            mock_err.assert_called_once_with(msg)
+        self.assertTrue(os.path.exists(renamed_path))
+        self.check_dir_perms(sess_path)
+
+    def test_creation_bad_perms(self):
+        """Directory exists with bad permissions
+
+        Test calls secure_make_dirs() when the geopm-service
+        directory is present with wrong permissions.  It asserts that
+        a warning message is printed to standard error and that the
+        permissions are changed.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service'
+        os.mkdir(sess_path, mode=0o755)
+        bad_user = mock.MagicMock()
+        bad_user.st_uid = os.getuid()
+        bad_user.st_gid = os.getgid()
+        bad_user.st_mode = 0o755
+        with mock.patch('os.stat', return_value=bad_user), \
+             mock.patch('os.path.islink', return_value=False), \
+             mock.patch('os.path.isdir', return_value=True), \
+             mock.patch('uuid.uuid4', return_value='uuid4'), \
+             mock.patch('sys.stderr.write', return_value=None) as mock_err:
+            secure_make_dirs(sess_path)
+            renamed_path = f'{sess_path}-uuid4-INVALID'
+            calls = [
+                mock.call(f'Warning: <geopm-service> {sess_path} has wrong permissions, it will be renamed to {renamed_path}'),
+                mock.call(f'Warning: <geopm-service> the wrong permissions were {oct(bad_user.st_mode)}')
+            ]
+            mock_err.assert_has_calls(calls)
+        self.assertTrue(os.path.exists(renamed_path))
+        self.check_dir_perms(sess_path)
+
+    def test_creation_bad_user_owner(self):
+        """Directory exists with wrong user owner
+
+        Test calls secure_make_dirs() when the geopm-service
+        directory is present with wrong ownership.  It asserts that
+        a warning message is printed to standard error and that the
+        ownership is changed.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service'
+        os.mkdir(sess_path, mode=0o700)
+        bad_user = mock.MagicMock()
+        bad_user.st_uid = os.getuid() + 1
+        bad_user.st_gid = os.getgid()
+        bad_user.st_mode = 0o700
+        with mock.patch('os.stat', return_value=bad_user), \
+             mock.patch('os.path.islink', return_value=False), \
+             mock.patch('os.path.isdir', return_value=True), \
+             mock.patch('uuid.uuid4', return_value='uuid4'), \
+             mock.patch('sys.stderr.write', return_value=None) as mock_err:
+            secure_make_dirs(sess_path)
+            renamed_path = f'{sess_path}-uuid4-INVALID'
+            calls = [
+                mock.call(f'Warning: <geopm-service> {sess_path} has wrong user owner, it will be renamed to {renamed_path}'),
+                mock.call(f'Warning: <geopm-service> the wrong user owner was {bad_user.st_uid}')
+            ]
+            mock_err.assert_has_calls(calls)
+        self.assertTrue(os.path.exists(renamed_path))
+        self.check_dir_perms(sess_path)
+
+    def test_creation_bad_group_owner(self):
+        """Directory exists with wrong group owner
+
+        Test calls secure_make_dirs() when the geopm-service
+        directory is present with wrong ownership.  It asserts that
+        a warning message is printed to standard error and that the
+        ownership is changed.
+
+        """
+        sess_path = f'{self._TEMP_DIR.name}/geopm-service'
+        os.mkdir(sess_path, mode=0o700)
+        bad_user = mock.MagicMock()
+        bad_user.st_uid = os.getuid()
+        bad_user.st_gid = os.getgid() + 1
+        bad_user.st_mode = 0o700
+        with mock.patch('os.stat', return_value=bad_user), \
+             mock.patch('os.path.islink', return_value=False), \
+             mock.patch('os.path.isdir', return_value=True), \
+             mock.patch('uuid.uuid4', return_value='uuid4'), \
+             mock.patch('sys.stderr.write', return_value=None) as mock_err:
+            secure_make_dirs(sess_path)
+            renamed_path = f'{sess_path}-uuid4-INVALID'
+            calls = [
+                mock.call(f'Warning: <geopm-service> {sess_path} has wrong group owner, it will be renamed to {renamed_path}'),
+                mock.call(f'Warning: <geopm-service> the wrong group owner was {bad_user.st_gid}')
             ]
             mock_err.assert_has_calls(calls)
         self.assertTrue(os.path.exists(renamed_path))
