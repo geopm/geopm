9773a1caa8d8ad495897be4557fbef0141cebd3d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-02-14 17:32:17 -0800

    Convert check_write_session.sh to use geopmread/geopmwrite
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/integration/test/do_write.sh
+++ service/integration/test/do_write.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #  Copyright (c) 2015 - 2021, Intel Corporation
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -29,25 +30,5 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-EXTRA_DIST += integration/README.md \
-              integration/build_dasbus.sh \
-              integration/install_service.sh \
-              integration/test/__init__.py \
-              integration/test/test_bash_examples.py \
-              integration/test/check_write_session.sh \
-              integration/test/test_batch_performance.py \
-              integration/test/test_sst_priority.sh \
-              integration/test/test_su_give_access.sh \
-              integration/test/test_su_restart.sh \
-              integration/test/do_write.sh \
-              # end
+geopmwrite $@ && sleep 10
 
-check_PROGRAMS += integration/test/test_batch_server \
-                  integration/test/test_batch_interface \
-                  #end
-
-integration_test_test_batch_server_SOURCES = integration/test/test_batch_server.cpp
-integration_test_test_batch_server_LDADD = libgeopmd.la
-
-integration_test_test_batch_interface_SOURCES = integration/test/test_batch_interface.cpp
-integration_test_test_batch_interface_LDADD = libgeopmd.la
