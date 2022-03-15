158b673e67789ff5401062cac01bb523933b786d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-03-15 16:20:47 -0700

    Define master_doc in sphinx-build configuration
    - Fixes #2159
    - Fixes the build when using sphinx-build version 1.8.5
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/docs/source/conf.py
+++ service/docs/source/conf.py
@@ -75,6 +75,8 @@ def get_description(rst_file_name):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
+
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
