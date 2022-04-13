51a814987d75d80060f40dd86b60ad8ef680f3cd
Konstantin Rebrov <konstantin.rebrov@intel.com>
2022-04-13 09:54:54 -0700

    Changes to FrequencyMapAgent to solve the JSON bug
    WIP
    
    Signed-off-by: Konstantin Rebrov <konstantin.rebrov@intel.com>

--- src/FrequencyMapAgent.hpp
+++ src/FrequencyMapAgent.hpp
@@ -52,6 +52,11 @@ namespace geopm
             static std::unique_ptr<Agent> make_plugin(void);
             static std::vector<std::string> policy_names(void);
             static std::vector<std::string> sample_names(void);
+            void set_maps(const std::map<uint64_t, double>& hash_freq_map,
+                          const std::set<uint64_t>& default_freq_hash) {
+                m_hash_freq_map = hash_freq_map;
+                m_default_freq_hash = default_freq_hash;
+            }
         private:
             void update_policy(const std::vector<double> &policy);
             void init_platform_io(void);
