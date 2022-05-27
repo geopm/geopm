710bae7e93dcfc3b2d02af9994b2c4858880d1d5
Daniel Wilson <daniel1.wilson@intel.com>
2022-05-27 11:34:45 -0700

    Enable autosectionlabel_prefix_document so auto-labeled sections have unique label names

--- service/docs/source/conf.py
+++ service/docs/source/conf.py
@@ -68,6 +68,8 @@ napoleon_google_docstring = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+autosectionlabel_prefix_document = True
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
