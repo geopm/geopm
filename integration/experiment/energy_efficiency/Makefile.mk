680c363aa4d067fa4e7e2dee9d270e0b88a70c7f
Lowren Lawson <lowren.h.lawson@intel.com>
2022-06-08 08:16:47 -0700

    Added workload experiment scripts to extra_dist
    Signed-off-by: Lowren Lawson <lowren.h.lawson@intel.com>

--- integration/experiment/energy_efficiency/Makefile.mk
+++ integration/experiment/energy_efficiency/Makefile.mk
@@ -8,6 +8,15 @@ EXTRA_DIST += integration/experiment/energy_efficiency/barrier_frequency_sweep.p
               integration/experiment/energy_efficiency/__init__.py \
               integration/experiment/energy_efficiency/power_balancer_energy.py \
               integration/experiment/energy_efficiency/run_barrier_frequency_sweep_nekbone.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_amg.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_dgemm.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_hpcg.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_hpl_mkl.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_hpl_netlib.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_minife.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_nasft.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_nekbone.py \
+              integration/experiment/energy_efficiency/run_cpu_activity_pennant.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_amg.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_dgemm.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_hpcg.py \
---
+++ integration/experiment/energy_efficiency/run_cpu_activity_nasft.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+'''
+CPU Activity experiment scaling core and uncore frequency
+against the NASFT workload
+'''
+import argparse
+
+from experiment.energy_efficiency import cpu_activity
+from experiment import machine
+from apps.nasft import nasft
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    cpu_activity.setup_run_args(parser)
+    args, extra_args = parser.parse_known_args()
+    mach = machine.init_output_dir(args.output_dir)
+    app_conf = nasft.NasftAppConf(mach)
+    cpu_activity.launch(app_conf=app_conf, args=args,
+                   experiment_cli_args=extra_args)
