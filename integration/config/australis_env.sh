4271d8e712756c7de9cd0caac47bb5bcf860d3ee
Brad Geltz <brad.geltz@intel.com>
2022-06-09 15:55:39 -0700

    Add Australis env
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

---
+++ integration/config/australis_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+# AUSTRALIS BUILD/RUN ENVIRONMENT
+#
+# This script is intended to be sourced within an existing script or shell ONLY.
+# It is NOT intended to be ./ executed.
+
+export CC=icc
+export CXX=icpc
+export MPICC=mpicc
+export MPICXX=mpic++
+export FC=ifort
+export F77=ifort
+export F90=ifort
+export MPIFORT=mpifort
+export MPIFC=mpifort
+export MPIF77=mpifort
+export MPIF90=mpifort
+export PALS_PMI=pmix
