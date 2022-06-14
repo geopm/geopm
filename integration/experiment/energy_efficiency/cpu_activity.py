6c17eb05dc5897bbb52a10c82a31f816cb1cc7fe
Lowren Lawson <lowren.h.lawson@intel.com>
2022-06-13 18:08:41 -0700

    Added cpu_activity command line args
    Signed-off-by: Lowren Lawson <lowren.h.lawson@intel.com>

--- integration/experiment/energy_efficiency/cpu_activity.py
+++ integration/experiment/energy_efficiency/cpu_activity.py
@@ -19,6 +19,19 @@ from experiment import machine
 
 def setup_run_args(parser):
     common_args.setup_run_args(parser)
+    parser.add_argument('--cpu-frequency-efficient', dest='cpu_fe',
+                        action='store', default='nan',
+                        help='The efficient frequency of the cpu (a.k.a. Fce) for this experiment')
+    parser.add_argument('--cpu-frequency-max', dest='cpu_fmax',
+                        action='store', default='nan',
+                        help='The maximum frequency of the cpu (a.k.a. Fcmax) for this experiment')
+    parser.add_argument('--uncore-frequency-efficient', dest='uncore_fe',
+                        action='store', default='nan',
+                        help='The efficient frequency of the uncore (a.k.a. Fue) for this experiment')
+    parser.add_argument('--uncore-frequency-max', dest='uncore_fmax',
+                        action='store', default='nan',
+                        help='The maximum frequency of the uncore (a.k.a. Fumax) for this experiment')
+    parser.add_argument('--uncore-mbm-list', nargs='+', help='A list of uncore_freq & max_mem_bw values in the form uncore_freq_0 max_mem_bw_0 uncore_freq_1 ...')
 
 def report_signals():
     return []
@@ -26,91 +39,32 @@ def report_signals():
 def trace_signals():
     return []
 
-def launch_configs(output_dir, app_conf):
+def launch_configs(output_dir, app_conf, cpu_fe, cpu_fmax, uncore_fe,
+                    uncore_fmax, uncore_mbm_list):
+
     mach = machine.init_output_dir(output_dir)
     sys_min = mach.frequency_min()
     sys_max = mach.frequency_max()
     sys_sticker = mach.frequency_sticker()
-    config_list = [{"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 1.0,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.9,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.8,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.7,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.6,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.5,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.4,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.3,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.2,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.1,
-                    "SAMPLE_PERIOD": 0.01},
-                   {"CPU_FREQ_MAX": float(nan),
-                    "CPU_FREQ_EFFICIENT": float(nan),
-                    "UNCORE_FREQ_MAX": float(nan),
-                    "UNCORE_FREQ_EFFICIENT": float(nan),
-                    "CPU_PHI": 0.0,
-                    "SAMPLE_PERIOD": 0.01},
-                  ]
-    config_names=['phi100',
-                  'phi90',
-                  'phi80',
-                  'phi70',
-                  'phi60',
-                  'phi50',
-                  'phi40',
-                  'phi30',
-                  'phi20',
-                  'phi10',
-                  'phi0']
+
+    uncore_mbm_policy = {}
+    if uncore_mbm_list is not None:
+        for idx,val in enumerate(uncore_mbm_list):
+            if (idx % 2) == 0:
+                uncore_mbm_policy["CPU_UNCORE_FREQ_{}".format(int(idx / 2))] = float(val)
+            else:
+                uncore_mbm_policy["MAX_MEMORY_BANDWIDTH_{}".format(int((idx - 1) / 2))] = float(val)
+
+    config_list = [{"CPU_FREQ_MAX" : float(cpu_fmax), "CPU_FREQ_EFFICIENT": float(cpu_fe),
+                    "CPU_UNCORE_FREQ_MAX" : float(uncore_fmax), "CPU_UNCORE_FREQ_EFFICIENT" : float(uncore_fe),
+                    "CPU_PHI" : float(phi/10), "SAMPLE_PERIOD": 0.01} for phi in range(0,11)]
+    config_names = ['phi'+str(x*10) for x in range(0,11)]
+
+    for config in config_list:
+        config.update(uncore_mbm_policy)
 
     targets = []
+
     agent = 'cpu_activity'
     for idx,config in enumerate(config_list):
         name = config_names[idx]
@@ -128,7 +82,8 @@ def launch(app_conf, args, experiment_cli_args):
                                                     trace_signals=trace_signals())
     extra_cli_args += experiment_cli_args
 
-    targets = launch_configs(output_dir, app_conf)
+    targets = launch_configs(output_dir, app_conf, args.cpu_fe, args.cpu_fmax, args.uncore_fe,
+                                args.uncore_fmax, args.uncore_mbm_list)
 
     launch_util.launch_all_runs(targets=targets,
                                 num_nodes=args.node_count,
