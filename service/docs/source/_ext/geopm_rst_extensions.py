181a950a3f26ae129b3e04d88c75a7dd5e0c97d9
Daniel Wilson <daniel1.wilson@intel.com>
2022-06-16 09:16:46 -0700

    Automatically cross-reference MSR definitions when an MSR is mentioned in another MSR description

--- service/docs/source/_ext/geopm_rst_extensions.py
+++ service/docs/source/_ext/geopm_rst_extensions.py
@@ -18,10 +18,50 @@ from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util import logging
+from sphinx import addnodes
 import json
+import re
 
 logger = logging.getLogger(__name__)
 
+MSR_REGEX = re.compile(r'(MSR::\w+:\w+)', flags=re.ASCII)
+
+def create_msr_description_paragraph(text):
+    """Create an MSR description in a docutils paragraph node. Automatically
+    wrap any named MSR signals/controls in hyperlinks to their descriptions.
+    """
+    msr_definition_paragraph = nodes.paragraph()
+    for text_part in re.split(MSR_REGEX, text):
+        match = re.match(MSR_REGEX, text_part)
+        if match:
+            # This part of the description names an MSR, so wrap the text in a
+            # link to the expected docutils reference ID. The ID should exist
+            # if the mentioned MSR has a description in our documentation.
+
+            # Use Sphinx's pending_xref instead of docutils' reference so that
+            # Sphinx will automatically check for broken references.
+
+            # This does not emit a very helpful error message (it just says
+            # undefined label at the line where we are loading the json file).
+            # We can implement our own context-aware xref checking and error
+            # messaging if we create a set of sphinx roles/directives/domains,
+            # e.g., to define and xref arbitrary signals/controls. Maybe
+            # something nice to add later on.
+            ref_node = addnodes.pending_xref(
+                '', refdomain='std', refexplicit='False',
+                reftarget=nodes.make_id(match.group(0)), reftype='ref',
+                refwarn='False')
+            resolved_ref = addnodes.pending_xref_condition(condition='resolved')
+            resolved_ref += nodes.Text(match.group(0))
+            unresolved_ref = addnodes.pending_xref_condition(condition='*')
+            unresolved_ref += nodes.Text('missing ' + match.group(0))
+            ref_node += resolved_ref
+            ref_node += unresolved_ref
+            msr_definition_paragraph += ref_node
+        else:
+            # This is not an MSR cross-reference, so just insert the text.
+            msr_definition_paragraph += nodes.Text(text_part)
+    return msr_definition_paragraph
 
 class GeopmMsrJson(SphinxDirective):
     has_content = False
@@ -68,10 +108,17 @@ class GeopmMsrJson(SphinxDirective):
                 geopm_msr_name = f'MSR::{msr_base_name}:{msr_field_name}'
                 msr_definition_body = nodes.definition()
 
+                # Create a global docutils ID so we can :ref:`msr-some-field` to it
+                docutils_id = nodes.make_id(geopm_msr_name)
+                self.env.app.env.domaindata["std"]["labels"][docutils_id] = (
+                    self.env.docname, docutils_id, geopm_msr_name)
+                self.env.app.env.domaindata["std"]["anonlabels"][docutils_id] = (
+                    self.env.docname, docutils_id)
+
                 # Signal/control names are formatted as ``literal`` terms in
                 # our documentation
                 msr_definition_name = nodes.term()
-                msr_definition_name += nodes.literal(text=geopm_msr_name)
+                msr_definition_name += nodes.literal(text=geopm_msr_name, ids=[docutils_id], names=[docutils_id])
 
                 # Signal/control descriptions start with a paragraph containing
                 # the high-level description
@@ -83,7 +130,7 @@ class GeopmMsrJson(SphinxDirective):
                     logger.info('Missing a description for %s in %s',
                                 geopm_msr_name,
                                 json_path, color='yellow')
-                msr_definition_body += nodes.paragraph(text=description_text)
+                msr_definition_body += create_msr_description_paragraph(description_text)
 
                 # The MSR description is followed by a list of properties.
                 msr_property_list = nodes.bullet_list()
