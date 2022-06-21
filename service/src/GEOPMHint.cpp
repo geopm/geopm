ad134da94f26812d821d4bbd19a46ccab4252c9a
Alejandro Vilches <alejandro.vilches@intel.com>
2022-06-21 09:38:47 -0700

    Misc. changes/fixes related to check_hint()
    - Placed check_hint() in geopm namespace
    - Use fully qualified name when invoking check_hint()
    - Removed GEOPM_MASK_REGION_HINT as it's no longer needed
    
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

--- service/src/GEOPMHint.cpp
+++ service/src/GEOPMHint.cpp
@@ -10,16 +10,10 @@
 #include "geopm/Helper.hpp"
 #include "geopm/Exception.hpp"
 
-extern "C" {
+namespace geopm {
 
     void check_hint(uint64_t hint)
     {
-        if ((hint & ~GEOPM_MASK_REGION_HINT) != 0ULL) {
-            throw geopm::Exception("Helper::" + std::string(__func__) +
-                            "(): invalid hint: " +
-                            geopm::string_format_hex(hint),
-                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
-        }
         if (hint >= GEOPM_SENTINEL_REGION_HINT) {
             throw geopm::Exception("Helper::" + std::string(__func__) +
                             "(): hint out of range: " +
