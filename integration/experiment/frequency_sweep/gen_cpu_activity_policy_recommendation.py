0bd309289cd4a7eecd8f1846aa1335a9937113a0
Lowren Lawson <lowren.h.lawson@intel.com>
2022-07-05 15:44:51 -0700

    Updated cpu activity policy recommendation script comments & hardcoded region
    Signed-off-by: Lowren Lawson <lowren.h.lawson@intel.com>

--- integration/experiment/frequency_sweep/gen_cpu_activity_policy_recommendation.py
+++ integration/experiment/frequency_sweep/gen_cpu_activity_policy_recommendation.py
@@ -53,8 +53,7 @@ def extract_columns(df, region_list = None):
 def analyze_efficient_energy(df, freq_col_name):
     """
     Find the frequency that provides the minimum package energy consumption
-    within the dataframe provided.  If a tolerance is given, allow for
-    frequency selection below the energy efficient frequency.
+    within the dataframe provided.
     """
 
     energy_min = df['package-energy (J)'].min()
@@ -69,12 +68,12 @@ def analyze_efficient_energy(df, freq_col_name):
 
     return energy_efficient_frequency
 
-def system_memory_bandwidth_characterization(df_region_group):
+def system_memory_bandwidth_characterization(df_region_group, region):
     """
     Perform characterization of the system memory bandwidth.  This
     is used by the agent to help determine the appropriate uncore frequency
     """
-    df = df_region_group.get_group('intensity_0')
+    df = df_region_group.get_group(region)
     uncore_freq_set = sorted(set(df['uncore-frequency (Hz)'].to_list()))
 
     mem_bw_dict = {}
@@ -109,20 +108,21 @@ def main(full_df, region_list):
     is a list of regions to include.
     """
     df = extract_columns(full_df, region_list)
-    df['frequency (Hz)'] = (df['frequency (Hz)']/1e09).round(decimals=1)*1e09
-    df['uncore-frequency (Hz)'] = (df['uncore-frequency (Hz)']/1e09).round(decimals=1)*1e09
-    df_region_group = df.groupby('region')
 
     # Handle any frequency clipping via rounding to the nearest 100Mhz
     # An alternative would be to use the FREQ_DEFAULT and FREQ_UNCORE
     # values from the policy
+    df['frequency (Hz)'] = (df['frequency (Hz)']/1e09).round(decimals=1)*1e09
+    df['uncore-frequency (Hz)'] = (df['uncore-frequency (Hz)']/1e09).round(decimals=1)*1e09
+    df_region_group = df.groupby('region')
 
-    # Characterize MBM metrics
-    mem_bw_characterization = system_memory_bandwidth_characterization(df_region_group)
+    # Characterize MBM metrics using the uncore sensitive
+    # region (intensity_0)
+    mem_bw_characterization = system_memory_bandwidth_characterization(df_region_group, region=region_list[0])
 
     # A multi-step approach is used.
     # First analyze the most uncore sensitive
-    # region (intensity_0)  to find the efficient
+    # region (intensity_0) to find the efficient
     # uncore frequency
     uncore_freq_recommendation = frequency_recommendation(df_region_group, region=region_list[0],
                                                            domain="UNCORE")
@@ -155,7 +155,9 @@ if __name__ == '__main__':
                         help='path containing reports and machine.json')
     parser.add_argument('--region-list', default="intensity_0,intensity_32", dest='region_list',
                         help='comma-separated list of the two regions to use, '
-                             'default is intensity_0,intensity_32')
+                             'with the first used for uncore frequency characterization '
+                             'and the second used for core frequency characteriztaion.  '
+                             'Default is intensity_0,intensity_32')
     args = parser.parse_args()
 
     region_list = args.region_list.split(',')
