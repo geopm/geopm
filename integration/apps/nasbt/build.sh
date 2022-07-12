5bc3083e97ee19d253d39daff3b69f1e196e256b
Daniel Wilson <daniel1.wilson@intel.com>
2022-07-12 07:34:09 -0700

    Add NPB BT appconf

---
+++ integration/apps/nasbt/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+set -x
+set -e
+
+# Get helper functions
+source ../build_func.sh
+
+# Set variables for workload
+DIRNAME=NPB3.4.1-MZ
+ARCHIVE=NPB3.4.1-MZ.tar.gz
+URL=https://www.nas.nasa.gov/assets/npb
+
+# Run helper functions
+clean_source ${DIRNAME}
+get_archive ${ARCHIVE} ${URL}
+unpack_archive ${ARCHIVE}
+setup_source_git ${DIRNAME}
+
+# Build application
+cd ${DIRNAME}/NPB3.4-MZ-MPI
+make suite
