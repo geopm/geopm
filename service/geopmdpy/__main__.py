d489c011d26489414de53018dd9ceadaf70febd4
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-07-15 16:41:10 -0700

    Fix issue where batch server process is left behind
    - Add an integration test that shows the problem
    - Tests that when a session is ended with SIGTERM, the batch server is also
      terminated.
    - Add signal handler to geopmd for SIGTERM
    - Add some exception handling
    - Use atomic move of session file to make sure session is only ended once
    - Add switch for case where the batch server PID is killed
    - Catch all exceptions in event loop to ensure that __Exit()
      is called.  Otherwise destructors are called twice due to fork.
    - When loading a session file to clean up resources, do not do valididty checks
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/geopmdpy/__main__.py
+++ service/geopmdpy/__main__.py
@@ -4,18 +4,32 @@
 
 from dasbus.loop import EventLoop
 from dasbus.connection import SystemMessageBus
+from signal import signal
+from signal import SIGTERM
 from . import service
 
+_bus = None
+
+def term_handler(signum, frame):
+    if signum == SIGTERM:
+        stop()
+
+def stop():
+    if _bus is not None:
+        _bus.disconnect()
+    exit(0)
 
 def main():
+    signal(SIGTERM, term_handler)
     loop = EventLoop()
-    bus = SystemMessageBus()
+    global _bus
+    _bus = SystemMessageBus()
     try:
-        bus.publish_object("/io/github/geopm", service.GEOPMService())
-        bus.register_service("io.github.geopm")
+        _bus.publish_object("/io/github/geopm", service.GEOPMService())
+        _bus.register_service("io.github.geopm")
         loop.run()
     finally:
-        bus.disconnect()
+        stop()
 
 if __name__ == '__main__':
     main()
