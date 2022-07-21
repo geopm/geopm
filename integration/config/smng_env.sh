a58e7857ad9e69671cd8515ede1d94d6bdfaea0c
Brad Geltz <brad.geltz@intel.com>
2022-07-21 15:34:31 -0700

    Update SMNG env file
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/config/smng_env.sh
+++ integration/config/smng_env.sh
@@ -11,20 +11,28 @@
 
 # Default Intel Toolchain compiler overrides
 # The MPI compiler wrappers are supported by Intel, but are not Intel specific.
-module swap intel intel/19.1
-module load python/3.6_intel
 unset OMP_NUM_THREADS # Set to 1 by a default lmod module
 
 # One time setup:
 # python -m pip install --user -U pip
 # python -m pip install --user --ignore-installed -r geopm/scripts/requirements.txt
+# python -m pip install --user --ignore-installed -r geopm/service/requirements.txt
 
+export CC=icc
+export CXX=icpc
+export FC=ifort
+export F77=ifort
+export F90=ifort
+export MPICC=mpiicc
+export MPICXX=mpiicpc
 export MPIFORT=mpiifort
 export MPIFC=mpiifort
 export MPIF77=mpiifort
 export MPIF90=mpiifort
 
-export GEOPM_USER_ACCOUNT=h1906
+export CPATH=${MKL_INCDIR}:${MKL_F90_INC}:${CPATH} # So that mkl.h can be found
+
+# export GEOPM_USER_ACCOUNT=h1906
 export GEOPM_SYSTEM_DEFAULT_QUEUE=test
 export GEOPM_SBATCH_EXTRA_LINES="
 #SBATCH --constraint=\"noscratch\"
