64f9608d61aeda4343b631c01169e15b72d2d8c7
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-08-15 10:59:09 -0700

    Update get_batch_server.py to have better error checking
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/integration/get_batch_server.py
+++ service/integration/get_batch_server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #
 #  Copyright (c) 2015 - 2022, Intel Corporation
 #  SPDX-License-Identifier: BSD-3-Clause
@@ -5,8 +6,18 @@
 
 import json
 import sys
+import psutil
+import os
 
-session_pid=sys.argv[1]
+if len(sys.argv) < 2:
+     print(f'Usage: {sys.argv[0]} SESSION_PID')
+     exit(-1)
+if os.getuid() != 0:
+     raise RuntimeError(f'Script must be run as root user')
+session_pid=int(sys.argv[1])
+if session_pid < 0 or not psutil.pid_exists(session_pid):
+     raise RuntimeError(f'{session_pid} is not a valid PID')
 with open(f'/run/geopm-service/session-{session_pid}.json') as fid:
-     print(json.loads(fid.read())['batch_server'])
-
+     server_pid = json.loads(fid.read()).get('batch_server')
+if server_pid is not None:
+     print(server_pid)
