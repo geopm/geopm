79e5be5d6809f99da042e8b969b2a222387ef549
Daniel Wilson <daniel1.wilson@intel.com>
2022-08-22 09:52:37 -0700

    Add NAS IS appconf

---
+++ integration/apps/nasis/Makefile.mk
@@ -0,0 +1,9 @@
+#  Copyright (c) 2015 - 2023, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+EXTRA_DIST += integration/apps/nasis/__init__.py \
+              integration/apps/nasis/build.sh \
+              integration/apps/nasis/nasis.py \
+              integration/apps/nasis/0001-Add-geopm-markup-to-build-intsort.patch \
+              # end
