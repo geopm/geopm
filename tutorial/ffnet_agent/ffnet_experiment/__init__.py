52479df8d6f3a16d71e5d177e9d48db5c40891cd
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2022-09-13 13:16:34 -0700

    Added TensorOneD class and tests.

--- tutorial/ffnet_agent/ffnet_experiment/__init__.py
+++
@@ -1,3 +0,0 @@
-#  Copyright (c) 2015 - 2022, Intel Corporation
-#  SPDX-License-Identifier: BSD-3-Clause
-#
--- tutorial/ffnet_agent/validate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from jsonschema import validate
-import json
-
-schema = json.load(open('nnet.schema.json'))
-nnet = json.load(open('region_id_9.json'))
-
-validate(instance=nnet, schema=schema)
