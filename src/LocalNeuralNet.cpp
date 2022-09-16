c846ba58d93e8fb9c09033efca6f5d8cd6d5ac31
Ali Mohammad <ali.mohammad@intel.com>
2022-09-15 22:46:57 -0700

    Removed unnecessary member variables, added appropriate error checks.

--- src/LocalNeuralNet.cpp
+++ src/LocalNeuralNet.cpp
@@ -4,63 +4,58 @@
  */
 
 #include <math.h>
-
+#include "geopm/Exception.hpp"
 #include "TensorOneD.hpp"
 #include "TensorTwoD.hpp"
+
 #include "LocalNeuralNet.hpp"
 
 LocalNeuralNet::LocalNeuralNet()
 {
-    m_nlayers = 0;
 }
 
 LocalNeuralNet::LocalNeuralNet(const LocalNeuralNet &other)
 {
-    m_nlayers = other.m_nlayers;
-    m_weights.resize(m_nlayers);
-    m_biases.resize(m_nlayers);
-    for (int idx_layer = 0; idx_layer < m_nlayers; idx_layer++) {
-        m_weights[idx_layer] = other.m_weights[idx_layer];
-        m_biases[idx_layer] = other.m_biases[idx_layer];
+    m_layers.resize(other.m_layers.size());
+    for (int idx = 0; idx < m_layers.size(); idx++) {
+        m_layers[idx] = other.m_layers[idx];
     }
 }
 
 LocalNeuralNet::LocalNeuralNet(json11::Json input)
 {
-    // TODO validate by throwing exceptions
-    m_nlayers = input.array_items().size();
-    m_weights.resize(m_nlayers);
-    m_biases.resize(m_nlayers);
-    for (int idx_layer = 0; idx_layer < m_nlayers; idx_layer++) {
-        m_weights[idx_layer] = TensorTwoD(input[idx_layer][0]);
-        m_biases[idx_layer] = TensorOneD(input[idx_layer][1]);
+    m_layers.resize(input.array_items().size());
+    for (int idx = 0; idx < m_layers.size(); idx++) {
+        m_layers[idx] = std::make_pair(TensorTwoD(input[idx][0]), TensorOneD(input[idx][1]));
+        if (m_layers[idx].first.get_rows() != m_layers[idx].second.get_dim()) {
+            throw geopm::Exception("Incompatible dimensions for weights and biases.",
+                                   GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        if (idx > 0 && m_layers[idx].first.get_cols() != m_layers[idx-1].first.get_rows()) {
+            throw geopm::Exception("Incompatible dimensions for consecutive layers.",
+                                   GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
     }
 }
 
 LocalNeuralNet& LocalNeuralNet::operator=(const LocalNeuralNet &other)
 {
-    m_nlayers = other.m_nlayers;
-    m_weights.resize(m_nlayers);
-    m_biases.resize(m_nlayers);
-    for (int idx_layer = 0; idx_layer < m_nlayers; idx_layer++) {
-        m_weights[idx_layer] = other.m_weights[idx_layer];
-        m_biases[idx_layer] = other.m_biases[idx_layer];
+    m_layers.resize(other.m_layers.size());
+    for (int idx = 0; idx < m_layers.size(); idx++) {
+        m_layers[idx] = other.m_layers[idx];
     }
 }
 
 TensorOneD LocalNeuralNet::model(TensorOneD inp)
 {
-    TensorOneD tmp;
-
-    tmp = inp;
-    for (int idx_layer = 0; idx_layer < m_nlayers; idx_layer++) {
+    TensorOneD tmp = inp;
+    for (int idx = 0; idx < m_layers.size(); idx++) {
         // Tensor operations
-        tmp = m_weights[idx_layer] * tmp + m_biases[idx_layer];
+        tmp = m_layers[idx].first * tmp + m_layers[idx].second;
 
-        if (idx_layer != m_nlayers - 1) {
+        if (idx != m_layers.size() - 1) {
             tmp = tmp.sigmoid();
         }
     }
-
     return tmp;
 }
