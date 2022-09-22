c6fe2482a0f17272406d5020ca8c841c147eddae
Alejandro Vilches <alejandro.vilches@intel.com>
2022-09-22 13:32:16 -0700

    Add initial ConstConfigIOGroup class
    Note: initial stubbed out class (to enable writing unit tests).
    
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

---
+++ service/src/ConstConfigIOGroup.cpp
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2015 - 2022, Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "ConstConfigIOGroup.hpp"
+
+#include <cmath>
+
+#include <iostream>
+#include <iterator>
+
+#include "geopm/Helper.hpp"
+#include "geopm/PlatformTopo.hpp"
+#include "geopm/Exception.hpp"
+#include "geopm/Agg.hpp"
+#include "geopm_topo.h"
+#include "config.h"
+
+#define GEOPM_CONSTCONFIG_IO_GROUP_PLUGIN_NAME "CONSTCONFIG"
+
+namespace geopm
+{
+    ConstConfigIOGroup::ConstConfigIOGroup()
+    {
+        // TODO
+    }
+
+    std::set<std::string> ConstConfigIOGroup::signal_names(void) const
+    {
+        std::set<std::string> result;
+        for (const auto &sv : m_signal_available) {
+            result.insert(sv.first);
+        }
+        return result;
+    }
+
+    std::set<std::string> ConstConfigIOGroup::control_names(void) const
+    {
+        return {};
+    }
+
+    bool ConstConfigIOGroup::is_valid_signal(const std::string &signal_name) const
+    {
+        return m_signal_available.find(signal_name) != m_signal_available.end();
+    }
+
+    bool ConstConfigIOGroup::is_valid_control(const std::string &control_name) const
+    {
+        return false;
+    }
+
+    int ConstConfigIOGroup::signal_domain_type(const std::string &signal_name) const
+    {
+        int result = GEOPM_DOMAIN_INVALID;
+        // TODO
+        return result;
+    }
+
+    int ConstConfigIOGroup::control_domain_type(const std::string &control_name) const
+    {
+        return GEOPM_DOMAIN_INVALID;
+    }
+
+    int ConstConfigIOGroup::push_signal(const std::string &signal_name, int domain_type, int domain_idx)
+    {
+        if (!is_valid_signal(signal_name)) {
+            throw Exception("ConstConfigIOGroup::push_signal(): " + signal_name +
+                            "not valid for ConstConfigIOGroup",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        // TODO
+        return 0;
+    }
+
+    int ConstConfigIOGroup::push_control(const std::string &control_name, int domain_type, int domain_idx)
+    {
+        throw Exception("ConstConfigIOGroup::push_control(): there are no controls supported by the ConstConfigIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    void ConstConfigIOGroup::read_batch(void)
+    {
+        // TODO: enforce proper behavior/sequence of calls: push_signal(),
+        // read_batch(), sample()?
+    }
+
+    void ConstConfigIOGroup::write_batch(void)
+    {
+    }
+
+    double ConstConfigIOGroup::sample(int batch_idx)
+    {
+        double result = NAN;
+        // TODO
+        return result;
+    }
+
+    void ConstConfigIOGroup::adjust(int batch_idx, double setting)
+    {
+        throw Exception("ConstConfigIOGroup::adjust(): there are no controls supported by the ConstConfigIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    double ConstConfigIOGroup::read_signal(const std::string &signal_name, int domain_type, int domain_idx)
+    {
+        if (!is_valid_signal(signal_name)) {
+            throw Exception("ConstConfigIOGroup::read_signal(): " + signal_name +
+                            "not valid for ConstConfigIOGroup",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        // TODO
+        return NAN;
+    }
+
+    void ConstConfigIOGroup::write_control(const std::string &control_name, int domain_type, int domain_idx, double setting)
+    {
+        throw Exception("ConstConfigIOGroup::write_control(): there are no controls supported by the ConstConfigIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    void ConstConfigIOGroup::save_control(void)
+    {
+    }
+
+    void ConstConfigIOGroup::restore_control(void)
+    {
+    }
+
+    std::function<double(const std::vector<double> &)> ConstConfigIOGroup::agg_function(const std::string &signal_name) const
+    {
+        auto it = m_signal_available.find(signal_name);
+        if (it == m_signal_available.end()) {
+            throw Exception("ConstConfigIOGroup::agg_function(): unknown how to aggregate \"" + signal_name + "\"",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        return it->second.agg_function;
+    }
+
+    std::function<std::string(double)> ConstConfigIOGroup::format_function(const std::string &signal_name) const
+    {
+        if (!is_valid_signal(signal_name)) {
+            throw Exception("ConstConfigIOGroup::format_function(): unknown how to format \"" + signal_name + "\"",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        // TODO?
+        return string_format_double;
+    }
+
+    std::string ConstConfigIOGroup::signal_description(const std::string &signal_name) const
+    {
+        auto it = m_signal_available.find(signal_name);
+        if (it == m_signal_available.end()) {
+            throw Exception("ConstConfigIOGroup::signal_description(): signal_name " + signal_name +
+                            " not valid for ConstConfigIOGroup",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+
+        std::string result;
+        result =  "    description: " + it->second.description + '\n'; // Includes alias_for if applicable
+        result += "    units: " + IOGroup::units_to_string(it->second.units) + '\n';
+        result += "    aggregation: " + Agg::function_to_name(it->second.agg_function) + '\n';
+        result += "    domain: " + platform_topo().domain_type_to_name(GEOPM_DOMAIN_BOARD) + '\n';
+        result += "    iogroup: ConstConfigIOGroup";
+        return result;
+    }
+
+    std::string ConstConfigIOGroup::control_description(const std::string &control_name) const
+    {
+        throw Exception("ConstConfigIOGroup::control_description: there are no controls supported by the ConstConfigIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    int ConstConfigIOGroup::signal_behavior(const std::string &signal_name) const
+    {
+        if (!is_valid_signal(signal_name)) {
+            throw Exception("ConstConfigIOGroup::signal_behavior(): signal_name " + signal_name +
+                            " not valid for ConstConfigIOGroup",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        return IOGroup::M_SIGNAL_BEHAVIOR_CONSTANT;
+    }
+
+    std::string ConstConfigIOGroup::name(void) const
+    {
+        return plugin_name();
+    }
+
+    std::string ConstConfigIOGroup::plugin_name(void)
+    {
+        return GEOPM_CONSTCONFIG_IO_GROUP_PLUGIN_NAME;
+    }
+
+    std::unique_ptr<IOGroup> ConstConfigIOGroup::make_plugin(void)
+    {
+        return geopm::make_unique<ConstConfigIOGroup>();
+    }
+
+    void ConstConfigIOGroup::save_control(const std::string &save_path)
+    {
+    }
+
+    void ConstConfigIOGroup::restore_control(const std::string &save_path)
+    {
+    }
+}
---
+++ service/src/ConstConfigIOGroup.hpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2015 - 2022, Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONSTCONFIGIOGROUP_HPP_INCLUDE
+#define CONSTCONFIGIOGROUP_HPP_INCLUDE
+
+#include <map>
+
+#include "geopm/IOGroup.hpp"
+
+namespace geopm
+{
+    class ConstConfigIOGroup : public IOGroup
+    {
+        public:
+            ConstConfigIOGroup();
+            /// @return the list of signal names provided by this IOGroup.
+            std::set<std::string> signal_names(void) const override;
+            /// @return empty set; this IOGroup does not provide any controls.
+            std::set<std::string> control_names(void) const override;
+            /// @return whether the given signal_name is supported by the
+            ///         ConstConfigIOGroup for the current configuration.
+            bool is_valid_signal(const std::string &signal_name) const override;
+            /// @return false; this IOGroup does not provide any controls.
+            bool is_valid_control(const std::string &control_name) const override;
+            /// @return the domain for the provided signal if the signal is valid for this IOGroup, otherwise GEOPM_DOMAIN_INVALID.
+            int signal_domain_type(const std::string &signal_name) const override;
+            /// @return GEOPM_DOMAIN_INVALID; this IOGroup does not provide any controls.
+            int control_domain_type(const std::string &control_name) const override;
+            /// @param signal_name Adds the signal specified to the list of signals to be read during read_batch()
+            ///
+            /// @param domain_type must be one of the PlatformTopo::m_domain_e enum values
+            ///
+            /// @param domain_idx
+            ///
+            /// @throws geopm::Exception
+            ///         if signal_name is not valid
+            ///         if domain_type is not valid
+            ///         if domain_idx is not valid (out of range)
+            int push_signal(const std::string &signal_name, int domain_type, int domain_idx) override;
+            /// @brief Should not be called; this IOGroup does not provide any controls.
+            ///
+            /// @throws geopm::Exception there are no controls supported by the ConstConfigIOGroup
+            int push_control(const std::string &control_name, int domain_type, int domain_idx) override;
+            /// @brief this is essentially a no-op as this IOGroup's signals are constant.
+            void read_batch(void) override;
+            /// @brief Does nothing; this IOGroup does not provide any controls.
+            void write_batch(void) override;
+            /// @param batch_idx Specifies a signal_idx returned from push_signal()
+            ///
+            /// @return the value of the signal specified by a signal_idx returned from push_signal().
+            double sample(int batch_idx) override;
+            /// @brief Should not be called; this IOGroup does not provide any controls.
+            ///
+            /// @throws geopm::Exception there are no controls supported by the ConstConfigIOGroup
+            void adjust(int batch_idx, double setting) override;
+            /// @param signal_name Specifies the name of the signal whose value you want to get.
+            ///
+            /// @param domain_type This must be == GEOPM_DOMAIN_BOARD
+            ///
+            /// @param domain_idx is ignored
+            ///
+            /// @throws geopm::Exception
+            ///         if signal_name is not valid
+            ///         if domain_type is not GEOPM_DOMAIN_BOARD
+            ///
+            /// @return the stored value for the given signal_name.
+            double read_signal(const std::string &signal_name, int domain_type, int domain_idx) override;
+            /// @brief Should not be called; this IOGroup does not provide any controls.
+            ///
+            /// @throws geopm::Exception there are no controls supported by the ConstConfigIOGroup
+            void write_control(const std::string &control_name, int domain_type, int domain_idx, double setting) override;
+            /// @brief Does nothing; this IOGroup does not provide any controls.
+            void save_control(void) override;
+            /// @brief Does nothing; this IOGroup does not provide any controls.
+            void restore_control(void) override;
+            /// @return  a function that should be used when aggregating the given signal.
+            ///
+            /// @see geopm::Agg
+            std::function<double(const std::vector<double> &)> agg_function(const std::string &signal_name) const override;
+            /// @return  a function that should be used when formatting the given signal.
+            ///
+            /// @see geopm::Agg
+            std::function<std::string(double)> format_function(const std::string &signal_name) const override;
+            /// @return a string description for signal_name, if defined.
+            std::string signal_description(const std::string &signal_name) const override;
+            /// @throws geopm::Exception there are no controls supported by the ConstConfigIOGroup
+            std::string control_description(const std::string &control_name) const override;
+            /// @return M_SIGNAL_BEHAVIOR_CONSTANT 
+            int signal_behavior(const std::string &signal_name) const override;
+            /// @brief Does nothing; this IOGroup does not provide any controls.
+            ///
+            /// @param save_path this argument is ignored
+            void save_control(const std::string &save_path) override;
+            /// @brief Does nothing; this IOGroup does not provide any controls.
+            ///
+            /// @param save_path this argument is ignored
+            void restore_control(const std::string &save_path) override;
+            /// @return the name of the IOGroup
+            std::string name(void) const override;
+            /// @return the name of the plugin to use when this plugin is
+            ///         registered with the IOGroup factory
+            ///
+            /// @see geopm::PluginFactory
+            static std::string plugin_name(void);
+            /// @return a pointer to a new ConstConfigIOGroup object
+            ///
+            /// @see geopm::PluginFactory
+            static std::unique_ptr<IOGroup> make_plugin(void);
+
+        private:
+            struct m_signal_info_s {
+                double value;
+                int units;
+                std::function<double(const std::vector<double> &)> agg_function;
+                std::string description;
+            };
+
+            std::map<std::string, m_signal_info_s> m_signal_available;
+    };
+}
+
+#endif
