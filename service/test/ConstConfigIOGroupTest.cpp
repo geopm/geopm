ee0aa6bad57a88e31af354c6cfa1a004219af529
Konstantin Rebrov <konstantin.rebrov@intel.com>
2022-10-20 10:41:30 -0700

    Adding positive and negative ConstConfigIOGroup tests
    - Testing all the query methods based on a valid inputs.
    
    Signed-off-by: Konstantin Rebrov <konstantin.rebrov@intel.com>

--- service/test/ConstConfigIOGroupTest.cpp
+++ service/test/ConstConfigIOGroupTest.cpp
@@ -18,6 +18,8 @@
 #include "geopm_topo.h"  // for geopm_domain_e
 #include "geopm_test.hpp" // for GEOPM_EXPECT_THROW_MESSAGE
 
+#include "geopm/Helper.hpp"
+#include "geopm/Agg.hpp"
 #include "geopm/Exception.hpp"
 #include "geopm/PlatformIO.hpp"
 #include "geopm/PlatformTopo.hpp"
@@ -58,7 +60,7 @@ TEST_F(ConstConfigIOGroupTest, input_gibberish)
     );
 }
 
-TEST_F(ConstConfigIOGroupTest, valid_json)
+TEST_F(ConstConfigIOGroupTest, valid_json_positive)
 {
     std::string json_string = "{"
     "    \"GPU_CORE_FREQUENCY\": {"
@@ -66,23 +68,194 @@ TEST_F(ConstConfigIOGroupTest, valid_json)
     "        \"description\": \"Provides GPU core frequency\","
     "        \"units\": \"hertz\","
     "        \"aggregation\": \"sum\","
-    "        \"values\": { 1500, 1600, 1700 }"
+    "        \"values\": [ 1500, 1600, 1700 ]"
+    "    },"
+    "    \"CPU_CORE_FREQUENCY\": {"
+    "        \"domain\": \"cpu\","
+    "        \"description\": \"Provides CPU core frequency\","
+    "        \"units\": \"watts\","
+    "        \"aggregation\": \"average\","
+    "        \"values\": [ 1050, 1060, 1070 ]"
     "    }"
     "}";
     ConstConfigIOGroup iogroup(json_string);
-    // EXPECT_EQ(iogroup.control_names(), std::set<std::string>{});
+    EXPECT_EQ(iogroup.signal_names(), (std::set<std::string>{
+        "CONST_CONFIG::CPU_CORE_FREQUENCY", "CONST_CONFIG::GPU_CORE_FREQUENCY"}));
+    EXPECT_EQ(iogroup.is_valid_signal("CONST_CONFIG::CPU_CORE_FREQUENCY"), true);
+    EXPECT_EQ(iogroup.is_valid_signal("CONST_CONFIG::GPU_CORE_FREQUENCY"), true);
+    EXPECT_EQ(iogroup.signal_domain_type("CONST_CONFIG::CPU_CORE_FREQUENCY"), GEOPM_DOMAIN_CPU);
+    EXPECT_EQ(iogroup.signal_domain_type("CONST_CONFIG::GPU_CORE_FREQUENCY"), GEOPM_DOMAIN_GPU);
+    EXPECT_EQ(iogroup.push_signal("CONST_CONFIG::CPU_CORE_FREQUENCY", GEOPM_DOMAIN_CPU, 1), 0);
+    EXPECT_EQ(iogroup.push_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, 2), 1);
+    EXPECT_EQ(iogroup.sample(0), 1060);
+    EXPECT_EQ(iogroup.sample(1), 1700);
+    EXPECT_EQ(iogroup.read_signal("CONST_CONFIG::CPU_CORE_FREQUENCY", GEOPM_DOMAIN_CPU, 2), 1070);
+    EXPECT_EQ(iogroup.read_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, 0), 1500);
+    {
+        auto expected_function = geopm::Agg::average;
+        auto actual_function = *iogroup.agg_function("CONST_CONFIG::CPU_CORE_FREQUENCY")
+            .target<double (*) (const std::vector<double>&)>();
+        EXPECT_EQ(expected_function, actual_function);
+    }
+    {
+        auto expected_function = geopm::Agg::sum;
+        auto actual_function = *iogroup.agg_function("CONST_CONFIG::GPU_CORE_FREQUENCY")
+            .target<double (*) (const std::vector<double>&)>();
+        EXPECT_EQ(expected_function, actual_function);
+    }
+    {
+        auto expected_function = geopm::string_format_double;
+        auto actual_function = *iogroup.format_function("CONST_CONFIG::CPU_CORE_FREQUENCY")
+            .target<std::string (*) (double)>();
+        EXPECT_EQ(expected_function, actual_function);
+    }
+    {
+        auto expected_function = geopm::string_format_double;
+        auto actual_function = *iogroup.format_function("CONST_CONFIG::GPU_CORE_FREQUENCY")
+            .target<std::string (*) (double)>();
+        EXPECT_EQ(expected_function, actual_function);
+    }
+    EXPECT_EQ(iogroup.signal_description("CONST_CONFIG::CPU_CORE_FREQUENCY"),
+        "    description: " "Provides CPU core frequency" "\n"
+        "    units: " "watts" "\n"
+        "    aggregation: " "average" "\n"
+        "    domain: " "board" "\n"
+        "    iogroup: ConstConfigIOGroup");
+    EXPECT_EQ(iogroup.signal_description("CONST_CONFIG::GPU_CORE_FREQUENCY"),
+        "    description: " "Provides GPU core frequency" "\n"
+        "    units: " "hertz" "\n"
+        "    aggregation: " "sum" "\n"
+        "    domain: " "board" "\n"
+        "    iogroup: ConstConfigIOGroup");
+    EXPECT_EQ(iogroup.signal_behavior("CONST_CONFIG::CPU_CORE_FREQUENCY"), geopm::IOGroup::M_SIGNAL_BEHAVIOR_CONSTANT);
+    EXPECT_EQ(iogroup.signal_behavior("CONST_CONFIG::GPU_CORE_FREQUENCY"), geopm::IOGroup::M_SIGNAL_BEHAVIOR_CONSTANT);
+    EXPECT_EQ(iogroup.name(), "CONST_CONFIG");
+    EXPECT_EQ(iogroup.plugin_name(), "CONST_CONFIG");
 }
 
-TEST_F(ConstConfigIOGroupTest, control_names)
+TEST_F(ConstConfigIOGroupTest, valid_json_negative)
 {
-    ConstConfigIOGroup iogroup("");
+    std::string json_string = "{"
+    "    \"GPU_CORE_FREQUENCY\": {"
+    "        \"domain\": \"gpu\","
+    "        \"description\": \"Provides GPU core frequency\","
+    "        \"units\": \"hertz\","
+    "        \"aggregation\": \"sum\","
+    "        \"values\": [ 1500, 1600, 1700 ]"
+    "    }"
+    "}";
+    ConstConfigIOGroup iogroup(json_string);
+
     EXPECT_EQ(iogroup.control_names(), std::set<std::string>{});
-}
-
-TEST_F(ConstConfigIOGroupTest, is_valid_control)
-{
-    ConstConfigIOGroup iogroup("");
+    EXPECT_EQ(iogroup.is_valid_signal("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"), false);
     EXPECT_EQ(iogroup.is_valid_control("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"), false);
+    EXPECT_EQ(iogroup.signal_domain_type("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"), GEOPM_DOMAIN_INVALID);
+    EXPECT_EQ(iogroup.control_domain_type("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"), GEOPM_DOMAIN_INVALID);
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.push_signal("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO", 0, 0),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::push_signal(): "
+        "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO" " not valid for ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.push_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_CPU, 0),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::push_signal(): domain_type "
+        "3" " not valid for ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.push_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, -1),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::push_signal(): domain_idx "
+        "-1" " out of range."
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.push_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, 3),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::push_signal(): domain_idx "
+        "3" " out of range."
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.push_control("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO", 0, 0),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::push_control(): there are no "
+        "controls supported by the ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.sample(3),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::sample(): batch_idx "
+        "3" " out of range."
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.adjust(0, 3.14),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::adjust(): there are no controls "
+        "supported by the ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.read_signal("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO", 0, 0),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::read_signal(): "
+        "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO" " not valid for ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.read_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_CPU, 0),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::read_signal(): domain_type "
+        "3"
+        " not valid for ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.read_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, -1),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::read_signal(): domain_idx "
+        "-1" " out of range."
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.read_signal("CONST_CONFIG::GPU_CORE_FREQUENCY", GEOPM_DOMAIN_GPU, 3),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::read_signal(): domain_idx "
+        "3" " out of range."
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.write_control("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO", 0, 0, 3.14),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::write_control(): there are no "
+        "controls supported by the ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.agg_function("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::agg_function(): unknown how "
+        "to aggregate \"" "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO" "\""
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.format_function("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::format_function(): unknown "
+        "how to format \"" "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO" "\""
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.signal_description("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::signal_description(): "
+        "signal_name " "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"
+        " not valid for ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.control_description("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::control_description: there are "
+        "no controls supported by the ConstConfigIOGroup"
+    );
+    GEOPM_EXPECT_THROW_MESSAGE(
+        iogroup.signal_behavior("CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"),
+        GEOPM_ERROR_INVALID,
+        "ConstConfigIOGroup::signal_behavior(): "
+        "signal_name " "CONST_CONFIG::UNCORE_RATIO_LIMIT:MIN_RATIO"
+        " not valid for ConstConfigIOGroup"
+    );
 }
 
 void ConstConfigIOGroupTest::TearDown()
