6fd8b2d19d324d294c1a52b320c3d372da46959d
lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>
2022-10-24 14:08:32 -0700

    updated GPU-CA experiment scripts to new naming scheme
    Signed-off-by: lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>

--- integration/experiment/energy_efficiency/Makefile.mk
+++ integration/experiment/energy_efficiency/Makefile.mk
@@ -17,8 +17,8 @@ EXTRA_DIST += integration/experiment/energy_efficiency/barrier_frequency_sweep.p
               integration/experiment/energy_efficiency/run_cpu_activity_nasft.py \
               integration/experiment/energy_efficiency/run_cpu_activity_nekbone.py \
               integration/experiment/energy_efficiency/run_cpu_activity_pennant.py \
-              integration/experiment/energy_efficiency/run_gpu_activity_parres_dgemm.py \
-              integration/experiment/energy_efficiency/run_gpu_activity_parres_nstream.py \
+              integration/experiment/energy_efficiency/run_gpu_activity_parres_dgemm_cuda.py \
+              integration/experiment/energy_efficiency/run_gpu_activity_parres_nstream_cuda.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_amg.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_dgemm.py \
               integration/experiment/energy_efficiency/run_power_balancer_energy_hpcg.py \
---
+++ integration/experiment/energy_efficiency/run_gpu_activity_parres_dgemm_cuda.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+'''
+Run ParRes dgemm with the gpu_activity agent.
+'''
+import argparse
+
+from experiment import machine
+from experiment.energy_efficiency import gpu_activity
+from apps.parres import parres
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    gpu_activity.setup_run_args(parser)
+    parres.setup_run_args(parser)
+    args, extra_args = parser.parse_known_args()
+    mach = machine.init_output_dir(args.output_dir)
+    app_conf = parres.create_dgemm_appconf_cuda(mach, args)
+    gpu_activity.launch(app_conf=app_conf, args=args,
+                        experiment_cli_args=extra_args)
