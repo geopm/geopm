d4c710b1f803669b55292d4ba26cdc1596ccbe9f
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2022-10-28 14:45:14 -0700

    Added docstrings to TensorOneD.hpp.

--- src/TensorOneD.hpp
+++ src/TensorOneD.hpp
@@ -11,21 +11,41 @@
 
 namespace geopm
 {
+    /// @brief Class to store and perform operations on 1D Tensors,
+    ///        aka vectors, suitable for use in feed-forward neural
+    ///        networks.
     class TensorOneD
     {
         public:
+            ///@brief Constructor
             TensorOneD();
+            ///@brief Constructor with size specified 
             TensorOneD(std::size_t n);
+            ///@brief Constructs a deep copy of the argument
             TensorOneD(const TensorOneD&);
+            ///@brief Constructor inputting from external JSON
             TensorOneD(json11::Json input);
+            ///@brief Set length of 1D tensor
+            ///
+            ///If the instance has more than n elements, it will
+            ///be truncated. If it has fewer than n elements, the
+            ///tensor will be expanded to a total size of n, uninitialized.
             void set_dim(std::size_t n);
+            ///@brief Get the length of the 1D tensor
             std::size_t get_dim() const;
+            ///@brief Add two 1D tensors
             TensorOneD operator+(const TensorOneD&);
+            ///@brief Subtract two 1D tensors
             TensorOneD operator-(const TensorOneD&);
+            ///@brief Multiply two 1D tensors
             float operator*(const TensorOneD&);
+            ///@brief Overload = operator with a deep copy
             TensorOneD& operator=(const TensorOneD&);
+            ///@brief Reference indexing of 1D Tensor value at idx
             float &operator[](std::size_t idx);
+            ///@brief Value access of 1D Tensor value at idx.
             float operator[](std::size_t idx) const;
+            ///@brief Compute logistic sigmoid function of 1D Tensor
             TensorOneD sigmoid() const;
 
         private:
