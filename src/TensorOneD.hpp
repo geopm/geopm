f819ac7da8e3b1d37b07b1235b789e65fe97e145
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2022-11-04 16:32:07 -0700

    Fixed doxygen formatting for TensorOneD.

--- src/TensorOneD.hpp
+++ src/TensorOneD.hpp
@@ -18,85 +18,84 @@ namespace geopm
     {
         public:
             TensorOneD() = default;
-            ///@brief Constructor with size specified 
+            /// @brief Constructor with size specified 
             ///
             /// @param [in] n Size of 1D tensor
             TensorOneD(std::size_t n);
-            ///@brief Constructs a deep copy of the argument
+            /// @brief Constructs a deep copy of the argument
             ///
-            ///@param [in] TensorOneD Tensor to copy
+            /// @param [in] TensorOneD Tensor to copy
             TensorOneD(const TensorOneD&);
 
             TensorOneD(TensorOneD &&other);
             /// @param [in] input json11::Json input The JSON array
+            ///
+            /// @throws geopm::Exception if input is not an array,
+            /// if input is empty, or if a non-numeric type is found in input.
             TensorOneD(json11::Json input);
-            ///@brief Set length of 1D tensor
+            /// @brief Set length of 1D tensor
             ///
-            ///If the instance has more than n elements, it will
-            ///be truncated. If it has fewer than n elements, the
-            ///tensor will be expanded to a total size of n, uninitialized.
+            /// If the instance has more than n elements, it will
+            /// be truncated. If it has fewer than n elements, the
+            /// tensor will be expanded to a total size of n, uninitialized.
             ///
-            ///@throws geopm::Exception if input is not an array,
-            ///if input is empty, or if a non-numeric type is found in input.
-            ///
-            ///@param [in] n Resulting size of 1D tensor
+            /// @param [in] n Resulting size of 1D tensor
             inline void set_dim(std::size_t dim)
             {
                 m_vec.resize(dim);
             }
-
-            ///@brief Get the length of the 1D tensor
+            /// @brief Get the length of the 1D tensor
             ///
-            ///@return Returns the length of the 1D tensor
+            /// @return Returns the length of the 1D tensor
             inline std::size_t get_dim() const
             {
                 return m_vec.size();
             }
-            ///@brief Add two 1D tensors, element-wise
+            /// @brief Add two 1D tensors, element-wise
             ///
-            ///The tensors need to be the same length. 
+            /// The tensors need to be the same length. 
             ///
-            ///@throws geopm::Exception if the lengths do not match.
+            /// @throws geopm::Exception if the lengths do not match.
             ///
-            ///@param [in] other The summand
+            /// @param [in] other The summand
             ///
-            ///@return Returns a 1D tensor, the sum of two 1D tensors
+            /// @return Returns a 1D tensor, the sum of two 1D tensors
             TensorOneD operator+(const TensorOneD& other);
-            ///@brief Subtract two 1D tensors, element-wise
+            /// @brief Subtract two 1D tensors, element-wise
             ///
-            ///@throws geopm::Exception if the lengths do not match.
+            /// @throws geopm::Exception if the lengths do not match.
             ///
-            ///@param [in] other The subtrahend
+            /// @param [in] other The subtrahend
             ///
             /// @return A 1D tensor, the difference of two 1D tensors.
             TensorOneD operator-(const TensorOneD& other);
-            ///@brief Multiply two 1D tensors, element-wise
+            /// @brief Multiply two 1D tensors, element-wise
             ///
-            ///@throws geopm::Exception if the lengths do not match.
+            /// @throws geopm::Exception if the lengths do not match.
             ///
-            ///@param [in] other The multiplicand
+            /// @param [in] other The multiplicand
             ///
-            ///@return Returns a 1D tensor, the product of two 1D tensors
+            /// @return Returns a 1D tensor, the product of two 1D tensors
             float operator*(const TensorOneD& other);
-            ///@brief Overload = operator with an in-place deep copy
+            /// @brief Overload = operator with an in-place deep copy
             ///
-            ///@param [in] other The assignee (tensor to be copied)
+            /// @param [in] other The assignee (tensor to be copied)
             TensorOneD& operator=(const TensorOneD& other);
 
             TensorOneD& operator=(TensorOneD &&other);
-            ///@brief Reference indexing of 1D tensor value at idx
+            /// @brief Reference indexing of 1D tensor value at idx
             ///
-            ///@param [in] idx The index at which to look for the value
+            /// @param [in] idx The index at which to look for the value
             ///
-            ///@return Returns a reference to the 1D tensor at idx
+            /// @return Returns a reference to the 1D tensor at idx
             float &operator[](std::size_t idx);
-            ///@brief Value access of 1D Tensor value at idx.
+            /// @brief Value access of 1D Tensor value at idx.
             ///
-            ///@param [in] idx The index at which to look for the value
+            /// @param [in] idx The index at which to look for the value
             ///
-            ///@return Returns the value of the 1D tensor at idx
+            /// @return Returns the value of the 1D tensor at idx
             float operator[](std::size_t idx) const;
-            ///@brief Compute logistic sigmoid function of 1D Tensor
+            /// @brief Compute logistic sigmoid function of 1D Tensor
             TensorOneD sigmoid() const;
 
         private:
