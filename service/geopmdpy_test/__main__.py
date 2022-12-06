96f9cc4b6fb00cecf419b29d343e7b9fdc58ae47
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-12-06 12:40:39 -0800

    Fixup imports in geopmdpy_test main
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/geopmdpy_test/__main__.py
+++ service/geopmdpy_test/__main__.py
@@ -8,18 +8,20 @@ import unittest
 
 from TestAccess import *
 from TestAccessLists import *
+from TestActiveSessions import *
 from TestController import *
 from TestDBusXML import *
 from TestError import *
+from TestMSRDataFiles import *
 from TestPIO import *
 from TestPlatformService import *
 from TestRequestQueue import *
+from TestSecureFiles import *
 from TestSession import *
 from TestTimedLoop import *
 from TestTopo import *
-from TestActiveSessions import *
-from TestSecureFiles import *
-from TestAccessLists import *
+from TestWriteLock import *
+
 
 if __name__ == '__main__':
     unittest.main()
