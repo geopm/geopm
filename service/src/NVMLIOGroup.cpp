b15a957b2e2d46107c457ea2a347a3a0dff8edd3
lhlawson <lhlawson@gmail.com>
2022-12-13 11:27:10 -0800

    Address review feedback: Signal description
    Co-authored-by: Christopher Cantalupo <cmcantalupo@gmail.com>

--- service/src/NVMLIOGroup.cpp
+++ service/src/NVMLIOGroup.cpp
@@ -173,7 +173,7 @@ namespace geopm
                                   }},
                               {M_NAME_PREFIX + "GPU_CORE_FREQUENCY_STEP", {
                                   "The Streaming Multiprocessor frequency step size in hertz.\n"
-                                  "If the step size is variable the average of all steps is provided.",
+                                  "The average step size is provided in the case where the step size is variable.",
                                   {},
                                   GEOPM_DOMAIN_GPU,
                                   Agg::expect_same,
