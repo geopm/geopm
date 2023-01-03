a6bf96850fe18e8d73113e001c5f123a08346840
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-01-03 07:08:20 -0800

    Remove k8 files for using dbus
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/kube/dbus-test-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-strace dbus-send --address='unix:path=/tmp/dbus-test-socket' \
-	  --dest=org.freedesktop.ExampleName \
-	  /org/freedesktop/sample/object/name \
-	  org.freedesktop.ExampleInterface.ExampleMethod
-echo $?
