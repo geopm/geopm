5eeb7c8ea27fd4303c34b8705b92bd5e49d44779
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-01-12 11:36:53 -0800

    Recommended interface changes
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/ControlRecommender.hpp
+++ src/ControlRecommender.hpp
@@ -6,40 +6,51 @@
 #ifndef CONTROLRECOMMENDER_HPP_INCLUDE
 #define CONTROLRECOMMENDER_HPP_INCLUDE
 
+#include <memory>
+#include <vector>
+#include <string>
+
+struct geopm_request_s; // Defined in "geopm/PlatformIO.hpp"
+
 namespace geopm
 {
-    class PlatformTopo;
-    class PlatformIO;
-
+    /// @brief Abstract class that recommends control settings
     class ControlRecommender
     {
         public:
-            /// @brief Construct object, query the system for signals at the domains
-            /// required for the algorithm
             ControlRecommender() = default;
             virtual ~ControlRecommender() = default;
-
-            /// @brief Provides calling classes a way to inform the recommender what controls it
-            ///        would like to set
-            /// @param [in] control_name Map of desired controls and domain of interest.
-            /// @param [in] domain GEOPM Domain of desired controls and domain of interest.
-            /// @returns Recommender control index.  -1 indicates control not supported
-            virtual int control_supported(std::string control_name, int domain) = 0;
-
-            /// @brief Queries all signals of interest and updates the recommendations for controls
-            virtual void update_recommendation() = 0;
-
-            /// @brief Provides recommendations for control index provided
-            /// @param [in] control_idx Control index provided by controls_supported
-            /// @returns recommended values for specified control at the domain of interest
-            virtual std::vector<double> sample_recommendation(int control_idx) const = 0;
-
-            /// @brief Validates the policy provided to the performance model.
-            ///        Policy sender can request default value with 'NaN'.
-            virtual void validate_policy(std::vector<double> &in_policy) const = 0;
-
-            /// @brief Applies the policy provided to the performance model.
-            virtual void apply_policy(std::vector<double> &in_policy) = 0;
+            /// @brief Factory constructor
+            /// @param [in] algorithm Name of algorithm to select
+            /// @returns Unique pointer to a ControlRecommender objecct
+            static std::unique_ptr<ControlRecommender> make_unique(const std::string &algorithm);
+            /// @brief Set the performance bias
+            /// @aparam [in] bias Abstract number between 0 and 1: a
+            ///         value of 0 is most biased toward energy
+            ///         efficiency, and 1 is most biased toward
+            ///         performance.
+            virtual void performance_bias(double bias) = 0;
+            /// @brief Submit a set of requests for recommendations
+            ///
+            /// Called once to configure the object and discover
+            /// festure support.
+            ///
+            /// @param [in] Vector of control requests submitted for
+            ///        recommendation.
+            /// @returns The subset of the attempted_requests that are
+            ///          supported.  The order of these requestes
+            ///          reflects ordering of the returned values from
+            ///          the settings method.
+            virtual std::vector<geopm_request_s> supported_requests(const std::vector<geopm_request_t> &attempted_requests);
+            /// @brief Update recommended settings, method is called
+            ///        once prior calling the settings() method one or
+            ///        more times.
+            virtual void update(void) = 0;
+            /// @brief Get latest recommendation based determined on
+            ///        last call to update.
+            /// @returns Vector of settings for the requests returned
+            ///          by suppored_requests()
+            virtual std::vector<double> settings(void) const = 0;
     };
 }
 
