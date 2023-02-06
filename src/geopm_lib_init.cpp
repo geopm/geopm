599f5023286db38366cc816ac03d0e1b8edd2208
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-02-06 08:21:46 -0800

    Move profile init into library init function
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/geopm_lib_init.cpp
+++ src/geopm_lib_init.cpp
@@ -7,13 +7,26 @@
 #include "Environment.hpp"
 #include "geopm_pio.h"
 
+extern "C" {
+    /**
+     * Helper that creates the DefaultProfile signleton (if not already created)
+     * and catches all exceptions.
+     */
+    int geopm_prof_init(void);
+}
+
 static void __attribute__((constructor)) geopm_lib_init(void)
 {
-    std::string profile_name = geopm::environment().profile();
-    geopm_pio_start_profile(profile_name.c_str());
+    if (geopm::environment().do_profile()) {
+        std::string profile_name = geopm::environment().profile();
+        geopm_pio_start_profile(profile_name.c_str());
+        geopm_prof_init();
+    }
 }
 
 static void __attribute__((destructor)) geopm_lib_fini(void)
 {
-    geopm_pio_stop_profile();
+    if (geopm::environment().do_profile()) {
+        geopm_pio_stop_profile();
+    }
 }
