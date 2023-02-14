44604556a79915b3c510a1c5e3d4c91ba2709f14
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-02-14 12:53:28 -0800

    Fixup issue with profile enablement
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/DefaultProfile.cpp
+++ src/DefaultProfile.cpp
@@ -2,6 +2,8 @@
  * Copyright (c) 2015 - 2023, Intel Corporation
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include "config.h"
+
 #include "Profile.hpp"
 
 #include <unistd.h>
@@ -10,11 +12,8 @@
 #include "geopm_prof.h"
 #include "geopm/Exception.hpp"
 
-#include "config.h"
-
 // Don't allow multithreaded use of non-tprof profile calls
-static __thread int g_pmpi_prof_enabled = 0;
-static int g_pmpi_tprof_enabled = 0;
+static __thread int g_prof_enabled = 0;
 
 namespace geopm
 {
@@ -22,28 +21,15 @@ namespace geopm
     {
         public:
             DefaultProfile();
-            virtual ~DefaultProfile();
-            void enable_pmpi(void) override;
+            virtual ~DefaultProfile() = default;
     };
 
     DefaultProfile::DefaultProfile()
         : ProfileImp()
     {
-        g_pmpi_prof_enabled = m_is_enabled;
-        g_pmpi_tprof_enabled = m_is_enabled;
+        g_prof_enabled = 1;
     }
 
-    DefaultProfile::~DefaultProfile()
-    {
-        g_pmpi_prof_enabled = 0;
-        g_pmpi_tprof_enabled = 0;
-    }
-
-    void DefaultProfile::enable_pmpi(void)
-    {
-        g_pmpi_prof_enabled = m_is_enabled;
-        g_pmpi_tprof_enabled = m_is_enabled;
-    }
 
     Profile &Profile::default_profile(void)
     {
@@ -59,8 +45,7 @@ extern "C"
     {
         int err = 0;
         try {
-            geopm::Profile::default_profile().init();
-            geopm::Profile::default_profile().enable_pmpi();
+            geopm::Profile::default_profile();
         }
         catch (...) {
             err = geopm::exception_handler(std::current_exception(), true);
@@ -71,7 +56,7 @@ extern "C"
     int geopm_prof_region(const char *region_name, uint64_t hint, uint64_t *region_id)
     {
         int err = 0;
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 *region_id = geopm::Profile::default_profile().region(std::string(region_name), hint);
             }
@@ -88,7 +73,7 @@ extern "C"
     int geopm_prof_enter(uint64_t region_id)
     {
         int err = 0;
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 geopm::Profile::default_profile().enter(region_id);
             }
@@ -105,7 +90,7 @@ extern "C"
     int geopm_prof_exit(uint64_t region_id)
     {
         int err = 0;
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 geopm::Profile::default_profile().exit(region_id);
             }
@@ -122,7 +107,7 @@ extern "C"
     int geopm_prof_epoch(void)
     {
         int err = 0;
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 geopm::Profile::default_profile().epoch();
             }
@@ -139,7 +124,7 @@ extern "C"
     int geopm_prof_shutdown(void)
     {
         int err = 0;
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 geopm::Profile::default_profile().shutdown();
             }
@@ -157,7 +142,7 @@ extern "C"
     {
         int err = 0;
         // Only the lead thread calls through to thread_init()
-        if (g_pmpi_prof_enabled) {
+        if (g_prof_enabled) {
             try {
                 geopm::Profile::default_profile().thread_init(num_work_unit);
             }
@@ -172,14 +157,12 @@ extern "C"
     {
         int err = 0;
         // All threads call through to thread_post()
-        if (g_pmpi_tprof_enabled) {
-            try {
-                int cpu = geopm::Profile::get_cpu();
-                geopm::Profile::default_profile().thread_post(cpu);
-            }
-            catch (...) {
-                err = geopm::exception_handler(std::current_exception(), true);
-            }
+        try {
+            int cpu = geopm::Profile::get_cpu();
+            geopm::Profile::default_profile().thread_post(cpu);
+        }
+        catch (...) {
+            err = geopm::exception_handler(std::current_exception(), true);
         }
         return err;
     }
