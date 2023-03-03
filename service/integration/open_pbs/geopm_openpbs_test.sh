c22b24ef9223e3b307e0bc7981d36bf6cc752d16
Alejandro Vilches <alejandro.vilches@intel.com>
2023-03-02 19:37:02 -0800

    Fix OpenPBS hook test error
    Some python modules were not being found
    
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

--- service/integration/open_pbs/geopm_openpbs_test.sh
+++ service/integration/open_pbs/geopm_openpbs_test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#  Copyright (c) 2015 - 2022, Intel Corporation
+#  Copyright (c) 2015 - 2023, Intel Corporation
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
 test_dir=$(dirname $0)
+real_path=$(dirname $(readlink -f $0))
+top_dir=$(readlink -f ${real_path}/../..)
+
 tests=`ls ${test_dir}/Test*.py`
+
+export PYTHONPATH=${top_dir}:${PYTHONPATH}
 
 for t in $tests; do
     ${t} --verbose >& ${t}.log
