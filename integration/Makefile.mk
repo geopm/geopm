82ea8f25134e96af7997982f32220544292efebe
Alejandro Vilches <alejandro.vilches@intel.com>
2023-03-14 16:29:29 -0700

    Non-MPI app support: fix test-dist
    Add integration/test-no-mpi to dist
    
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

--- integration/Makefile.mk
+++ integration/Makefile.mk
@@ -15,6 +15,18 @@ EXTRA_DIST += integration/README.md \
               integration/config/smng_env.sh \
               integration/config/theta_env.sh \
               integration/requirements.txt \
+              integration/test-no-mpi/example_output/test_sleep_region_map_report.yaml \
+              integration/test-no-mpi/example_output/test_sleep_region_map_trace_profile.csv \
+              integration/test-no-mpi/example_output/test_sleep_region_report.yaml \
+              integration/test-no-mpi/example_output/test_sleep_region_trace_profile.csv \
+              integration/test-no-mpi/example_output/test_sleep_report.yaml \
+              integration/test-no-mpi/example_output/test_sleep_trace_profile.csv \
+              integration/test-no-mpi/README.md \
+              integration/test-no-mpi/Makefile \
+              integration/test-no-mpi/test_sleep_region.c \
+              integration/test-no-mpi/test_sleep_region_map.sh \
+              integration/test-no-mpi/test_sleep_region.sh \
+              integration/test-no-mpi/test_sleep.sh \
               # end
 
 include integration/apps/Makefile.mk
