7bc74f062e621225f842a7e2c8c7775fbc0e4a1a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-03-15 17:45:42 -0700

    Use versioned shared object file for preload
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- scripts/geopmpy/launcher.py
+++ scripts/geopmpy/launcher.py
@@ -818,7 +818,7 @@ Warning: <geopm> geopmpy.launcher: Incompatible CPU frequency governor
     def preload_option(self):
         if self.config and self.config.get_preload():
             self.environ_ext['LD_PRELOAD'] = ':'.join((ll for ll in
-                                                       ('libgeopmload.so', os.getenv('LD_PRELOAD'))
+                                                       ('libgeopmload.so.1.0.0', os.getenv('LD_PRELOAD'))
                                                        if ll is not None))
         return []
 
@@ -1135,7 +1135,7 @@ class SrunLauncher(Launcher):
         result = []
         if self.config and self.config.get_preload():
             value = ':'.join((ll for ll in
-                              ('libgeopmload.so', os.getenv('LD_PRELOAD'))
+                              ('libgeopmload.so.1.0.0', os.getenv('LD_PRELOAD'))
                               if ll is not None))
             result = ["--export=LD_PRELOAD={},ALL".format(value)]
         return result
@@ -1684,7 +1684,7 @@ class AprunLauncher(Launcher):
         result = []
         if self.config and self.config.get_preload():
             value = ':'.join((ll for ll in
-                              ('libgeopmload.so', os.getenv('LD_PRELOAD'))
+                              ('libgeopmload.so.1.0.0', os.getenv('LD_PRELOAD'))
                               if ll is not None))
             result = ['-e',  'LD_PRELOAD={}'.format(value)]
         return result
