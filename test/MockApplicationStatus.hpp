5e88a37c2bda08a989d205c5236143cf9dbbe1bf
Daniel Wilson <daniel1.wilson@intel.com>
2023-03-23 12:07:29 -0700

    Fix mismatched mock for ApplicationStatus
    Signed-off-by: Daniel Wilson <daniel1.wilson@intel.com>

--- test/MockApplicationStatus.hpp
+++ test/MockApplicationStatus.hpp
@@ -23,7 +23,7 @@ class MockApplicationStatus : public geopm::ApplicationStatus
                     (override));
         MOCK_METHOD(void, increment_work_unit, (int cpu_idx), (override));
         MOCK_METHOD(double, get_progress_cpu, (int cpu_idx), (const, override));
-        MOCK_METHOD(void, set_valid_cpu, (const std::set<int> &cpu_idx, bool is_valid), (override));
+        MOCK_METHOD(void, set_valid_cpu, (const std::set<int> &cpu_idx), (override));
         MOCK_METHOD(void, update_cache, (), (override));
 };
 
