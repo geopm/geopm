8074a796f3ae54e8392117a4711454b3a5b59e05
lhlawson <lowren.h.lawson@intel.com>
2023-03-28 11:20:54 -0700

    Legacy tests passing
    Signed-off-by: lhlawson <lowren.h.lawson@intel.com>

--- libgeopmd/test/LevelZeroIOGroupTest.cpp
+++ libgeopmd/test/LevelZeroIOGroupTest.cpp
@@ -210,9 +210,22 @@ void LevelZeroIOGroupTest::SetUpDefaultExpectCalls()
         // 1. Per gpu-chip / save_control: GPU_CORE_PERFORMANCE_FACTOR_CONTROL
         // 2. Per gpu-chip / control pruning: GPU_CORE_PERFORMANCE_FACTOR_CONTROL (Alias to PERFORMANCE_FACTOR)
         EXPECT_CALL(*m_device_pool, // GPU_CORE_PERFORMANCE_FACTOR
-                    performance_factor(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_COMPUTE)).Times(2);
+                    performance_factor(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_COMPUTE)).Times(3);
+        EXPECT_CALL(*m_device_pool, // GPU_CORE_TEMPERATURE_MAXIMUM
+                    temperature_max(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_COMPUTE));
+        EXPECT_CALL(*m_device_pool, // GPU_MEMORY_TEMPERATURE_MAXIMUM
+                    temperature_max(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_MEMORY));
 
-        // GPU_CORE_FREQUENCY_MAX_CONTROL, GPU_CORE_FREQUENCY_MIN_CONTROL, and the restore_control() call
+        // ZET testing
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::XVE_ACTIVE
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "XVE_ACTIVE"));
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::XVE_STALL
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "XVE_STALL"));
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::NUM_REPORTS
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "NUM_REPORTS"));
+
+        // control pruning expectations
+        // GPU_CORE_FREQUENCY_MAX_CONTROL, GPU_CORE_FREQUENCY_MIN_CONTROL, and the restore_control() direct call.
         EXPECT_CALL(*m_device_pool,
                     frequency_control(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_COMPUTE,
                                       0, 0)).Times(3);
@@ -889,6 +902,14 @@ TEST_F(LevelZeroIOGroupTest, signal_and_control_trimming)
         //                               0, 0)).Times(3);
         EXPECT_CALL(*m_device_pool, // GPU_CORE_PERFORMANCE_FACTOR_CONTROL
                     performance_factor_control(GEOPM_DOMAIN_GPU_CHIP, sub_idx, MockLevelZero::M_DOMAIN_COMPUTE, 0)).Times(2);
+
+        // ZET testing
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::XVE_ACTIVE
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "XVE_ACTIVE"));
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::XVE_STALL
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "XVE_STALL"));
+        EXPECT_CALL(*m_device_pool, // LEVELZERO::METRIC::NUM_REPORTS
+                    metric_sample(GEOPM_DOMAIN_GPU_CHIP, sub_idx, "NUM_REPORTS"));
     }
 
     // Expectations for signal pruning code in the constructor
