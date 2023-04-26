7cbd227bba57c5554ee9acc92deff026237b2a3b
Brad Geltz <brad.geltz@intel.com>
2023-04-26 16:30:35 -0700

    Rely on the system install of the service for the legacy nightlies
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/test/geopm_test_loop.sh
+++ integration/test/geopm_test_loop.sh
@@ -20,30 +20,12 @@ do
 
     if [ ${ERR} -eq 0 ]; then
         echo "TEST LOOPER: Continuing loop ${COUNT} service tests..." > >(tee -a ${LOG_FILE})
-        # Requirements for service testing
-        srun -N${SLURM_NNODES} -- sudo /usr/sbin/install_service.sh $(cat ${GEOPM_SOURCE}/VERSION) ${USER}
-
-        # Set initial access
-        cat > grant_access.sh <<EOF
-#!/bin/bash
-sudo geopmaccess -a | sudo geopmaccess -w
-sudo geopmaccess -ac | sudo geopmaccess -wc
-EOF
-        chmod +x grant_access.sh
-        srun -N ${SLURM_NNODES} -- grant_access.sh
-
         python3 -m unittest discover \
                 --top-level-directory ${GEOPM_SOURCE} \
                 --start-directory ${GEOPM_SOURCE}/service/integration/test \
                 --pattern 'test_*.py' \
                 --verbose &> >(tee -a service_${LOG_FILE})
         ERR=$?
-
-        if [ ${ERR} -eq 0 ]; then
-            # Only uninstall the service if the tests were clean.
-            # Otherwise leave the service in place so it can be debugged in its current state.
-            srun -N${SLURM_NNODES} -- sudo /usr/sbin/install_service.sh --remove
-        fi
     fi
     echo "TEST LOOPER: Loop ${COUNT} done." > >(tee -a ${LOG_FILE})
 done
