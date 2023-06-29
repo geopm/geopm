127d90777c3caacc3e45379ba856b4119a9cba1a
Brad Geltz <brad.geltz@intel.com>
2023-06-29 09:26:32 -0700

    Use the new Intel compilers
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/config/australis_env.sh
+++ integration/config/australis_env.sh
@@ -9,13 +9,13 @@
 # This script is intended to be sourced within an existing script or shell ONLY.
 # It is NOT intended to be ./ executed.
 
-export CC=icc
-export CXX=icpc
+export CC=icx
+export CXX=icpx
 export MPICC=mpicc
 export MPICXX=mpic++
-export FC=ifort
-export F77=ifort
-export F90=ifort
+export FC=ifx
+export F77=ifx
+export F90=ifx
 export MPIFORT=mpifort
 export MPIFC=mpifort
 export MPIF77=mpifort
