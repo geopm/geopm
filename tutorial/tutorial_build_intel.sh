7c03c450676863da16b08320300773e25f0c1772
Brad Geltz <brad.geltz@intel.com>
2023-07-18 09:57:15 -0700

    Fix build issues related to the clang-based compilers
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- tutorial/tutorial_build_intel.sh
+++ tutorial/tutorial_build_intel.sh
@@ -11,8 +11,8 @@ if [ ! "$OMP_FLAGS" ]; then
     OMP_FLAGS="-qopenmp"
 fi
 
-export CC=${CC:-icc}
-export CXX=${CXX:-icpc}
+export CC=${CC:-icx}
+export CXX=${CXX:-icpx}
 export MPICC=${MPICC:-mpiicc}
 export MPICXX=${MPICXX:-mpiicpc}
 export MPIFC=${MPIFC:-mpiifort}
@@ -21,4 +21,4 @@ export MPIF77=${MPIF77:-mpiifort}
 make \
 CFLAGS="$GEOPM_CFLAGS $OMP_FLAGS -DTUTORIAL_ENABLE_MKL -D_GNU_SOURCE -std=c99 -xAVX $CFLAGS" \
 CXXFLAGS="$GEOPM_CFLAGS $OMP_FLAGS -DTUTORIAL_ENABLE_MKL -D_GNU_SOURCE -std=c++17 -xAVX $CXXFLAGS" \
-LDFLAGS="$GEOPM_LDFLAGS $OMP_FLAGS -lm -lrt -mkl -xAVX $LDFLAGS"
+LDFLAGS="$GEOPM_LDFLAGS $OMP_FLAGS -lm -lrt -qmkl -xAVX $LDFLAGS"
