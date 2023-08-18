a92f102bd0e8e7a11619eaa1b6fe8bf95ba8717a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-08-18 15:16:11 -0700

    Add back in connect method along with comment explaining why it is there
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/geopm_lib_init.cpp
+++ src/geopm_lib_init.cpp
@@ -17,6 +17,10 @@ static void __attribute__((constructor)) geopm_lib_init(void)
         try {
             geopm_time_s zero = geopm::time_zero();
             auto &prof = geopm::Profile::default_profile();
+            // If this is a forked process, it will need to call
+            // connect() since DefaultProfile constructor was called
+            // by parent process.
+            prof.connect();
             prof.overhead(geopm_time_since(&zero));
         }
         catch (...) {
