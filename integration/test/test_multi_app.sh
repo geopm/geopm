094b8e1d82684ddcb77c4944394ec5226507005e
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-08-18 09:42:15 -0700

    Modify test_multi_app to use GEOPM_PROGRAM_FILTER
    - Set LD_PRELOAD for entire script, and profile only stress-ng and geopmbench.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/test/test_multi_app.sh
+++ integration/test/test_multi_app.sh
@@ -3,6 +3,11 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
+TEST_NAME=test_multi_app
+export GEOPM_PROFILE=${TEST_NAME}
+export GEOPM_PROGRAM_FILTER=geopmbench,stress-ng
+export LD_PRELOAD=libgeopm.so.1.0.0
+
 cat > temp_config.json << "EOF"
 {
     "loop-count": 2,
@@ -13,20 +18,17 @@ EOF
 
 source ${GEOPM_SOURCE}/integration/config/run_env.sh
 
-TEST_NAME=test_multi_app
-export GEOPM_REPORT=${TEST_NAME}_report.yaml
-export GEOPM_TRACE=${TEST_NAME}_trace.csv
-export GEOPM_TRACE_PROFILE=${TEST_NAME}_trace_profile.csv
-export GEOPM_PROFILE=${TEST_NAME}
-export GEOPM_REPORT_SIGNALS=TIME@package
-export GEOPM_NUM_PROC=2
-
+GEOPM_REPORT=${TEST_NAME}_report.yaml \
+GEOPM_TRACE=${TEST_NAME}_trace.csv \
+GEOPM_TRACE_PROFILE=${TEST_NAME}_trace_profile.csv \
+GEOPM_REPORT_SIGNALS=TIME@package \
+GEOPM_NUM_PROC=2 \
 geopmctl &
 
 # geopmbench
-numactl --cpunodebind=0 -- sh -c "LD_PRELOAD=libgeopm.so.1.0.0 geopmbench temp_config.json" &
+numactl --cpunodebind=0 -- geopmbench temp_config.json &
 
 # stress-ng
-numactl --cpunodebind=1 -- sh -c "LD_PRELOAD=libgeopm.so.1.0.0 stress-ng --cpu 1 --timeout 10" &
+numactl --cpunodebind=1 -- stress-ng --cpu 1 --timeout 10 &
 
 wait $(jobs -p)
