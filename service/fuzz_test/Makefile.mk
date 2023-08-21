be61c88bc07f5a2d03876d550ff281f761730025
Alejandro Vilches <alejandro.vilches@intel.com>
2023-08-21 09:33:37 -0700

    Fix fuzz_test paths in makefiles
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

--- service/fuzz_test/Makefile.mk
+++ service/fuzz_test/Makefile.mk
@@ -2,14 +2,14 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
-check_PROGRAMS += fuzz_tests/geopmhash_fuzz_test \
-                  fuzz_tests/geopmhash_reg_test \
+check_PROGRAMS += fuzz_test/geopmhash_fuzz_test \
+                  fuzz_test/geopmhash_reg_test \
                   #end
-fuzz_tests_geopmhash_fuzz_test_SOURCES = fuzz_tests/geopmhash_harness.cpp
-fuzz_tests_geopmhash_reg_test_SOURCES = fuzz_tests/geopmhash_harness.cpp \
-                                        fuzz_tests/StandaloneFuzzTargetMain.c \
-                                        # end
-fuzz_tests_geopmhash_fuzz_test_CXXFLAGS = $(AM_CXXFLAGS) -fsanitize=fuzzer -fno-inline
-fuzz_tests_geopmhash_reg_test_CXXFLAGS = $(AM_CXXFLAGS) -fno-inline
-fuzz_tests_geopmhash_fuzz_test_LDADD = libgeopmd.la
-fuzz_tests_geopmhash_reg_test_LDADD = libgeopmd.la
+fuzz_test_geopmhash_fuzz_test_SOURCES = fuzz_test/geopmhash_harness.cpp
+fuzz_test_geopmhash_reg_test_SOURCES = fuzz_test/geopmhash_harness.cpp \
+                                       fuzz_test/StandaloneFuzzTargetMain.c \
+                                       # end
+fuzz_test_geopmhash_fuzz_test_CXXFLAGS = $(AM_CXXFLAGS) -fsanitize=fuzzer -fno-inline
+fuzz_test_geopmhash_reg_test_CXXFLAGS = $(AM_CXXFLAGS) -fno-inline
+fuzz_test_geopmhash_fuzz_test_LDADD = libgeopmd.la
+fuzz_test_geopmhash_reg_test_LDADD = libgeopmd.la
