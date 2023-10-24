8dd1f71248e26ae7472061ffcae553645c76ac88
Daniel Wilson <daniel1.wilson@intel.com>
2023-10-24 11:10:51 -0700

    Optionally print CSV headers in geopmsession
    - Related to #1691
    - Resolves #3250

--- service/geopmdpy_test/Makefile.mk
+++ service/geopmdpy_test/Makefile.mk
@@ -112,6 +112,7 @@ GEOPMDPY_TESTS = geopmdpy_test/pytest_links/TestAccessLists.test__read_allowed_i
                  geopmdpy_test/pytest_links/TestSession.test_format_signals \
                  geopmdpy_test/pytest_links/TestSession.test_format_signals_invalid \
                  geopmdpy_test/pytest_links/TestSession.test_run \
+                 geopmdpy_test/pytest_links/TestSession.test_run_with_header \
                  geopmdpy_test/pytest_links/TestSession.test_run_read \
                  geopmdpy_test/pytest_links/TestSession.test_run_read_pid_exit \
                  geopmdpy_test/pytest_links/TestSession.test_run_read_pid_perm \
