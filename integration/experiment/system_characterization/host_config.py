90f938037051784385b36e9c2bb2daa6bc432914
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-11-16 13:28:25 -0800

    Switch to using a directory full of host files
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/experiment/system_characterization/host_config.py
+++ integration/experiment/system_characterization/host_config.py
@@ -6,40 +6,54 @@
 
 import sys
 import json
+import glob
+import os
 from argparse import ArgumentParser
 
 
-def run(host_name, append_file):
-    if host_name is None:
-        raise RuntimeError('The command line option -n/--host-name is required')
-    host_data = json.load(sys.stdin)
+def combine(directory, append_file):
     if append_file:
         with open(append_file) as fid:
-            append_data = json.load(fid)
+            output = json.load(fid)
+        if type(output) is not dict:
+            raise TypeError('Append file is not JSON object')
     else:
-        append_data = dict()
-    if type(host_data) is not dict:
-        raise TypeError('Host file is not json object')
-    if type(append_data) is not dict:
-        raise TypeError('Append file is not json object')
-    for key, value in host_data.items():
-        append_data[f'{key}@{host_name}'] = value
-    sys.stdout.write(json.dumps(append_data, sort_keys=True, indent=4))
+        output = dict()
+    host_files = glob.glob(f'{directory}/*')
+    for host_file in host_files:
+        host_name = os.path.basename(host_file).split('.json')[0]
+        with open(host_file) as fid:
+            host_data = json.load(fid)
+        if type(host_data) is not dict:
+            sys.stderr.write(f'Warning: File {host_file} is not a JSON object, it will be ignored')
+        else:
+            for key, value in host_data.items():
+                output[f'{key}@{host_name}'] = value
+    return output
+
+def run(directory, append_file):
+    output = combine(directory, append_file)
+    sys.stdout.write(json.dumps(output, sort_keys=True, indent=4))
 
 def main():
     """Convert ConstConfig input files into host specific parameters.
 
-    Reads from standard in, writes converted file to standard out.
+    Reads from a directory containing per host configuration files.
+    These files must be named after the host name, and may contain the
+    ".json" extension.  This may be combined with a file that contains
+    parameters that are not host specific.
 
     """
     err = 0
     prog = sys.argv[0]
     parser = ArgumentParser(description=main.__doc__)
-    parser.add_argument('host_name', type=str)
-    parser.add_argument('-a', '--append', dest='file', type=str, help='File with data that is not host specific to be appended without modification')
+    parser.add_argument('directory', type=str,
+                        help='Directory containing a configuration file per host')
+    parser.add_argument('-a', '--append', dest='file', type=str,
+                        help='File with data that is not host specific to be appended without modification')
     opts = parser.parse_args(sys.argv[1:])
     try:
-        run(opts.host_name, opts.file)
+        run(opts.directory, opts.file)
     except Exception as ex:
         sys.stderr.write(f'Error: {ex}\n')
         err = -1
