5dd53b132ad8849b6f7866c233353a4c62b0d30a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-03-11 11:26:33 -0700

    Add an empty implementation of the gRPC callbacks
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ src/RuntimeService.cpp
@@ -0,0 +1,42 @@
+#include <grpcpp/grpcpp.h>
+#include "geopm_runtime.grpc.pb.h"
+
+
+namespace geopm {
+
+    class RuntimeServiceImp final : public GEOPMRuntime::Service
+    {
+        public:
+            RuntimeServiceImp() = default;
+            virtual ~RuntimeServiceImp() = default;
+            ::grpc::Status SetPolicy(::grpc::ServerContext* context, const ::Policy* request, ::Policy* response) override;
+            ::grpc::Status GetReport(::grpc::ServerContext* context, const ::TimeSpec* request, ::Report* response) override;
+            ::grpc::Status AddChildHost(::grpc::ServerContext* context, const ::Url* request, ::TimeSpec* response) override;
+            ::grpc::Status RemoveChildHost(::grpc::ServerContext* context, const ::Url* request, ::TimeSpec* response) override;
+    };
+
+    ::grpc::Status RuntimeServiceImp::SetPolicy(::grpc::ServerContext* context, const ::Policy* request, ::Policy* response)
+    {
+        ::grpc::Status result;
+        return result;
+    }
+
+    ::grpc::Status RuntimeServiceImp::GetReport(::grpc::ServerContext* context, const ::TimeSpec* request, ::Report* response)
+    {
+        ::grpc::Status result;
+        return result;
+    }
+
+    ::grpc::Status RuntimeServiceImp::AddChildHost(::grpc::ServerContext* context, const ::Url* request, ::TimeSpec* response)
+    {
+        ::grpc::Status result;
+        return result;
+    }
+
+    ::grpc::Status RuntimeServiceImp::RemoveChildHost(::grpc::ServerContext* context, const ::Url* request, ::TimeSpec* response)
+    {
+        ::grpc::Status result;
+        return result;
+    }
+
+}
