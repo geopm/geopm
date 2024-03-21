a44f7511677f2da6b22ea2e99da3d1fb4cd28ffb
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-03-21 09:23:08 -0700

    Make report ids unique
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- scripts/geopmpy/runtime.py
+++ scripts/geopmpy/runtime.py
@@ -11,11 +11,16 @@
 import sys
 import grpc
 import sqlite3
+import random
 from geopmdpy.loop import TimedLoop
 from . import geopm_runtime_pb2_grpc
 from . import geopm_runtime_pb2
 
-def run(server_address, db_path, duration, agent):
+def run(server_address, db_path, duration, agent, profile):
+    run_id = random.randint(0, 0xEFFFFFFF)
+    params_id = run_id
+    policy_id = run_id
+    report_id = run_id
     fast_period = 0.01
     slow_period = 1
     db_con = sqlite3.connect(db_path)
@@ -32,40 +37,39 @@ def run(server_address, db_path, duration, agent):
             raise
     with grpc.insecure_channel(server_address) as channel:
         rt_proxy = geopm_runtime_pb2_grpc.GEOPMRuntimeStub(channel)
-        params_id = 0
-        policy_id = 0
-        profile = ''
         db_cur.execute("INSERT INTO policy VALUES(?,?,?,?,?)",
                        (policy_id, agent, fast_period, profile, params_id))
         policy = geopm_runtime_pb2.Policy(agent=agent,
                                           period=fast_period,
-                                          profile='',
+                                          profile=profile,
                                           params=dict())
         rt_proxy.SetPolicy(policy)
         request = geopm_runtime_pb2.ReportRequest()
         num_period = int(duration / slow_period)
-        for report_idx in TimedLoop(slow_period, num_period):
+        for loop_idx in TimedLoop(slow_period, num_period):
             report_list = rt_proxy.GetReport(request)
             report_command = "INSERT INTO report VALUES(?,?,?,?,?,?,?)"
             report_values = []
             stats_command = "INSERT INTO stats VALUES(?,?,?,?,?,?,?,?,?)"
             stats_values = []
             for report in report_list.list:
-                report_values.append((report_idx, report.host.url, report.begin.sec, report.begin.nsec, report.end.sec, report.end.nsec, policy_id))
+                report_values.append((report_id, report.host.url, report.begin.sec, report.begin.nsec, report.end.sec, report.end.nsec, policy_id))
                 for stats in report.stats:
-                    stats_values.append((report_idx, stats.name, stats.count, stats.first, stats.last, stats.min, stats.max, stats.mean, stats.std))
+                    stats_values.append((report_id, stats.name, stats.count, stats.first, stats.last, stats.min, stats.max, stats.mean, stats.std))
+                report_id += 1
             db_cur.executemany(stats_command, stats_values)
             db_cur.executemany(report_command, report_values)
             db_con.commit()
 
 def main():
-    usage = f'{sys.argv[0]} SERVER_ADDRESS DB_PATH DURATION AGENT'
-    if len(sys.argv) != 5:
+    usage = f'{sys.argv[0]} SERVER_ADDRESS DB_PATH DURATION AGENT PROFILE'
+    if len(sys.argv) != 6:
         print(usage)
     else:
         server_address = sys.argv[1]
         db_path = sys.argv[2]
         duration = float(sys.argv[3])
         agent = sys.argv[4]
-        run(server_address, db_path, duration, agent)
+        profile = sys.argv[5]
+        run(server_address, db_path, duration, agent, profile)
     return 0
