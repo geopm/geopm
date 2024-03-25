05eac748ce32c4169eed6cf202630a4ee0d0a06e
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-03-25 11:22:47 -0700

    Rely on python setuptools to get version from git repo
    - Enables a single source of truth for version information
      now it is geopmdpy/version.py.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/autogen.sh
+++ service/autogen.sh
@@ -3,29 +3,20 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
-# If the VERSION file does not exist, then create it based on git
-# describe or if not in a git repo just set VERSION to 0.0.0.
-if [ -f VERSION_OVERRIDE ]; then
-    cp VERSION_OVERRIDE VERSION
-elif git describe --long > /dev/null 2>&1; then
-    sha=$(git describe --long | awk -F- '{print $(NF)}')
-    release=$(git describe --long | awk -F- '{print $(NF-1)}')
-    version=$(git describe --long | sed -e "s|+rc|~rc|" -e "s|\(.*\)-$release-$sha|\1|" -e "s|-|+|g" -e "s|^v||")
-    if [ "${release}" != "0" ]; then
-        version=${version}+dev${release}${sha}
-    fi
-    echo $version > VERSION
-elif [ ! -f VERSION ]; then
-    echo "WARNING:  VERSION file does not exist and git describe failed, setting version to 0.0.0" 1>&2
-    echo "0.0.0" > VERSION
-fi
-
+# Copy repository files from base directory
 for ff in AUTHORS CODE_OF_CONDUCT.md CONTRIBUTING.rst COPYING COPYING-TPP; do
     if [ ! -f $ff ]; then
         cp ../$ff .
     fi
 done
-
 python3 -m build --sdist
+if [ ! -e VERSION ]; then
+    PYTHONPATH=: \
+    python3 -c "from geopmdpy.version import __version__; vv=__version__.replace('.dev','+dev', 1).replace('+g','g', 1); vv=vv[:vv.rfind('.d20')]; print(vv)" > VERSION || \
+    echo "0.0.0" > VERSION && \
+    echo "WARNING:  VERSION file does not exist and git describe failed, setting version to 0.0.0" 1>&2
+fi
+# Recreate the JSON schema files in case they have changed, remember to git commit the generated files if they have.
 python3 geopmdpy/schemas.py docs/json_schemas
+# Create configure script
 autoreconf -i -f
