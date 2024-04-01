f6dab8fec4a7f1eae34f96d397d8f48303709818
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-01 09:39:05 -0700

    Add packaging for geopmdpy
    - Whitespace changes to the geopmdpy/debian/control file
    - Prune requirements.txt
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ geopmpy/make_deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x -e
+python3 -m build --sdist | tee make_deb-sdist.log
+archive=$(cat make_deb-sdist.log | tail -n 1 | sed 's|^Successfully built ||')
+tar -xvf dist/$archive
+dir=$(echo $archive | sed 's|\.tar\.gz||')
+cd $dir
+dpkg-buildpackage -us -uc
