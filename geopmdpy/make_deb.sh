5fba93c40edb1bd4295d92cd0e7b586b7667a978
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-04 13:03:15 -0700

    Remove dependency on top level script
    - Clean up log files from sdist
    - Introduce a python script specifically for creating an sdist
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmdpy/make_deb.sh
+++ geopmdpy/make_deb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 set -x -e
-python3 -m build --sdist | tee make_deb-sdist.log
-archive=$(cat make_deb-sdist.log | tail -n 1 | sed 's|^Successfully built ||')
+
+VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
+echo ${VERSION} > geopmdpy/VERSION
+python3 -m build --sdist | tee make_sdist.log
+archive=$(cat make_sdist.log | tail -n 1 | sed 's|^Successfully built ||')
 tar -xvf dist/$archive
 dir=$(echo $archive | sed 's|\.tar\.gz||')
 cd $dir
