fbdcf122aaf85135a74f4c881472ef5d35c984b1
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-05 15:49:01 -0700

    Update version tooling for geopmpy
    - Clean up .gitignore files
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmpy/make_sdist.py
+++ geopmpy/make_sdist.py
@@ -8,7 +8,7 @@ import os
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 version = get_version(f'{script_dir}/..')
-with open(f'{script_dir}/geopmdpy/VERSION', 'w') as fid:
+with open(f'{script_dir}/geopmpy/VERSION', 'w') as fid:
    fid.write(version)
 pb = ProjectBuilder(script_dir)
 pb.build('sdist', 'dist')
