9c455369e38c265e749c2a7888bd29839eaabb0d
Brad Geltz <brad.geltz@intel.com>
2024-04-05 14:17:40 -0700

    Update make_deb to use make_sdist
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- geopmdpy/make_deb.sh
+++ geopmdpy/make_deb.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-set -x -e
+set -xe
 
-VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
-echo ${VERSION} > geopmdpy/VERSION
-python3 -m build --sdist | tee make_sdist.log
-archive=$(cat make_sdist.log | tail -n 1 | sed 's|^Successfully built ||')
-tar -xvf dist/$archive
-dir=$(echo $archive | sed 's|\.tar\.gz||')
-cd $dir
+./make_sdist.sh
+
+PACKAGE_NAME=geopmdpy
+VERSION=$(cat geopmdpy/VERSION)
+ARCHIVE=${PACKAGE_NAME}-${VERSION}.tar.gz
+
+tar -xvf dist/${ARCHIVE}
+DIR=$(echo ${ARCHIVE} | sed 's|\.tar\.gz||')
+cd ${DIR}
 dpkg-buildpackage -us -uc
