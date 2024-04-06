571d4740c1e1c7ae5446e1293f6602911310939c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-05 17:13:25 -0700

    Update debian changelog when packaging python
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmpy/make_sdist.py
+++ geopmpy/make_sdist.py
@@ -5,10 +5,17 @@
 from setuptools_scm import get_version
 from build import ProjectBuilder
 import os
+from datetime import datetime
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 version = get_version(f'{script_dir}/..')
 with open(f'{script_dir}/geopmpy/VERSION', 'w') as fid:
    fid.write(version)
+with open(f'{script_dir}/debian/changelog.in') as fid:
+   changelog = fid.read()
+with open(f'{script_dir}/debian/changelog', 'w') as fid:
+   date = datetime.today().astimezone().strftime('%a, %d %b %Y %H:%M:%S %z')
+   changelog = changelog.replace('@VERSION@', version).replace('@DATE@', date)
+   fid.write(changelog)
 pb = ProjectBuilder(script_dir)
 pb.build('sdist', 'dist')
