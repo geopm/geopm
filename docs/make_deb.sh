a9ed859250224d34979e5a019abbf9a477c72a2c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-05 17:13:25 -0700

    Update debian changelog when packaging python
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- docs/make_deb.sh
+++ docs/make_deb.sh
@@ -2,6 +2,8 @@
 
 VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
 echo $VERSION > VERSION
+DATE=$(date +'%a, %d %b %Y %H:%M:%S %z')
+cat debian/changelog.in | sed -e "s|@VERSION@|$VERSION|g" -e "s|@DATE@|$DATE|g" > debian/changelog
 cd ..
 tar --transform="s|docs|geopm-docs-$VERSION|" -zcvf geopm-docs-$VERSION.tar.gz docs
 mv geopm-docs-$VERSION.tar.gz docs
