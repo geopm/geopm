ef8b4f31444ebf204fbbd81c2b31744937bde417
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-07 11:52:31 -0700

    Add spec file and make script for geopmpy
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ geopmpy/make_rpm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xe
+
+./make_sdist.sh
+
+PACKAGE_NAME=geopmpy
+VERSION=$(cat geopmpy/VERSION)
+ARCHIVE=${PACKAGE_NAME}-${VERSION}.tar.gz
+
+sed -e "s/@ARCHIVE@/${ARCHIVE}/" -e "s/@VERSION@/${VERSION}/" geopmpy.spec.in > geopmpy.spec
+
+# On systems where rpmbuild is not available (i.e. GitHub CI builder used to create
+# the distribution tarball that is uploaded to OBS) the following will be skipped.
+if [ -x "$(command -v rpmbuild)" ]; then
+    RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
+    mkdir -p ${RPM_TOPDIR}/SOURCES
+    mkdir -p ${RPM_TOPDIR}/SPECS
+    cp dist/${ARCHIVE} ${RPM_TOPDIR}/SOURCES
+    cp geopmpy.spec ${RPM_TOPDIR}/SPECS
+    rpmbuild -ba ${RPM_TOPDIR}/SPECS/geopmpy.spec
+fi
