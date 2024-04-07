348b9eb97dd9ebf174a6fffa16cba3b5b7b86b67
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-07 16:48:06 -0700

    Move some scripting into docs/Makefile
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- docs/make_deb.sh
+++ docs/make_deb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
-echo $VERSION > VERSION
-DATE=$(date +'%a, %d %b %Y %H:%M:%S %z')
-cat debian/changelog.in | sed -e "s|@VERSION@|$VERSION|g" -e "s|@DATE@|$DATE|g" > debian/changelog
-cd ..
-tar --transform="s|docs|geopm-docs-$VERSION|" -zcvf geopm-docs-$VERSION.tar.gz docs
-mv geopm-docs-$VERSION.tar.gz docs
-cd docs
+if [ $0 != './make_deb.sh' ]; then
+    echo "Error: Working directory must be geopm/docs" 1>&2
+    exit -1
+fi
+
+set -e
+
+make clean
+make dist
+VERSION=$(cat VERSION)
 tar xvf geopm-docs-$VERSION.tar.gz
 cd geopm-docs-$VERSION
-PYTHONPATH=$PWD/../../geopmdpy:$PWD/../../geopmpy:$PYTHONPATH dpkg-buildpackage -us -uc
+dpkg-buildpackage -us -uc
