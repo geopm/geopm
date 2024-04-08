1ac85ef129b930c99dab13e5dab90d6ae1eab546
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-08 16:00:17 -0700

    Get rid of VERSION file for geopmdpy
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmdpy/geopmdpy/version.py
+++ geopmdpy/geopmdpy/version.py
@@ -4,22 +4,11 @@
 
 import os
 import sys
+import importlib_metadata as metadata
 
 def get_version():
-    # Try VERSION file
-    module_dir = os.path.dirname(os.path.realpath(__file__))
-    version_path = os.path.join(module_dir, 'VERSION')
-    with open(version_path) as fid:
-        version = fid.read().strip()
-    if version == '0.0.0':
-        pkg_name = __name__.split('.')[0]
-        # If in git repo, always use for verioning
-        try:
-            from setuptools_scm import get_version
-            version = get_version(f'{module_dir}/../..')
-        except ImportError:
-            # If all else fails print a warning and return 0.0.0
-            sys.stderr.write('Warning: Unable to determine GEOPM version, setting to 0.0.0\n\n')
+    pkg_name = __name__.split('.')[0]
+    version = metadata.version(pkg_name)
     return version
 
 version = __version__ = get_version()
