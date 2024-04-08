e0072303d2c49a2aaea1bf29d1257d2c44705f9c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-08 15:36:57 -0700

    Work around for scm version issues
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmdpy/make_sdist.py
+++ geopmdpy/make_sdist.py
@@ -7,15 +7,23 @@ from build import ProjectBuilder
 import os
 from datetime import datetime
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-version = get_version(f'{script_dir}/..')
-with open(f'{script_dir}/geopmdpy/VERSION', 'w') as fid:
-   fid.write(version)
-with open(f'{script_dir}/debian/changelog.in') as fid:
-   changelog = fid.read()
-with open(f'{script_dir}/debian/changelog', 'w') as fid:
-   date = datetime.today().astimezone().strftime('%a, %d %b %Y %H:%M:%S %z')
-   changelog = changelog.replace('@VERSION@', version).replace('@DATE@', date)
-   fid.write(changelog)
-pb = ProjectBuilder(script_dir)
-pb.build('sdist', 'dist')
+def setup():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    version = get_version(f'{script_dir}/..')
+    with open(f'{script_dir}/geopmdpy/VERSION', 'w') as fid:
+       fid.write(version)
+    with open(f'{script_dir}/debian/changelog.in') as fid:
+       changelog = fid.read()
+    date = datetime.today().astimezone().strftime('%a, %d %b %Y %H:%M:%S %z')
+    changelog = changelog.replace('@VERSION@', version).replace('@DATE@', date)
+    with open(f'{script_dir}/debian/changelog', 'w') as fid:
+        fid.write(changelog)
+    return script_dir
+
+def build():
+    script_dir = setup()
+    pb = ProjectBuilder(script_dir)
+    pb.build('sdist', 'dist')
+
+if __name__ == '__main__':
+    build()
