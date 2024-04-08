ecb64378b5c4fb7843258a18f040e6842ac4597b
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-08 15:36:57 -0700

    Work around for scm version issues
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmdpy/geopmdpy/version.py
+++ geopmdpy/geopmdpy/version.py
@@ -6,27 +6,20 @@ import os
 import sys
 
 def get_version():
+    # Try VERSION file
     module_dir = os.path.dirname(os.path.realpath(__file__))
-    try:
+    version_path = os.path.join(module_dir, 'VERSION')
+    with open(version_path) as fid:
+        version = fid.read().strip()
+    if version == '0.0.0':
         pkg_name = __name__.split('.')[0]
         # If in git repo, always use for verioning
-        from setuptools_scm import get_version
-        version = get_version(f'{module_dir}/../..')
-    except (ImportError, LookupError):
-        # Fall back to VERSION file
-        version_path = os.path.join(module_dir, 'VERSION')
         try:
-            with open(version_path) as fid:
-                version = fid.read().strip()
-        except FileNotFoundError:
-            # Fallback to metadata if python version is new enough
-            try:
-                from importlib import metadata
-                version = metadata.version(pkg_name)
-            except ImportError:
-                # If all else fails print a warning and return 0.0.0
-                sys.stderr.write('Warning: Unable to determine GEOPM version, setting to 0.0.0\n\n')
-                version = '0.0.0'
+            from setuptools_scm import get_version
+            version = get_version(f'{module_dir}/../..')
+        except ImportError:
+            # If all else fails print a warning and return 0.0.0
+            sys.stderr.write('Warning: Unable to determine GEOPM version, setting to 0.0.0\n\n')
     return version
 
 version = __version__ = get_version()
