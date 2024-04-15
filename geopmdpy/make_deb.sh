628c622448b0437b726a43ba217254898ee60455
Brad Geltz <brad.geltz@intel.com>
2024-04-15 13:10:41 -0700

    Refactor rpm/deb/sdist scripts for python packages
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- geopmdpy/make_deb.sh
+++ geopmdpy/make_deb.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -xe
-PACKAGE_NAME=geopmdpy
 
 ./make_sdist.sh
 
-VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
-ARCHIVE=${PACKAGE_NAME}-${VERSION}.tar.gz
-
+PACKAGE_NAME=geopmdpy
+ARCHIVE=${PACKAGE_NAME}-$(cat VERSION).tar.gz
 tar -xvf dist/${ARCHIVE}
 DIR=$(echo ${ARCHIVE} | sed 's|\.tar\.gz||')
 cd ${DIR}
--- geopmpy/make_deb.sh
+++ geopmpy/make_deb.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -x -e
-PACKAGE_NAME=geopmpy
 
 ./make_sdist.sh
 
-VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
-ARCHIVE=${PACKAGE_NAME}-${VERSION}.tar.gz
-
+PACKAGE_NAME=geopmpy
+ARCHIVE=${PACKAGE_NAME}-$(cat VERSION).tar.gz
 tar -xvf dist/${ARCHIVE}
 DIR=$(echo ${ARCHIVE} | sed 's|\.tar\.gz||')
 cd ${DIR}
