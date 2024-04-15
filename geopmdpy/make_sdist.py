6a77b6b98bf6c0864815862b436857bc429adb4a
Brad Geltz <brad.geltz@intel.com>
2024-04-15 13:10:41 -0700

    Refactor rpm/deb/sdist scripts for python packages
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- geopmdpy/make_sdist.py
+++ geopmdpy/make_sdist.py
@@ -9,13 +9,22 @@ from datetime import datetime
 
 def setup():
     script_dir = os.path.dirname(os.path.realpath(__file__))
+    package_name = os.path.basename(script_dir)
     version = get_version(f'{script_dir}/..')
+    with open(f'{script_dir}/VERSION', 'w') as fid:
+        fid.write(version)
     date = datetime.today().astimezone().strftime('%a, %d %b %Y %H:%M:%S %z')
     with open(f'{script_dir}/debian/changelog.in') as fid:
        changelog = fid.read()
     changelog = changelog.replace('@VERSION@', version).replace('@DATE@', date)
     with open(f'{script_dir}/debian/changelog', 'w') as fid:
         fid.write(changelog)
+    with open(f'{script_dir}/{package_name}.spec.in') as fid:
+        specfile = fid.read()
+    archive = f'{package_name}-{version}.tar.gz'
+    specfile = specfile.replace('@VERSION@', version).replace('@ARCHIVE@', archive)
+    with open(f'{script_dir}/{package_name}.spec', 'w') as fid:
+        fid.write(specfile)
     return script_dir
 
 def build():
