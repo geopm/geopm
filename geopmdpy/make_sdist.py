e150f7b7258df8b902cbfc380c6e33d85403fafc
Brad Geltz <brad.geltz@intel.com>
2024-04-15 15:45:53 -0700

    Add try/except for importlib_metadata usage
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- geopmdpy/make_sdist.py
+++ geopmdpy/make_sdist.py
@@ -11,7 +11,7 @@ def setup():
     script_dir = os.path.dirname(os.path.realpath(__file__))
     package_name = os.path.basename(script_dir)
     version = get_version(f'{script_dir}/..')
-    with open(f'{script_dir}/VERSION', 'w') as fid:
+    with open(f'{script_dir}/{package_name}/VERSION', 'w') as fid:
         fid.write(version)
     date = datetime.today().astimezone().strftime('%a, %d %b %Y %H:%M:%S %z')
     with open(f'{script_dir}/debian/changelog.in') as fid:
--- geopmpy/make_rpm.sh
+++ geopmpy/make_rpm.sh
@@ -5,7 +5,7 @@ set -xe
 ./make_sdist.sh
 
 PACKAGE_NAME=geopmpy
-ARCHIVE=${PACKAGE_NAME}-$(cat VERSION).tar.gz
+ARCHIVE=${PACKAGE_NAME}-$(cat ${PACKAGE_NAME}/VERSION).tar.gz
 RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
 mkdir -p ${RPM_TOPDIR}/SOURCES
 mkdir -p ${RPM_TOPDIR}/SPECS
