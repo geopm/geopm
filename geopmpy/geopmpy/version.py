1a95e0e4af0ea39fa568880f811dbafb725451a4
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-16 16:25:20 -0700

    Fix doc rpm build for older versions of python
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmpy/geopmpy/version.py
+++ geopmpy/geopmpy/version.py
@@ -8,8 +8,12 @@ import sys
 def get_version():
     module_dir = os.path.dirname(os.path.realpath(__file__))
     version_path = os.path.join(module_dir, 'VERSION')
-    with open(version_path) as fid:
-        version = fid.read().strip()
+    try:
+        with open(version_path) as fid:
+            version = fid.read().strip()
+    except FileNotFoundError:
+        sys.stderr.write(f'Warning: importing {__file__} from an uninstalled module, setting version to "0.0.0"\n\n')
+        version = '0.0.0'
     return version
 
 version = __version__ = get_version()
