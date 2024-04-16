b80f46834ec4332cb0b7ba7ae4404505436847ee
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-04-16 16:25:20 -0700

    Fix doc rpm build for older versions of python
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- docs/make_rpm.sh
+++ docs/make_rpm.sh
@@ -7,6 +7,6 @@ make dist
 RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
 mkdir -p ${RPM_TOPDIR}/SOURCES
 mkdir -p ${RPM_TOPDIR}/SPECS
-cp ${ARCHIVE} ${RPM_TOPDIR}/SOURCES
+cp geopm-doc-$(cat VERSION).tar.gz ${RPM_TOPDIR}/SOURCES
 cp geopm-doc.spec ${RPM_TOPDIR}/SPECS
 rpmbuild -ba ${RPM_TOPDIR}/SPECS/geopm-doc.spec
