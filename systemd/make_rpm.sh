7e0e4c485fca85bebd908f116e1fe01bdb95af51
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-05-08 17:31:09 -0700

    Move systemd files into separate directory
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- systemd/make_rpm.sh
+++ systemd/make_rpm.sh
@@ -5,11 +5,10 @@
 
 set -xe
 
-make clean_all
 make dist
 RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
 mkdir -p ${RPM_TOPDIR}/SOURCES
 mkdir -p ${RPM_TOPDIR}/SPECS
-cp geopm-doc-$(cat VERSION).tar.gz ${RPM_TOPDIR}/SOURCES
-cp geopm-doc.spec ${RPM_TOPDIR}/SPECS
-rpmbuild -ba ${RPM_TOPDIR}/SPECS/geopm-doc.spec
+cp geopm-systemd-$(cat VERSION).tar.gz ${RPM_TOPDIR}/SOURCES
+cp geopm-systemd.spec ${RPM_TOPDIR}/SPECS
+rpmbuild -ba ${RPM_TOPDIR}/SPECS/geopm-systemd.spec
