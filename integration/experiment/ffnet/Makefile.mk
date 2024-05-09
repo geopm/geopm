fe84ba7cc42b40aec029cc9852f4b2b954b1032c
Asma H. Al-Rawi <asma.h.al-rawi@intel.com>
2024-05-09 12:59:39 -0700

    Added scripts to generate neural net jsons and to run ffnet experiment.

---
+++ integration/experiment/ffnet/Makefile.mk
@@ -0,0 +1,15 @@
+#  Copyright (c) 2015 - 2024, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+EXTRA_DIST += experiment/ffnet/__init__.py \
+              experiment/ffnet/ffnet.py \
+	      experiment/ffnet/gen_hdf_from_fsweep.py \
+	      experiment/ffnet/gen_neural_net.py \
+	      experiment/ffnet/gen_region_parameters.py \
+	      experiment/ffnet/gen_sweep_to_ffnet.py \
+	      experiment/ffnet/neural_net_sweep.py \
+              experiment/ffnet/README.md \
+	      experiment/ffnet/run_ffnet_geopmbench.py \
+	      experiment/ffnet/run_neural_net_sweep_geopmbench.py \
+              # end
