04169e0614a74cbae4212863fb285c7c97c30c5f
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-06-25 18:04:56 -0700

    Fix import error handling for setuptools_scm
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmpy/setup.py
+++ geopmpy/setup.py
@@ -8,9 +8,9 @@ import os
 
 package_name = 'geopmpy'
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
 try:
     from setuptools_scm import get_version
-    script_dir = os.path.dirname(os.path.realpath(__file__))
     version = get_version(f'{script_dir}/..')
     with open(f'{script_dir}/{package_name}/VERSION', 'w') as fid:
         fid.write(version)
