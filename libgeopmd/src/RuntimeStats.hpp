4942aaf16af93c49bc3890eb3c802ade7d90c1a8
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-08-08 11:44:59 -0700

    Add doc strings for StatsCollector and RuntimeStats
    - Reset timestamp when reset is called
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- libgeopmd/src/RuntimeStats.hpp
+++ libgeopmd/src/RuntimeStats.hpp
@@ -11,22 +11,84 @@
 
 namespace geopm
 {
+    /// @brief Class that aggregates statistics without buffered data
     class RuntimeStats
     {
         public:
+            /// @brief Default null RuntimeStats with no signals
             RuntimeStats() = default;
+            /// @brief Constructor that records the names of all metrics
             RuntimeStats(const std::vector<std::string> &metric_names);
+            /// @brief Default virtual destructor
             virtual ~RuntimeStats() = default;
+            /// @brief Number of metrics aggregated
+            ///
+            /// @return The number of metrics specified at construction
             int num_metric(void) const;
+            /// @brief Name of one metrics
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Name of metric
             std::string metric_name(int metric_idx) const;
+            /// @brief Number of non-null values sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Number of non-null samples
             uint64_t count(int metric_idx) const;
+            /// @brief First non-null value sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return First value of metric
             double first(int metric_idx) const;
+            /// @brief Last non-null value sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Last value of metric
             double last(int metric_idx) const;
+            /// @brief Minimum value sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Minimum value of metric
             double min(int metric_idx) const;
+            /// @brief Maximum value sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Maximum value of metric
             double max(int metric_idx) const;
+            /// @brief Mean value sampled
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Mean value of metric
             double mean(int metric_idx) const;
+            /// @brief Estimate of standard deviation
+            ///
+            /// @param [in] metric_idx Index of the metric as specified at
+            ///             construction
+            ///
+            /// @return Standard deviation estimate of metric
             double std(int metric_idx) const;
+            /// @brief Reset all aggregated statistics
             void reset(void);
+            /// @brief Update statistics with new sample
+            ///
+            /// Update all metrics being aggregated.
+            ///
+            /// @param [in] sample Vector of sample values: one for each metric
+            ///        name specified at construction
             void update(const std::vector<double> &sample);
         private:
             void check_index(int metric_idx, const std::string &func, int line) const;
--- libgeopmd/src/StatsCollector.hpp
+++ libgeopmd/src/StatsCollector.hpp
@@ -17,15 +17,39 @@ namespace geopm
     class PlatformIO;
     class RuntimeStats;
 
+    /// @brief Class that accumulates statistics based on PlatformIO requests
     class StatsCollector
     {
         public:
+            /// @brief Default null constructor without requests
             StatsCollector();
+            /// @brief Standard constructor with requests
+            ///
+            /// User specifies a vector of PlatformIO signal requests to be
+            /// accumulated.  The report will generate statistics about each
+            /// signal request.
+            ///
+            /// @param [in] requests All signals for monitoring and reporting
             StatsCollector(const std::vector<geopm_request_s> &requests);
+            /// @brief Test constructor used to mock PlatformIO
             StatsCollector(const std::vector<geopm_request_s> &requests, PlatformIO &pio);
+            /// @brief Default virtual destructor
             virtual ~StatsCollector() = default;
+            /// @brief Sample PlatformIO and update all tracked signals
             void update(void);
+            /// @brief Generate a YAML report of statistics
+            ///
+            /// Returns a YAML formatted report providing statisics about all
+            /// samples gathered since object construction or since last call to
+            /// reset().
+            ///
+            /// @return YAML report string that includes hostname, start date,
+            ///         end date, and signal statistics
             std::string report_yaml(void) const;
+            /// @brief May be called after report_yaml() to reset statistics
+            ///
+            /// Used to generate independent reports by clearing all gathered
+            /// statistics and resetting the begin time.
             void reset(void);
         private:
             PlatformIO &m_pio;
