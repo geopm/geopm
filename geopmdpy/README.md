7dc1b768b9769384101ead95a646e0f4c01d4479
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2024-12-16 13:01:52 -0800

    Modify git interactions to be env configured
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- geopmdpy/README.md
+++ geopmdpy/README.md
@@ -23,38 +23,58 @@ dependencies. If you follow these steps, then you do not need to modify your
 `PYTHONPATH` variable when executing tests.
 
 The `geopmdpy` package wraps `libgeopmd`. Be sure to follow the build
-instructions in the directory for that library as part of setting up a geopmdpy
-development environment. Either install that builds somewhere on your path, or
+instructions in the directory for that library as part of setting up a `geopmdpy`
+development environment. Either install `libgeopmd` somewhere on your path, or
 manually add it to your `LD_LIBRARY_PATH` (examples in the next section).
 
 ### Building Against Non-System-Installed Libraries
 
-If you want to build geopmdpy against non-system-installed headers and ``libgeopmd``
+If you want to build `geopmdpy` against non-system-installed headers and ``libgeopmd``
 binaries, you need to set your compiler's environment variables to tell it
 where to search for GEOPM. For example, if you built and installed ``libgeopmd``
 with ``--prefix=$HOME/build/geopm`` and your python extensions are compiled
 with gcc, then run:
 
-    CC=gcc LIBRARY_PATH=$HOME/build/geopm/lib C_INCLUDE_PATH=$HOME/build/geopm/include pip install ./
+```bash
+CC=gcc \
+LIBRARY_PATH=$HOME/build/geopm/lib \
+C_INCLUDE_PATH=$HOME/build/geopm/include \
+python3 -m pip install ./
+
+```
 
 to build and install this package.
 
 ### Helper Script to Build Latest Development Snapshot
 
 A script called `install_user.sh` is provided which builds and installs
-libgeopmd and geopmdpy based on the latest commit to the `dev` branch in the
-upstream geopm Git repository.  This script will make a new clone of the geopm
-git repository in your current working directory, and so this script is a
-stand-alone solution:
+`libgeopmd` and `geopmdpy` for a single user.  This script may be simple helper for
+running the build of the two directories `geopm/geopmdpy` and `geopm/libgeopmd`, but
+it may also be used as a stand-alone solution in place of
 
-```
-    wget https://raw.githubusercontent.com/geopm/geopm/refs/heads/dev/geopmdpy/install_user.sh
-    chmod a+x install_user.sh
-    ./install_user.sh --prefix=$HOME/geopm-build --enable-levelzero --disable-io-uring --disable-libcap
+```bash
+python -m pip install "geopmdpy @ git+https://github.com/geopm/geopm.git#subdirectory=geopmdpy"
 
 ```
 
-See the output from `./install_user.sh --help` for more information.
+The above command will install the `geopmdpy` package from the dev branch on
+github, but it will not install a development snapshot of `libgeopmd` which may
+also be required.  To combining these two installation steps, run the following
+commands instead:
+
+```bash
+wget https://raw.githubusercontent.com/geopm/geopm/refs/heads/dev/geopmdpy/install_user.sh
+chmod a+x install_user.sh
+export GEOPM_GIT_PATH=$(mktemp -d)/geopm
+./install_user.sh --prefix=$HOME/geopm-build --enable-levelzero
+rm -rf $(dirname $GEOPM_GIT_PATH)
+export LD_LIBRARY_PATH=$HOME/geopm-build/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+```
+
+Note that the options provided to `install_user.sh` above are one example.  See
+the output from `./install_user.sh --help` for more information about how to use
+the script.
 
 ## Executing Tests
 
--- geopmdpy/install_user.sh
+++ geopmdpy/install_user.sh
@@ -14,33 +14,50 @@ libgeopmd.so used at runtime must provide all the interfaces required by
 geompdpy to avoid an import error.  The easiest way to ensure this is the case
 is to install a version of libgeopmd.so that matches the pip installed version.
 
-This script enables this use by by installing geopmdpy and libgeopmd.so based on
-latest upstream dev branch.  All command line arguments provided to this script are
-forwarded to the configure command for libgeopmd.  User is expected to provide
-the --prefix option which will install libgeopmd in a user defined directory.
-The user may also provide other options as required by their environment.  For
-more details on how to configure libgeopmd.so see:
+This script enables this use by installing geopmdpy and libgeopmd.so based on a
+checkout of the geopm Git repo.  All command line arguments provided to this
+script are forwarded to the configure command for libgeopmd.  User is expected
+to provide the --prefix option which will install libgeopmd in a user defined
+directory.  The user may also provide other options as required by their
+environment.  For more details on how to configure libgeopmd.so see:
 
     https://geopm.github.io/devel.html#configuring-the-build
 
-The script will clone a new GEOPM git repo called \"install_user_geopm_git\" if
-this directory does not exist in the user's current working directory.
+The user must set the environment variable GEOPM_GIT_PATH to the path of a geopm
+Git repository when the script is moved or downloaded in isolation from the rest
+of the GEOPM Git repo.  In the case where this environment variable is set, but
+does not point to an existing repository, the script will clone a new geopm Git
+repo.  The interactions with Git are controlled by environment variables
+documented below.
 
 Environment Variables:
 
-    GEOPM_GIT_URL: URL to clone if install_user_geopm_git directory does not exist.
-                   Defaults to \"https://github.com/geopm/geopm.git\".
+    GEOPM_GIT_PATH: Path to Git repository. Defaults to two directories above
+                    the location of this script.
+
+    GEOPM_GIT_URL: URL to clone if GEOPM_GIT_PATH is set, but does not evaluate
+                   to an existing directory.  Default value is
+                   \"https://github.com/geopm/geopm.git\". To disable Git clone,
+                   set GEOPM_GIT_PATH to an existing clone of the geopm Git
+                   repository.
 
     GEOPM_GIT_CHECKOUT: Branch, sha or other commit-ish to checkout from the
-                        repo.  Defaults to \"dev\".  Rebase against origin is
-                        disabled when this environment variable is set.
+                        repo.  Both checkout and rebase are disabled If
+                        GEOPM_GIT_CHECKOUT=\"\" or is unset.
+
+    GEOPM_GIT_REMOTE: Git remote to use for rebase.  Defaults to \"origin\" when
+                      environment variable is unset.  Set GEOPM_GIT_REMOTE=\"\"
+                      to disable rebase.
+
 
 Example:"'
 
     wget https://raw.githubusercontent.com/geopm/geopm/refs/heads/dev/geopmdpy/install_user.sh
     chmod a+x install_user.sh
-    ./install_user.sh --prefix=$HOME/geopm-build
-    export LD_LIBRARY_PATH=$HOME/geopm-build/lib:$LD_LIBRARY_PATH
+    export GEOPM_GIT_PATH=$(mktemp -d)/geopm
+    ./install_user.sh --prefix=$HOME/geopm-build --enable-levelzero
+    export LD_LIBRARY_PATH=$HOME/geopm-build/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+    rm -rf $(dirname $GEOPM_GIT_PATH)
 
 
 '
@@ -48,23 +65,22 @@ Example:"'
 fi
 set -x
 set -e
-if [ -z "${GEOPM_GIT_URL}" ]; then
-    GEOPM_GIT_URL="https://github.com/geopm/geopm.git"
+DEFAULT_GIT_PATH=$(dirname $(dirname $(realpath $0)))
+GEOPM_GIT_PATH=${GEOPM_GIT_PATH:-${DEFAULT_GIT_PATH}}
+GEOPM_GIT_URL=${GEOPM_GIT_URL:-"https://github.com/geopm/geopm.git"}
+GEOPM_GIT_REMOTE=${GEOPM_GIT_REMOTE:-"origin"}
+
+if [ ! -d ${GEOPM_GIT_PATH} ]; then
+    git clone ${GEOPM_GIT_URL} ${GEOPM_GIT_PATH}
 fi
-if [ -z "${GEOPM_GIT_CHECKOUT}" ]; then
-    GEOPM_GIT_CHECKOUT="dev"
-    DO_REBASE="true"
+cd ${GEOPM_GIT_PATH}
+if [ -n  "${GEOPM_GIT_CHECKOUT}" ]; then
+    git checkout ${GEOPM_GIT_CHECKOUT}
+    if [ -n "${GEOPM_GIT_REMOTE}" ]; then
+        git fetch ${GEOPM_GIT_REMOTE}
+        git rebase ${GEOPM_GIT_REMOTE}/${GEOPM_GIT_CHECKOUT}
+    fi
 fi
-if [ ! -d install_user_geopm_git ]; then
-    git clone ${GEOPM_GIT_URL} install_user_geopm_git
-fi
-cd install_user_geopm_git
-git checkout ${GEOPM_GIT_CHECKOUT}
-if [ ${DO_REBASE} == "true" ]; then
-    git fetch origin
-    git rebase origin/${GEOPM_GIT_CHECKOUT}
-fi
-git clean -dfx
 cd libgeopmd
 ./autogen.sh
 ./configure $@
@@ -79,5 +95,5 @@ set +x
 echo "--------------------------------------------------------------------------"
 echo "SUCCESS:"
 echo "   Be sure to add the installed libgeopmd.so to your load library path:"
-echo "   export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib:\$LD_LIBRARY_PATH"
+echo "   export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib"'${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}'
 echo "--------------------------------------------------------------------------"
