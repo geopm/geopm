3d904301d01f3f02db4cb007bdcdbf74c50f8f7a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-01-06 09:17:23 -0800

    Add doc.go file
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ geopmdgo/geopmdgo/doc.go
@@ -0,0 +1,9 @@
+/*
+Golang Bindings for libgeopmd
+
+This directory contains the geopmdgo package which provides bindings to some
+of the C interfaces provided by libgeopmd.  These bindings allow the user to
+interact with the PlatformIO and PlatformTopo interfaces in the Go language.
+
+*/
+package geopmdgo
