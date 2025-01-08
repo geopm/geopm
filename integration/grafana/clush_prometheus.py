b1f9a2c8ed7defbc8d5cb6571a58c61b09f82551
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-01-08 10:11:33 -0800

    Rename and repurpose prometheus script
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/grafana/clush_prometheus.py
+++ integration/grafana/clush_prometheus.py
@@ -14,10 +14,11 @@ from argparse import ArgumentParser
 from time import sleep
 from tempfile import NamedTemporaryFile
 
-def get_host_list(jobid):
+def pbs_host_list(jobid):
     """Get list of hostnames from a PBS jobid
 
     """
+    PBS_POLL_TIME = 15
     jobid = str(jobid)
     cmd = ['qstat', jobid]
     is_ready = False
@@ -28,7 +29,7 @@ def get_host_list(jobid):
         if len(line_split) >= 5 and line_split[4] == 'R':
             is_ready = True
         else:
-            sleep(1)
+            sleep(PBS_POLL_TIME)
     cmd = ['qstat', '-x', '-f', '-w', jobid]
     run_ret = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
     host_str = None
@@ -93,7 +94,7 @@ def create_host_file(hosts):
         result = fid.name
     return result
 
-def run(prom_dir, graf_dir, prom_port, graf_port, client_port, geopm_dir, jobid):
+def run(prom_dir, graf_dir, prom_port, graf_port, client_port, geopm_dir, pbs_jobid, hostfile_path):
     """Entry function with inputs derived from CLI
 
     """
@@ -122,16 +123,25 @@ def run(prom_dir, graf_dir, prom_port, graf_port, client_port, geopm_dir, jobid)
 
     # Determine target hosts if tracking a job
     hosts = []
-    if jobid is not None:
-        hosts = get_host_list(jobid)
+    clush_pid = None
+    if None not in (pbs_jobid, hostfile_path):
+        raise RuntimeError('Both pbs_jobid and hostfile_path cannot both be specified')
+    if pbs_jobid is not None:
+        hosts = pbs_host_list(pbs_jobid)
         hostfile_path = create_host_file(hosts)
-        # Launch geopmexporter on tracked hosts
+    elif hostfile_path is not None:
+        with open(hostfile_path) as fid:
+            hosts = [ll.strip() for ll in fid.readlines() if (len(ll.strip()) != 0 and ll.strip()[0] != '#')]
+    # Launch geopmexporter on tracked hosts
+    if len(hosts) != 0:
         clush_cmd = ['clush', f'--hostfile={hostfile_path}', '--',
                      'env', f'LD_LIBRARY_PATH={geopm_dir}/lib:{geopm_dir}/lib64:${{LD_LIBRARY_PATH}}',
                      'geopmexporter', '-p', f'{client_port}']
         clush_pid = popen_wrapper(clush_cmd, 'clush', clush_log_path)
+
     configure_prom(prom_dir, hosts, client_port)
     graf_conf_path = configure_graf(graf_dir, graf_port)
+
     # Launch Prometheus server
     prom_cmd = [prom_path,
                 f'--config.file={prom_dir}/prometheus.yml',
@@ -150,7 +160,7 @@ def run(prom_dir, graf_dir, prom_port, graf_port, client_port, geopm_dir, jobid)
                 f'cfg:default.paths.provisioning={graf_dir}/conf/provisioning']
     graf_pid = popen_wrapper(graf_cmd, 'grafana', graf_log_path)
     # Finish up
-    if jobid is not None:
+    if clush_pid is not None:
         clush_pid.wait()
         print_log('clush', clush_log_path)
         os.unlink(hostfile_path)
@@ -164,16 +174,18 @@ def run(prom_dir, graf_dir, prom_port, graf_port, client_port, geopm_dir, jobid)
     print_log('grafana', graf_log_path)
 
 def main():
-    """Monitor PBS jobs with geopmexporter.
+    """Monitor a set of hosts with geopmexporter using clush.
 
     Script will launch a Prometheus server and Grafana server on the login node.
-    To inspect historically recorded data, run without the --jobid option.  The
-    user will be prompted to press ENTER to terminate the servers.
+    To inspect historically recorded data, run without the
+    --pbs-jobid/--hostfile option.  The user will be prompted to press ENTER to
+    terminate the servers.
 
-    In order to collect new data with this script, provide the --jobid option to
-    run the geopmexporter Prometheus Client on the compute nodes of an PBS job
-    allocation while the allocation is valid.  When the PBS allocation
-    terminates the two servers and the client process will also be terminated.
+    In order to collect new data with this script, provide the
+    --pbs-jobid/--hostfile option to run the geopmexporter Prometheus Client on
+    the compute nodes of an PBS job allocation while the allocation is valid.
+    When the PBS allocation terminates the two servers and the client process
+    will also be terminated.
 
     """
     parser = ArgumentParser(description=main.__doc__)
@@ -189,12 +201,15 @@ def main():
                         help='Port for geopmexporter Prometheus client')
     parser.add_argument('--geopm-prefix', default='/usr',
                         help='Path pprefix for user install of GEOPM')
-    parser.add_argument('--jobid', type=str, default=None,
-                        help='PBS job ID to monitor')
+    host_group = parser.add_mutually_exclusive_group()
+    host_group.add_argument('--pbs-jobid', type=str, default=None,
+                            help='PBS job ID to monitor')
+    host_group.add_argument('--hostfile', type=str, default=None,
+                            help='File with one hostname to monitor on each line')
     args = parser.parse_args()
 
     run(args.PROMETHEUS_DIR, args.GRAFANA_DIR, args.prom_port, args.graf_port,
-        args.client_port, args.geopm_prefix, args.jobid)
+        args.client_port, args.geopm_prefix, args.pbs_jobid, args.hostfile)
     return 0
 
 if __name__ == '__main__':
