b6f3ac4308479e6a0846c3ba19c81542ec67f736
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-01-22 16:25:58 -0800

    Remove refs to cmcantalupo in k8 example
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/k8/docker-build.sh
+++ integration/k8/docker-build.sh
@@ -3,13 +3,10 @@
 set -e
 set -x
 
-docker build . -f geopm-prometheus-pkg.Dockerfile >& geopm-prometheus-pkg.log
-id=$(grep "writing image sha256:" geopm-prometheus-pkg.log | tail -n 1 | awk -F: '{print $2}' | awk '{print $1}')
-docker tag ${id} geopm-prometheus-pkg
+docker build . -f geopm-prometheus-pkg.Dockerfile -t geopm-prometheus-pkg >& geopm-prometheus-pkg.log
 id=$(docker create geopm-prometheus-pkg)
 rm -rf geopm-prometheus
 docker cp ${id}:/mnt/geopm-prometheus geopm-prometheus
 docker rm -v ${id}
-docker build . -f geopm-prometheus.Dockerfile >& geopm-prometheus.log
-id=$(grep "writing image sha256:" geopm-prometheus.log | tail -n 1 | awk -F: '{print $2}' | awk '{print $1}')
-docker tag ${id} geopm-prometheus
+docker build . -f geopm-prometheus.Dockerfile -t geopm-prometheus  >& geopm-prometheus.log
+
