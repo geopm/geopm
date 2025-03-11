31a72f020c3e92839404eac0b3e5ef0fc77395a9
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-03-11 16:41:57 -0700

    Add an integration test that validates the geopm report schema
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ integration/service/test/test_geopmsession_report_valid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+TMP_FILE=$(mktemp)
+printf "TIME * *" | geopmsession -o /dev/null -r ${TMP_FILE} -t 1 -p 0.1
+test -s ${TMP_FILE}
+python3 check_geopmsession_report_valid.py ${TMP_FILE}
+
