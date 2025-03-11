cc770e2ecf4c4fd7fce18680415544ef012311aa
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-03-11 16:55:44 -0700

    Clean up temporary report
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/service/test/test_geopmsession_report_valid.sh
+++ integration/service/test/test_geopmsession_report_valid.sh
@@ -6,4 +6,4 @@ TMP_FILE=$(mktemp)
 printf "TIME * *" | geopmsession -o /dev/null -r ${TMP_FILE} -t 1 -p 0.1
 test -s ${TMP_FILE}
 python3 check_geopmsession_report_valid.py ${TMP_FILE}
-
+rm -f ${TMP_FILE}
