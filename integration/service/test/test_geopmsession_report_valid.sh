0fd981d66cc45e59d4b3ce411dda70322b97f463
Brad Geltz <brad.geltz@intel.com>
2025-03-21 10:50:26 -0700

    Do not assume PWD when running integration test
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/service/test/test_geopmsession_report_valid.sh
+++ integration/service/test/test_geopmsession_report_valid.sh
@@ -4,8 +4,9 @@
 
 set -e
 set -x
+SCRIPT_DIR=$(dirname $(realpath $0))
 TMP_FILE=$(mktemp)
 printf "TIME * *" | geopmsession -o /dev/null -r ${TMP_FILE} -t 1 -p 0.1
 test -s ${TMP_FILE}
-python3 check_geopmsession_report_valid.py ${TMP_FILE}
+python3 ${SCRIPT_DIR}/check_geopmsession_report_valid.py ${TMP_FILE}
 rm -f ${TMP_FILE}
