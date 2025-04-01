36663ad1a45231d15b157eb3f9e4da09840269f5
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2025-04-01 15:28:36 -0700

    Added test for num geopmsession reports w/ mpi

--- integration/service/test/check_geopmsession_report_valid.py
+++ integration/service/test/check_geopmsession_report_valid.py
@@ -6,23 +6,56 @@ from argparse import ArgumentParser
 import json
 import jsonschema
 import os
+import re
 import yaml
+
+def yaml_is_valid(file_path, schema_path):
+    reports = yaml.safe_load_all(open(file_path, "r"))
+    report_schema = json.load(open(schema_path, "r"))
+    for report_data in reports:
+        if not jsonschema.validate(report_data, schema=report_schema):
+            return False
+    return True
+
+def num_yaml_reports(file_path):
+    reports = yaml.safe_load_all(open(file_path, "r"))
+    return len(list(reports))
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("report")
+    parser.add_argument("-r", "--report", dest="report")
+    parser.add_argument("-n", "--num-nodes", dest="num_nodes", default=1)
     args = parser.parse_args()
+
     if args.report is None:
         raise ValueError('The report argument is required')
+
+    # Find YAML schema file
     report_schema_file = os.path.dirname(os.path.abspath(__file__)) + \
                           "/../../../docs/json_schemas/geopmsession_report.schema.json"
-    with open(report_schema_file) as fid:
-        report_schema = json.load(fid)
-    with open(args.report, "r") as fid:
-        reports = yaml.safe_load_all(fid)
 
-        for idx, report_data in enumerate(reports):
-            jsonschema.validate(report_data, schema=report_schema)
+    # Add support for floating point numbers of the form "1e+10"
+    # where the decimal point is missing
+    yaml.SafeLoader.add_implicit_resolver(
+        u'tag:yaml.org,2002:float',
+        re.compile(r'''^(?:[-+]?(?:[0-9][0-9_]*)\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+                           |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+                           |\.[0-9_]+(?:[eE][-+]?[0-9]+)?
+                           |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\.[0-9_]*
+                           |[-+]?\.(?:inf|Inf|INF)
+                           |\.(?:nan|NaN|NAN))$''', re.X),
+        list(u'-+0123456789.'))
+
+    # Test for valid yaml reports
+    if not yaml_is_valid(args.report, report_schema_file):
+        print(f"YAML report does not match schema at {report_schema_file}")
+        #raise RuntimeError(f"YAML report does not match schema at {report_schema_file}")
+
+    # Check that number of yaml reports corresponds to the number of nodes
+    num_reports = num_yaml_reports(args.report)
+    if args.num_nodes != num_reports:
+        print(f'Incorrect number of YAML reports. Expected {args.num_nodes}, found {num_reports}')
+        #raise AssertionError(f'Incorrect number of YAML reports. Expected {args.num_nodes}, found {num_reports}')
 
 if __name__ == '__main__':
     main()
--- integration/service/test/test_geopmsession_mpiexec.sh
+++ integration/service/test/test_geopmsession_mpiexec.sh
@@ -10,13 +10,14 @@ TMP_REPORT_FILE=$(mktemp)
 TMP_TRACE_FILE=$(mktemp)
 SCRIPT_DIR=$(dirname $(realpath $0))
 
-printf "TIME board 0\nCPU_POWER board 0\nCPU_FREQUENCY_STATUS board 0" | mpiexec -n ${NNODE} -ppn 1 geopmsession -t 10 -p 0.005 -r ${TMP_REPORT_FILE} -o ${TMP_TRACE_FILE} --enable-mpi
+printf "TIME board 0\nCPU_POWER board 0\nCPU_FREQUENCY_STATUS board 0" | mpiexec -n ${NNODE} -ppn 1 geopmsession -t 5 -p 0.005 -r ${TMP_REPORT_FILE} -o ${TMP_TRACE_FILE} --enable-mpi
 
 test -s $TMP_REPORT_FILE
+#Check that report is valid yaml and that we got one report per node
+python3 ${SCRIPT_DIR}/check_geopmsession_report_valid.py --report ${TMP_REPORT_FILE}\
+        --num-nodes ${NNODE}
 #Check that we got one report per node, separated by "---"
-[ $((1+`grep "^---$" /tmp/dmp | wc -l`)) == $NNODE ]
-#Check that report is valid yaml
-python3 ${SCRIPT_DIR}/check_geopmsession_report_valid.py ${TMP_REPORT_FILE}
+#[ $((1+`grep "^---$" ${TMP_REPORT_FILE} | wc -l`)) == $NNODE ]
 
 test -s $TMP_TRACE_FILE
 
