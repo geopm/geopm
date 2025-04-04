2527d84e1f4f22697b2aaaadc695684a94412f80
Ali Mohammad <ali.mohammad@intel.com>
2025-04-04 10:06:57 -0700

    Removed obsolte run script and fixed format bugs in ffnet.

--- integration/experiment/ffnet/ffnet.py
+++ integration/experiment/ffnet/ffnet.py
@@ -102,12 +102,12 @@ def launch(app_conf, args, experiment_cli_args):
     #Set and initialize required counters for nn training
     init_control_path = 'neural_net_init.controls'
     with open(init_control_path, 'w') as outfile:
-        outfile.write(f"MSR::PQR_ASSOC:RMID board 0 {}\n".format(0)
-                      "# Assigns all cores to resource monitoring association ID 0\n"
-                      "# Next, assign resource monitoring ID for QM events to match\n"
-                      f"MSR::QM_EVTSEL:RMID board 0 {}".format(0)
-                      "# Then determine Xeon Uncore Utilization\n"
-                      f"MSR::QM_EVTSEL:EVENT_ID board 0 {}\n".format(0))
+        outfile.write("MSR::PQR_ASSOC:RMID board 0 {}\n".format(0) +
+                      "# Assigns all cores to resource monitoring association ID 0\n" +
+                      "# Next, assign resource monitoring ID for QM events to match\n" +
+                      "MSR::QM_EVTSEL:RMID board 0 {}".format(0) +
+                      "# Then determine Xeon Uncore Utilization\n" +
+                      "MSR::QM_EVTSEL:EVENT_ID board 0 {}\n".format(0))
 
     launch_util.launch_all_runs(targets=targets,
                                 num_nodes=args.node_count,
