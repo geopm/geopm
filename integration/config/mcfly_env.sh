ce5ce1b64fcad9264deec2fd6cfc60abd20682fc
Siddhartha Jana <siddhartha.jana@intel.com>
2025-04-09 16:05:32 +0000

    Adding config file for build env on mcfly

---
+++ integration/config/mcfly_env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#  Copyright (c) 2015 - 2025 Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+# MCFLY BUILD/RUN ENVIRONMENT
+#
+# This script is intended to be sourced within an existing script or shell ONLY.
+# It is NOT intended to be ./ executed.
+
+export CC=icx
+export CXX=icpx
+export MPICC=mpiicc
+export MPICXX=mpiicpc
+export FC=ifx
+export F77=ifx
+export F90=ifx
+export MPIFORT=mpiifort
+export MPIFC=mpiifort
+export MPIF77=mpiifort
+export MPIF90=mpiifort
+export PALS_PMI=pmix
+export GEOPM_LAUNCHER=srun
