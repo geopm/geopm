c4893040ee70b119a2219471ce1855a51a4b205a
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2025-04-24 14:07:13 -0700

    Fixed ffnet makefile tabs.

--- integration/experiment/ffnet/Makefile.mk
+++ integration/experiment/ffnet/Makefile.mk
@@ -4,12 +4,12 @@
 
 EXTRA_DIST += experiment/ffnet/__init__.py \
               experiment/ffnet/ffnet.py \
-	      experiment/ffnet/gen_hdf_from_fsweep.py \
-	      experiment/ffnet/gen_neural_net.py \
-	      experiment/ffnet/gen_region_parameters.py \
-	      experiment/ffnet/gen_sweep_to_ffnet.py \
-	      experiment/ffnet/neural_net_sweep.py \
+              experiment/ffnet/gen_hdf_from_fsweep.py \
+              experiment/ffnet/gen_neural_net.py \
+              experiment/ffnet/gen_region_parameters.py \
+              experiment/ffnet/gen_sweep_to_ffnet.py \
+              experiment/ffnet/neural_net_sweep.py \
               experiment/ffnet/README.md \
-	      experiment/ffnet/run_ffnet_geopmbench.py \
-	      experiment/ffnet/run_neural_net_sweep_geopmbench.py \
+              experiment/ffnet/run_ffnet_geopmbench.py \
+              experiment/ffnet/run_neural_net_sweep_geopmbench.py \
               # end
