5f14fcc840b90b6270f3c64d3197b554641c7e9b
Asma H. Al-Rawi <asma.al-rawi@intel.com>
2025-06-04 13:57:49 -0700

    Added L3 local memory bandwidth to neural net.

--- integration/experiment/ffnet/neural_net_sweep.py
+++ integration/experiment/ffnet/neural_net_sweep.py
@@ -161,7 +161,7 @@ def launch(app_conf, args, experiment_cli_args):
                       "# Next, assign resource monitoring ID for QM events to match\n"
                       "MSR::QM_EVTSEL:RMID board 0 0\n"
                       "# Then determine Xeon Uncore Utilization\n"
-                      "MSR::QM_EVTSEL:EVENT_ID board 0 0")
+                      "MSR::QM_EVTSEL:EVENT_ID board 0 3")
 
     launch_util.launch_all_runs(targets=targets,
                                 num_nodes=args.node_count,
