96ee9bc7202146737d2a3c74eef28baeccbd1122
Brad Geltz <brad.geltz@intel.com>
2025-08-07 11:25:47 -0700

    Fixups
    - Remmove unneeded code.
    - Align names of resources across install script and hook implementation.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/service/open_pbs/geopm_install_pbs_power_limit_server.sh
+++ integration/service/open_pbs/geopm_install_pbs_power_limit_server.sh
@@ -3,14 +3,13 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
-# Resources needed for the server
-NODE_RESOURCE="geopm-node-power-limit"
-NODE_MAX_RESOURCE="geopm-max-node-power-limit"
-NODE_MIN_RESOURCE="geopm-min-node-power-limit"
-JOB_RESOURCE="geopm-job-power-limit"
+POWER_LIMIT_RESOURCE="geopm-node-power-limit"
+MAX_POWER_LIMIT_RESOURCE="geopm-max-node-power-limit"
+MIN_POWER_LIMIT_RESOURCE="geopm-min-node-power-limit"
+JOB_POWER_LIMIT_RESOURCE="geopm-job-power-limit"
 JOB_TYPE_RESOURCE="geopm-job-type"
 DEFAULT_SLOWDOWN_RESOURCE="geopm-default-slowdown"
-QUEUEJOB_HOOK="geopm_power_limit_queuejob"
+SERVER_HOOK="geopm_power_limit_server"
 REMOVE_OPT="--remove"
 
 SCHED_CONFIG_MODIFIED_MESSAGE="\
@@ -34,7 +33,7 @@ print_usage() {
 
 install() {
     # Create all resources needed for the server side
-    for resource in $NODE_RESOURCE $NODE_MAX_RESOURCE $NODE_MIN_RESOURCE $JOB_RESOURCE $DEFAULT_SLOWDOWN_RESOURCE
+    for resource in $POWER_LIMIT_RESOURCE $MAX_POWER_LIMIT_RESOURCE $MIN_POWER_LIMIT_RESOURCE $JOB_POWER_LIMIT_RESOURCE $DEFAULT_SLOWDOWN_RESOURCE
     do
         out=`qmgr -c "list resource" | grep "$resource"`
         if [ -z "$out" ]; then
@@ -44,11 +43,11 @@ install() {
             echo "$resource resource already exists"
         fi
     done
-    echo "Marking $JOB_RESOURCE as a server/queue resource..."
-    qmgr -c "set resource $JOB_RESOURCE flag=q" || exit 1
-    echo "Marking $NODE_MAX_RESOURCE and $NODE_MIN_RESOURCE as read-only resources..."
-    qmgr -c "set resource $NODE_MAX_RESOURCE flag=r" || exit 1
-    qmgr -c "set resource $NODE_MIN_RESOURCE flag=r" || exit 1
+    echo "Marking $JOB_POWER_LIMIT_RESOURCE as a server/queue resource..."
+    qmgr -c "set resource $JOB_POWER_LIMIT_RESOURCE flag=q" || exit 1
+    echo "Marking $MAX_POWER_LIMIT_RESOURCE and $MIN_POWER_LIMIT_RESOURCE as read-only resources..."
+    qmgr -c "set resource $MAX_POWER_LIMIT_RESOURCE flag=r" || exit 1
+    qmgr -c "set resource $MIN_POWER_LIMIT_RESOURCE flag=r" || exit 1
 
     out=`qmgr -c "list resource" | grep "$JOB_TYPE_RESOURCE"`
     if [ -z "$out" ]; then
@@ -58,38 +57,38 @@ install() {
         echo "$JOB_TYPE_RESOURCE resource already exists"
     fi
 
-    if grep -q "^resources: .*$JOB_RESOURCE" "${PBS_HOME}/sched_priv/sched_config"; then
-            echo "$JOB_RESOURCE is already defined as a consumable scheduler resource"
+    if grep -q "^resources: .*$JOB_POWER_LIMIT_RESOURCE" "${PBS_HOME}/sched_priv/sched_config"; then
+            echo "$JOB_POWER_LIMIT_RESOURCE is already defined as a consumable scheduler resource"
     else
-            echo "Appending $JOB_RESOURCE as a consumable scheduler resource"
-            sed -i -e "s/^resources: \"\([^\"]\+\)\"$/resources: \"\1, ${JOB_RESOURCE}\"/g" "${PBS_HOME}/sched_priv/sched_config"
+            echo "Appending $JOB_POWER_LIMIT_RESOURCE as a consumable scheduler resource"
+            sed -i -e "s/^resources: \"\([^\"]\+\)\"$/resources: \"\1, ${JOB_POWER_LIMIT_RESOURCE}\"/g" "${PBS_HOME}/sched_priv/sched_config"
     fi
 
-    # Set up the queuejob hook
-    out=`qmgr -c "list hook" | grep "$QUEUEJOB_HOOK"`
+    # Set up the server hook
+    out=`qmgr -c "list hook" | grep "$SERVER_HOOK"`
     if [ -z "$out" ]; then
-        echo "Creating $QUEUEJOB_HOOK hook..."
-        qmgr -c "create hook $QUEUEJOB_HOOK" || exit 1
+        echo "Creating $SERVER_HOOK hook..."
+        qmgr -c "create hook $SERVER_HOOK" || exit 1
     else
-        echo "$QUEUEJOB_HOOK hook already exists"
+        echo "$SERVER_HOOK hook already exists"
     fi
-    echo "Importing and configuring queuejob hook..."
-    qmgr -c "import hook $QUEUEJOB_HOOK application/x-python default geopm_power_limit_server.py" || exit 1
-    qmgr -c "set hook $QUEUEJOB_HOOK event='queuejob'" || exit 1
+    echo "Importing and configuring server hook..."
+    qmgr -c "import hook $SERVER_HOOK application/x-python default $SERVER_HOOK.py" || exit 1
+    qmgr -c "set hook $SERVER_HOOK event='queuejob'" || exit 1
 
     echo "Done."
 }
 
 remove() {
-    if grep -q "^resources: .*$JOB_RESOURCE" "${PBS_HOME}/sched_priv/sched_config"; then
-            echo "Removing $JOB_RESOURCE from the list of consumable scheduler resources"
-            sed -i -e "s/^resources: \"\([^\"]\+\), ${JOB_RESOURCE}\(, [^\"]\+\)\?\"$/resources: \"\1\2\"/g" "${PBS_HOME}/sched_priv/sched_config"
+    if grep -q "^resources: .*$JOB_POWER_LIMIT_RESOURCE" "${PBS_HOME}/sched_priv/sched_config"; then
+            echo "Removing $JOB_POWER_LIMIT_RESOURCE from the list of consumable scheduler resources"
+            sed -i -e "s/^resources: \"\([^\"]\+\), ${JOB_POWER_LIMIT_RESOURCE}\(, [^\"]\+\)\?\"$/resources: \"\1\2\"/g" "${PBS_HOME}/sched_priv/sched_config"
     else
-            echo "$JOB_RESOURCE not found in the list of consumable scheduler resources"
+            echo "$JOB_POWER_LIMIT_RESOURCE not found in the list of consumable scheduler resources"
     fi
 
     # Remove resources
-    for resource in $NODE_RESOURCE $NODE_MAX_RESOURCE $NODE_MIN_RESOURCE $JOB_RESOURCE $DEFAULT_SLOWDOWN_RESOURCE $JOB_TYPE_RESOURCE
+    for resource in $POWER_LIMIT_RESOURCE $MAX_POWER_LIMIT_RESOURCE $MIN_POWER_LIMIT_RESOURCE $JOB_POWER_LIMIT_RESOURCE $DEFAULT_SLOWDOWN_RESOURCE $JOB_TYPE_RESOURCE
     do
         out=`qmgr -c "list resource" | grep "$resource"`
         if [ -z "$out" ]; then
@@ -100,13 +99,13 @@ remove() {
         fi
     done
 
-    # Remove the queuejob hook
-    out=`qmgr -c "list hook" | grep "$QUEUEJOB_HOOK"`
+    # Remove the server hook
+    out=`qmgr -c "list hook" | grep "$SERVER_HOOK"`
     if [ -z "$out" ]; then
-        echo "$QUEUEJOB_HOOK hook not found"
+        echo "$SERVER_HOOK hook not found"
     else
-        echo "Removing $QUEUEJOB_HOOK hook..."
-        qmgr -c "delete hook $QUEUEJOB_HOOK" || exit 1
+        echo "Removing $SERVER_HOOK hook..."
+        qmgr -c "delete hook $SERVER_HOOK" || exit 1
     fi
 
     echo "Done."
@@ -117,11 +116,11 @@ if [ $# -eq 0 ]; then
     echo "$SCHED_CONFIG_MODIFIED_MESSAGE"
     echo ""
     echo "GEOPM PBS server hook has been installed but is not yet configured."
-    echo "To set a power limit across jobs, set resources_available for $JOB_RESOURCE"
-    echo "To set a minimum node power limit, use $NODE_MIN_RESOURCE"
-    echo "To set a maximum node power limit, use $NODE_MAX_RESOURCE"
+    echo "To set a power limit across jobs, set resources_available for $JOB_POWER_LIMIT_RESOURCE"
+    echo "To set a minimum node power limit, use $MIN_POWER_LIMIT_RESOURCE"
+    echo "To set a maximum node power limit, use $MAX_POWER_LIMIT_RESOURCE"
     echo "To set a default job slowdown target, use $DEFAULT_SLOWDOWN_RESOURCE"
-    echo "Example to set a power limit: qmgr -c 'set server resources_available.${JOB_RESOURCE}=<max sum of node power (W)>'"
+    echo "Example to set a power limit: qmgr -c 'set server resources_available.${JOB_POWER_LIMIT_RESOURCE}=<max sum of node power (W)>'"
 elif [ $# -eq 1 ]; then
     if [ "$1" == "$REMOVE_OPT" ]; then
         remove
