2224700f556a7747deb76af6b1706159ef2214bb
Siddhartha Jana <siddhartha.jana@intel.com>
2025-11-04 17:07:03 +0000

    Reverting to explicitly setting SWEEP_TYPE

--- integration/experiment/system_characterization/set_vars.sh
+++ integration/experiment/system_characterization/set_vars.sh
@@ -7,18 +7,13 @@
 
 # Sweep variables setup
 
-if [ -z "${1}" ]; then
-    echo "Error: No argument supplied for parameter 1. Enter either CPU or GPU"
-    exit 1
-elif [[ "${1}" == "CPU" ]]; then
-    SWEEP_TYPE="CPU"  # can be either "CPU" or "GPU"
+if [[ "${SWEEP_TYPE}" == "CPU" ]]; then
     PROGRAM_NAME="bench_avx512"
     BINARY_PATH_PLUS_FLAGS="${GEOPM_SOURCE}/integration/apps/arithmetic_intensity/ARITHMETIC_INTENSITY/bench_avx512 -i 10 -b 1 16"
     BOARD_POWER_STEP=100
     BOARD_MIN_POWER=1600
     BOARD_MAX_POWER=2700
-elif [[ "${1}" == "GPU" ]]; then
-    SWEEP_TYPE="GPU"  # can be either "CPU" or "GPU"
+elif [[ "${SWEEP_TYPE}" == "GPU" ]]; then
     PROGRAM_NAME="nstream-onemkl"
     BINARY_PATH_PLUS_FLAGS="${GEOPM_SOURCE}/integration/apps/parres/Kernels/Cxx11/nstream-onemkl 75 3000000000"
     BOARD_POWER_STEP=100
