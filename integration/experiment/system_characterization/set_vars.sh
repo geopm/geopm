34a6364d28597a0585edbf4b1b1f01bc72aa4759
Siddhartha Jana <siddhartha.jana@intel.com>
2025-11-04 01:44:18 +0000

    Updating min/max board power limits for CPU sweeps using empirical data

--- integration/experiment/system_characterization/set_vars.sh
+++ integration/experiment/system_characterization/set_vars.sh
@@ -14,10 +14,16 @@ elif [[ "${1}" == "CPU" ]]; then
     SWEEP_TYPE="CPU"  # can be either "CPU" or "GPU"
     PROGRAM_NAME="bench_avx512"
     BINARY_PATH_PLUS_FLAGS="${GEOPM_SOURCE}/integration/apps/arithmetic_intensity/ARITHMETIC_INTENSITY/bench_avx512 -i 10 -b 1 16"
+    BOARD_POWER_STEP=100
+    BOARD_MIN_POWER=1600
+    BOARD_MAX_POWER=2700
 elif [[ "${1}" == "GPU" ]]; then
     SWEEP_TYPE="GPU"  # can be either "CPU" or "GPU"
     PROGRAM_NAME="nstream-onemkl"
     BINARY_PATH_PLUS_FLAGS="${GEOPM_SOURCE}/integration/apps/parres/Kernels/Cxx11/nstream-onemkl 75 3000000000"
+    BOARD_POWER_STEP=100
+    BOARD_MIN_POWER=2000
+    BOARD_MAX_POWER=2600
 else
     echo "Error: Incorrect sweep type in first parameter"
     exit 1
@@ -26,13 +32,6 @@ fi
 
 TRIAL_COUNT=3 # characterization time increases linearly with this parameter
 CORE_SKIP_LIST="0,1,52,53" # e.g., skip first 2 cores on each 52-core socket on a dual-socket system
-
-
-# Platform Power Cap specific variables
-BOARD_POWER_STEP=100
-BOARD_MIN_POWER=2000    # lower the value, lower the power headroom for the components
-BOARD_MAX_POWER=2600    # higher the value, more the power headroom for the components
-
 
 # Aurora environment setup
 module load geopm-runtime
