9d92005f9a8ee7049b00537b5b4fe8faecd6d878
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-11-24 16:17:46 -0800

    [WIP] Add integration test for geopmopt
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

---
+++ integration/service/test/test_geopmopt_dgemm_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#  Copyright (c) 2015 - 2025 Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+
+set -ex
+
+BENCH_CONF=bench.conf
+trap 'rm -f "${BENCH_CONF}"' EXIT
+cat <<EOF > $BENCH_CONF
+{
+  "loop-count": 10,
+  "region": ["dgemm"],
+  "big-o": [0.2]
+}
+EOF
+echo "CPU_FREQUENCY_GOVERNOR_CONTROL board 0 0" > init-control.config
+if [ $# -eq 2 ]; then
+    cat $1 >> init-control.config
+fi
+geopmlaunch pals -n 4 -ppn 2 \
+                 --geopm-program-filter=geopmbench \
+                 --geopm-report=report.yaml \
+                 --geopm-init-control=init-control.config \
+                 -- geopmbench --verbose ${BENCH_CONF}
+
+python3 <<EOF
+from yaml import safe_load
+with open('report.yaml') as fid:
+    report = safe_load(fid)
+hosts = list(report['Hosts'].keys())
+fom = sum([report['Hosts'][hh]['Epoch Totals']['runtime (s)'] for hh in hosts]) / len(hosts)
+print(f'GEOPMOPT-FOM: {fom}')
+EOF
