b726d8aa9caf89868910aa8e4b6a37fb6aa5558b
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-12-02 15:31:33 -0800

    Fixup single-node session based integration test
    - Backport some of the clean up to the ctl based test.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/service/test/check_geopmopt_dgemm_ctl_run.sh
+++ integration/service/test/check_geopmopt_dgemm_ctl_run.sh
@@ -5,26 +5,28 @@
 set -ex
 
 BENCH_CONF=bench.conf
-trap 'rm -f "${BENCH_CONF}"' EXIT
-cat <<EOF > $BENCH_CONF
+CONTROL_CONFIG=init-control.config
+REPORT_OUTPUT=test_geopmopt_dgemm_ctl_report.yaml
+trap 'rm -f "${BENCH_CONF}" "{CONTROL_CONFIG}" "${REPORT_OUTPUT}"' EXIT
+cat <<EOF > ${BENCH_CONF}
 {
   "loop-count": 100,
   "region": ["dgemm"],
   "big-o": [0.2]
 }
 EOF
-echo "CPU_FREQUENCY_GOVERNOR_CONTROL board 0 0" > init-control.config
+echo "CPU_FREQUENCY_GOVERNOR_CONTROL board 0 0" > ${CONTROL_CONFIG}
 if [ $# -eq 1 ]; then
-    cat $1 >> init-control.config
+    cat $1 >> ${CONTROL_CONFIG}
 fi
-GEOPM_REPORT=report.yaml GEOPM_INIT_CONTROL=init-control.config geopmctl &
+GEOPM_REPORT=${REPORT_OUTPUT} GEOPM_INIT_CONTROL=${CONTROL_CONFIG} GEOPM_PERIOD=1e-2 geopmctl &
 GEOPMCTL_PID=$!
 GEOPM_PROGRAM_FILTER=geopmbench geopmbench --verbose ${BENCH_CONF}
 wait
 python3 <<EOF
 from yaml import safe_load
 from socket import gethostname
-with open(f'report.yaml-{gethostname()}') as fid:
+with open(f"$REPORT_OUTPUT-{gethostname()}") as fid:
     report = safe_load(fid)
 hosts = list(report['Hosts'].keys())
 fom = sum([report['Hosts'][hh]['Epoch Totals']['runtime (s)'] for hh in hosts]) / len(hosts)
--- integration/service/test/test_geopmopt_dgemm_session.sh
+++ integration/service/test/test_geopmopt_dgemm_session.sh
@@ -3,16 +3,17 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 
 set -ex
-
+STICKER_FREQ=$(geopmread CPU_FREQUENCY_STICKER board 0)
+MAX_FREQ=$((STICKER_FREQ - 300000000))
 geopmopt --verbosity=2 \
-         --cpu-frequency board \
+         --cpu-frequency=board \
+         --cpu-frequency-max=${MAX_FREQ} \
          --defer-write \
          --minimize \
-         --output-file optimal-frequency.config \
+         --output-file=optimal-frequency.config \
          --metric-regex='GEOPMOPT-FOM: ([0-9.]+)' \
-         -- ./check_geopmopt_dgemm_session_run.sh optimial-frequency.config
+         --trials=20 \
+         -- ./check_geopmopt_dgemm_session_run.sh optimal-frequency.config
 
 # For DGEMM expect maximum frequency minimizes time in dgemm
-NUM_FREQ=$(geopmgrid --cpu-frequency board --coordinate-range)
-geopmgrid --cpu-frequency board --coordinate $(($NUM_FREQ-1)) > expected.config
-diff expected.config optimal-frequency.config
+grep ${MAX_FREQ} optimal-frequency.config
