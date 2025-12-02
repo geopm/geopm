d3aba6c86fdb8f39b7a28cbd3afb88a59bf6676a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-12-02 15:31:33 -0800

    Fixup single-node session based integration test
    - Backport some of the clean up to the ctl based test.
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/service/test/check_geopmopt_dgemm_session_run.sh
+++ integration/service/test/check_geopmopt_dgemm_session_run.sh
@@ -4,15 +4,14 @@
 
 set -ex
 
-
 BENCH_CONF=bench.conf # Shared file
 CONTROL_CONFIG=init-control.config # Shared file
 DAEMON_PID_FILE=/tmp/test_geopm_dgemm_session_daemon.pid # Node local file
 SIGNAL_CONFIG=test_geopm_dgemm_session_signal.conf # Shared file
 REPORT_OUTPUT=test_geopm_dgemm_session_report.yaml # Shared file with hostname appended
-trap 'rm -f "${BENCH_CONF}" "${SIGNAL_CONFIG}" "${CONTROL_CONFIG}"; rm -f "${DAEMON_PID_FILE}"' EXIT
+trap 'test -e "${DAEMON_PID_FILE}" && kill $(cat "${DAEMON_PID_FILE}") >&/dev/null || true; rm -f "${BENCH_CONF}" "${CONTROL_CONFIG}" "${DAEMON_PID_FILE}" "${SIGNAL_CONFIG}" "${REPORT_OUTPUT}" >& /dev/null' EXIT
 
-cat <<EOF > $BENCH_CONF
+cat <<EOF > ${BENCH_CONF}
 {
   "loop-count": 10,
   "region": ["dgemm"],
@@ -20,7 +19,7 @@ cat <<EOF > $BENCH_CONF
 }
 EOF
 echo "CPU_FREQUENCY_GOVERNOR_CONTROL board 0 0" > ${CONTROL_CONFIG}
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ]; then
     cat $1 >> ${CONTROL_CONFIG}
 fi
 echo "TIME board 0" > ${SIGNAL_CONFIG}
@@ -32,15 +31,18 @@ geopmsession --daemon ${DAEMON_PID_FILE} \
              --signal-config ${SIGNAL_CONFIG} \
              --control-config ${CONTROL_CONFIG}
 geopmbench --verbose ${BENCH_CONF}
-kill $(cat ${DAEMON_PID_FILE}); tail -f /dev/null --pid $(cat ${DAEMON_PID_FILE}); rm -f ${DAEMON_PID_FILE}
+DAEMON_PID=$(cat ${DAEMON_PID_FILE})
+kill ${DAEMON_PID}
+tail -f /dev/null --pid ${DAEMON_PID}
+rm -f ${DAEMON_PID_FILE}
 
 python3 <<EOF
 from yaml import safe_load
 from glob import glob
 energy_total = 0.0
 host_count = 0
-with open('test_geopm_dgemm_session_report.yaml') as fid:
+with open("$REPORT_OUTPUT") as fid:
     rpt = safe_load(fid)
-energy_total += rpt['metrics']['CPU_ENERGY']['last'] - rpt['metrics']['CPU_ENERGY']['first']
+energy_total += rpt['metrics']['TIME']['last'] - rpt['metrics']['TIME']['first']
 print(f'GEOPMOPT-FOM: {energy_total}')
 EOF
