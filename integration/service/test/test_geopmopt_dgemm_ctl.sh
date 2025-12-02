9b4092cb4cf03fd9f9dcdc27e2c96d1e150bef6c
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-12-02 14:44:31 -0800

    Lower max frequency by 300 MHz to reduce impact of noise
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- integration/service/test/test_geopmopt_dgemm_ctl.sh
+++ integration/service/test/test_geopmopt_dgemm_ctl.sh
@@ -4,19 +4,18 @@
 
 set -ex
 STICKER_FREQ=$(geopmread CPU_FREQUENCY_STICKER board 0)
-
+MAX_FREQ=$((STICKER_FREQ - 300000000))
 geopmopt --verbosity=2 \
          --cpu-frequency board \
-         --cpu-frequency-max=${STICKER_FREQ} \
+         --cpu-frequency-max=${MAX_FREQ} \
          --defer-write \
          --minimize \
          --output-file optimal-frequency.config \
          --metric-regex='GEOPMOPT-FOM: ([0-9.]+)' \
          --trials=20 \
-         --print-stdout \
          -- ./check_geopmopt_dgemm_ctl_run.sh optimal-frequency.config
 
 # For DGEMM expect maximum frequency minimizes time in dgemm
 NUM_FREQ=$(geopmgrid --cpu-frequency board --coordinate-range)
 geopmgrid --cpu-frequency board --coordinate $(($NUM_FREQ-1)) > expected.config
-grep ${STICKER_FREQ} optimal-frequency.config
+grep ${MAX_FREQ} optimal-frequency.config
