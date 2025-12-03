95dc5ad9d9aead0cdfc8b645f47a75f4c15d3dbf
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2025-12-02 16:07:45 -0800

    Update 4node test

--- integration/service/test/check_geopmopt_dgemm_session_4node_run.sh
+++ integration/service/test/check_geopmopt_dgemm_session_4node_run.sh
@@ -4,15 +4,16 @@
 
 set -ex
 
-
 BENCH_CONF=bench.conf # Shared file
 CONTROL_CONFIG=init-control.config # Shared file
-DAEMON_PID_FILE=/tmp/test_geopm_dgemm_session_daemon.pid # Node local file
-SIGNAL_CONFIG=test_geopm_dgemm_session_signal.conf # Shared file
-REPORT_OUTPUT=test_geopm_dgemm_session_report.yaml # Shared file with hostname appended
-trap 'rm -f "${BENCH_CONF}" "${SIGNAL_CONFIG}" "${CONTROL_CONFIG}"; mpiexec -n 2 -ppn 1 -- rm -f "${DAEMON_PID_FILE}"' EXIT
-
-cat <<EOF > $BENCH_CONF
+DAEMON_PID_FILE=/tmp/test_geopm_dgemm_session_4node_daemon.pid # Node local file
+SIGNAL_CONFIG=test_geopm_dgemm_session_4node_signal.conf # Shared file
+REPORT_OUTPUT=test_geopm_dgemm_session_4node_report.yaml # Shared file with hostname appended
+MPI_EXEC='mpiexec'
+MPI_ARGS='-ppn 1 -n 4 --'
+REMOTE_TRAP='test -e "${DAEMON_PID_FILE}" && kill $(cat "{DAEMON_PID_FILE}") >&/dev/null || true; rm -f "${DAEMON_PID_FILE}"'
+#trap '"${MPI_EXEC}" bash -c "${REMOTE_TRAP}"; rm -f "${BENCH_CONF}" "${SIGNAL_CONFIG}" "${CONTROL_CONFIG}"' EXIT
+cat <<EOF > ${BENCH_CONF}
 {
   "loop-count": 10,
   "region": ["dgemm"],
@@ -20,38 +21,36 @@ cat <<EOF > $BENCH_CONF
 }
 EOF
 echo "CPU_FREQUENCY_GOVERNOR_CONTROL board 0 0" > ${CONTROL_CONFIG}
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ]; then
     cat $1 >> ${CONTROL_CONFIG}
 fi
 echo "TIME board 0" > ${SIGNAL_CONFIG}
 echo "CPU_ENERGY board 0" >> ${SIGNAL_CONFIG}
-mpiexec -n 2 -ppn 1
-        -- geopmsession --daemon ${DAEMON_PID_FILE} \
-                        --period 1e-2 \
-                        --report-out ${REPORT_OUTPUT} \
-                        --trace-out /dev/null \
-                        --signal-config ${SIGNAL_CONFIG} \
-			--control-config ${CONTROL_CONFIG} \
-                        --append-hostname
+${MPI_EXEC} ${MPI_ARGS} \
+geopmsession --daemon ${DAEMON_PID_FILE} \
+             --period 1e-2 \
+             --report-out ${REPORT_OUTPUT} \
+             --trace-out /dev/null \
+             --signal-config ${SIGNAL_CONFIG} \
+             --control-config ${CONTROL_CONFIG} \
+             --append-hostname
 
-mpiexec -n 4 -ppn 2 \
-	-- geopmbench --verbose ${BENCH_CONF}
+${MPI_EXEC} ${MPI_ARGS} \
+geopmbench --verbose ${BENCH_CONF}
 
-mpiexec -n 2 -ppn 1 \
-	-- bash -c 'kill $(cat '${DAEMON_PID_FILE}'); while ps $(cat '${DAEMON_PID_FILE}'); do sleep 1; done; rm -f ${DAEMON_PID_FILE}'
+${MPI_EXEC} ${MPI_ARGS} \
+bash -c 'DAEMON_PID=$(cat ${DAEMON_PID_FILE}); kill ${DAEMON_PID}; tail -f /dev/null --pid ${DAEMON_PID}; rm -f ${DAEMON_PID_FILE}'
 
-for rr in ${REPORT_OUTPUT}-*.yaml; do
 python3 <<EOF
 from yaml import safe_load
 from glob import glob
-energy_total = 0.0
+total = 0.0
 host_count = 0
-for rr in glob('test_geopm_dgemm_session_report.yaml-*'):
-    with open(rr) as fid:
+for rf in glob("$REPORT-OUTPUT" + "-*'):
+    with open(rf) as fid:
         rpt = safe_load(fid)
-        energy_total += rpt['metrics']['CPU_ENERGY']['last'] -
-                        rpt['metrics']['CPU_ENERGY']['first']
+        total += rpt['metrics']['TIME']['last'] - rpt['metrics']['TIME']['first']
         host_count += 1
-fom = enrgy_total / host_count
+fom = total / host_count
 print(f'GEOPMOPT-FOM: {fom}')
 EOF
